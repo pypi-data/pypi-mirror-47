4518cfe9b47baab02a1b040f748ded73dcd25f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:59 +0000

    Add package ataleek/0.1/ataleek-0.1.tar.gz

---
+++ packages/ataleek/ataleek-0.1.tar.gz/ataleek-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ataleek',
+      version='0.1',
+      description='Command Line Interface To Ataleek Platform',
+      url='https://github.com/ataleek/ataleek-cli',
+      author='Ahmad Bilal Khalid',
+      author_email='ahmedbilal96@gmail.com',
+      license='MIT',
+      packages=['ataleek'],
+      zip_safe=False,
+      scripts=['bin/ataleek-cli']) 
