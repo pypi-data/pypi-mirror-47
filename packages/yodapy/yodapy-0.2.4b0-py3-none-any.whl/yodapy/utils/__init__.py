17a77f1800e3cffe57658c93f76cab29a7abde0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:33 +0000

    Add package yodapy/0.2.4b0/yodapy-0.2.4b0-py3-none-any.whl

---
+++ packages/yodapy/yodapy-0.2.4b0-py3-none-any.whl/yodapy/utils/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (division,
+                        absolute_import,
+                        print_function,
+                        unicode_literals)
+
+from yodapy.utils.creds import set_credentials_file
+
+__all__ = ['conn', 'creds', 'files', 'meta', 'parser']
---
+++ packages/yodapy/yodapy-0.2.4b0-py3-none-any.whl/yodapy/utils/meta.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (division,
+                        absolute_import,
+                        print_function,
+                        unicode_literals)
+
+import os
+import warnings
+
+from yodapy.utils.files import (YODAPY_DIR, check_file_permissions)
+
+
+def create_folder(source_name):
+    if check_file_permissions():
+        fold_name = source_name.lower()
+        fold_path = os.path.join(YODAPY_DIR, fold_name)
+
+        if not os.path.exists(YODAPY_DIR):
+            os.mkdir(YODAPY_DIR)
+
+        if not os.path.exists(fold_path):
+            os.mkdir(fold_path)
+
+        return fold_path
+    else:   # pragma: no cover
+        warnings.warn('You don\'t have a read-write permission '
+                      'to your home (\'~\') directory!')
