c6cf092c90c7136995a6c6cafd86a9a7c4cd9748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package tmalibrary/3.6.7/tmalibrary-3.6.7.tar.gz

---
+++ packages/tmalibrary/tmalibrary-3.6.7.tar.gz/tmalibrary-3.6.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tmalibrary',
+    version='3.6.7',
+    packages=find_packages(),
+    url='https://github.com/rfrsilva/tmalibrary',
+    license='Apache 2.0',
+    author='Rui Silva',
+    author_email='rfsilva@student.dei.uc.pt',
+    description='Libraries to TMA'
+)
