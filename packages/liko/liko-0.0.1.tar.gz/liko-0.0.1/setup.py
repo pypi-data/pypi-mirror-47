c4e7cfe66787b30541106cae2533f1c5d3a6517b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:38 +0000

    Add package liko/0.0.1/liko-0.0.1.tar.gz

---
+++ packages/liko/liko-0.0.1.tar.gz/liko-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Used in pypi.org as the README description of your package
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='liko', 
+        version='0.0.1',
+        description='learning today make you better tomorrow',
+        author='eko',
+        author_email='eko@altoshift.com',
+        license="MIT",
+        url="https://altoshift.com",
+        packages=['liko'],
+        #scripts=['scripts/some_script.py'],
+        #python_requires='>=3',
+        requires=[],
+        install_requires=[
+            'pandas',
+            'multiprocessing'
+        ],
+        long_description=long_description
+)
