506028868a881e8aa4b59480d3be1d09d8c38faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:50 +0000

    Add package ulysses/0.2/ulysses-0.2.tar.gz

---
+++ packages/ulysses/ulysses-0.2.tar.gz/ulysses-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'ulysses',
+  version = '0.2',
+  description = 'ULYSSES: Universal LeptogeneSiS Equation Solver',
+  url = 'https://github.com/iamholger/ulysses',
+  author = 'Kris Moffat, Jessica Turner, Holger Schulz',
+  author_email = 'hschulz@fnal.gov',
+  packages = find_packages(),
+  include_package_data = True,
+  install_requires = [
+    'numpy',
+    'scipy',
+    'numba',
+    'matplotlib',
+    'pymultinest'
+  ],
+  python_requires='>3.5.2',
+  scripts=['bin/uls-calc', 'bin/uls-scan', 'bin/uls-nest'],
+  extras_require = {
+  },
+  entry_points = {
+  },
+  dependency_links = [
+  ]
+)
