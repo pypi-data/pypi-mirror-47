6c28cdaf101c9f0b8225b6c409dbe7db3633464f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:27 +0000

    Add package androwarn/1.6/androwarn-1.6.tar.gz

---
+++ packages/androwarn/androwarn-1.6.tar.gz/androwarn-1.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='androwarn',
+      version='1.6',
+      description='Yet another static code analyzer for malicious Android applications',
+      long_description_content_type='text/markdown; charset=UTF-8;',
+      long_description=open('androwarn/README.md').read(),
+      url='https://github.com/maaaaz/androwarn',
+      author='Thomas D.',
+      author_email='tdebize@mail.com',
+      license='LGPL',
+      classifiers=[
+        'Topic :: Security',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'],
+      keywords='androwarn android apk dalvik static malicious behaviours behaviors reverse disassemble',
+      packages=find_packages(),
+      install_requires=['androguard >= 3.2.1', 'argparse', 'future', 'jinja2', 'play_scraper'],
+      python_requires='>=2.7',
+      entry_points = {
+        'console_scripts': ['androwarn=androwarn.androwarn:main'],
+      },
+      include_package_data=True)
