f5a5a08d16fbe1d0bc86646d92fb4224c57c3fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:56 +0000

    Add package cadence-client/0.0.1/cadence-client-0.0.1.tar.gz

---
+++ packages/cadence-client/cadence-client-0.0.1.tar.gz/cadence-client-0.0.1/cadence/exceptions.py
@@ -0,0 +1,34 @@
+from cadence.cadence_types import TimeoutType
+
+
+class IllegalStateException(BaseException):
+    pass
+
+
+class IllegalArgumentException(BaseException):
+    pass
+
+
+class WorkflowTypeNotFound(Exception):
+    pass
+
+
+class NonDeterministicWorkflowException(BaseException):
+    pass
+
+
+class ActivityTaskFailedException(Exception):
+
+    def __init__(self, reason: str, details: bytes) -> None:
+        super().__init__(reason)
+        self.reason = reason
+        self.details = details
+
+
+class ActivityTaskTimeoutException(Exception):
+
+    def __init__(self, event_id: int, timeout_type: TimeoutType, details: bytes, *args: object) -> None:
+        super().__init__(*args)
+        self.details = details
+        self.timeout_type = timeout_type
+        self.event_id = event_id
