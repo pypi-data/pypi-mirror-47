3a4d1f968cbc671816c4bedb4ee36105bb4bbe1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:30 +0000

    Add package ssmash/1.0.0/ssmash-1.0.0.tar.gz

---
+++ packages/ssmash/ssmash-1.0.0.tar.gz/ssmash-1.0.0/CHANGELOG.rst
@@ -0,0 +1,33 @@
+=========
+Changelog
+=========
+
+All notable changes to this project will be documented in this file.
+
+The format is based on `Keep a Changelog <https://keepachangelog.com/en/1.0.0/>`_,
+and this project adheres to `Semantic Versioning <https://semver.org/spec/v2.0.0.html>`_.
+
+.. Comment
+
+    [Unreleased]
+    ------------
+
+    Changed
+    ~~~~~~~
+
+v1.0.0 (2019-05-24)
+-------------------
+
+Added
+~~~~~
+* ``ssmash`` script to create String SSM Parameters from a simple config file stored in YAML
+* Basic documentation in README
+
+v0.1.0 (2019-05-14)
+-------------------
+
+Added
+~~~~~
+
+* First release on PyPI.
+* Cookiecutter skeleton only, no functionality
---
+++ packages/ssmash/ssmash-1.0.0.tar.gz/ssmash-1.0.0/tests/cli_test.py
@@ -0,0 +1,104 @@
+"""Tests for the command line interface."""
+
+from datetime import datetime, timezone
+
+import semver
+import yaml
+from click.testing import CliRunner
+from freezegun import freeze_time
+
+from ssmash import cli
+
+SIMPLE_INPUT = """foo: bar"""
+SIMPLE_OUTPUT_LINE = "Name: /foo"
+
+
+class TestBasicCLI:
+    def test_should_display_help(self):
+        runner = CliRunner()
+        help_result = runner.invoke(cli.create_stack, ["--help"])
+        assert help_result.exit_code == 0
+        assert "--help              Show this message and exit." in help_result.output
+
+    def test_should_exit_cleanly_with_empty_input(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.create_stack)
+        assert result.exit_code == 0
+        assert not result.stderr_bytes
+
+
+class TestCloudFormationMetadata:
+    def test_stack_should_have_description(self):
+        # Setup
+        description = "Some lengthy text"
+
+        # Exercise
+        runner = CliRunner()
+        result = runner.invoke(cli.create_stack, args=["--description", description])
+
+        # Verify
+        cfn = yaml.safe_load(result.stdout)
+        assert cfn["Description"] == description
+
+    def test_output_should_contain_version(self):
+        from ssmash import __version__ as package_version
+
+        # Exercise
+        runner = CliRunner()
+        result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
+
+        # Verify
+        cfn = yaml.safe_load(result.stdout)
+        ssmash_version = cfn["Metadata"]["ssmash"]["version"]
+        assert ssmash_version == package_version
+        semver.parse(ssmash_version)
+
+    def test_output_should_contain_timestamp(self):
+        expected_time = datetime(2019, 5, 22, 1, 2, 3, tzinfo=timezone.utc)
+
+        # Exercise
+        runner = CliRunner()
+
+        with freeze_time(expected_time):
+            result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
+
+        # Verify
+        cfn = yaml.safe_load(result.stdout)
+        timestamp = cfn["Metadata"]["ssmash"]["generated_timestamp"]
+        assert timestamp == "2019-05-22T01:02:03+00:00"
+
+
+class TestCloudFormationIsProduced:
+    def test_should_convert_simple_input_with_default_pipes(self):
+        # Exercise
+        runner = CliRunner()
+        result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
+
+        # Verify
+        assert result.exit_code == 0
+        assert not result.stderr_bytes
+
+        assert SIMPLE_OUTPUT_LINE in result.stdout
+
+    def test_should_convert_simple_input_with_files(self):
+        input_filename = "input.yaml"
+        output_filename = "output.yaml"
+
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open(input_filename, "w") as f:
+                f.write(SIMPLE_INPUT)
+
+            # Exercise
+            result = runner.invoke(
+                cli.create_stack, args=[input_filename, output_filename]
+            )
+
+            # Verify
+            assert result.exit_code == 0
+            assert not result.stdout
+            assert not result.stderr_bytes
+
+            with open(output_filename, "r") as f:
+                actual_output = f.read()
+            assert SIMPLE_OUTPUT_LINE in actual_output
