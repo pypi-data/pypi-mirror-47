6be1fa92881a0f24e264ab7e71c028c496fb8bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package urlifyhelper/0.1/urlifyhelper-0.1.tar.gz

---
+++ packages/urlifyhelper/urlifyhelper-0.1.tar.gz/urlifyhelper-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='urlifyhelper',
+    version='0.1',
+    author='KamilKujawa',
+    packages=['urlifyhelper'],
+    url='https://github.com/KamilKujawa/uniti',
+    license='MIT',
+)
