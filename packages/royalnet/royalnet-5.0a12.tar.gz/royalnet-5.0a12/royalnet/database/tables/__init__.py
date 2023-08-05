f1e4afab0e0591d93118f936a745b4ac231080e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:50 +0000

    Add package royalnet/5.0a12/royalnet-5.0a12.tar.gz

---
+++ packages/royalnet/royalnet-5.0a12.tar.gz/royalnet-5.0a12/royalnet/database/tables/__init__.py
@@ -0,0 +1,10 @@
+from .royals import Royal
+from .telegram import Telegram
+from .diario import Diario
+from .aliases import Alias
+from .activekvgroup import ActiveKvGroup
+from .keyvalue import Keyvalue
+from .keygroup import Keygroup
+from .discord import Discord
+
+__all__ = ["Royal", "Telegram", "Diario", "Alias", "ActiveKvGroup", "Keyvalue", "Keygroup", "Discord"]
---
+++ packages/royalnet/royalnet-5.0a12.tar.gz/royalnet-5.0a12/royalnet/web/blueprints/__init__.py
@@ -0,0 +1,5 @@
+from .helloworld import bp as helloworld
+from .testing import bp as testing
+from .home import bp as home
+
+__all__ = ["helloworld", "testing", "home"]
