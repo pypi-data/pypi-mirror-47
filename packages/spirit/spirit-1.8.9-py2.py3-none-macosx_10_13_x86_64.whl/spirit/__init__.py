5c4d4ac2a87d50daec131b892aea68a5a46d5c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:07 +0000

    Add package spirit/1.8.9/spirit-1.8.9-py2.py3-none-macosx_10_13_x86_64.whl

---
+++ packages/spirit/spirit-1.8.9-py2.py3-none-macosx_10_13_x86_64.whl/spirit/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.8.9"
+__title__ = "Spirit"
+__description__ = "Optimizations and Dynamics Framework for atomistic Spin systems"
+__uri__ = "https://spirit-code.github.io"
+__license__ = "MIT"
+__author__ = "Gideon Mueller"
+__email__ = "g.mueller@fz-juelich.de"
---
+++ packages/spirit/spirit-1.8.9-py2.py3-none-macosx_10_13_x86_64.whl/spirit/parameters/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["gneb", "llg", "mc"]
+
+from spirit.parameters import *
