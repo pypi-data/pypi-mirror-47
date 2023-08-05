5b5c88c276d4b5c0adacdfa1f011d3e97c06a2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package webviewhooks/0.0.4/webviewhooks-0.0.4.tar.gz

---
+++ packages/webviewhooks/webviewhooks-0.0.4.tar.gz/webviewhooks-0.0.4/hook/__init__.py
@@ -0,0 +1,15 @@
+from hook import _hook
+
+_web_view_hook_instance = None
+
+
+def hook(package_name, device_id, callback, adb_name='adb'):
+    global _web_view_hook_instance
+    _web_view_hook_instance = _hook.WebViewHook(package_name, device_id, callback, adb_name)
+    _web_view_hook_instance.hook()
+
+
+def clear():
+    if _web_view_hook_instance is None:
+        raise RuntimeError('sorry you should call hook first')
+    _web_view_hook_instance.clear()
