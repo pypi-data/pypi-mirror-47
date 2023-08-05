d8e8cbddbf204e3173826e64ddfd0cd59c5a6f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package iscc-cli/0.1.0/iscc_cli-0.1.0-py3-none-any.whl

---
+++ packages/iscc-cli/iscc_cli-0.1.0-py3-none-any.whl/iscc_cli/const.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+SUPPORTED_EXTENSIONS = ("jpg", "png", "pdf", "epub")
+
+SUPPORTED_MIME_TYPES = (
+    "image/jpeg",
+    "image/png",
+    "application/pdf",
+    "application/epub+zip",
+)
+
+
+class GMT:
+    """Generic Media Type"""
+
+    IMAGE = "image"
+    TEXT = "text"
