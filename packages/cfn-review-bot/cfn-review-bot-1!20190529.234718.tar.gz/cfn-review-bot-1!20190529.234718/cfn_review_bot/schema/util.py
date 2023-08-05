2eeff67b0ce0995cce40fe83d41a518653e01ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:42 +0000

    Add package cfn-review-bot/1!20190529.234718/cfn-review-bot-1!20190529.234718.tar.gz

---
+++ packages/cfn-review-bot/cfn-review-bot-1!20190529.234718.tar.gz/cfn-review-bot-1!20190529.234718/cfn_review_bot/schema/util.py
@@ -0,0 +1,12 @@
+'''
+Utilities used in schema definitions.
+'''
+
+from schema import And, Or, Use
+
+
+def Any(data):
+  return True
+
+def OneOrMany(schema):
+  return Or(And(schema, Use(lambda x: [x])), [schema])
