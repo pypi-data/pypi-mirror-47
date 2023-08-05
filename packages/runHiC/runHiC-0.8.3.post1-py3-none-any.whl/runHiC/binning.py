c986a577145710d22a6fedd961fa4bddf3a9a6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:16 +0000

    Add package runHiC/0.8.3.post1/runHiC-0.8.3.post1-py3-none-any.whl

---
+++ packages/runHiC/runHiC-0.8.3.post1-py3-none-any.whl/runHiC/binning.py
@@ -0,0 +1,30 @@
+# Created on Thu Sep 13 10:44:19 2018
+
+# Author: XiaoTao Wang
+
+import os, subprocess, logging
+from runHiC.utilities import chromsizes_from_pairs
+
+log = logging.getLogger(__name__)
+
+def binning_from_pairs(pairpath, res, outpath, ignore_diags=1, nproc=1, mad_max=5, min_nnz=10,
+                       min_count=0, max_split=2):
+
+    chromsizes_file, assembly = chromsizes_from_pairs(pairpath)
+    
+    log.log(21, 'Building contact matrix ...')
+    bin_label = ':'.join([chromsizes_file, str(res)])
+    bin_command = ['cooler', 'cload', 'pairix', '--assembly', assembly, '--nproc', str(nproc),
+                   '--max-split', str(max_split), bin_label, pairpath, outpath]
+    subprocess.check_call(' '.join(bin_command), shell=True)
+    log.log(21, 'Done')
+
+    log.log(21, 'Perform ICE ...')
+    ice_command = ['cooler', 'balance', '--nproc', str(nproc), '--ignore-diags', str(ignore_diags),
+                   '--mad-max', str(mad_max), '--min-nnz', str(min_nnz), '--min-count', str(min_count),
+                   outpath]
+    subprocess.check_call(' '.join(ice_command), shell=True)
+    log.log(21, 'Done')
+
+
+
