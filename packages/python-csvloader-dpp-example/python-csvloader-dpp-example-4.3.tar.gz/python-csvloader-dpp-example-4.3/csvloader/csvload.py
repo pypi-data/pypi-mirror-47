fa54729b9cd55510bc1b32e0d1bc198cc22aec27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package python-csvloader-dpp-example/4.3/python-csvloader-dpp-example-4.3.tar.gz

---
+++ packages/python-csvloader-dpp-example/python-csvloader-dpp-example-4.3.tar.gz/python-csvloader-dpp-example-4.3/csvloader/csvload.py
@@ -0,0 +1,11 @@
+import csv, pandas
+
+def load_csv(path):
+    with open(path,'rt')as f:
+        data = csv.reader(f)
+        for row in data:
+            print(row)
+
+def load_pandas(path):
+    result = pandas.read_csv(path)
+    print(result)
