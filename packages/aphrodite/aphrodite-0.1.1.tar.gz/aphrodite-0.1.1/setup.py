431802d03d85071fd205488ceb3e44a486750afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package aphrodite/0.1.1/aphrodite-0.1.1.tar.gz

---
+++ packages/aphrodite/aphrodite-0.1.1.tar.gz/aphrodite-0.1.1/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+setup(
+    name='aphrodite',
+    version='0.1.1',
+    description='ENOS API',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Kid QU',
+    license='GPLv3',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=find_packages(),
+    platforms=['all'],
+    zip_safe=False,
+    install_requires=[
+        'pycryptodome>=3.8.2',
+        'simplejson>=3.16.0',
+    ],
+)
