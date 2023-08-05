e03ff046d450dd1f4fa8bc3776f51ab5a3d321fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:29 +0000

    Add package galvandi-sorting-magic2/0.1.3/galvandi_sorting_magic2-0.1.3.tar.gz

---
+++ packages/galvandi-sorting-magic2/galvandi_sorting_magic2-0.1.3.tar.gz/galvandi_sorting_magic2-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='galvandi_sorting_magic2',
+      version='0.1.3',
+      description='Just some magic',
+      url='http://github.com/galvandi',
+      author='galvandi',
+      author_email='galvandi@derp.com',
+      license='MIT',
+      packages=['galvandi_sorting_magic2'],
+      zip_safe=False),
