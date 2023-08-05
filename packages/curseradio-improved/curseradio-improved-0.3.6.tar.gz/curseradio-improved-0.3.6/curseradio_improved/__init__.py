adcd2d57384917f32eb851478acc904194c83c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:23 +0000

    Add package curseradio-improved/0.3.6/curseradio-improved-0.3.6.tar.gz

---
+++ packages/curseradio-improved/curseradio-improved-0.3.6.tar.gz/curseradio-improved-0.3.6/curseradio_improved/__init__.py
@@ -0,0 +1,5 @@
+__author__ = """
+Gordon Ball <gordon@chronitis.net>
+Daniel Schuette <d.schuette@online.de>
+"""
+__version__ = "v0.3.6"
---
+++ packages/curseradio-improved/curseradio-improved-0.3.6.tar.gz/curseradio-improved-0.3.6/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import os
+
+from setuptools import setup
+
+__version__ = None
+description = "An improved Curses interface for listening to internet radio."
+
+with open(
+        os.path.join(os.path.dirname(__file__),
+                     "curseradio_improved", "__init__.py")
+) as f:
+    for line in f.readlines():
+        if line.startswith("__version__"):
+            exec(line)
+
+
+def readme():
+    """
+    Import README.md as a long package description.
+    """
+    with open(
+            os.path.join(os.path.dirname(__file__),
+                         "README.md"), encoding="utf-8"
+    ) as f:
+        return f.read()
+
+
+setup(
+    name="curseradio-improved",
+    version=__version__,
+    description=description,
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    author="Daniel Schuette",
+    author_email="d.schuette@online.de",
+    url="https://github.com/DanielSchuette/curseradio-improved",
+    packages=["curseradio_improved"],
+    license="MIT",
+    requires=["lxml", "requests", "pyxdg"],
+    python_requires=">=3.7",
+    package_data={
+        "curseradio_improved": ["curseradio_improved/configs.json"],
+    },
+    entry_points={
+        "console_scripts":
+        "curseradio-improved = curseradio_improved.__main__:main"
+    }
+)
