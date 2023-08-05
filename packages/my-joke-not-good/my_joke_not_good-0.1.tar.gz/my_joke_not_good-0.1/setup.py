2d0fa76caa3b9336eec7c7387ed2a7f155fea218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package my-joke-not-good/0.1/my_joke_not_good-0.1.tar.gz

---
+++ packages/my-joke-not-good/my_joke_not_good-0.1.tar.gz/my_joke_not_good-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='my_joke_not_good',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['my_joke_not_good'],
+      zip_safe=False)
