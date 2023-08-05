fc2bdc7a0f3b0a09f8794b1e02b3bba195382aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package quic-qpack/0.0/quic-qpack-0.0.tar.gz

---
+++ packages/quic-qpack/quic-qpack-0.0.tar.gz/quic-qpack-0.0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+
+setup(name="quic-qpack", version="0.0", author="Seth Michael Larson", author_email="sethmichaellarson@gmail.com", url="https://pypi.org/project/quic-qpack")
