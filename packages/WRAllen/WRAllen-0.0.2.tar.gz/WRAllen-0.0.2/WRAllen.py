d5e4a8d5b2960cfc4a821b4627c4321a32c61a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package WRAllen/0.0.2/WRAllen-0.0.2.tar.gz

---
+++ packages/WRAllen/WRAllen-0.0.2.tar.gz/WRAllen-0.0.2/WRAllen.py
@@ -0,0 +1,4 @@
+def wrallen_print(str):
+	print("欢迎使用WRAllen打印功能")
+	print("您想打印的是：%s" % str)
+	print("打印结束")
---
+++ packages/WRAllen/WRAllen-0.0.2.tar.gz/WRAllen-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='WRAllen',
+    version='0.0.2',
+    py_modules=['WRAllen'],
+    author="WRAllen",
+    author_email="1072274105@qq.com",
+    description="这是一个测试版本",
+)
