274f6d0903be26367d9033b8ed9c8f7087e7007b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:07 +0000

    Add package sixecho/0.1.7.1/sixecho-0.1.7.1.tar.gz

---
+++ packages/sixecho/sixecho-0.1.7.1.tar.gz/sixecho-0.1.7.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRED_PACKAGES = ['datasketch>=1.4.3,<1.5.0', 'deepcut>=0.6.1.0']
+
+setuptools.setup(
+    name="sixecho",
+    version="0.1.7.1",
+    author="six.network",
+    author_email="dev-lead@six.network",
+    description="digital contents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thesixnetwork/SIXEcho",
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    license='MIT',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
