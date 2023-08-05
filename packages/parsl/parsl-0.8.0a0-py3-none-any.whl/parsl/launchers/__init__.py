f6500c5d0e6feec18a04fa79d7207cd678c46e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package parsl/0.8.0a0/parsl-0.8.0a0-py3-none-any.whl

---
+++ packages/parsl/parsl-0.8.0a0-py3-none-any.whl/parsl/launchers/__init__.py
@@ -0,0 +1,13 @@
+from parsl.launchers.launchers import SimpleLauncher, SingleNodeLauncher, \
+    SrunLauncher, AprunLauncher, SrunMPILauncher, AprunLauncher, \
+    GnuParallelLauncher, MpiExecLauncher, MpiRunLauncher
+
+__all__ = ['SimpleLauncher',
+           'SingleNodeLauncher',
+           'SrunLauncher',
+           'AprunLauncher',
+           'SrunMPILauncher',
+           'AprunLauncher',
+           'GnuParallelLauncher',
+           'MpiExecLauncher',
+           'MpiRunLauncher']
