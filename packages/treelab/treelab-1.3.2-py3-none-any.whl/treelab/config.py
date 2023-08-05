85007e6d2e85f05daaec666a7b4f6650d0c5f5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:32 +0000

    Add package treelab/1.3.2/treelab-1.3.2-py3-none-any.whl

---
+++ packages/treelab/treelab-1.3.2-py3-none-any.whl/treelab/config.py
@@ -0,0 +1,22 @@
+# server
+workspace_id = 'wsp3cfc3e8421875757'
+# core 1
+core_id_1 = 'cre3cfc4a350087707b'
+view_id_1 = 'viw3cfc6afce18a90e5'
+table_id_1 = 'tbl3cfc54063e86a42a'
+column_id_1 = 'col3cfde176e806f3ac'
+column_name_1 = 'col_name1'
+view_name_1 = 'viewname'
+row_id_1 = 'row3cfdb38a44890b45'
+
+
+# # local
+# workspace_id = 'wspb59083e8e085e9af'
+# # core 1
+# core_id_1 = 'creb59083ec830851cc'
+# view_id_1 = 'viwb59083f069813b22'
+# table_id_1 = 'tblb59083ee9505054d'
+# column_id_1 = 'colb59084087b8259b7'
+# column_name_1 = 'col_name1'
+# view_name_1 = 'viewname'
+# row_id_1 = 'rowb590840785034627'
