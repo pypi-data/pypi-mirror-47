bbc8588865c49fda57b1be3a224b0b722c7ca4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:32 +0000

    Add package tmalibrary/4.7/tmalibrary-4.7.tar.gz

---
+++ packages/tmalibrary/tmalibrary-4.7.tar.gz/tmalibrary-4.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+    name='tmalibrary',
+    version='4.7',
+    author='Rui Silva',
+    author_email='rfsilv@student.dei.uc.pt',
+    description='Libraries to TMA',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url='https://github.com/rfrsilva/tmalibrary',
+    packages=find_packages(),
+    classifiers=[
+    	"Programming Language :: Python :: 3",
+    	"License :: OSI Approved :: MIT License",
+    	"Operating System :: OS Independent",
+    ],
+)
---
+++ packages/tmalibrary/tmalibrary-4.7.tar.gz/tmalibrary-4.7/tmalibrary/probes/__init__.py
@@ -0,0 +1,5 @@
+from tmalibrary.probes.communication import Communication
+from tmalibrary.probes.data import Data
+from tmalibrary.probes.message import Message
+from tmalibrary.probes.message import ComplexEncoder
+from tmalibrary.probes.observation import Observation
