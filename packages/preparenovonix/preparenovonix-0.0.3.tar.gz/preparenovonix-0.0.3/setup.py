8090ff9e8662ca842fa336d3dfd48a5a64405313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package preparenovonix/0.0.3/preparenovonix-0.0.3.tar.gz

---
+++ packages/preparenovonix/preparenovonix-0.0.3.tar.gz/preparenovonix-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+desc = "Clean and add extra information to data " + \
+    "produced by the battery cyclers from Novonix."
+
+setuptools.setup(
+    name='preparenovonix',
+    version='0.0.3',
+    py_modules=['preparenovonix'],
+    description=desc,
+    long_description=long_description,
+    url="https://github.com/BatLabLancaster/preparenovonix",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
