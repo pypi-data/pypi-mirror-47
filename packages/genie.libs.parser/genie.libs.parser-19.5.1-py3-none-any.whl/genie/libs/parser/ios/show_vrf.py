7160e1496a2a5d72bb4fc0c2af928e8734389b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:33 +0000

    Add package genie.libs.parser/19.5.1/genie.libs.parser-19.5.1-py3-none-any.whl

---
+++ packages/genie.libs.parser/genie.libs.parser-19.5.1-py3-none-any.whl/genie/libs/parser/ios/show_vrf.py
@@ -0,0 +1,23 @@
+"""show_vrf.py
+
+IOS parsers for the following show commands:
+    * 'show vrf detail'
+    * 'show vrf
+"""
+
+# Python
+import re
+import xmltodict
+
+# genieparser
+from genie.libs.parser.iosxe.show_vrf import ShowVrfDetail as ShowVrfDetail_iosxe
+from genie.libs.parser.iosxe.show_vrf import ShowVrf as ShowVrf_iosxe
+
+
+class ShowVrfDetail(ShowVrfDetail_iosxe):
+    """Parser for show vrf detail"""
+    pass
+
+class ShowVrf(ShowVrf_iosxe):
+    """Parser for show vrf"""
+    pass
