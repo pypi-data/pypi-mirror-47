bd39c4eca1da419e461806dc8cec54853aa5f665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:07 +0000

    Add package both/0.1.1/both-0.1.1.tar.gz

---
+++ packages/both/both-0.1.1.tar.gz/both-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='both',
+    version='0.1.1',
+    author='Grant Bakker',
+    author_email='grant@bakker.pw',
+    description='Python compatibility layer',
+    url='https://bakkerthehacker.github.io/both/',
+    classifiers=(
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ),
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    install_requires=['six', 'source-transform>=0.1.2', 'future'],
+)
