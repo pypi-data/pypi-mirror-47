1abf36320259410fbeb732466e700fc62c846d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:25 +0000

    Add package stytra/0.8.11/stytra-0.8.11.tar.gz

---
+++ packages/stytra/stytra-0.8.11.tar.gz/stytra-0.8.11/stytra/examples/eye_tracking_exp.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from stytra import Stytra
+from stytra.examples.windmill_exp import WindmillProtocol
+
+
+class TrackingWindmillProtocol(WindmillProtocol):
+    name = "windmill"
+
+    # To add tracking to a protocol, we simply need to add a tracking
+    # argument to the stytra_config:
+    stytra_config = dict(
+        tracking=dict(embedded=True, method="eyes_tail"),
+        camera=dict(
+            video_file="/Users/luigipetrucco/Desktop/160222_1.avi"
+            #video_file=str(Path(__file__).parent / "assets" /
+            # "fish_compressed.h5")
+        ),
+    )
+
+
+if __name__ == "__main__":
+    s = Stytra(protocol=TrackingWindmillProtocol())
