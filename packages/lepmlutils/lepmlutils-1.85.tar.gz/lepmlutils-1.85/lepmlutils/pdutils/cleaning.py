88499a0681656ef8e2f5f6731998887de01ae8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:46 +0000

    Add package lepmlutils/1.85/lepmlutils-1.85.tar.gz

---
+++ packages/lepmlutils/lepmlutils-1.85.tar.gz/lepmlutils-1.85/lepmlutils/pdutils/cleaning.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+# for every column in the dataframe with at least 1 bad value
+# another column is created with 1's for all entities with
+# bad values in the original column.
+def add_bad_indicator_vars(df: pd.DataFrame):
+    df.apply(process_col, args=(df,))
+
+def process_col(col: pd.Series, df: pd.DataFrame):
+    pass
+    if col.isna().any():
+        bad_col_name = col.name + "_is_bad"
+        assert(bad_col_name not in df)
+
+        df[bad_col_name] = 0
+        df[bad_col_name].loc[col.isna()] = 1
+
+def replace_bad_values_with_median(df: pd.DataFrame):
+        df.apply(replace_bad_col_values_with_median, args=(df,))
+
+def replace_bad_col_values_with_median(col: pd.Series, df:pd.DataFrame):
+    if col.isna().any():
+        try:
+                df[col.name] = col.fillna(col.median())
+        except TypeError: 
+                assert "unknown" not in col.values
+                df[col.name] = col.fillna("unknown")
