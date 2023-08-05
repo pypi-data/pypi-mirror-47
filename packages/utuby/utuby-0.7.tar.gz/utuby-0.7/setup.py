1dcf0a46672ec4bcb9deaa47eddfd8191f17def1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:44 +0000

    Add package utuby/0.7/utuby-0.7.tar.gz

---
+++ packages/utuby/utuby-0.7.tar.gz/utuby-0.7/setup.py
@@ -0,0 +1,40 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://santhoshse7en.github.io/utuby/
+https://santhoshse7en.github.io/utuby_doc/
+"""
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+# Always prefer setuptools over distutils
+import setuptools
+
+keywords = ['youtube', "comments", "without-api", "youtube_scraper", 'youtube_comments', 'vaderSentiment',
+'bs4', 'textblob', 'youtube_sentiment', "sentiment_analysis", 'youtube_analysis', 'lxml', 'ajax']
+
+setuptools.setup(
+    name="utuby",
+    version="0.7",
+    author="M Santhosh Kumar",
+    author_email="santhoshse7en@gmail.com",
+    description="Python package which to access the YouTube database without API",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://santhoshse7en.github.io/utuby/",
+    keywords = keywords,
+    install_requires=['beautifulsoup4', 'pandas', 'vaderSentiment', 'textblob', 'lxml', 'cssselect', 'requests'],
+    packages = setuptools.find_packages(),
+    classifiers=['Development Status :: 4 - Beta',
+              'Intended Audience :: End Users/Desktop',
+              'Intended Audience :: Developers',
+              'Intended Audience :: System Administrators',
+              'License :: OSI Approved :: MIT License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Topic :: Communications :: Email',
+              'Topic :: Office/Business',
+              'Topic :: Software Development :: Bug Tracking',
+              ],
+)
