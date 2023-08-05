c0246b215891532b9b714203d65a9217d496119d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:32 +0000

    Add package snakepyi/0.0.1/snakepyi-0.0.1.tar.gz

---
+++ packages/snakepyi/snakepyi-0.0.1.tar.gz/snakepyi-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='snakepyi',
+      version='0.0.1',
+      description='rapid api development framework in python',
+      url='https://snakepi.com',
+      author='oxbits',
+      author_email='alex@oxbits.com',
+      packages=['snakepi'],
+      zip_safe=False)
+
