0c65eba4e6543d835866a055872a11dd3eb6dea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package lepmlutils/1.81/lepmlutils-1.81.tar.gz

---
+++ packages/lepmlutils/lepmlutils-1.81.tar.gz/lepmlutils-1.81/lepmlutils/xgbutils/__init__.py
@@ -0,0 +1,5 @@
+# This package adds helper methods to the XGBoost package. More info on this package here: https://xgboost.readthedocs.io/en/latest/
+
+from .settuner import SetTuner
+from .tuner import Tuner
+from .partition import Partition
