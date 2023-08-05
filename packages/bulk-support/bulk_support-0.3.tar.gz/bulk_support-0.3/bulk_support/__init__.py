96da614cedbd80dd511940b5adf6b1a64453b0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:57 +0000

    Add package bulk-support/0.3/bulk_support-0.3.tar.gz

---
+++ packages/bulk-support/bulk_support-0.3.tar.gz/bulk_support-0.3/bulk_support/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from .bulk_support import SalesforceBulk, BulkApiError, UploadResult
+from .csv_adapter import CsvDictsAdapter
+from .__version__ import __version__
