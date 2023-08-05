654b0bc02e9fe1c06e54b201f878bc95e674e397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:49 +0000

    Add package xyscript/0.2.4.2/xyscript-0.2.4.2.tar.gz

---
+++ packages/xyscript/xyscript-0.2.4.2.tar.gz/xyscript-0.2.4.2/xyscript/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+#-*- encoding:utf-8 -*-
+# from __future__ import print_function
+
+__version__ = '0.2.4.2'
+__des__     = 'added version checking function at the end of each execution'
+
+#缓存文件存储路径
+log_store_local = "/dist/" 
+
+class Config:
+    def get(self,project):
+        print(project)
+        # pass
+    def get_version(self):
+        return __version__
+        
