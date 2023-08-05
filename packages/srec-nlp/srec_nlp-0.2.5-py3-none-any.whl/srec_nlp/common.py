673c2af1c55abe075965a8884c34653b42b3e093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:04 +0000

    Add package srec-nlp/0.2.5/srec_nlp-0.2.5-py3-none-any.whl

---
+++ packages/srec-nlp/srec_nlp-0.2.5-py3-none-any.whl/srec_nlp/common.py
@@ -0,0 +1,108 @@
+import functools
+import itertools
+from io import IOBase
+from pathlib import Path
+from typing import List, Dict, Callable, Union
+import copy
+import attr
+import asyncio
+
+
+@attr.s(frozen=True)
+class Score:
+    positive: float = attr.ib(factory=float)
+    negative: float = attr.ib(factory=float)
+    classification: str = ""
+
+    def __attrs_post_init__(self):
+        classes = ("positive", "negative")
+        filt = [getattr(self, c) for c in classes]
+        mx = max(filt)
+        object.__setattr__(
+            self,
+            "classification",
+            next(itertools.compress(classes, map(mx.__eq__, filt))),
+        )
+
+    @classmethod
+    def fromsingle(cls, value):
+        """
+        :param value: single value score
+        :return:
+        """
+        negative = 1 - value
+        positive = value
+
+        return Score(positive=positive, negative=negative)
+
+    @classmethod
+    def fromtriplet(cls, positive, neutral, negative):
+        """
+        :param positive:  Confidence
+        :param neutral: Confidence
+        :param negative: Confidence
+        :return: Score
+        """
+        positive += neutral / 2
+        negative += neutral / 2
+        return Score(positive=positive, negative=negative)
+
+
+@attr.s(frozen=True)
+class Query:
+    content: str = attr.ib(factory=str)
+    score: Score = attr.ib(default=Score())
+
+
+def _ensure_utf(
+    func: Callable[[str], Query]
+) -> Callable[[Union[IOBase, str, Path, bytes]], Query]:
+    """
+
+    :param func: Query function to wrap
+    :return: Returns a wrapper for the provided function.
+    """
+
+    @functools.singledispatch
+    def _text_is_utf(text: str):
+        return text.replace("\n", "")
+
+    @_text_is_utf.register(bytes)
+    def _(text: bytes):
+        return text.decode("utf-8").replace("\n", "")
+
+    @_text_is_utf.register(Path)
+    def _(path: Path):
+        text = path.read_text("utf-8").replace("\n", "")
+        return text
+
+    @_text_is_utf.register(IOBase)
+    def _(fp: IOBase):
+        text = "".join(map(str, fp.readlines())).replace("\n", "")
+        return text
+
+    @functools.wraps(func)
+    def wrapper(self, text):
+        text = _text_is_utf(text).replace("\n", "")
+        return func(self, text)
+
+    return wrapper
+
+
+def _parallel(func):
+    def listd(self, query):
+        async def list_internal(self, query):
+            return func(self, query)
+        async def asyncwrapper():
+            return await asyncio.gather(*(list_internal(self, q) for q in query))
+       
+        return asyncio.run(asyncwrapper())
+        
+
+    @functools.wraps(func)
+    def internal(self, query):
+        if isinstance(query, list):
+            return listd(self, query)
+        return func(self, query)
+
+    return internal
