7ec016a5a715d53510b53734fc1e8f4da64165fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package forecastlib/0.1.37/forecastlib-0.1.37-py3-none-any.whl

---
+++ packages/forecastlib/forecastlib-0.1.37-py3-none-any.whl/forecastlib/training/Metrics.py
@@ -0,0 +1,16 @@
+import keras.backend as K
+import tensorflow as tf
+
+
+class Metrics(object):
+
+	@staticmethod
+	def absolute_relative_error(y, y_pred):
+		error = K.sum(K.abs(y - y_pred))
+		return error/K.sum(y)
+
+	@staticmethod
+	def model_accuracy(y, y_pred):
+		with tf.Session().as_default():
+			return 1 - Metrics.absolute_relative_error(y, y_pred).eval()
+
