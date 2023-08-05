e21ee01da44257d1f976edeee48af3babdca1c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:25 +0000

    Add package ec2ools/0.1.5/ec2ools-0.1.5-py2.py3-none-any.whl

---
+++ packages/ec2ools/ec2ools-0.1.5-py2.py3-none-any.whl/ec2ools/cli_eip.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for ec2ools."""
+import sys
+import click
+from ec2ools.aws.eip import EipBase
+
+
+@click.group()
+def main(args=None):
+    pass
+
+
+@main.command(name='allocate')
+@click.argument('id',
+              type=str,
+              help='Allocation-id for EIP or path to file containing allocation-id')
+def eip_allocate(**kw):
+
+    print(kw)
+
+    eip = EipBase()
+    eip.allocate_eip(kw.get('id'))
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
