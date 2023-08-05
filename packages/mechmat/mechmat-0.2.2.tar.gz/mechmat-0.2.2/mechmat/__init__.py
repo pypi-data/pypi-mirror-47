2f26317cf7a44b47b23b820f5149274ade00af54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package mechmat/0.2.2/mechmat-0.2.2.tar.gz

---
+++ packages/mechmat/mechmat-0.2.2.tar.gz/mechmat-0.2.2/mechmat/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for mechmat."""
+
+__author__ = """Jelle Spijker"""
+__email__ = 'spijker.jelle@gmail.com'
+__version__ = '0.2.2'
+
+from pint import UnitRegistry, set_application_registry
+
+ureg = UnitRegistry(autoconvert_offset_to_baseunit=True, default_as_delta=False)
+Q_ = ureg.Quantity
+set_application_registry(ureg)
+
+from mechmat.core.bibliography import bib
+from mechmat.core.chainable import Chainable, Guarded
+from mechmat.material import material_factory
