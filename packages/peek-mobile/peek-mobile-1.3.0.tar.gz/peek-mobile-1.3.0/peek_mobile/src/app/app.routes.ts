b46732b2ab398eb75b39ee6bf0d0ea2cae5a9c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:06 +0000

    Add package peek-mobile/1.3.0/peek-mobile-1.3.0.tar.gz

---
+++ packages/peek-mobile/peek-mobile-1.3.0.tar.gz/peek-mobile-1.3.0/peek_mobile/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import {MainHomeComponent} from "./main-home/main-home.component";
+import {MainConfigComponent} from "./main-config/main-config.component";
+import {UnknownRouteComponent} from "./unknown-route/unknown-route.component";
+
+import {DeviceEnrolledGuard} from "@peek/peek_core_device";
+
+import {pluginAppRoutes} from "../plugin-app-routes";
+import {pluginCfgRoutes} from "../plugin-cfg-routes";
+
+export const staticRoutes = [
+    {
+        path: 'peek_core_device',
+        loadChildren: "peek_core_device/device.module#DeviceModule"
+    },
+    // All routes require the device to be enrolled
+    {
+        path: '',
+        canActivate: [DeviceEnrolledGuard],
+        children: [
+            {
+                path: '',
+                component: MainHomeComponent
+            },
+            ...pluginAppRoutes,
+            ...pluginCfgRoutes
+        ]
+    },
+    {
+        path: 'config',
+        component: MainConfigComponent
+    },
+    {
+        path: "**",
+        component: UnknownRouteComponent
+    }
+];
