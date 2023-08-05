724deb571fc0dcd0a5760b505c2dd5309c5647c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:33 +0000

    Add package gsheet-df/0.0.2/gsheet_df-0.0.2-py3-none-any.whl

---
+++ packages/gsheet-df/gsheet_df-0.0.2-py3-none-any.whl/gsheet_df/code.py
@@ -0,0 +1,24 @@
+import requests
+import pandas as pd
+from io import StringIO
+
+def download(url, table, element_sep = '@', row_sep = '\n', auth='', str_only=False):
+    """
+    download(url, table, element_sep = '@', row_sep = '\\n', auth='', str_only=False)
+    
+    url: the url of googlesheet
+    table: names of tables, separated by ','
+    auth: secret key for authentication
+    srt_only: return csv text instead of pd.dataframe
+    """
+    data = {
+        'auth': auth,
+        'table': table,
+        'elementsep': element_sep,
+        'rowsep': row_sep,
+    }
+    response = requests.post(url, data=data)
+    if str_only:
+        return response.text
+    else:
+        return pd.read_csv(StringIO(response.text), sep=element_sep)
