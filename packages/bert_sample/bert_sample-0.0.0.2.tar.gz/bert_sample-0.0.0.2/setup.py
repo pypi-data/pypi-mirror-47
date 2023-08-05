a15c90f2ad5d860e0da87691e20fd5e7960e7dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:55 +0000

    Add package bert_sample/0.0.0.2/bert_sample-0.0.0.2.tar.gz

---
+++ packages/bert_sample/bert_sample-0.0.0.2.tar.gz/bert_sample-0.0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+setup(
+    name='bert_sample',
+    version='0.0.0.2',
+    description='bert',
+    #long_description=open('README.MD', 'r').read(),
+    author='Terry Chan',  #
+    author_email='napoler2008@gmail.com',
+    url='https://www.terrychan.org',
+    # packages=find_packages(),
+    packages=['bert_sample'],  #
+    install_requires=[
+        'numpy',
+        'flask',
+        'tqdm',
+        'Terry_toolkit',
+        'pytorch_pretrained_bert',
+        'torch'
+
+
+    ]
+
+
+
+    )
+
+# python setup.py sdist
+# #python setup.py install
+# python setup.py sdist upload
