f8e56d0843092d0586f5c406707d8ffcca786721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:23 +0000

    Add package snakepy/0.0.1/snakepy-0.0.1.tar.gz

---
+++ packages/snakepy/snakepy-0.0.1.tar.gz/snakepy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='snakepy',
+      version='0.0.1',
+      description='rapid api development framework in python',
+      url='https://snakepy.com',
+      author='oxbits',
+      author_email='alex@oxbits.com',
+      packages=['snakepy'],
+      zip_safe=False)
+
---
+++ packages/snakepy/snakepy-0.0.1.tar.gz/snakepy-0.0.1/snakepy/__init__.py
@@ -0,0 +1,3 @@
+def snakepy():
+    return (u'snakepy')
+
