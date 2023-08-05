59753f942022fd7be7794af8aa2dde92c042da47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:08 +0000

    Add package tio/3.3/tio-3.3.tar.gz

---
+++ packages/tio/tio-3.3.tar.gz/tio-3.3/tio/tio_session.py
@@ -0,0 +1,426 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+Twinleaf IO (tio) - A serialization for instrumentation
+Copyright 2017-2019 Twinleaf LLC
+License: MIT
+
+A simple protocol for working with sensors through single-port/serial connections.
+"""
+
+import serial
+import socket
+import threading
+import struct
+import urllib.parse
+import time
+import queue
+import slip
+import hexdump
+import logging
+import pickle
+import tempfile
+import os
+from .tio_protocol import *
+
+class TLRPCException(Exception):
+    pass
+
+class TIOSession(object):
+  def __init__(self, url="tcp://localhost", verbose=False, connectingMessage = True, rpcs=[], stateCache = True):
+
+    if verbose:
+      logLevel = logging.DEBUG
+    else:
+      logLevel = logging.ERROR
+    logging.basicConfig(level=logLevel)
+    self.logger = logging.getLogger('tio-session')
+
+    # Connect to either TCP socket or serial port
+    self.uri = urllib.parse.urlparse(url)
+    if self.uri.scheme in ["tcp", "udp"]:
+      if self.uri.port is None:
+        self.port = 7855
+      else:
+        self.port = self.uri.port
+      routingStrings = self.uri.path.split('/')[1:]
+      if self.uri.scheme == "tcp":
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.uri.hostname, self.port))
+      elif self.uri.scheme == "udp":
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #self.socket.bind(("", self.port))
+      self.socket.settimeout(0) # Non-blocking mode
+    else:
+      # Try treating as serial
+      # Deal with non-standard url for routing
+      # linux: /dev/tty0/0/1
+      # mac: /dev/cu.usbmodem1421/0/1
+      # windows: COM1/0/1
+      spliturl = url.split('/')
+      try:
+        if spliturl[0].upper().startswith('COM'): # Windows
+          url = spliturl[0]
+          routingStrings = spliturl[1:]
+        elif spliturl[1].lower()=='dev': # *nix
+          url = '/'.join(spliturl[:3])
+          routingStrings = spliturl[3:]
+        else:
+          raise
+      except:
+        raise Exception("Unknown url format.")
+      self.buffer = bytearray()
+      self.serial = serial.serial_for_url(url, baudrate=115200, timeout=1)
+      self.serial.reset_input_buffer()
+
+    routingStrings = filter(lambda a: a != '', routingStrings)
+    try:
+      self.routing = [ int(address) for address in routingStrings ]
+      self.routing = self.routing[::-1] # First child node is outermost routing byte 
+    except:
+      raise Exception(f'Bad routing path: {routingString}')
+
+    # Init TIO protocol state
+    self.protocol = TIOProtocol(routing = self.routing)
+
+    # Initialize queues and threading controls
+    self.pub_queue = queue.Queue(maxsize=100)
+    self.req_queue = queue.Queue(maxsize=1)
+    self.rep_queue = queue.Queue(maxsize=1)
+    self.lock = threading.Lock()
+    self.alive = True
+
+    # Launch socket management thread
+    self.socket_recv_thread = threading.Thread(target=self.recv_thread)
+    self.socket_recv_thread.daemon = True
+    self.socket_recv_thread.name = 'recv-thread'
+    self.socket_recv_thread.start()
+    self.socket_send_thread = threading.Thread(target=self.send_thread)
+    self.socket_send_thread.daemon = True
+    self.socket_send_thread.name = 'send-thread'
+    self.socket_send_thread.start()
+
+    # Startup RPCs
+    for topic, rpcType, value in rpcs:
+      if type(rpcType) is str: # Find type from dict of types
+        rpcType = list(TYPES.keys())[ [entry[1] for entry in TYPES.values()].index(rpcType) ]
+      if rpcType == NONE_T:
+        self.rpc(topic, value.encode('utf-8'))
+      else:
+        if rpcType == FLOAT32_T or rpcType == FLOAT64_T:
+          value = float(value)
+        else:
+          value = int(value)
+        self.rpc_val(topic, rpcType, value)
+
+    # Do a quick first name check
+    desc = self.rpc('dev.desc').decode('utf-8')
+    if connectingMessage:
+      print(f"{desc}")
+
+    # Query rpcs and streams
+    
+    # Try to load from cache!
+    cacheFilename = desc.replace('/','-')+".pickle"
+    pickleCacheDir = os.path.join(tempfile.gettempdir(), 'com.twinleaf.tio.python.cache')
+    pickleCacheFile = os.path.join(pickleCacheDir, cacheFilename)
+    if os.path.isfile(pickleCacheFile) and stateCache:
+      with open(pickleCacheFile, "rb") as f:
+        [protocolState, rpcState] = pickle.load(f)
+      # Perform other qualification checks!
+      [self.rpcs, self.rpcNames] = rpcState
+      self.protocol.stateImport(protocolState)
+    else:
+      # RPCs are stashed here
+      self.rpcs = []
+      self.rpcNames = {}
+      self.rpcList()
+      self.data_send_all()
+      time.sleep(0.5) # Wait to make sure all the send_all info came through
+      rpcState = [self.rpcs, self.rpcNames]
+      protocolState = self.protocol.stateExport()
+      if not os.path.exists(pickleCacheDir):
+        os.makedirs(pickleCacheDir)
+      with open(pickleCacheFile, "wb") as f:
+        pickle.dump( [protocolState, rpcState], f)
+    self.logger.info(f"Found {len(self.rpcs)} RPCs and {len(self.protocol.sources)} data sources")
+
+  def close(self):
+    # TODO: Notify threads to quit
+    pass
+
+  def recv_thread(self):
+    while True:
+      try:
+        packet = self.recv()
+      except IOError as e:
+        # for now, just exit, TODO: reconnect?
+        # probably some I/O problem such as disconnected USB serial
+        #print("\x1Bc") # fix up after interactive python crash, TODO
+        self.logger.error(f"Error: {e}")
+        import os
+        os._exit(0)
+      # Handle stream
+      if packet['type'] == TL_PTYPE_STREAM0:
+        try:
+          self.pub_queue.put(packet, block=False)
+        except queue.Full:
+          self.pub_queue.get() # Toss a packet
+          self.pub_queue.put(packet, block=False)
+        # except queue.Empty:
+        #   self.logger.error(f"No response. Timeout.")
+        #   import os
+        #   os._exit(0)
+      # Handle RPCs
+      elif packet['type'] == TL_PTYPE_RPC_REP or packet['type'] == TL_PTYPE_RPC_ERROR:
+        try:
+          self.rep_queue.put(packet, block=False)
+        except queue.Full:
+          self.rep_queue.get() # Toss a packet
+          self.rep_queue.put(packet, block=False)
+          self.logger.error("Tossing an unclaimed REP!")
+
+
+  def send_thread(self):
+    while True:
+      # Blocks
+      try:
+        self.send(self.req_queue.get(timeout=0.5))
+      except queue.Empty:
+        pass
+      # Send heartbeat; need to regulate this somewhat
+      #print("❤️")
+      self.send(self.protocol.heartbeat())
+
+  def pub_flush(self):
+    while not self.pub_queue.empty():
+      try:
+        self.pub_queue.get(block=False)
+      except:
+        break
+
+  def recv_tcp_packet(self):
+    try:
+      header = bytes(self.socket.recv(4))
+    except BlockingIOError:
+      return b''
+    if len(header) != 4:
+      raise IOError("Lost connection")
+    headerFields = struct.unpack("<BBH", header )
+    payloadType, routingSize, payloadSize = headerFields
+    if payloadSize > TL_PACKET_MAX_SIZE or routingSize>TL_PACKET_MAX_ROUTING_SIZE:
+      return b''
+    payload = bytes(self.socket.recv(payloadSize+routingSize))
+    return header+payload
+
+  def recv_udp_packet(self):
+    try:
+      d = self.socket.recvfrom(512)
+      packet = d[0]
+      address = d[1]
+    except BlockingIOError:
+      return b''
+    if len(packet) < 4:
+      return b''
+    headerFields = struct.unpack("<BBH", packet[0:4] )
+    payloadType, routingSize, payloadSize = headerFields
+    if payloadSize > TL_PACKET_MAX_SIZE or routingSize>TL_PACKET_MAX_ROUTING_SIZE:
+      return b''
+    return packet
+
+  def recv_slip_packet(self):
+    while self.alive and self.serial.is_open:
+      try:
+        # read all that is there or wait for one byte (blocking)
+        data = self.serial.read(self.serial.in_waiting or 1)
+      except serial.SerialException as e:
+        raise IOError(f"serial error: {e}")
+      else:
+        if data:
+          self.buffer.extend(data)
+          #print(len(self.buffer))
+          if len(self.buffer)>2000:
+            self.warn_overload()
+          while slip.SLIP_END_CHAR in self.buffer:
+            packet, self.buffer = self.buffer.split(slip.SLIP_END_CHAR, 1)
+            try:
+              return slip.decode(packet)
+            except slip.SLIPEncodingError as error:
+              self.logger.debug(error);
+              #hexdump.hexdump(packet)
+              #self.logger.exception(error)
+              return b""
+
+  def send(self, packet):
+    if self.uri.scheme == "tcp":
+      self.socket.send(packet)
+    elif self.uri.scheme == "udp":
+      self.socket.sendto(packet,(self.uri.hostname, self.port))
+    else:
+      #hexdump.hexdump(slip.encode(packet))
+      self.serial.write(slip.encode(packet))
+
+  def recv(self):
+    if self.uri.scheme == "tcp":
+      packet = self.recv_tcp_packet()
+    elif self.uri.scheme == "udp":
+      packet = self.recv_udp_packet()
+    else:
+      packet = self.recv_slip_packet()
+    try:
+      # Filter routing here? TODO
+      return self.protocol.decode_packet(packet)
+    except Exception as error:
+      self.logger.debug('Error decoding packet:');
+      hexdump.hexdump(packet)
+      self.logger.exception(error)
+      return { 'type':TL_PTYPE_INVALID }
+
+  def send_req(self, topic = "dev.desc", payload = None):
+    msg, requestID = self.protocol.req(topic, payload)
+    self.req_queue.put(msg)
+    return requestID
+
+  def recv_rep(self, requestID = None):
+    parsedPacket = self.rep_queue.get(timeout=1.5)
+    if requestID is None or requestID == parsedPacket['requestid']:
+      if parsedPacket['type'] == TL_PTYPE_RPC_ERROR:
+        raise TLRPCException( TL_RPC_ERRORS[parsedPacket['error']] )
+      if parsedPacket['payload'] == b'':
+        return None
+      else:
+        return parsedPacket['payload']
+
+  def rpc(self, topic = "dev.desc", payload = None):
+    requestID = self.send_req(topic, payload)
+    try: 
+      return self.recv_rep(requestID)
+    except TLRPCException as e:
+      self.logger.error(f"RPC ERROR {topic}: {e}" )
+      raise
+    except queue.Empty as e:
+      self.logger.error(f"RPC TIMEOUT {topic}: {e}" )
+      raise
+
+  def rpc_val(self, topic = "data.source.list", rpcType = FLOAT32_T, value = None, returnRaw = False):
+    if value is not None:
+      if rpcType == STRING_T:
+        reqPayload = value.encode('utf-8')
+      else:
+        reqPayload = struct.pack("<"+TYPES[rpcType][0], value)
+    else:
+      reqPayload = None
+    reply = self.rpc(topic, reqPayload)
+    if reply is not None:
+      if returnRaw:
+        return reply
+      if len(reply) > 0:
+        if rpcType == STRING_T:
+          return reply.decode('utf-8')
+        else:
+          return struct.unpack("<"+TYPES[rpcType][0], reply)[0]
+    return None
+
+  def rpcList(self):
+    self.rpcs = []
+    rpcCount = self.rpc_val("rpc.list", UINT16_T)
+    for rpcNumber in range(rpcCount):
+      rpcInfo = self.rpc_val("rpc.listinfo", UINT16_T, rpcNumber, returnRaw = True)
+      rpcType = rpcInfo[0]
+      rpcFlags = rpcInfo[1]
+      rpcName = rpcInfo[2:].decode('utf-8')
+      rpcMetadataValid = rpcFlags & 0x80 == 0x80
+      rpcWritable = rpcFlags & 0x1 == 0x1
+      rpcReadable = rpcFlags & 0x2 == 0x2
+      rpcStored = rpcFlags & 0x4 == 0x4
+      self.rpcNames[rpcName] = rpcNumber
+      self.rpcs += [{
+        "name":rpcName, 
+        "datatype":rpcType, 
+        "r":rpcReadable, 
+        "w":rpcWritable, 
+        "valid":rpcMetadataValid, 
+        "stored":rpcStored
+      }]
+      self.logger.debug(f"{rpcNumber}: {rpcName}, type: {rpcType}, read: {rpcReadable}, write: {rpcWritable}, valid metadata: {rpcMetadataValid}")
+    #self.rpcs.sort(key=lambda x:x['name'])
+    return self.rpcs
+
+  def data_send_all(self):
+    self.rpc("data.send_all")
+
+  def source_active(self, topic, set=None):
+    if set is not None:
+      self.rpc_val(topic+".data.active", UINT8_T, int(set))
+    else:
+      #return bool(self.rpc_val(topic+".data.active", UINT8_T))
+      return topic in self.protocol.columnsByName.keys()
+
+  def stream_read_raw(self, rows = 1, duration=None, flush=True):
+    if flush:
+      self.pub_flush()
+    data = []
+    while True:
+      parsedPacket = self.pub_queue.get()
+      if parsedPacket['type'] == TL_PTYPE_STREAM0:
+        data += [ self.protocol.stream_data(parsedPacket) ]
+        if len(data) == rows:
+          break
+    if rows == 1:
+      data = data[0]
+    return data
+
+  def stream_read_topic_raw(self, topic, samples = 10):
+    streamInfo = self.protocol.columnsByName[topic]
+    column = streamInfo['stream_column_start']
+    channels = streamInfo['source_channels']
+    data_flat = []
+    while True:
+      parsedPacket = self.pub_queue.get()
+      if parsedPacket['type'] == TL_PTYPE_STREAM0:
+        row = self.protocol.stream_data(parsedPacket) 
+        data_flat += row[column:column+channels]
+        if int(len(data_flat)/channels) >= samples: 
+          break
+    data_flat = data_flat[:channels*samples] # truncate at specified point
+    data = [[row for row in data_flat[column::channels]] for column in range(channels)] # group data by channel
+    if samples == 1:
+      data = [datum[0] for datum in data]
+    if len(data) == 1:
+      data = data[0]
+    return data
+
+  def stream_read_topic(self, topic, samples = 1, duration = None, autoActivate=True, flush=True):
+    if autoActivate:
+      wasActive = self.source_active(topic)
+      if not wasActive:
+        self.source_active(topic, True)
+    if duration is not None:
+      samples = int(duration * self.protocol.sources[topic]['Fs'])
+    if flush:
+      self.pub_flush()
+    data = self.stream_read_topic_raw(topic, samples)
+    if autoActivate and not wasActive:
+      self.source_active(topic, False)
+    return data
+
+  def source_rate(self, topic):
+    streamInfo = self.protocol.columnsByName[topic]
+    return streamInfo['Fs']
+
+  def pub_warn_overload(self):
+    if self.pub_queue.qsize() > .95*self.pub_queue.maxsize:
+      self.warn_overload()
+
+  def warn_overload(self):
+    self.logger.error("Buffer overfow. Python didn't keep up with the incoming data rate.")
+    self.logger.error("Option 1: Reduce the data rate; for 10 Hz add '--rpc data.rate:f32:10'")
+    self.logger.error("Option 2: Reduce the data rate; for 10 Hz add '--rpc gmr.data.decimation:u32:80'")
+    self.logger.error("Option 3: Use the tio proxy to offload the SLIP decoding: https://github.com/twinleaf/tio-tools")
+    import os
+    os._exit(0)
+
+
+
+
+
---
+++ packages/tio/tio-3.3.tar.gz/tio-3.3/tldevice/tldevice.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+Twinleaf Generic Device Control
+Copyright 2017 Twinleaf LLC
+License: MIT
+
+"""
+
+import tio
+from .tl_cmd_devinfo import *
+from .tl_cmd_data import *
+import types
+import re
+
+class Device():
+  def __init__(self, url="tcp://localhost", verbose=False, rpcs=[], stateCache=True):
+    self._tio = tio.TIOSession(url, verbose=verbose, rpcs=rpcs, stateCache=stateCache)
+    self.dev = TwinleafDevInfoController(self)
+    self._add_sources()
+    self._add_rpcs()
+    if self._tio.protocol.sources != {}:
+      self.data = TwinleafDataController(self)
+    self._longname = self.dev.desc()
+    clean = lambda varStr: re.sub('\W|^(?=\d)','_', varStr)
+    self._shortname = clean(self.dev.name()).lower()
+
+  def _interact(self):
+    imported_objects = locals()
+    imported_objects[self._shortname] = self
+    #banner = f"{self._longname} REPL"
+    banner=""
+    exit_msg = f"{self._shortname} thanks you."
+    try:
+      from IPython import embed
+      embed(
+        user_ns=imported_objects, 
+        banner1=banner, 
+        banner2=f"Use   : {self._shortname}.<tab>",
+        exit_msg=exit_msg)
+    except ImportError:
+      import code
+      repl = code.InteractiveConsole(locals=imported_objects)
+      repl.interact(
+        banner=banner, 
+        exitmsg = exit_msg)
+
+  def _add_rpc_method(parent,parentClass=None,name="test", rpcName="", rpcType=tio.FLOAT32_T, callWithValue=True):
+    def  __init__(self):
+      self._tio = parent._tio
+      self._rpcName = rpcName
+      self._rpcType = rpcType
+    def rpcCallWithValue(self, value=None):
+      return self._tio.rpc_val(self._rpcName, self._rpcType, value)
+    def rpcCallNoValue(self):
+      return self._tio.rpc_val(self._rpcName, self._rpcType)
+    if rpcName is not "":
+      if callWithValue:
+        cls = type(name,(), {'__init__':__init__, '__call__':rpcCallWithValue})
+      else:
+        cls = type(name,(), {'__init__':__init__, '__call__':rpcCallNoValue})
+    else:
+      cls = type(name,(), {'__init__':__init__})
+    clsInstance = cls()
+    setattr(parentClass, name, clsInstance)
+
+  def _add_rpc_path(self,path="this.here.command", rpcType=tio.FLOAT32_T, callWithValue=True):
+    parts = path.split('.')
+    parent = self
+    for part in parts[:-1]:
+      if part not in vars(parent).keys():
+        self._add_rpc_method(parentClass=parent, name=part)
+      parent = parent.__dict__[part]
+    self._add_rpc_method(parentClass=parent, name=parts[-1], rpcName=path, rpcType=rpcType, callWithValue=callWithValue)
+
+  def _add_rpcs(self):
+    sorted_rpcs = self._tio.rpcs
+    sorted_rpcs.sort(key=lambda x:x['name']) # Make sure x() is registered before x.offset()
+    for rpc in sorted_rpcs:
+      if rpc['valid']:
+        self._add_rpc_path(path=rpc['name'], rpcType=rpc['datatype'], callWithValue=rpc['w'])
+      # else:
+      #   print(f"Invalid metadata: {rpc['name']}")
+
+  def _add_source_method(parent, parentClass=None, name="test", sourceName=""):
+    def __init__(self):
+      self._tio = parent._tio
+      self._sourceName = sourceName
+    def __call__(self, samples=1, duration=None, flush=True):
+      return self._tio.stream_read_topic(self._sourceName, samples, duration, flush)
+    def rate(self):
+     return self._tio.source_rate(self._sourceName)
+    if sourceName is not "":
+      cls = type(name,(), {'__init__':__init__, '__call__':__call__, 'rate':rate})
+    else:
+      cls = type(name,(), {'__init__':__init__})
+    clsInstance = cls()
+    setattr(parentClass, name, clsInstance)
+
+  def _add_source_path(self, path="that.stream"):
+    parts = path.split('.')
+    parent = self
+    for part in parts[:-1]:
+      if part not in vars(parent).keys():
+        self._add_source_method(parentClass=parent, name=part)
+      parent = parent.__dict__[part]
+    self._add_source_method(parentClass=parent, name=parts[-1], sourceName=path)
+
+  def _add_sources(self):
+    for source in self._tio.protocol.sources.values():
+      self._add_source_path(path=source['source_name'])
+
+  def _close(self):
+    self._tio.close()
+
+if __name__ == "__main__":
+  device = Device()
+  device._interact()
