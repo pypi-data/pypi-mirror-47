369a3bc21313143bd3dd3e3d5569e838dbb06c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package microkit/0.1/microkit-0.1.tar.gz

---
+++ packages/microkit/microkit-0.1.tar.gz/microkit-0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+  name='microkit',
+  packages=['microkit'],
+  version='0.1',
+  license='GNU General Public License',    
+  description='A helper kit to power the APIs',
+  author='Ayush Aggarwal',
+  author_email='ayushaggarwal.97@gmail.com',
+  url='https://github.com/innovaccer/microkit',
+  download_url='https://github.com/innovaccer/microkit/archive/V1.0.tar.gz',
+  keywords=['python', 'flask', 'response', 'logger'],
+  install_requires=["flask", ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',    
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 2.7', 
+
+  ],
+)
