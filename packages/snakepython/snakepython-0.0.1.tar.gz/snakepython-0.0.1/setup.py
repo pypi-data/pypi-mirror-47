68d2f3d6d16448d1f7540792e3389e7a169086d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:40 +0000

    Add package snakepython/0.0.1/snakepython-0.0.1.tar.gz

---
+++ packages/snakepython/snakepython-0.0.1.tar.gz/snakepython-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='snakepython',
+      version='0.0.1',
+      description='rapid api development framework in python',
+      url='https://snakepython.com',
+      author='oxbits',
+      author_email='alex@oxbits.com',
+      packages=['snakepython'],
+      zip_safe=False)
+
---
+++ packages/snakepython/snakepython-0.0.1.tar.gz/snakepython-0.0.1/snakepython/__init__.py
@@ -0,0 +1,3 @@
+def snakepython():
+    return (u'snakepython')
+
