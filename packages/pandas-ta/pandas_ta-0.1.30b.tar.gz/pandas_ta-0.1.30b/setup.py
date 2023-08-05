b8f72aef2a9bb9ce3fc2beef4e416a233ca1bc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:26 +0000

    Add package pandas-ta/0.1.30b/pandas_ta-0.1.30b.tar.gz

---
+++ packages/pandas-ta/pandas_ta-0.1.30b.tar.gz/pandas_ta-0.1.30b/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+long_description = "An easy to use Python 3 Pandas Extension of Technical Analysis Indicators"
+
+setup(
+    name = "pandas_ta",
+    packages = ["pandas_ta"],
+    version = "0.1.30b",
+    description=long_description,
+    long_description=long_description,
+    author = "Kevin Johnson",
+    author_email = "appliedmathkj@gmail.com",
+    url = "https://github.com/twopirllc/pandas-ta",
+    maintainer="Kevin Johnson",
+    maintainer_email="appliedmathkj@gmail.com",
+    # install_requires=['numpy','pandas'],
+    download_url = "https://github.com/twopirllc/pandas-ta.git",
+    keywords = ['technical analysis', 'python3', 'pandas'],
+    license="The MIT License (MIT)",
+    classifiers = [
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Science/Research',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    package_data={
+        'data': ['data/*.csv'],
+    },
+    install_requires=['pandas'],
+
+    # List additional groups of dependencies here (e.g. development dependencies).
+    # You can install these using the following syntax, for example:
+    # $ pip install -e .[dev,test]
+    extras_require = {
+        'dev': ['ta-lib', 'jupyterlab'],
+        'test': ['ta-lib'],
+    },
+)
