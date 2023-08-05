24ce583568691dbf3e2f7697a5134a331a77735a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:19 +0000

    Add package wxviews/0.2.0/wxviews-0.2.0.tar.gz

---
+++ packages/wxviews/wxviews-0.2.0.tar.gz/wxviews-0.2.0/wxviews/__init__.py
@@ -0,0 +1,10 @@
+"""Package adapts pyviews for using with wxpython"""
+
+from pyviews.rendering import import_global, inject_global, set_global
+from pyviews.code import Code
+from wxviews.binding import bind
+from wxviews.containers import Container, View, For, If
+from wxviews.sizers import GrowableCol, GrowableRow, sizer
+from wxviews.styles import Style, StylesView, style
+
+__version__ = '0.2.0'
