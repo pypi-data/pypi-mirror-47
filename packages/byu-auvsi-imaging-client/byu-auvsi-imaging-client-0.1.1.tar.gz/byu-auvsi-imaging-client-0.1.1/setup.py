b517a0b54a0a05da76e368168c7d5ce7fc57744b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:49 +0000

    Add package byu-auvsi-imaging-client/0.1.1/byu-auvsi-imaging-client-0.1.1.tar.gz

---
+++ packages/byu-auvsi-imaging-client/byu-auvsi-imaging-client-0.1.1.tar.gz/byu-auvsi-imaging-client-0.1.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+pkgs = setuptools.find_packages()
+pkgs.remove('lib.test')
+pkgs.append('assets')
+
+setuptools.setup(
+     name='byu-auvsi-imaging-client',  
+     version='0.1.1',
+     scripts=['gui.py'],
+     entry_points={
+        'gui_scripts': [
+            'img_gui = gui:main',
+        ]
+     },
+     author="BYU AUVSI Team",
+     author_email="tylerm15@gmail.com",
+     description="Imaging Client GUI for manual classification",
+     install_requires=['requests==2.20.1', 'pillow==5.3.0', 'opencv-python==3.4.2.*', 'ttkthemes==2.2.0', 'imutils==0.5.2'],
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     packages=pkgs,
+     package_data={
+        '': ['*.png', '*.jpg'],
+     },
+     url="https://github.com/BYU-AUVSI/imaging",
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )
