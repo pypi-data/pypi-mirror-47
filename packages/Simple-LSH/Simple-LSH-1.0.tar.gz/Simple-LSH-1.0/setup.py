73f63bbb93e2e5bb4660189d268982a227d33b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:51 +0000

    Add package Simple-LSH/1.0/Simple-LSH-1.0.tar.gz

---
+++ packages/Simple-LSH/Simple-LSH-1.0.tar.gz/Simple-LSH-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Simple-LSH',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Mehmet Akdel',
+      author_email='akdel.mehmet@gmail.com',
+      url='https://github.com/akdel/local-sensitivity-hashing',
+      packages=['LSH'],
+      install_requires=["numpy", "numba"]
+     )
