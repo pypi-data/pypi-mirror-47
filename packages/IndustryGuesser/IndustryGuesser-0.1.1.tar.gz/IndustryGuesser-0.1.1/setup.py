d19479c70c272a388792dd7e3f21835c4662daaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package IndustryGuesser/0.1.1/IndustryGuesser-0.1.1.tar.gz

---
+++ packages/IndustryGuesser/IndustryGuesser-0.1.1.tar.gz/IndustryGuesser-0.1.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+
+__version__ = '0.1.1'
+__author__ = 'yang zhengzhi'
+__email__ = 'yangzhengzhi.roy@gmail.com'
+__url__ = 'https://pypi.org/project/IndustryGuesser/'
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(ROOT_DIR, 'requirements.txt'), 'r') as f:
+    requirements = f.read().split('\n')
+
+setup(
+    name='IndustryGuesser',
+    version=__version__,
+    description='Guess the company industry based on the company name',
+    author=__author__,
+    author_email=__email__,
+    url=__url__,
+    download_url='https://github.com/yangzhengzhiroy/IndustryGuesser/archive/v0.1.1.tar.gz',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    long_description_content_type='text/markdown'
+)
