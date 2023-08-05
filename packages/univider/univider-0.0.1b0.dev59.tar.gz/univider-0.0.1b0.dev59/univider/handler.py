25413ead68a647068ab9cdd4507412b183ad0bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:59 +0000

    Add package univider/0.0.1b0.dev59/univider-0.0.1b0.dev59.tar.gz

---
+++ packages/univider/univider-0.0.1b0.dev59.tar.gz/univider-0.0.1b0.dev59/univider/handler.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import json
+
+from flask import Flask, request, jsonify
+import socket
+from univider.settings import app_user
+from univider.logger import Logger
+
+app = Flask(__name__)
+logger = Logger(__name__).getlogger()
+
+@app.route("/crawl", methods=['GET', 'POST'])
+def crawl():
+    # parse needs
+    data = request.get_data()
+    params = json.loads(data)
+    node = socket.gethostname()
+    # authentication
+    if (params.has_key("user") and params.has_key("secret") and params["secret"] == app_user.get(params["user"])):
+        # handle needs
+        from univider.fetcher import Fetcher
+        fetcher = Fetcher()
+        result = fetcher.fetch_page_with_cache(params)
+    else:
+        if (params.has_key("user")):
+            logger.info(params["user"] + ' Authentication failed ' )
+        else:
+            logger.info('Unknown user Authentication failed '+str(params))
+        result = {
+            'status': 'error',
+            'node' : node,
+            'description': 'Authentication failed.'
+        }
+
+    # return needs
+    return jsonify(result)
+    # return result["html"].decode('gbk')
+
+@app.route('/get_ip_list', methods=['POST','GET'])
+def get_ip_list():
+    import cx_Oracle
+    results = []
+    db_conn = cx_Oracle.connect('spd_dm/spd_dm_1Q#@PDB_SPIDER')
+    oreader = db_conn.cursor()
+    sql = "select * from ip_pool_all"
+    #print sql
+    oreader.execute(sql)
+    result = oreader.fetchall()
+    oreader.close()
+    db_conn.close()
+    for each in result:
+        row = {}
+        row['ip'] = each[0]
+        row['time'] = each[1]
+        results.append(row)
+    return jsonify(results)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5010,debug=False)
+
+def main():
+    app.run(host='0.0.0.0',port=5010,debug=False)
