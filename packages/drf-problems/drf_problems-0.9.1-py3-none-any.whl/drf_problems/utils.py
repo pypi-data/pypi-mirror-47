d2cd3a0bca85cd0ecdb3d4636dee75454bb85132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:02 +0000

    Add package drf-problems/0.9.1/drf_problems-0.9.1-py3-none-any.whl

---
+++ packages/drf-problems/drf_problems-0.9.1-py3-none-any.whl/drf_problems/utils.py
@@ -0,0 +1,7 @@
+from drf_problems import PROBLEM_CODE_CHOICES, PROBLEM_EXCEPTION_MAP
+
+
+def register_exception(exc_cls):
+    code = getattr(exc_cls, 'code', exc_cls.default_code)
+    PROBLEM_EXCEPTION_MAP[code] = exc_cls
+    PROBLEM_CODE_CHOICES.append((code, code))
