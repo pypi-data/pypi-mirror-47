16e7bc194f0118035448253e0e90cbfd6d418cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:25 +0000

    Add package feeeg/0.0.1/feeeg-0.0.1-py2.py3-none-any.whl

---
+++ packages/feeeg/feeeg-0.0.1-py2.py3-none-any.whl/feeeg/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for feeeg."""
+
+__author__ = """huyhoang17"""
+__email__ = 'hoangphan0710@gmail.com'
+__version__ = '0.0.1'
+
+
+from .test import add  # noqa
