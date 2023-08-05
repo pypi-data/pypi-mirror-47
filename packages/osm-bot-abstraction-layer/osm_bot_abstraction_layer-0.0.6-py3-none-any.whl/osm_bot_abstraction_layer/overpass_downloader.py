00f9d77d80eabee3c1ccd0e59a25e0de99c2fbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:14 +0000

    Add package osm-bot-abstraction-layer/0.0.6/osm_bot_abstraction_layer-0.0.6-py3-none-any.whl

---
+++ packages/osm-bot-abstraction-layer/osm_bot_abstraction_layer-0.0.6-py3-none-any.whl/osm_bot_abstraction_layer/overpass_downloader.py
@@ -0,0 +1,27 @@
+import urllib.request, urllib.error, urllib.parse
+import time
+
+def download_overpass_query(query, filepath):
+    query = urllib.parse.quote(query)
+    url = "http://overpass-api.de/api/interpreter?data=" + query
+    with open(filepath, 'w+') as file:
+        file.write(overpass_download(url))
+
+def overpass_download(url):
+    while True:
+        try:
+            print("downloading " + url)
+            resource = urllib.request.urlopen(url)
+            return resource.read().decode('utf-8')
+        except urllib.error.HTTPError as e:
+            print(e.getcode())
+            if e.getcode() == 429 or e.getcode() == 503:
+                print("sleeping before retry due to ", e.getcode(), "error code")
+                time.sleep(60)
+                print("retrying")
+                continue
+            raise e
+        except urllib.error.URLError as e:
+            print(("URLError for url " + url))
+            print(e)
+            return
