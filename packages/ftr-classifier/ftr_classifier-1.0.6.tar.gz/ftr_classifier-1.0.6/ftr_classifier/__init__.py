313f26e5f379d6ba52874acc21ac8419c0dc5112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:44 +0000

    Add package ftr-classifier/1.0.6/ftr_classifier-1.0.6.tar.gz

---
+++ packages/ftr-classifier/ftr_classifier-1.0.6.tar.gz/ftr_classifier-1.0.6/ftr_classifier/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 31 22:32:52 2019
+
+@author: cole roberson
+"""
+#imports 
+from ftr_classifier import ftr_classify as ftr
+from ftr_classifier.info import INFO
+
+__version__ = INFO['version']
+description = INFO['description']
+long_description = INFO['long_description']
