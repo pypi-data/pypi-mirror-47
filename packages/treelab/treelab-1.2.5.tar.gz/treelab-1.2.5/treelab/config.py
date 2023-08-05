fb9faaaf59304a7b11a0aafe129bbbdebe532a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:58 +0000

    Add package treelab/1.2.5/treelab-1.2.5.tar.gz

---
+++ packages/treelab/treelab-1.2.5.tar.gz/treelab-1.2.5/treelab/config.py
@@ -0,0 +1,24 @@
+# # server
+workspace_id = 'wsp011dc0662a897504'
+# core 1
+core_id_1 = 'cre011dc31cf608f1e4'
+view_id_1 = 'viw011dc9afdd014af3'
+table_id_1 = 'tbl011dc6c5f40f7103'
+
+# core 2
+core_id_2 = 'cre011dc31cf608f1e4'
+view_id_2 = 'viw011dd283d78f855f'
+table_id_2 = 'tbl011dcff7e583ecca'
+
+# # local
+# workspace_id = 'wspb580a4b6028b19f5'
+# # core 1
+# core_id_1 = 'creb580a4b622831199'
+# view_id_1 = 'viwb580a4b6a6068005'
+# table_id_1 = 'tblb580a4b6688459f2'
+# row_id_1 = 'rowff098671b10cfe30'
+# column_id_1 = 'colff099876420981fa'
+# # core 2
+# core_id_2 = 'creb580a4b622831199'
+# view_id_2 = 'viwb580a4b74902a242'
+# table_id_2 = 'tblb580a4b70d8cf590'
