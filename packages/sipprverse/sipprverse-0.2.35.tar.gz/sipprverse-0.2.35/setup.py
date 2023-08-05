b8929336f5a8d9d349220bc63529312d2ddd390a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:19 +0000

    Add package sipprverse/0.2.35/sipprverse-0.2.35.tar.gz

---
+++ packages/sipprverse/sipprverse-0.2.35.tar.gz/sipprverse-0.2.35/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+__author__ = 'adamkoziol'
+
+setup(
+    name="sipprverse",
+    version="0.2.35",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[
+        os.path.join('sippr', 'sippr.py'),
+        os.path.join('sippr', 'method.py')
+    ],
+    license='MIT',
+    author='Adam Koziol',
+    author_email='adam.koziol@inspection.gc.ca',
+    description='Object oriented raw read typing software',
+    url='https://github.com/OLC-Bioinformatics/sipprverse',
+    long_description=open('README.md').read(),
+)
