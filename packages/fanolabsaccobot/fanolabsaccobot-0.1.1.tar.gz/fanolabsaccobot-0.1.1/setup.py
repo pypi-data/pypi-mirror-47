7895b90333e2d2d5b1281a64bf07456c1eb86556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:32 +0000

    Add package fanolabsaccobot/0.1.1/fanolabsaccobot-0.1.1.tar.gz

---
+++ packages/fanolabsaccobot/fanolabsaccobot-0.1.1.tar.gz/fanolabsaccobot-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fanolabsaccobot",
+    version="0.1.1",
+    author="Henry Zheng",
+    author_email="henry.zheng@fano.ai",
+    description="FanoLabs Accobot module for Python3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.fanoai.cn/SDKs/accobot-python",
+    packages=setuptools.find_packages('src'),
+    package_dir = {'':'src'},
+    install_requires=[
+        'requests',
+    ],
+    license='MIT',
+)
