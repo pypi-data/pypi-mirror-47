99a838f983e222422b476ca8765a7816c7b1639a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:11 +0000

    Add package MultiSC/0.0.8/MultiSC-0.0.8.tar.gz

---
+++ packages/MultiSC/MultiSC-0.0.8.tar.gz/MultiSC-0.0.8/Examples/quick_setup_server.py
@@ -0,0 +1,25 @@
+from MultiSC.MultiServer.quick_setup.manager import (
+    ProtocolsManager,
+    MonitorManager,
+    Runner,
+)
+
+
+@MonitorManager.add("client_info_printer")
+def monitor(self, query):
+    self.logger.info(query.other)
+    # self.monitor_actions["CloseConnection"].start_action()
+
+
+@ProtocolsManager.add("printer", "name")
+def func(query):
+    return query["name"]
+
+
+@ProtocolsManager.add("math", "sum")
+def func2(query):
+    return query["a"] + query["b"]
+
+
+Server = Runner()
+Server.run()
