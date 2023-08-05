e54d40bf1a9bf39421bd350c8673cf9e6bc43d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:13 +0000

    Add package clarite/0.3.0/clarite-0.3.0-py3-none-any.whl

---
+++ packages/clarite/clarite-0.3.0-py3-none-any.whl/clarite/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from ._version import get_versions
+
+from .clarite import ClariteDataframeAccessor
+
+from .ewas import SurveyDesignSpec, ewas, add_corrected_pvalues
+from .utilities import make_bin, make_categorical, make_continuous
+from .io import load_data
+
+__version__ = get_versions()['version']
+del get_versions
