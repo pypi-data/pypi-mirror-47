a11e16a83df2dc10a2472ef9af9b7ac19b25d62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:36 +0000

    Add package EulerOS/1.0.0/EulerOS-1.0.0.tar.gz

---
+++ packages/EulerOS/EulerOS-1.0.0.tar.gz/EulerOS-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+
+    name="EulerOS",
+    version="1.0.0",
+    packages=["Core001"],
+    description="Euler Math Thinking: Small Dog Edition",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Small Dog",
+    license="Small Dog Inc., UCB",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
