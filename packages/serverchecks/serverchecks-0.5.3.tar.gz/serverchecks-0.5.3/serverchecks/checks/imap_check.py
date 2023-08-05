8851427cba2b9af49c77a2dac6e687ab8e1fb5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:14 +0000

    Add package serverchecks/0.5.3/serverchecks-0.5.3.tar.gz

---
+++ packages/serverchecks/serverchecks-0.5.3.tar.gz/serverchecks-0.5.3/serverchecks/checks/imap_check.py
@@ -0,0 +1,62 @@
+import imaplib
+from typing import Union, Optional
+
+from serverchecks import Outcome
+from serverchecks.checks import AbstractCheck
+
+
+class ImapCheck(AbstractCheck):
+    name = 'IMAP'
+
+    def __init__(self, **kwargs) -> None:
+        self.host: str = kwargs.get('host')
+        self.tls_mode: str = kwargs.get('tls_mode')
+        allowed = ('tls', 'starttls')
+        if self.tls_mode not in allowed:
+            raise ValueError(f'tls_mode must be one of {allowed}: {self.tls_mode}')
+
+        self.username: Optional[str] = kwargs.get('username', None)
+        self.password: Optional[str] = kwargs.get('password', None)
+        self.imap: Optional[Union[imaplib.IMAP4_SSL, imaplib.IMAP4]] = None
+
+        if self.host is None:
+            raise ValueError(f'{self.name} required `host` parameter is missing')
+
+    async def check(self) -> Outcome:
+        if self.tls_mode == 'tls':
+            self.imap: imaplib.IMAP4_SSL = imaplib.IMAP4_SSL(self.host)
+        else:
+            self.imap: imaplib.IMAP4 = imaplib.IMAP4(self.host)
+            try:
+                self.imap.starttls()
+            except self.imap.error as e:
+                return Outcome(False, f'IMAP STARTTLS failed {self.imap.host}:{self.imap.port}: {e}')
+
+        if not len(self.imap.capabilities) > 0:
+            return Outcome(False,
+                           f'IMAP capabilities are empty on {self.imap.host}:{self.imap.port}: {self.imap.capabilities}')
+
+        # skip any further tests in non-authenticated mode
+        if not self.username:
+            return Outcome(True, f'IMAP test successful on {self.imap.host}:{self.imap.port} (not authenticated)')
+
+        try:
+            self.imap.login(self.username, self.password)
+        except self.imap.error as e:
+            return Outcome(False, f'IMAP login failed on {self.imap.host}:{self.imap.port}: {e}')
+
+        self.imap.select()
+
+        status, messages = self.imap.search(None, 'ALL')
+        if status != 'OK':
+            return Outcome(False, f'Cannot search messages on {self.imap.host}:{self.imap.port}')
+
+        self.imap.close()
+
+        return Outcome(True, f'IMAP test successful on {self.imap.host}:{self.imap.port} (authenticated)')
+
+    def __str__(self):
+        return f'<{self.name} {self.host}>'
+
+
+check_class = ImapCheck
---
+++ packages/serverchecks/serverchecks-0.5.3.tar.gz/serverchecks-0.5.3/serverchecks/checks/tls_check.py
@@ -0,0 +1,65 @@
+import socket
+import ssl
+from datetime import datetime, timedelta
+from typing import Optional, Dict
+
+from serverchecks import Outcome
+from serverchecks.checks import AbstractCheck
+
+
+class TlsCheck(AbstractCheck):
+    name = 'TLS'
+
+    def __init__(self, **kwargs) -> None:
+        self.host: str = kwargs.get('host')
+        self.port: int = kwargs.get('port', 443)
+        self.cert_days: int = kwargs.get('cert_days', 5)
+        self.timeout: float = kwargs.get('timeout', 2.0)
+
+        if self.host is None:
+            raise ValueError(f'{self.name} required `host` parameter is missing')
+
+    def _date(self, d: str) -> Optional[datetime]:
+        try:
+            # try decoding locale representation first
+            return datetime.strptime(d, '%c')
+        except ValueError:
+            try:
+                # try 'Jan 30 23:00:15 2019 GMT' representation
+                return datetime.strptime(d, '%b %d %H:%M:%S %Y %Z')
+            except ValueError:
+                return None
+
+    async def check(self) -> Outcome:
+        context: ssl.SSLContext = ssl.create_default_context()
+
+        try:
+            with socket.create_connection((self.host, self.port), self.timeout) as sock:
+                with context.wrap_socket(sock, server_hostname=self.host) as ssl_sock:
+                    cert: Dict = ssl_sock.getpeercert()
+        except ssl.SSLError as e:
+            return Outcome(False, f'TLS error for {self.host}: {e}')
+        else:
+            not_before: datetime = self._date(cert.get('notBefore'))
+            not_after: datetime = self._date(cert.get('notAfter'))
+
+            now: datetime = datetime.now()
+
+            if not not_before or not not_after:
+                return Outcome(False, f'Unable to determine notBefore or notAfter dates in {cert}')
+
+            if now < not_before:
+                return Outcome(False, f'TLS certificate for {self.host} not valid as {now} is before {not_before}')
+
+            target_date: datetime = now + timedelta(days=self.cert_days)
+            if target_date > not_after:
+                return Outcome(False,
+                               f'TLS certificate for {self.host} not valid as {target_date} is after {not_after}')
+
+            return Outcome(True, f'Certificate for {self.host} valid until {not_after}')
+
+    def __str__(self):
+        return f'<{self.name} {self.host}>'
+
+
+check_class = TlsCheck
