e825b281a4808a8680cd9201e415402401a2584a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:53 +0000

    Add package nester-markov/1.0.1/nester-markov-1.0.1.tar.gz

---
+++ packages/nester-markov/nester-markov-1.0.1.tar.gz/nester-markov-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester-markov',
+	version		= '1.0.1',
+	py_modules	= ['nester-markov'],
+	author		= 'markovojvodic84',
+	author_email	= 'markovojvodic84@gmail.com',
+	url		= 'http://ludvigcomputers.com',
+	description	= 'A simple printer of nested lists',
+     )
