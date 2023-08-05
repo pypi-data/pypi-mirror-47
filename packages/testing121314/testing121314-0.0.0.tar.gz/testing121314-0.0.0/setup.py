17626ac3298240e63ef23701769177dac932452d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:19 +0000

    Add package testing121314/0.0.0/testing121314-0.0.0.tar.gz

---
+++ packages/testing121314/testing121314-0.0.0.tar.gz/testing121314-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="testing121314",
+    version="0.0.0",
+    author="Placeholder",
+    author_email="corpeng@planet.com",
+    description="placeholder",
+    packages=setuptools.find_packages(),
+)
