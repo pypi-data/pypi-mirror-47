efe5090625463ce596f712def5195e3270d92717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package rfc7230/0.0/rfc7230-0.0.tar.gz

---
+++ packages/rfc7230/rfc7230-0.0.tar.gz/rfc7230-0.0/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+
+setup(name="rfc7230", version="0.0", url="https://github.com/sethmlarson/rfc7230", author="Seth Michael Larson", author_email="sethmichaellarson@gmail.com")
+
