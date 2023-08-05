80ddde55df6cb9a0154e1bd260685ac3dc08ed19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:34 +0000

    Add package h5netcdf/0.7.4/h5netcdf-0.7.4.tar.gz

---
+++ packages/h5netcdf/h5netcdf-0.7.4.tar.gz/h5netcdf-0.7.4/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Scientific/Engineering',
+]
+
+
+setup(name='h5netcdf',
+      description='netCDF4 via h5py',
+      long_description=(open('README.rst').read()
+                        if os.path.exists('README.rst')
+                        else ''),
+      version='0.7.4',
+      license='BSD',
+      classifiers=CLASSIFIERS,
+      author='Stephan Hoyer',
+      author_email='shoyer@gmail.com',
+      url='https://github.com/shoyer/h5netcdf',
+      install_requires=['h5py'],
+      tests_require=['netCDF4', 'pytest'],
+      packages=find_packages())
