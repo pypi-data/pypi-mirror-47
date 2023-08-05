917d36dae95381f2b00839a24b9afbf10ebbe9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:30 +0000

    Add package sbp/2.5.6/sbp-2.5.6-cp27-cp27m-win_amd64.whl

---
+++ packages/sbp/sbp-2.5.6-cp27-cp27m-win_amd64.whl/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '2.5.6'
+full_version = '2.5.6'
+git_revision = '5559a70c'
+is_released = True
+if not is_released:
+    version = full_version
