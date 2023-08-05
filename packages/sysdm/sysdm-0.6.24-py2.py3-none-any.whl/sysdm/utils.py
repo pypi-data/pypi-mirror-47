3c67eb21267db9d25129d55a9ba8a7115e4e862f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package sysdm/0.6.24/sysdm-0.6.24-py2.py3-none-any.whl

---
+++ packages/sysdm/sysdm-0.6.24-py2.py3-none-any.whl/sysdm/utils.py
@@ -0,0 +1,62 @@
+import os
+import subprocess
+
+
+def get_output(cmd):
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    (out, _) = proc.communicate()
+    return out.decode().strip()
+
+
+is_sudo = bool(get_output("echo $SUDO_USER"))
+
+
+def run_quiet(cmd):
+    with open(os.devnull, 'w') as devnull:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=devnull, shell=True)
+        (out, _) = proc.communicate()
+        return out.decode().strip()
+
+
+def is_unit_running(unit):
+    return systemctl('is-active {unit}'.format(unit=unit)) == "active"
+
+
+def is_unit_enabled(unit):
+    return systemctl('is-enabled {unit}'.format(unit=unit)) == "enabled"
+
+
+def read_command_from_unit(systempath, service_name):
+    with open(os.path.join(systempath, service_name) + ".service") as f:
+        for line in f.read().split("\n"):
+            if line.startswith("ExecStart="):
+                return line[10:].strip()
+
+
+def read_ps_aux_by_unit(systempath, unit):
+    cmd = read_command_from_unit(systempath, unit)
+    z = get_output("ps ax -o pid,%cpu,%mem,ppid,args -ww")
+
+    for num, line in enumerate(z.split("\n")):
+        if num == 0:
+            continue
+        pid, cpu, mem, ppid, *rest = line.split()
+        if ppid != "1":
+            continue
+        rest = " ".join(rest)
+        if cmd.endswith(rest) or rest.endswith(cmd):
+            return pid, cpu, mem
+
+
+def is_git_ignored(abspath):
+    return bool(get_output("git check-ignore {}".format(abspath)).strip())
+
+
+def to_sn(fname_or_cmd):
+    return fname_or_cmd.split()[0].split("/")[-1].replace(".", "_")
+
+
+def systemctl(rest):
+    cmd = "sudo systemctl " if is_sudo else "systemctl --user "
+    cmd += rest
+    return get_output(cmd)
