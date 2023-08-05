831e39df257e4ee13de6df4cc5287ea8be6b7c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package higra/0.3.1/higra-0.3.1-cp36-cp36m-win_amd64.whl

---
+++ packages/higra/higra-0.3.1-cp36-cp36m-win_amd64.whl/higra/hierarchy/__init__.py
@@ -0,0 +1,15 @@
+############################################################################
+# Copyright ESIEE Paris (2018)                                             #
+#                                                                          #
+# Contributor(s) : Benjamin Perret                                         #
+#                                                                          #
+# Distributed under the terms of the CECILL-B License.                     #
+#                                                                          #
+# The full license is in the file LICENSE, distributed with this software. #
+############################################################################
+
+from .binary_partition_tree import *
+from .component_tree import *
+from .constrained_connectivity_hierarchy import *
+from .hierarchy_core import *
+from .watershed_hierarchy import *
