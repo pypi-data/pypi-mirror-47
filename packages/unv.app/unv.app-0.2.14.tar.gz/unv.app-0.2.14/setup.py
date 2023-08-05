86a3c236853f735dbfc2945aa4b6facb6a17ab4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:26 +0000

    Add package unv.app/0.2.14/unv.app-0.2.14.tar.gz

---
+++ packages/unv.app/unv.app-0.2.14.tar.gz/unv.app-0.2.14/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unv.app',
+    version='0.2.14',
+    description="""Core app package with settings manipulation""",
+    url='http://github.com/c137digital/unv_app',
+    author='Morty Space',
+    author_email='morty.space@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'cerberus',
+        'unv.utils'
+    ],
+    zip_safe=True
+)
