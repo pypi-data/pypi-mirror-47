3238c6077cc045edfa7ca00b7a39ca382576e86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package python-tooling-example/0.0.1/python_tooling_example-0.0.1.tar.gz

---
+++ packages/python-tooling-example/python_tooling_example-0.0.1.tar.gz/python_tooling_example-0.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import python_tooling_example
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='python_tooling_example',
+      version=python_tooling_example.__version__,
+      description='Example Python package using Pytest, Sphinx, Travis, Versioneer and Github.',
+      long_description=readme(),
+      author='Bruno Beltran',
+      author_email='brunobeltran0@gmail.com',
+      packages=['python_tooling_example',
+                'python_tooling_example.example_subpackage'],
+      package_data={'python_tooling_example': ['very_large_constants.csv'],
+                    'python_tooling_example.example_subpackage': ['very_large_string.txt']},
+      license='MIT',
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Utilities'
+      ],
+      keywords='python tooling meta example',
+      url='https://github.com/brunobeltran/python_tooling_example',
+      install_requires=['pandas'],
+)
