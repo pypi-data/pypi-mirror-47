8440d2f090c85cd989ba5e09ef52874893bf86d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:45 +0000

    Add package ConfigHelper/0.0.2/ConfigHelper-0.0.2.tar.gz

---
+++ packages/ConfigHelper/ConfigHelper-0.0.2.tar.gz/ConfigHelper-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='ConfigHelper',
+      version='0.0.2',
+      url='https://github.com/Sotaneum/ConfigHelper',
+      author='Donggun LEE',
+      author_email='gnyotnu39@gmail.com',
+      description='Can use it to save or recall preferences from Python.',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      zip_safe=False,
+      classifiers=[
+          'License :: OSI Approved :: MIT License'
+      ]
+)
