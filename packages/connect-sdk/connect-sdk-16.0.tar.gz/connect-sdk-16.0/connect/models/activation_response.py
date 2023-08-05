91fdcaa664eec4a798452388b01d4a8e7d6c8327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:54 +0000

    Add package connect-sdk/16.0/connect-sdk-16.0.tar.gz

---
+++ packages/connect-sdk/connect-sdk-16.0.tar.gz/connect-sdk-16.0/connect/models/activation_response.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of the Ingram Micro Cloud Blue Connect SDK.
+# Copyright (c) 2019 Ingram Micro. All Rights Reserved.
+
+import json
+
+
+class ActivationTileResponse(object):
+    tile = 'Activation succeeded'  # type: str
+
+    def __init__(self, markdown=''):
+        # type: (str) -> None
+        try:
+            self.tile = json.loads(markdown)
+        except ValueError:
+            self.tile = markdown or self.__class__.tile
+
+
+class ActivationTemplateResponse(object):
+    template_id = None  # type: str
+
+    def __init__(self, template_id):
+        # type: (str) -> None
+        self.template_id = template_id
