7df434e91ef488b28a35685a87f8d89fe0655c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:41 +0000

    Add package Vigenere-cipher-package2/0.5/Vigenere-cipher-package2-0.5.tar.gz

---
+++ packages/Vigenere-cipher-package2/Vigenere-cipher-package2-0.5.tar.gz/Vigenere-cipher-package2-0.5/cipherPackage/Cipher.py
@@ -0,0 +1,73 @@
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return (key)
+    else:
+        for i in range(len(string) -
+                       len(key)):
+            key.append(key[i % len(key)])
+    return ("".join(key))
+
+
+# def cipherText(string, key):
+    # cipher_text = []
+    # for i in range(len(string)):
+        # x = (ord(string[i]) +
+             # ord(key[i])) % 26
+        # x += ord('A')
+        # cipher_text.append(chr(x))
+    # return ("".join(cipher_text))
+
+
+# def originalText(cipher_text, key):
+    # orig_text = []
+    # for i in range(len(cipher_text)):
+        # x = (ord(cipher_text[i]) -
+             # ord(key[i]) + 26) % 26
+        # x += ord('A')
+        # orig_text.append(chr(x))
+    # return ("".join(orig_text))
+
+	
+def cipherText(txt='', key=''):
+    if not txt:
+        print ('Needs text')
+        return
+    if not key:
+        print ('Needs key')
+        return
+
+    k_len = len(key)
+    k_ints = [ord(i) for i in key]
+    txt_ints = [ord(i) for i in txt]
+    ret_txt = ''
+    for i in range(len(txt_ints)):
+        adder = k_ints[i % k_len]
+
+        v = (txt_ints[i] - 32 + adder) % 95
+
+        ret_txt += chr(v + 32)
+
+    return (ret_txt)
+	
+def originalText(txt='', key=''):
+    if not txt:
+        print ('Needs text')
+        return
+    if not key:
+        print ('Needs key')
+        return
+
+    k_len = len(key)
+    k_ints = [ord(i) for i in key]
+    txt_ints = [ord(i) for i in txt]
+    ret_txt = ''
+    for i in range(len(txt_ints)):
+        adder = k_ints[i % k_len]
+        adder *= -1
+
+        v = (txt_ints[i] - 32 + adder) % 95
+
+        ret_txt += chr(v + 32)
+
+    return (ret_txt)
