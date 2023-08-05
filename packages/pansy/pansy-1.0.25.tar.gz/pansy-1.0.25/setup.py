9a1720bb2c8d9d7c84c878c19fae30d71ae29f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:43 +0000

    Add package pansy/1.0.25/pansy-1.0.25.tar.gz

---
+++ packages/pansy/pansy-1.0.25.tar.gz/pansy-1.0.25/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pansy",
+    version='1.0.25',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['setproctitle', 'netkit', 'pyglet', 'events'],
+    url="https://github.com/dantezhu/pansy",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="pansy",
+)
