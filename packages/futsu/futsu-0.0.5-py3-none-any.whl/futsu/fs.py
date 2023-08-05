f9662410b21184b1bbd6537bdb44b4e2c211b4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package futsu/0.0.5/futsu-0.0.5-py3-none-any.whl

---
+++ packages/futsu/futsu-0.0.5-py3-none-any.whl/futsu/fs.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+def makedirs(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+def reset_dir(out_dir):
+    shutil.rmtree(out_dir,ignore_errors=True)
+    os.makedirs(out_dir)
