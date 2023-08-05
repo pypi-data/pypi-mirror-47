07c5115a8f6f3113fbfc92ef3a892e2e2c08dd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:33 +0000

    Add package mopidyapi/0.6.1/mopidyapi-0.6.1.tar.gz

---
+++ packages/mopidyapi/mopidyapi-0.6.1.tar.gz/mopidyapi-0.6.1/mopidyapi/__init__.py
@@ -0,0 +1,12 @@
+"""
+MopidyAPI
+~~~~~~~~~
+MopidyAPI is a Python (3.6+) library for interacting with Mopidy,
+using its JSON RPC API.
+"""
+
+__all__ = ['client', 'wsclient']
+__version__ = '0.6.1'
+
+from .client import MopidyAPI
+from .wsclient import MopidyWSClient
