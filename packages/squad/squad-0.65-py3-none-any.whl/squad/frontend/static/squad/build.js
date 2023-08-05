287baf9f403042986b9c7aca0417bd15d571810c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:07 +0000

    Add package squad/0.65/squad-0.65-py3-none-any.whl

---
+++ packages/squad/squad-0.65-py3-none-any.whl/squad/frontend/static/squad/build.js
@@ -0,0 +1,41 @@
+import {AnnotationController} from './controllers/annotation.js'
+import {FilterController} from './controllers/filter.js'
+import {ResubmitController} from './controllers/resubmit.js'
+import {Config as appConfig} from './config.js'
+
+var app = angular.module('Build', []);
+
+app.value('build', window.build);
+
+appConfig(app, ['httpProvider']);
+
+app.controller(
+    'AnnotationController',
+    [
+        '$scope',
+        '$http',
+        '$httpParamSerializerJQLike',
+        AnnotationController
+    ]
+)
+
+app.controller(
+    'FilterController',
+    [
+        '$scope',
+        '$attrs',
+        '$location',
+        FilterController
+    ]
+)
+
+app.controller(
+    'ResubmitController',
+    [
+        '$scope',
+        '$http',
+        '$location',
+        '$timeout',
+        ResubmitController
+    ]
+);
