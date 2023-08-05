5bd42808b246170d7ce4dbd199cebabc83be4c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:29 +0000

    Add package galvandi-sorting-magic2/0.1.2/galvandi_sorting_magic2-0.1.2.tar.gz

---
+++ packages/galvandi-sorting-magic2/galvandi_sorting_magic2-0.1.2.tar.gz/galvandi_sorting_magic2-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='galvandi_sorting_magic2',
+      version='0.1.2',
+      description='Just some magic',
+      url='http://github.com/galvandi',
+      author='galvandi',
+      author_email='galvandi@derp.com',
+      license='MIT',
+      packages=['galvandi_sorting_magic2'],
+      zip_safe=False)
