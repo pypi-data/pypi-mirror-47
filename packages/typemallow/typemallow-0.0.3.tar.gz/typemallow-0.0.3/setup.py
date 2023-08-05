afdb03e6dee4b4085e61f07db57530dd076e5e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:00 +0000

    Add package typemallow/0.0.3/typemallow-0.0.3.tar.gz

---
+++ packages/typemallow/typemallow-0.0.3.tar.gz/typemallow-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import os
+import setuptools
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="typemallow",
+    version="0.0.3",
+    url="https://github.com/adenh93/typemallow",
+
+    author="Aden Herold",
+    author_email="aden.herold1@gmail.com",
+
+    description="An elegant and automatic solution for generating/outputting Typescript interfaces from your Marshmallow Schemas.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['Marshmallow', 'Typescript', 'Python', 'Flask', 'Django'],
+
+    packages=['typemallow'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+
+    install_requires=[
+        'marshmallow'
+    ]
+)
