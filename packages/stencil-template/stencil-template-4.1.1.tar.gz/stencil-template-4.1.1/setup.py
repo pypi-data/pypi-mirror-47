15681fe82d8368f01473315adebf38b4e538370b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:43 +0000

    Add package stencil-template/4.1.1/stencil-template-4.1.1.tar.gz

---
+++ packages/stencil-template/stencil-template-4.1.1.tar.gz/stencil-template-4.1.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from setuptools import setup
+
+
+def test_suite():
+    """Return tests suite."""
+    test_loader = unittest.defaultTestLoader
+    return test_loader.discover('tests', pattern='test*.py')
+
+
+setup(
+    name='stencil-template',
+    version='4.1.1',
+    description='A template engine light enough to embed in your project.',
+    url='https://github.com/funkybob/stencil/',
+    author='Curtis Maloney',
+    license='CC BY-SA 3.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='template',
+    py_modules=['stencil'],
+    test_suite='__main__.test_suite',
+)
