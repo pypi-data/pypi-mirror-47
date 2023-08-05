15b37908877050c9bea35a9e1e70b114e6d9aff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package nanome-loaders/0.0.1/nanome-loaders-0.0.1.tar.gz

---
+++ packages/nanome-loaders/nanome-loaders-0.0.1.tar.gz/nanome-loaders-0.0.1/README.md
@@ -0,0 +1,27 @@
+# Nanome - Ligand Focus
+
+Ligand Focus allows user to highlight a ligand and its binding site in one click.
+
+### Installation
+
+```sh
+$ pip install nanome-ligand-focus
+```
+
+### Usage
+
+To start the plugin:
+
+```sh
+$ nanome-ligand-focus -a plugin_server_address
+```
+
+In Nanome:
+
+- Activate Plugin
+- Click Run
+- The ligand will be selected and focused on
+
+### License
+
+MIT
---
+++ packages/nanome-loaders/nanome-loaders-0.0.1.tar.gz/nanome-loaders-0.0.1/nanome_loaders/WebLoader.py
@@ -0,0 +1,83 @@
+import nanome
+from nanome.util import Logs
+from nanome.api.structure import Complex
+
+from _WebLoaderServer import WebLoaderServer
+from _WebLoaderMenu import WebLoaderMenu
+
+import os
+import socket
+from timeit import default_timer as timer
+
+SERVER_PORT = 80
+
+# Plugin instance (for Nanome)
+class WebLoader(nanome.PluginInstance):
+    def start(self):
+        self.__menu = WebLoaderMenu(self)
+        self.__menu.build_menu(WebLoader.get_server_url())
+        self.__refresh()
+        self.__timer = timer()
+
+    def update(self):
+        if timer() - self.__timer >= 3.0:
+            self.__refresh()
+            self.__timer = timer()
+
+    def __refresh(self):
+        file_list = [filename for filename in os.listdir("_WebLoader") if WebLoaderServer.file_filter(filename)]
+        self.__menu.update_list(file_list)
+
+    def on_run(self):
+        self.__menu.open_menu()
+
+    def load_molecule(self, name):
+        extension = name.split(".")[-1]
+        if extension == "pdb":
+            complex = Complex.io.from_pdb("_WebLoader/" + name)
+            self.add_bonds([complex], self.bonds_ready)
+            return
+        elif extension == "sdf":
+            complex = Complex.io.from_sdf("_WebLoader/" + name)
+            self.bonds_ready([complex])
+        elif extension == "cif":
+            complex = Complex.io.from_mmcif("_WebLoader/" + name)
+            self.add_bonds([complex], self.bonds_ready)
+            return
+        else:
+            Logs.warning("Unknown file extension for file", name)
+            return
+
+    def bonds_ready(self, complex_list):
+        self.add_dssp(complex_list, self.send_complexes)
+
+    def send_complexes(self, complex_list):
+        self.add_to_workspace(complex_list)
+
+    @staticmethod
+    def get_server_url():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(('10.255.255.255', 1))
+            ip = s.getsockname()[0]
+        except:
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+
+        if SERVER_PORT != 80:
+            ip += ":" + str(SERVER_PORT)
+        return ip
+
+def main():
+    # Plugin server (for Web)
+    server = WebLoaderServer(SERVER_PORT)
+    server.start()
+
+    # Plugin
+    plugin = nanome.Plugin("Load from Web", "Gives access to a folder of molecules that can be modified by a Web UI", "Loading", False)
+    plugin.set_plugin_class(WebLoader)
+    plugin.run('127.0.0.1', 8888)
+
+if __name__ == "__main__":
+    main()
