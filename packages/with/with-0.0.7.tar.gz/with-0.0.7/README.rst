dcdadab45af5645b4760fc0769583d8f46712a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:17 +0000

    Add package with/0.0.7/with-0.0.7.tar.gz

---
+++ packages/with/with-0.0.7.tar.gz/with-0.0.7/README.rst
@@ -0,0 +1,35 @@
+with
+====
+
+|Build Status| |PyPI version| |Coverage|
+
+A shell context manager
+
+Install
+-------
+
+Use `pip`_ (Python 3 only)
+
+::
+
+    pip3 install with
+
+Usage
+-----
+
+Type ``with`` followed to the command you don’t want to repeat, than
+type the subcommand. Eg:
+
+.. figure:: http://i.imgur.com/P6QzNrl.gif
+   :alt: with git
+
+Type Ctrl-D to quit.
+
+.. _pip: https://pip.pypa.io/en/stable/installing/
+
+.. |Build Status| image:: https://travis-ci.org/renanivo/with.svg
+   :target: https://travis-ci.org/renanivo/with
+.. |PyPI version| image:: https://badge.fury.io/py/with.svg
+   :target: https://badge.fury.io/py/with
+.. |Coverage| image:: https://codecov.io/github/renanivo/with/coverage.svg?branch=master
+   :target: https://codecov.io/github/renanivo/with?branch=master
---
+++ packages/with/with-0.0.7.tar.gz/with-0.0.7/setup.py
@@ -0,0 +1,41 @@
+import os
+
+from setuptools import setup
+
+from withtool import __version__
+
+
+def read(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    with open(path, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='with',
+    version=__version__,
+    description='A shell context manager',
+    long_description=read('README.rst'),
+    author='Renan Ivo',
+    author_email='renanivom@gmail.com',
+    url='https://github.com/renanivo/with',
+    keywords='context manager shell command line repl',
+    scripts=['bin/with'],
+    install_requires=[
+        'appdirs>=1.4.3',
+        'docopt>=0.6.2',
+        'prompt-toolkit~=1.0.14',
+        'python-slugify>=1.2.3',
+    ],
+    packages=['withtool'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
