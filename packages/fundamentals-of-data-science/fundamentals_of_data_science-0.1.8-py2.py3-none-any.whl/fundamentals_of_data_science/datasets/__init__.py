bbffcacc33a78a483b3f3fb045786bf3f2fd0a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:16 +0000

    Add package fundamentals-of-data-science/0.1.8/fundamentals_of_data_science-0.1.8-py2.py3-none-any.whl

---
+++ packages/fundamentals-of-data-science/fundamentals_of_data_science-0.1.8-py2.py3-none-any.whl/fundamentals_of_data_science/datasets/__init__.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+
+
+def example():
+    """
+    Returns the example dataset.
+
+    Usage
+    -----
+        >>> import fundamentals_of_data_science as fun
+        >>> df = fun.datasets.example()
+        >>> df.head()
+    """
+    filename = os.path.abspath(os.path.join(os.path.dirname(__file__), 'imports-85.data.csv'))
+    return(pd.read_csv(filename))
