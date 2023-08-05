b10779d483e32c9c820ace7c1ddfaaea2306df46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:27 +0000

    Add package datatype-tools/0.0.7/datatype-tools-0.0.7.tar.gz

---
+++ packages/datatype-tools/datatype-tools-0.0.7.tar.gz/datatype-tools-0.0.7/datatype_tools/lib/Str.py
@@ -0,0 +1,11 @@
+from forbiddenfruit import curse
+from datatype_tools.utils.string_functions import (replace_multiple,
+	format_date, find_nth)
+
+#: String Curses
+
+curse(str, 'replace_multiple', replace_multiple)
+curse(str, 'format_date', format_date)
+curse(str, 'find_nth', find_nth)
+
+#::: END PROGRAM :::
