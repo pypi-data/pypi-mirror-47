61b6e224d1695d751ad2aafbd1d69aa1ee49a11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:01 +0000

    Add package usagelogger/0.1.1/usagelogger-0.1.1.tar.gz

---
+++ packages/usagelogger/usagelogger-0.1.1.tar.gz/usagelogger-0.1.1/usagelogger/__init__.py
@@ -0,0 +1,12 @@
+# © 2016-2019 Resurface Labs Inc.
+
+__version__ = '0.1.1'
+
+import usagelogger.base_logger
+import usagelogger.http_logger
+import usagelogger.http_message
+import usagelogger.http_request_impl
+import usagelogger.http_response_impl
+import usagelogger.http_rule
+import usagelogger.http_rules
+import usagelogger.usage_loggers
