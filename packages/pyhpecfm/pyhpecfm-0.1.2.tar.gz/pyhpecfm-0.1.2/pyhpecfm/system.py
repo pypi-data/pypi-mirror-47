6532b03073d8ca8b24e8ffe368db0afb97a74d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:48 +0000

    Add package pyhpecfm/0.1.2/pyhpecfm-0.1.2.tar.gz

---
+++ packages/pyhpecfm/pyhpecfm-0.1.2.tar.gz/pyhpecfm-0.1.2/pyhpecfm/system.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module contains functions related to working with the system characteristics of the
+desired HPE Composable Fabric Manager instance
+"""
+
+
+def get_version(cfmclient):
+    """
+    Function takes input cfmclient type object to authenticate against CFM API and queries
+    versions API to return the version number of the system represented by the CFCMclient object
+    Current supported params are
+
+    :param cfmclient: object of type CFMClient
+    :return: list of dicts
+    """
+    path = 'versions'
+    return cfmclient.get(path).json().get('result')
+
+
+def get_audit_logs(cfmclient):
+    """
+    Get :List of audit log records currently defined in Composable Fabric.
+    :param cfmclient:
+    :return: list of Dictionary objects where each dictionary represents a single entry in the
+    audit log of the HPE Composable Fabric Manager
+    :rtype: list
+    """
+    path = 'audits'
+    return cfmclient.get(path).json().get('result')
