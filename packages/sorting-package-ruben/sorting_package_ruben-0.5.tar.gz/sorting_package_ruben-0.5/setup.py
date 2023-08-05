324618f3ee02d2eac0634be4724562a43ab2ae0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:20 +0000

    Add package sorting-package-ruben/0.5/sorting_package_ruben-0.5.tar.gz

---
+++ packages/sorting-package-ruben/sorting_package_ruben-0.5.tar.gz/sorting_package_ruben-0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='sorting_package_ruben',
+      version='0.5',
+      description='sorting Algorithms to be uploaded in Pypi',
+      url='http://github.com/sorting_packsge_ruben',
+      author='Ruben Villalon',
+      author_email='ruben@example.com',
+      license='MIT',
+      packages=['sorting_package_ruben'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
---
+++ packages/sorting-package-ruben/sorting_package_ruben-0.5.tar.gz/sorting_package_ruben-0.5/sorting_package_ruben/__init__.py
@@ -0,0 +1,4 @@
+from .bubble_sort import bubble_sort
+from .insertion_sort import insertionSort
+from .quick_sort import quick_sort
+from .selection_sort import selectionSort
