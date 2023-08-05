59bb415687c8460ea95566fce115971f9983d36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:03 +0000

    Add package tamarco/0.0.1/tamarco-0.0.1.tar.gz

---
+++ packages/tamarco/tamarco-0.0.1.tar.gz/tamarco-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tamarco',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='System73',
+    author_email='',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
