bc4a212964d253d16523e940fecd90f17c00ede0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:07 +0000

    Add package django-dummy-thumbnails/0.2.4/django-dummy-thumbnails-0.2.4.tar.gz

---
+++ packages/django-dummy-thumbnails/django-dummy-thumbnails-0.2.4.tar.gz/django-dummy-thumbnails-0.2.4/CHANGELOG.rst
@@ -0,0 +1,121 @@
+Release history and notes
+=========================
+`Sequence based identifiers
+<http://en.wikipedia.org/wiki/Software_versioning#Sequence-based_identifiers>`_
+are used for versioning (schema follows below):
+
+.. code-block:: none
+
+    major.minor[.revision]
+
+- It's always safe to upgrade within the same minor version (for example, from
+  0.3 to 0.3.4).
+- Minor version changes might be backwards incompatible. Read the
+  release notes carefully before upgrading (for example, when upgrading from
+  0.3.4 to 0.4).
+- All backwards incompatible changes are mentioned in this document.
+
+0.2.4
+-----
+2019-06-04
+
+- Tested against Python 3.6.
+- Tested against Django 2.1 and 2.2.
+- Dropped Python 3.4 support.
+
+0.2.3
+-----
+2017-01-08
+
+- Example project upgraded to work with Django 2.0.
+- Year change in copyright and licenses.
+
+0.2.2
+-----
+2017-04-13
+
+- Tested against Django 1.11 release.
+
+0.2.1
+-----
+2017-02-21
+
+- Tested against Python 3.6 and Django 1.11.
+
+0.2
+---
+2017-01-30
+
+- Added a feature to import dummy images from feed.
+
+0.1.10
+------
+2017-01-26
+
+- Added a ``--list`` option to the management command
+  ``dummy_thumbnails_verify_dummy_images``.
+
+0.1.9
+-----
+2017-01-26
+
+- Minor fixes.
+
+0.1.8
+-----
+2017-01-25
+
+- Minor fixes.
+
+0.1.7
+-----
+2017-01-25
+
+- Minor fixes. Skip sub directories when listing files 
+  in ``base.get_random_image``.
+- Make it possible to detect and remove broken dummy images.
+
+0.1.6
+-----
+2016-12-23
+
+- Minor fixes in tests against Django 1.8.
+- Minor code cleanup.
+
+0.1.5
+-----
+2016-12-19
+
+- Minor fixes in tests.
+
+0.1.4
+-----
+2016-12-17
+
+- Minor fixes in tests.
+
+0.1.3
+-----
+2016-12-16
+
+- Minor fixes/improvements.
+- Added ``django-imagekit`` support.
+
+0.1.2
+-----
+2016-12-15
+
+- Minor fixes in docs and tests.
+- Installable demo script added.
+
+0.1.1
+-----
+2016-12-15
+
+- Include forgotten images (fix in manifest) in the PyPI release.
+
+0.1
+---
+2016-12-14
+
+- Initial release.
---
+++ packages/django-dummy-thumbnails/django-dummy-thumbnails-0.2.4.tar.gz/django-dummy-thumbnails-0.2.4/src/dummy_thumbnails/contrib/thumbnailers/easy_thumbnails/source_generators.py
@@ -0,0 +1,88 @@
+"""
+easy-thumbnails contrib module. Since thumbnail generation in easy-thumbnails
+goes through source generators, we should make a new source generator for the
+thumbnail generation, which will replace broken thumbnails with dummy ones.
+In settings we set patched source generator in ``THUMBNAIL_SOURCE_GENERATORS``
+setting.
+
+- dummy_thumbnail: Dummy thumbnails generator for easy-thumbnails.
+"""
+
+import codecs
+import os
+
+from django.conf import settings as django_settings
+
+from easy_thumbnails.utils import exif_orientation as utils_exif_orientation
+
+from six import BytesIO
+
+from ....base import get_random_image
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+__title__ = 'dummy_thumbnails.contrib.thumbnailers.easy_thumbnails.' \
+            'source_generators'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2016-2018 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = (
+    'dummy_thumbnail',
+)
+
+
+def dummy_thumbnail(source, exif_orientation=True, **options):
+    """Try to open the source file directly using PIL, ignoring any errors.
+
+    exif_orientation
+
+        If EXIF orientation data is present, perform any required reorientation
+        before passing the data along the processing pipeline.
+
+    """
+    # Use a BytesIO wrapper because if the source is an incomplete file like
+    # object, PIL may have problems with it. For example, some image types
+    # require tell and seek methods that are not present on all storage
+    # File objects.
+    media_root = os.path.abspath(django_settings.MEDIA_ROOT)
+
+    if not (
+            source and
+            hasattr(source, 'path') and
+            (os.path.exists(source.path) or os.path.isfile(source.path))
+    ):
+        source = load_random_file(media_root)
+
+    buf = BytesIO(source.read())
+
+    image = Image.open(buf)
+    # Fully load the image now to catch any problems with the image contents.
+    try:
+        # An "Image file truncated" exception can occur for some images that
+        # are still mostly valid -- we'll swallow the exception.
+        image.load()
+    except IOError:
+        pass
+
+    # Try a second time to catch any other potential exceptions.
+    image.load()
+
+    if exif_orientation:
+        image = utils_exif_orientation(image)
+    return image
+
+
+def load_random_file(media_root):
+    """Load random file."""
+    # We should load ``ThumbnailFile`` here, since otherwise we might get
+    # ``AppRegistryNotReady`` exception.
+    from easy_thumbnails.files import ThumbnailFile
+
+    random_image = get_random_image()
+    random_file = codecs.open(random_image)
+    source = ThumbnailFile(random_file.name, random_file)
+    source.name = source.file.name.replace(media_root, '')[1:]
+    return source
