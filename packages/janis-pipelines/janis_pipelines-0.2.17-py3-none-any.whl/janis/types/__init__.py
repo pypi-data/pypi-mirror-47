bb32d42d7cc0f98f03a5a03c136640e93d2c823c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package janis-pipelines/0.2.17/janis_pipelines-0.2.17-py3-none-any.whl

---
+++ packages/janis-pipelines/janis_pipelines-0.2.17-py3-none-any.whl/janis/types/__init__.py
@@ -0,0 +1,32 @@
+from abc import ABC
+
+
+class Selector(ABC):
+    pass
+
+
+class InputSelector(Selector):
+
+    def __init__(self, input_to_select, suffix=None, prefix=None, use_basename=None):
+        self.input_to_select = input_to_select
+        self.prefix = prefix
+        self.suffix = suffix
+        self.use_basename = use_basename
+
+
+class WildcardSelector(Selector):
+    def __init__(self, wildcard):
+        self.wildcard = wildcard
+
+
+class MemorySelector(Selector):
+    def __init__(self, suffix=None, prefix=None, default=None):
+        self.suffix = suffix
+        self.prefix = prefix
+        self.default = default
+
+
+class CpuSelector(Selector):
+    def __init__(self, default=None):
+        super().__init__()
+        self.default = default
