22151d45f2732c97b0e4c33c01aba30427074467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:51 +0000

    Add package pytorch-swats/0.1.0/pytorch-swats-0.1.0.tar.gz

---
+++ packages/pytorch-swats/pytorch-swats-0.1.0.tar.gz/pytorch-swats-0.1.0/swats/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+@author:    Patrik Purgai
+@copyright: Copyright 2019, swats
+@license:   MIT
+@email:     purgai.patrik@gmail.com
+@date:      2019.05.30.
+"""
+
+from .optim import SWATS
+
+__all__ = ['SWATS']
