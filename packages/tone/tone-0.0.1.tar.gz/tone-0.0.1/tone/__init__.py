03ba2a90b2beec1c01f2d10e8675410108089dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:58 +0000

    Add package tone/0.0.1/tone-0.0.1.tar.gz

---
+++ packages/tone/tone-0.0.1.tar.gz/tone-0.0.1/tone/__init__.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+# Deal with tone
+
+__version__ = '0.0.1'
---
+++ packages/tone/tone-0.0.1.tar.gz/tone-0.0.1/tone/utils/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+from .logger import get_logger  # noqa
+from .attrdict import attrdict  # noqa
+from .attrdict import defaultattrdict  # noqa
