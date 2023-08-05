cb4ad223fd9c0d5f5693473ff0d945f4304029cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:07 +0000

    Add package smartcloudadmin/0.7.2/smartcloudadmin-0.7.2.tar.gz

---
+++ packages/smartcloudadmin/smartcloudadmin-0.7.2.tar.gz/smartcloudadmin-0.7.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='smartcloudadmin',
+    version='0.7.2',
+    packages=['smartcloudadmin', 'smartcloudadmin.utils', 'smartcloudadmin.models', 'smartcloudadmin.json'],
+    url='https://github.com/cathaldi/smartcloud-administrator',
+    include_package_data=True,
+    license='apache2',
+    download_url='https://github.com/cathaldi/smartcloud-administrator/releases/download/0.7.2/smartcloudadmin-0.7.2.tar.gz',
+    author='Cathal A. Dinneen',
+    install_requires=['requests'],
+    author_email='cathal.a.dinneen@gmail.com',
+    description='A package that provides functions to help interacting with companies, subscriptions and subscribers on IBM Smartcloud'
+)
