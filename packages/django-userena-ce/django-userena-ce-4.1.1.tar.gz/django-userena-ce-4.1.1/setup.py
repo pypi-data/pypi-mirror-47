121d184aaca8a602b317f2cc8e826c538c8cdf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:03 +0000

    Add package django-userena-ce/4.1.1/django-userena-ce-4.1.1.tar.gz

---
+++ packages/django-userena-ce/django-userena-ce-4.1.1.tar.gz/django-userena-ce-4.1.1/setup.py
@@ -0,0 +1,58 @@
+import sys
+
+from setuptools import setup, find_packages
+
+userena = __import__('userena')
+
+readme_file = 'README.md'
+try:
+    long_description = open(readme_file).read()
+except IOError:
+    sys.stderr.write(
+        "[ERROR] Cannot find file specified as "
+        "``long_description`` (%s)\n" % readme_file
+    )
+    sys.exit(1)
+
+install_requires = [
+    'easy_thumbnails',
+    'django-guardian>=1.4.2',
+    'html2text',
+    'Django>=1.11',
+]
+
+setup(name='django-userena-ce',
+      version=userena.get_version(),
+      description='Complete user management application for Django',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False,
+      author='James Meakin',
+      author_email='jamesmeakin@gmail.com',
+      url='https://github.com/django-userena-ce/django-userena-ce/',
+      download_url='https://github.com/django-userena-ce/django-userena-ce/downloads',
+      packages=find_packages(exclude=['demo', 'demo.*']),
+      include_package_data=True,
+      install_requires=install_requires,
+      test_suite='tests.main',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Framework :: Django :: 1.11',
+          'Framework :: Django :: 2.1',
+          'Framework :: Django :: 2.2',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Utilities'
+      ],
+      )
---
+++ packages/django-userena-ce/django-userena-ce-4.1.1.tar.gz/django-userena-ce-4.1.1/userena/compat.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# SiteProfileNotAvailable compatibility
+class SiteProfileNotAvailable(Exception):
+    pass
