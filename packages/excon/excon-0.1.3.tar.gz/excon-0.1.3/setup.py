7ea9dc9f0cfe8e017f914586dacdead050cd64fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:13 +0000

    Add package excon/0.1.3/excon-0.1.3.tar.gz

---
+++ packages/excon/excon-0.1.3.tar.gz/excon-0.1.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, Extension, find_packages
+
+
+with open('/Users/RT/projects/excon/README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='excon',
+    version='0.1.3',
+    description='Extract and convert data from PDFs',
+    long_description="this is a long description of the module",
+    long_description_content_type="text/plain",
+    author='Ryan Trigg',
+    author_email='rbutustree@gmail.com',
+    url='https://github.com/rbutus/excon.git',
+    license="MIT License",
+    packages=find_packages(), 
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"
+    ],
+)
+
