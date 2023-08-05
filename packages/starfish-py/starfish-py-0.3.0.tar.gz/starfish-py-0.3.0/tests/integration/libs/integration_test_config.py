7196f2f41e908cbd115e0f03908446fc72808ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package starfish-py/0.3.0/starfish-py-0.3.0.tar.gz

---
+++ packages/starfish-py/starfish-py-0.3.0.tar.gz/starfish-py-0.3.0/tests/integration/libs/integration_test_config.py
@@ -0,0 +1,38 @@
+
+import configparser
+
+
+class IntegrationTestConfig():
+    def __init__(self, filename):
+
+        config = configparser.ConfigParser()
+        config.read(filename)
+        self.keeper_url = config.get('ocean', 'keeper_url')
+        self.contracts_path = config.get('ocean', 'contracts_path')
+        self.gas_limit = config.get('ocean', 'gas_limit')
+
+        self.publisher_account = {
+            'address': config.get('publisher account', 'address'),
+            'password': config.get('publisher account', 'password')
+        }
+        self.purchaser_account = {
+            'address': config.get('purchaser account', 'address'),
+            'password': config.get('purchaser account', 'password')
+        }
+        self.agent_account = {
+            'address': config.get('agent account', 'address'),
+            'password': config.get('agent account', 'password')
+        }
+
+        items = config.items('squid agent')
+        self.squid_config = {}
+        for item in items:
+            self.squid_config[item[0]] = item[1]
+
+        self.surfer_username=config.get('surfer', 'username')
+        self.surfer_password=config.get('surfer', 'password')
+        self.surfer_url=config.get('surfer', 'surfer_url')
+
+        self.koi_url=config.get('invoke', 'koi_url')
+
+
