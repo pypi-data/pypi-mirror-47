0ca01f4c9a84046563adafb4dc3348ffa0ea9121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:20 +0000

    Add package test1241/0.0.0/test1241-0.0.0.tar.gz

---
+++ packages/test1241/test1241-0.0.0.tar.gz/test1241-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="test1241",
+    version="0.0.0",
+    author="Placeholder",
+    author_email="corpeng@planet.com",
+    description="placeholder",
+    packages=setuptools.find_packages(),
+)
