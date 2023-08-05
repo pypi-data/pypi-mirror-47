357d2f1d75bfa9f10ae533969f7ab636f0546368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:55 +0000

    Add package nester-markov/1.1.0/nester-markov-1.1.0.tar.gz

---
+++ packages/nester-markov/nester-markov-1.1.0.tar.gz/nester-markov-1.1.0/nester-markov.py
@@ -0,0 +1,13 @@
+"""This module is a recursive function that prints out every nested
+item on a single line, no matter how deeply it is nested"""
+
+def print_lol(the_list, level):
+    """It starts iteration and checks if the current list item is a
+list. If it is a list, the function occures again on that item etc... If it is a single item, then it is printed on a single line"""
+    for each_item in the_list:
+        if isinstance (each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_space in range(level):
+                print("\t", end='')
+            print(each_item)
