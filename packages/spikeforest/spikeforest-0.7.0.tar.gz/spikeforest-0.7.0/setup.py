abb7393227b9f53e51884d5c666b73b84415e70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:44 +0000

    Add package spikeforest/0.7.0/spikeforest-0.7.0.tar.gz

---
+++ packages/spikeforest/spikeforest-0.7.0.tar.gz/spikeforest-0.7.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+pkg_name = "spikeforest"
+
+# NOTE: you should install this project in development mode
+# > python setup.py develop
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.7.0",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="Spike sorting",
+    packages=setuptools.find_packages(),
+    package_dir={
+        'spikesorters': 'spikesorters',
+        'spikeforestwidgets': 'spikeforestwidgets',
+        'forestview': 'forestview'
+    },
+    include_package_data=True,
+    install_requires=[
+        'numpy', 'scipy', 'matplotlib', 'requests', 'pillow', 'pandas', 'ipython', 'h5py', 'setuptools-git', 'scikit-learn', 'python-frontmatter', 'spikeextractors>=0.4.2,<0.5'
+    ],
+    scripts=['bin/forestview'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    )
+)
