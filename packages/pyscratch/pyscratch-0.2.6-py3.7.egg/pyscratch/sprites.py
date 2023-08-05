1f3356cbcf2283d753f73e48d2840ee9ecd99932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:50 +0000

    Add package pyscratch/0.2.6/pyscratch-0.2.6-py3.7.egg

---
+++ packages/pyscratch/pyscratch-0.2.6-py3.7.egg/pyscratch/sprites.py
@@ -0,0 +1,11 @@
+import pyscratch
+
+
+class Ball(pyscratch.Sprite):
+    def __init__(self, x=0, y=0):
+        pyscratch.Sprite.__init__(self, "Ball", None, x, y)
+
+
+class Ball(pyscratch.Sprite):
+    def __init__(self, x=0, y=0):
+        pyscratch.Sprite.__init__(self, "Cat", None, x, y)
