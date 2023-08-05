da2c793d392ff4f2aec509212315d0b7a218f9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:47 +0000

    Add package operator-courier/2.1.0/operator-courier-2.1.0.tar.gz

---
+++ packages/operator-courier/operator-courier-2.1.0.tar.gz/operator-courier-2.1.0/operatorcourier/identify.py
@@ -0,0 +1,35 @@
+from yaml import safe_load
+from yaml import MarkedYAMLError
+import logging
+from operatorcourier.errors import OpCourierBadYaml, OpCourierBadArtifact
+from operatorcourier.manifest_parser import CRD_STR, CSV_STR, PKG_STR
+
+logger = logging.getLogger(__name__)
+
+
+def get_operator_artifact_type(operatorArtifactString):
+    """get_operator_artifact_type takes a yaml string and determines if it is
+    one of the expected bundle types: ClusterServiceVersion,
+    CustomResourceDefinition, or Package.
+
+    :param operatorArtifactString: Yaml string to type check
+    """
+    try:
+        operatorArtifact = safe_load(operatorArtifactString)
+    except MarkedYAMLError:
+        msg = "Courier requires valid input YAML files"
+        logger.error(msg)
+        raise OpCourierBadYaml(msg)
+    else:
+        artifact_type = None
+        if isinstance(operatorArtifact, dict):
+            if "packageName" in operatorArtifact:
+                artifact_type = PKG_STR
+            elif operatorArtifact.get("kind") in {CRD_STR, CSV_STR}:
+                artifact_type = operatorArtifact["kind"]
+            if artifact_type is not None:
+                return artifact_type
+
+        msg = 'Courier requires valid CSV, CRD, and Package files'
+        logger.error(msg)
+        raise OpCourierBadArtifact(msg)
---
+++ packages/operator-courier/operator-courier-2.1.0.tar.gz/operator-courier-2.1.0/tests/test_identify.py
@@ -0,0 +1,48 @@
+import pytest
+import operatorcourier.identify as identify
+from operatorcourier.errors import OpCourierBadYaml, OpCourierBadArtifact
+from testfixtures import LogCapture
+
+
+@pytest.mark.parametrize('fname,expected', [
+    ("tests/test_files/csv.yaml", "ClusterServiceVersion"),
+    ("tests/test_files/crd.yaml", "CustomResourceDefinition"),
+    ("tests/test_files/package.yaml", "Package"),
+])
+def test_get_operator_artifact_type(fname, expected):
+    with open(fname) as f:
+        yaml = f.read()
+        assert identify.get_operator_artifact_type(yaml) == expected
+
+
+@pytest.mark.parametrize('fname', [
+    ("tests/test_files/invalid.yaml"),
+    ("tests/test_files/empty.yaml"),
+])
+def test_get_operator_artifact_type_assertions(fname):
+    with open(fname) as f:
+        yaml = f.read()
+    with pytest.raises(OpCourierBadArtifact) as e, LogCapture() as logs:
+        identify.get_operator_artifact_type(yaml)
+
+    logs.check(('operatorcourier.identify',
+                'ERROR',
+                'Courier requires valid CSV, CRD, and Package files'),)
+    assert 'Courier requires valid CSV, CRD, and Package files' == str(e.value)
+
+
+@pytest.mark.parametrize('fname', [
+    "tests/test_files/invalid.malformed.parser.error.yaml",
+    "tests/test_files/invalid.malformed.scanner.error.yaml",
+])
+def test_get_operator_artifact_type_with_invalid_yaml(fname):
+    with open(fname) as f:
+        yaml = f.read()
+
+    with pytest.raises(OpCourierBadYaml) as e, LogCapture() as logs:
+        identify.get_operator_artifact_type(yaml)
+
+    logs.check(('operatorcourier.identify',
+                'ERROR',
+                'Courier requires valid input YAML files'),)
+    assert 'Courier requires valid input YAML files' == str(e.value)
