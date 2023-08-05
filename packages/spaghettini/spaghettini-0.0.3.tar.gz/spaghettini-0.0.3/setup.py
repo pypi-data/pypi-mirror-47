53982ea8eb36abbec45bb8759abb47a08a4add14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package spaghettini/0.0.3/spaghettini-0.0.3.tar.gz

---
+++ packages/spaghettini/spaghettini-0.0.3.tar.gz/spaghettini-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import os
+
+working_dir = path.abspath(path.dirname(__file__))
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+# Read the README.
+with open(os.path.join(ROOT, 'README.md'), encoding="utf-8") as f:
+    README = f.read()
+
+setup(name='spaghettini',
+      version='0.0.3',
+      package=find_packages(),
+      description='',
+      long_description=README,
+      long_description_content_type='text/markdown',
+      install_requires=["oyaml"],
+)
