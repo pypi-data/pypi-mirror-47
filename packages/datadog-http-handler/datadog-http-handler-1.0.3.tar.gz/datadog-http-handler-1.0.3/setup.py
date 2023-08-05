28e1c4a5b7c43238f0e23793a7d1597b416a51fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package datadog-http-handler/1.0.3/datadog-http-handler-1.0.3.tar.gz

---
+++ packages/datadog-http-handler/datadog-http-handler-1.0.3.tar.gz/datadog-http-handler-1.0.3/setup.py
@@ -0,0 +1,29 @@
+'''Python logging module allowing you to log directly to Datadog via https.'''
+
+import sys
+from setuptools import setup
+
+from m2r import convert
+
+if sys.argv[-1] == 'sdist':
+      with open('README.md', 'r') as handle:         
+            long_description = convert(handle.read())
+
+setup(name='datadog-http-handler',
+      version='1.0.3',
+      description='Python logging module allowing you to log directly to Datadog via https',
+      long_description=long_description,
+      url='https://github.com/nrccua/datadog-http-handler',
+      author='NRCCUA Architects',
+      author_email='architecture@nrccua.org',
+      license='MIT',
+      packages=['datadog_http_handler'],
+      install_requires=['requests', 'm2r'],
+      setup_requires=['pytest-runner', 'twine'],
+      tests_require=['pytest'],
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ])
