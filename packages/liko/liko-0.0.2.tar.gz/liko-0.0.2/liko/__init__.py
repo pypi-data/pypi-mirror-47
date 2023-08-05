11ef57bfd80381d291a7bfa4eff793d176570f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:40 +0000

    Add package liko/0.0.2/liko-0.0.2.tar.gz

---
+++ packages/liko/liko-0.0.2.tar.gz/liko-0.0.2/liko/__init__.py
@@ -0,0 +1,29 @@
+import os
+import multiprocessing as mp
+import liko
+
+def split_list(self, alist, wanted_parts=1):
+    length = len(alist)
+    return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] for i in range(wanted_parts) ]
+
+def raker(self,dt):
+    resp = []
+    func = dt['func']
+    for x in dt['data']:
+        resp.append(func(x))
+    return resp 
+
+def multi(self,func,param,multiple=mp.cpu_count()):
+    result = []
+    total_param = len(param)
+    datas = self.split_list(param,multiple)
+    datas = [{'func':func ,'data':x} for x in datas]
+    maper = mp.Pool(mp.cpu_count())
+
+    result = maper.map(self.raker,datas)
+    maper.close()
+    return result
+
+
+if(__name__ == "__main__"):
+    print('Believe in Future.')
