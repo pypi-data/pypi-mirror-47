f93a6ff492d1fd65c343d6e64d114a83fe9d1fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:08 +0000

    Add package minixes2/0.6.5/minixes2-0.6.5.tar.gz

---
+++ packages/minixes2/minixes2-0.6.5.tar.gz/minixes2-0.6.5/setup.py
@@ -0,0 +1,41 @@
+'''
+ Copyright (c) 2014, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+from setuptools import setup, find_packages
+import minixs
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+import platform
+from os.path import join, expanduser
+if platform.system() == "Windows":
+    data_files = [('Desktop/minixes2' ,
+                   ['bin/process_rixs', 'bin/process_xes'])]
+else:
+    data_files = []
+setup(name='minixes2',
+      version=minixs.__version__,
+      description='Python program to process MiniXES data from sector 20',
+      long_description = long_description,
+      long_description_content_type='text/markdown',
+      author = 'Brian Mattern, John Hammonds',
+      author_email = 'JPHammonds@anl.gov',
+      maintainer = 'John Hammonds',
+      maintainer_email = 'JPHammonds@anl.gov',
+      
+      url = '',
+      packages = find_packages() ,
+#       include_package_data=True,
+      package_data = {},
+      install_requires = [],
+      data_files = data_files,
+      license = 'See LICENSE File',
+      platforms = 'any',
+      scripts = ['bin/process_rixs',
+               'bin/process_xes',
+               'bin/run_calibrate.bat',
+               'bin/run_calibrate']
+      )
