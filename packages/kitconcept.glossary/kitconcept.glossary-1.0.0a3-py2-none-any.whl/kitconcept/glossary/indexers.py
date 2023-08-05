45d92c78cea1069898c4144e6a4d4b1043a264eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:37 +0000

    Add package kitconcept.glossary/1.0.0a3/kitconcept.glossary-1.0.0a3-py2-none-any.whl

---
+++ packages/kitconcept.glossary/kitconcept.glossary-1.0.0a3-py2-none-any.whl/kitconcept/glossary/indexers.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from kitconcept.glossary.interfaces import ITerm
+from plone.app.textfield.interfaces import ITransformer
+from plone.i18n.normalizer.base import baseNormalize
+from plone.indexer import indexer
+from Products.CMFPlone.utils import safe_unicode
+
+
+@indexer(ITerm)
+def textIndexer(context):
+    """SearchableText contains id, title, variants and definition
+    text as plain text.
+    """
+    transformer = ITransformer(context)
+
+    try:
+        definition = transformer(context.definition, 'text/plain')
+    except AttributeError:
+        definition = u''
+    try:
+        variants = u' '.join(context.variants)
+    except TypeError:
+        variants = u''
+
+    return u' '.join((
+        safe_unicode(context.id),
+        safe_unicode(context.title) or u'',
+        safe_unicode(variants),
+        safe_unicode(definition),
+    ))
+
+
+@indexer(ITerm)
+def variantsIndexer(context):
+    if not context.variants:
+        return []
+    return context.variants
+
+
+@indexer(ITerm)
+def definitionIndexer(context):
+    return context.definition.raw
+
+
+@indexer(ITerm)
+def letterIndexer(context):
+    if not context.title:
+        return u''
+    return baseNormalize(context.title)[0].upper()
