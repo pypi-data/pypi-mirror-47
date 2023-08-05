5ae92a98bf37c3cd2916f3822a15f7bd9eaa5d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:59 +0000

    Add package beyoncify/1.0.1/beyoncify-1.0.1.tar.gz

---
+++ packages/beyoncify/beyoncify-1.0.1.tar.gz/beyoncify-1.0.1/README.md
@@ -0,0 +1,30 @@
+# beyoncify #1.0.1
+
+beyoncify yourself
+
+![beyoncify](https://github.com/ebsaral/beyoncify/blob/master/beyoncify.png?raw=true)
+
+# install
+
+```
+pip install beyoncify
+```
+
+# usage
+
+```python
+import beyoncify
+
+beyonce_to_be = beyoncify.beyoncify('emin')
+beyoncify.in_yo_face_mf(beyonce_to_be)
+>>> 'émin'
+normal = beyoncify.go_back_to_being_normal(beyonce_to_be)
+beyonce.in_yo_face_mf(normal)
+>>> 'emin'
+```
+
+# contributors
+
+- @ebsaral - author
+
+copyrights of the photos belong to their owners.
---
+++ packages/beyoncify/beyoncify-1.0.1.tar.gz/beyoncify-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='beyoncify',
+      version='1.0.1',
+      description='beyoncify',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='beyonce beyoncify',
+      url='https://github.com/ebsaral/beyoncify',
+      author='Emin Bugra Saral',
+      author_email='eminbugrasaral@me.com',
+      license='BSD',
+      packages=[],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False,
+      project_urls={
+        'Documentation': 'https://github.com/ebsaral/beyoncify',
+        'Funding': 'https://github.com/ebsaral/beyoncify',
+        'Source': 'https://github.com/ebsaral/beyoncify',
+      },
+      tests_require=[],
+)
