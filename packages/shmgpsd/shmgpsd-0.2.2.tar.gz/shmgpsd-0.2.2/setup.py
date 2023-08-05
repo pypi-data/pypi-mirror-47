cc22ac78d00e8bb74ead67cd53ac8241532e29e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:19 +0000

    Add package shmgpsd/0.2.2/shmgpsd-0.2.2.tar.gz

---
+++ packages/shmgpsd/shmgpsd-0.2.2.tar.gz/shmgpsd-0.2.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='shmgpsd',
+      version='0.2.2',
+      description='GPSD shared memory tool',
+      py_modules=['shmgpsd'],
+      )
