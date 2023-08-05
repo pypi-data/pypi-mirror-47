c95be6b05bb6270d967a8bbeb977423d0b999486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package kblab-client/0.0.5a0/kblab-client-0.0.5a0.tar.gz

---
+++ packages/kblab-client/kblab-client-0.0.5a0.tar.gz/kblab-client-0.0.5a0/kblab/__init__.py
@@ -0,0 +1,8 @@
+from kblab.package import Package
+from kblab.httppackage import HttpPackage
+from kblab.archive import Archive
+from kblab.httparchive import HttpArchive
+VERIFY_CA=True
+
+name = "kblab"
+
