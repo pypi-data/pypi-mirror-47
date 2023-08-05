5a0f2ae390f47f20aeb6891a6537a6a633ee5019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package mastercard-disbursements/1.0.9/mastercard_disbursements-1.0.9.tar.gz

---
+++ packages/mastercard-disbursements/mastercard_disbursements-1.0.9.tar.gz/mastercard_disbursements-1.0.9/mastercarddisbursements/__init__.py
@@ -0,0 +1,13 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercarddisbursements.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercarddisbursements.accountbalance import *
+from mastercarddisbursements.accountinfo import *
+from mastercarddisbursements.accountmapping import *
+from mastercarddisbursements.accountverification import *
+from mastercarddisbursements.disbursement import *
+from mastercarddisbursements.transfernotification import *
