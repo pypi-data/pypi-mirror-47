d558b0848aa257e70deaec9d76852718b1fbe654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:42 +0000

    Add package nameko-wrapper/0.0.5/nameko_wrapper-0.0.5.tar.gz

---
+++ packages/nameko-wrapper/nameko_wrapper-0.0.5.tar.gz/nameko_wrapper-0.0.5/nameko_wrapper/elasticsearch/__init__.py
@@ -0,0 +1,6 @@
+from .documents import ExtendDocument, init_document_index
+
+from .exceptions import *
+
+from .searches import ESSearch
+from .utils.formatters import SearchFormatter, SearchResultFormatter
