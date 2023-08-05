65916f2f9a847a4ded498a4448c8b3f7663f1665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:29 +0000

    Add package arrow/0.14.0/arrow-0.14.0.tar.gz

---
+++ packages/arrow/arrow-0.14.0.tar.gz/arrow-0.14.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+from arrow import __version__
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+setup(
+    name="arrow",
+    version=__version__,
+    description="Better dates and times for Python",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    url="https://arrow.readthedocs.io/en/latest/",
+    author="Chris Smith",
+    author_email="crsmithdev@gmail.com",
+    license="Apache 2.0",
+    packages=["arrow"],
+    zip_safe=False,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=["python-dateutil"],
+    extras_require={":python_version=='2.7'": ["backports.functools_lru_cache>=1.2.1"]},
+    test_suite="tests",
+    tests_require=["chai"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="arrow date time datetime",
+    project_urls={
+        "Repository": "https://github.com/crsmithdev/arrow",
+        "Bug Reports": "https://github.com/crsmithdev/arrow/issues",
+    },
+)
