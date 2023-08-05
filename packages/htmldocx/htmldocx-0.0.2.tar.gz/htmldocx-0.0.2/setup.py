40273177a33bd127a66f6d3ffa726017eb90a853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:48 +0000

    Add package htmldocx/0.0.2/htmldocx-0.0.2.tar.gz

---
+++ packages/htmldocx/htmldocx-0.0.2.tar.gz/htmldocx-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="htmldocx",
+    version="0.0.2",
+    author="pqzx",
+    author_email="jc3664@gmail.com",
+    description="Convert html to docx",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pqzx/h2d",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "python-docx >= 0.8.9",
+    ]
+)
