69997f1c12c5df0fa597f5dabb807eff7485b108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:51 +0000

    Add package pykarbon/1.0.2/pykarbon-1.0.2.tar.gz

---
+++ packages/pykarbon/pykarbon-1.0.2.tar.gz/pykarbon-1.0.2/setup.py
@@ -0,0 +1,18 @@
+'''
+Setup file that configures the package.
+'''
+from setuptools import setup
+
+setup(
+    name='pykarbon',
+    url='https://gitlab.logicsupply.com/engineering/karbon/pykarbon_project',
+    author='Jacob Caughfield',
+    author_email='jacob.caughfield@logicsupply.com',
+    packages=['pykarbon'],
+    install_requires=['pyserial'],
+    scripts=['scripts/update_firmware.py'],
+    version='1.0.2',
+    license='MIT',
+    description='Tools for Karbon hardware interfaces.',
+    long_description=open('README.rst').read()
+)
