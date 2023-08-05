e46e1223f378b234ba731cd07e566c42839ba710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:20 +0000

    Add package signalworks/0.1.8/signalworks-0.1.8.tar.gz

---
+++ packages/signalworks/signalworks-0.1.8.tar.gz/signalworks-0.1.8/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+setup(
+    author="Alex Kain",
+    author_email="lxkain@gmail.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="Library to handle signal data and perform signal processing computations",
+    entry_points={"console_scripts": ["signalworks=signalworks.cli:main"]},
+    install_requires=["numpy", "scipy", "numba==0.42.0", "llvmlite==0.27.0"],
+    extras_require={"label": ["pyedflib", "openxdf"], "robust_audio": ["soundfile"]},
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="signalworks",
+    name="signalworks",
+    packages=find_packages(),
+    setup_requires=["pytest-runner"],
+    test_suite="tests",
+    url="https://github.com/lxkain/signalworks",
+    version="0.1.8",
+    zip_safe=False,
+)
