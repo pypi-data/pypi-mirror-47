d180b4f4ef35990ea6e95be9e263bac999d734b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:40 +0000

    Add package gitless/0.8.8/gitless-0.8.8.tar.gz

---
+++ packages/gitless/gitless-0.8.8.tar.gz/gitless-0.8.8/gitless/cli/gl.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# Gitless - a version control system built on top of Git
+# Licensed under MIT
+
+"""gl - Main Gitless's command. Dispatcher to the other cmds."""
+
+
+from __future__ import unicode_literals
+
+import sys
+import argparse
+import traceback
+import pygit2
+
+if sys.platform != 'win32':
+  from sh import ErrorReturnCode
+else:
+  from pbs import ErrorReturnCode
+
+from clint.textui import colored
+
+from gitless import core
+
+from . import (
+    gl_track, gl_untrack, gl_status, gl_diff, gl_commit, gl_branch, gl_tag,
+    gl_checkout, gl_merge, gl_resolve, gl_fuse, gl_remote, gl_publish,
+    gl_switch, gl_init, gl_history)
+from . import pprint
+
+
+SUCCESS = 0
+ERRORS_FOUND = 1
+# 2 is used by argparse to indicate cmd syntax errors.
+INTERNAL_ERROR = 3
+NOT_IN_GL_REPO = 4
+
+__version__ = '0.8.8'
+URL = 'http://gitless.com'
+
+
+repo = None
+try:
+  repo = core.Repository()
+  try:
+    colored.DISABLE_COLOR = not repo.config.get_bool('color.ui')
+  except pygit2.GitError:
+    colored.DISABLE_COLOR = (
+        repo.config['color.ui'] in ['no', 'never'])
+except (core.NotInRepoError, KeyError):
+  pass
+
+
+def print_help(parser):
+  """print help for humans"""
+  print(parser.description)
+  print('\ncommands:\n')
+
+  # https://stackoverflow.com/questions/20094215/argparse-subparser-monolithic-help-output
+  # retrieve subparsers from parser
+  subparsers_actions = [
+      action for action in parser._actions
+      if isinstance(action, argparse._SubParsersAction)]
+  # there will probably only be one subparser_action,
+  # but better safe than sorry
+  for subparsers_action in subparsers_actions:
+      # get all subparsers and print help
+      for choice in subparsers_action._choices_actions:
+          print('    {:<19} {}'.format(choice.dest, choice.help))
+
+def main():
+  parser = argparse.ArgumentParser(
+      description=(
+          'Gitless: a version control system built on top of Git.\nMore info, '
+          'downloads and documentation at {0}'.format(URL)),
+      formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument(
+      '--version', action='version', version=(
+         'GL Version: {0}\nYou can check if there\'s a new version of Gitless '
+         'available at {1}'.format(__version__, URL)))
+  subparsers = parser.add_subparsers(title='subcommands', dest='subcmd_name')
+  subparsers.required = True
+
+  sub_cmds = [
+      gl_track, gl_untrack, gl_status, gl_diff, gl_commit, gl_branch, gl_tag,
+      gl_checkout, gl_merge, gl_resolve, gl_fuse, gl_remote, gl_publish,
+      gl_switch, gl_init, gl_history]
+  for sub_cmd in sub_cmds:
+    sub_cmd.parser(subparsers, repo)
+
+  if len(sys.argv) == 1:
+    print_help(parser)
+    return SUCCESS
+
+  args = parser.parse_args()
+  try:
+    if args.subcmd_name != 'init' and not repo:
+      raise core.NotInRepoError('You are not in a Gitless\'s repository')
+
+    return SUCCESS if args.func(args, repo) else ERRORS_FOUND
+  except KeyboardInterrupt:
+    pprint.puts('\n')
+    pprint.msg('Keyboard interrupt detected, operation aborted')
+    return SUCCESS
+  except core.NotInRepoError as e:
+    pprint.err(e)
+    pprint.err_exp('do gl init to turn this directory into an empty repository')
+    pprint.err_exp('do gl init remote_repo to clone an existing repository')
+    return NOT_IN_GL_REPO
+  except (ValueError, pygit2.GitError, core.GlError) as e:
+    pprint.err(e)
+    return ERRORS_FOUND
+  except ErrorReturnCode as e:
+    pprint.err(e.stderr)
+    return ERRORS_FOUND
+  except:
+    pprint.err('Some internal error occurred')
+    pprint.err_exp(
+        'If you want to help, see {0} for info on how to report bugs and '
+        'include the following information:\n\n{1}\n\n{2}'.format(
+            URL, __version__, traceback.format_exc()))
+    return INTERNAL_ERROR
---
+++ packages/gitless/gitless-0.8.8.tar.gz/gitless-0.8.8/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import ast
+import re
+import sys
+
+from setuptools import setup
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+
+with open('gitless/cli/gl.py', 'rb') as f:
+  version = str(ast.literal_eval(_version_re.search(
+    f.read().decode('utf-8')).group(1)))
+
+
+# Build helper
+if sys.argv[-1] == 'gl-build':
+  from sh import pyinstaller
+  import shutil
+  import tarfile
+  import platform
+
+  rel = 'gl-v{0}-{1}-{2}'.format(
+      version, platform.system().lower(), platform.machine())
+
+  print('running pyinstaller...')
+  pyinstaller(
+      'gl.spec', clean=True, distpath=rel, _out=sys.stdout, _err=sys.stderr)
+  print('success!! gl binary should be at {0}/gl'.format(rel))
+
+  print('creating tar.gz file')
+  shutil.copy('README.md', rel)
+  shutil.copy('LICENSE.md', rel)
+
+  with tarfile.open(rel + '.tar.gz', 'w:gz') as tar:
+    tar.add(rel)
+  print('success!! binary release at {0}'.format(rel + '.tar.gz'))
+
+  sys.exit()
+
+
+ld = """
+Gitless is an experimental version control system built on top of Git.
+Many people complain that Git is hard to use. We think the problem lies
+deeper than the user interface, in the concepts underlying Git. Gitless
+is an experiment to see what happens if you put a simple veneer on an
+app that changes the underlying concepts. Because Gitless is implemented
+on top of Git (could be considered what Git pros call a \"porcelain\" of
+Git), you can always fall back on Git. And of course your coworkers you
+share a repo with need never know that you're not a Git aficionado.
+
+More info, downloads and documentation @ `Gitless's
+website <http://gitless.com>`__.
+"""
+
+setup(
+    name='gitless',
+    version=version,
+    description='A version control system built on top of Git',
+    long_description=ld,
+    author='Santiago Perez De Rosso',
+    author_email='sperezde@csail.mit.edu',
+    url='http://gitless.com',
+    packages=['gitless', 'gitless.cli'],
+    install_requires=[
+      # make sure it matches requirements.txt
+      'pygit2==0.28.2', # requires libgit2 0.28
+      'clint>=0.3.6',
+      'sh>=1.11' if sys.platform != 'win32' else 'pbs>=0.11'
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Version Control'],
+    entry_points={
+        'console_scripts': [
+            'gl = gitless.cli.gl:main'
+        ]},
+    test_suite='gitless.tests')
