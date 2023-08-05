aee35f1e07d8b6f1b6861a2fe1c5864ca7273760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package logkit/0.3.15/logkit-0.3.15.tar.gz

---
+++ packages/logkit/logkit-0.3.15.tar.gz/logkit-0.3.15/logkit/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+__version__ = "0.3.15"
+
+import logkit.log
+from logkit.log import debug, info, warning, error, critical, increment, gauge, event, get_instance
---
+++ packages/logkit/logkit-0.3.15.tar.gz/logkit-0.3.15/logkit/log.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""
+Static functions for using the logger. The logger is supposed to be set up
+with lazy loading, and to be configured via the generated .env file.
+"""
+
+import logging
+from typing import Union
+from logkit.logger import Logger
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+
+
+def debug(message, data=None, truncated: bool=False):
+    __log_with_level(message, data, logging.DEBUG, truncated)
+
+
+def info(message, data=None, truncated: bool=False):
+    __log_with_level(message, data, logging.INFO, truncated)
+
+
+def warning(message, data=None, truncated: bool=False):
+    __log_with_level(message, data, logging.WARNING, truncated)
+
+
+def error(message, data=None, truncated: bool=False):
+    __log_with_level(message, data, logging.ERROR, truncated)
+
+
+def critical(message, data=None, truncated: bool=False):
+    __log_with_level(message, data, logging.CRITICAL, truncated)
+
+
+def with_divider(message):
+    logger = Logger.get_instance()
+    logger.write_with_divider(message)
+
+
+def increment(metric_name: str, value: Union[float, int]=1):
+    get_instance().increment(metric_name, value)
+
+
+def gauge(metric_name: str, value: Union[float, int]):
+    get_instance().gauge(metric_name, value)
+
+
+def event(title: str, text: str, data: dict):
+    get_instance().event(title, text, data)
+
+
+def get_instance():
+    return Logger.get_instance()
+
+
+def __log_with_level(message, data, level, truncated):
+    logger = Logger.get_instance()
+    logger.write(message, data, level, truncated)
