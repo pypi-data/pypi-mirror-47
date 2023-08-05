91b1506c80bdfd03f158d91ad907264d7118f4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package tcy-test2/0.0.3/tcy_test2-0.0.3.tar.gz

---
+++ packages/tcy-test2/tcy_test2-0.0.3.tar.gz/tcy_test2-0.0.3/setup.py
@@ -0,0 +1,21 @@
+﻿import setuptools
+
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tcy_test2",
+    version="0.0.3",
+    author="Tcy",
+    author_email="979442421@qq.com",
+    description="A small example package测试包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
