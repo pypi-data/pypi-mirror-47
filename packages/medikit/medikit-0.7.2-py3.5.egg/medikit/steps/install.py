f18ef9290b60ebd308ca8ceb7eba446651758d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package medikit/0.7.2/medikit-0.7.2-py3.5.egg

---
+++ packages/medikit/medikit-0.7.2-py3.5.egg/medikit/steps/install.py
@@ -0,0 +1,12 @@
+import sys
+
+from medikit.steps.exec import System
+
+
+class Install(System):
+    def __init__(self, *packages):
+        if not len(packages):
+            packages = ("pip ~=18.0", "wheel", "twine")
+        packages = list(sorted(packages))
+        super().__init__(sys.executable + " -m pip install --upgrade " + " ".join(map(repr, packages)))
+        self.__args__ = (*packages,)
