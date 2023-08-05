ee64f54c620ff84a98328ee708d4d10a687976b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:50 +0000

    Add package notechat/0.0.9.4/notechat-0.0.9.4-py3-none-any.whl

---
+++ packages/notechat/notechat-0.0.9.4-py3-none-any.whl/notechat/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/04/02 20:01
+# @Author  : niuliangtao
+# @Site    :
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
+__all__ = ['game', 'weidian']
