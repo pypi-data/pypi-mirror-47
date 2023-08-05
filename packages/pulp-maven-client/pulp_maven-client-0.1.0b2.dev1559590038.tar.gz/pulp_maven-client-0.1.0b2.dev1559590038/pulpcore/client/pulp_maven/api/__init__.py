6ef5aa5195e92c2e06219dc5b85235528556ad0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:14 +0000

    Add package pulp-maven-client/0.1.0b2.dev1559590038/pulp_maven-client-0.1.0b2.dev1559590038.tar.gz

---
+++ packages/pulp-maven-client/pulp_maven-client-0.1.0b2.dev1559590038.tar.gz/pulp_maven-client-0.1.0b2.dev1559590038/pulpcore/client/pulp_maven/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_maven.api.content_api import ContentApi
+from pulpcore.client.pulp_maven.api.distributions_api import DistributionsApi
+from pulpcore.client.pulp_maven.api.remotes_api import RemotesApi
