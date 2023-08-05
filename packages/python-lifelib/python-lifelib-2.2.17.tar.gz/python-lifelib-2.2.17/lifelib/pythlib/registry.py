98fd8945583392cedcdc20f7f5ab9f9c34c06003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:33 +0000

    Add package python-lifelib/2.2.17/python-lifelib-2.2.17.tar.gz

---
+++ packages/python-lifelib/python-lifelib-2.2.17.tar.gz/python-lifelib-2.2.17/lifelib/pythlib/registry.py
@@ -0,0 +1,49 @@
+
+registry = {}
+lastused = {}
+
+def register_pattern_callback(saveid, pattern):
+
+    registry[saveid] = pattern
+    commence_listening()
+
+def rewrite(saveid, rle):
+
+    from hashlib import sha256
+    sha = sha256(rle.encode()).hexdigest()
+
+    if (saveid in lastused) and (lastused[saveid] == sha):
+        return
+
+    lastused[saveid] = sha
+
+    x = registry[saveid] 
+    newpat = x.owner.pattern(rle, x.getrule())
+    x -= x
+    x += newpat
+
+def commence_listening():
+
+    from IPython.display import display, HTML
+
+    display(HTML('''
+    <div>
+    <p style="color:green">Press <b>Ctrl+S</b> to save LifeViewer edits.</p>
+    <script>
+    var declaredListenerYet;
+    if (!declaredListenerYet) {
+        window.addEventListener('message',function(e) {
+            var key = e.message ? 'message' : 'data';
+            var data = e[key]
+            console.log(data)
+            if (data['rle']) {
+                var rle = data['rle'].replace(/"/g, ''); // sanitise
+                var saveid = data['uuid'].replace(/"/g, '');
+                IPython.notebook.kernel.execute('import lifelib; lifelib.registry.rewrite("""' + saveid + '""", """' + rle + '""")');
+            }
+        },false);
+        declaredListenerYet = true;
+    }
+    </script>
+    </div>
+    '''))
