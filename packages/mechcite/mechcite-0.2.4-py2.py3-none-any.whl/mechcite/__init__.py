4ab1c6e0430a53d1fbf01109dda6db83cbc1dc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:55 +0000

    Add package mechcite/0.2.4/mechcite-0.2.4-py2.py3-none-any.whl

---
+++ packages/mechcite/mechcite-0.2.4-py2.py3-none-any.whl/mechcite/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for mechmat."""
+
+__author__ = """Jelle Spijker"""
+__email__ = 'spijker.jelle@gmail.com'
+__version__ = '0.2.4'
+
+from mechcite.bibliography import Bibliography, bib
+from mechcite.citation import cite
