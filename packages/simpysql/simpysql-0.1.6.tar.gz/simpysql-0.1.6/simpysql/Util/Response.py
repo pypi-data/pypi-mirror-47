cceacde7abfec3f27e3e2f46ad77eaf6ae9857d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:02 +0000

    Add package simpysql/0.1.6/simpysql-0.1.6.tar.gz

---
+++ packages/simpysql/simpysql-0.1.6.tar.gz/simpysql-0.1.6/simpysql/Util/Response.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas
+
+
+class Response(object):
+
+    def __init__(self, data):
+        self.__data__ = pandas.DataFrame(data)
+
+    def data(self):
+        return self.__data__
+
+    def tolist(self, columns):
+        return self.__data__[columns].values
