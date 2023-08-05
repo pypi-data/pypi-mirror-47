08b4e6c6b20926e4fafdddc45be0dae92c964c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:05 +0000

    Add package thirukuzhal/0.1/thirukuzhal-0.1.tar.gz

---
+++ packages/thirukuzhal/thirukuzhal-0.1.tar.gz/thirukuzhal-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='thirukuzhal',
+      version='0.1',
+      description='ThiruKuzhal Test in the world',
+      url='',
+      author='Thirumoorthy',
+      author_email='thirumoorthy.gmk@gmail.com',
+      license='Mepco',
+      packages=['thirukuzhal'],
+      zip_safe=False)
