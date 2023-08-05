4f8505c78522d63f8f4a8c3c028c1791216fd142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:54 +0000

    Add package peerplays/0.3.11/peerplays-0.3.11-py3-none-any.whl

---
+++ packages/peerplays/peerplays-0.3.11-py3-none-any.whl/peerplaysbase/chains.py
@@ -0,0 +1,32 @@
+known_chains = {
+    "ALICE": {
+        "chain_id": "6b6b5f0ce7a36d323768e534f3edb41c6d6332a541a95725b98e28d140850134",
+        "core_symbol": "PPY",
+        "prefix": "PPY",
+    },
+    "ALPHA": {
+        "chain_id": "80df3c34bff8469bdaf47e55a2fb6fb6f1eb4a49d6375aaa5689d3ca9e51bff5",
+        "core_symbol": "PPY",
+        "prefix": "PPY",
+    },
+    "BEATRICE": {
+        "chain_id": "b3f7fe1e5ad0d2deca40a626a4404524f78e65c3a48137551c33ea4e7c365672",
+        "core_symbol": "TEST",
+        "prefix": "TEST",
+    },
+    "CHARLIE": {
+        "chain_id": "3de67447ef7e7390371cdf8f2c775b4a6a9b8692ab4376942746dea73f5a37dd",
+        "core_symbol": "TEST",
+        "prefix": "TEST",
+    },
+    "DICK": {
+        "chain_id": "318cea7d376eb81a0beba3623e12af66ab76b736860ae50739d0aaf5cecb5a91",
+        "core_symbol": "TEST",
+        "prefix": "TEST",
+    },
+    "ELIZABETH": {
+        "chain_id": "8cbc8c5dcfa8ea450ae0c80e3749231334df9983c3521146f18493513bbd417d",
+        "core_symbol": "TEST",
+        "prefix": "TEST",
+    },
+}
