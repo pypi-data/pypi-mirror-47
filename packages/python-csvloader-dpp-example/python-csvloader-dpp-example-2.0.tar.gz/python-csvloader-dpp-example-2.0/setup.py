1dd2b9c171f32b95f225034b0aa58915807badf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package python-csvloader-dpp-example/2.0/python-csvloader-dpp-example-2.0.tar.gz

---
+++ packages/python-csvloader-dpp-example/python-csvloader-dpp-example-2.0.tar.gz/python-csvloader-dpp-example-2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-csvloader-dpp-example',
+    version='2.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.md').read(),
+    install_requires=['csv'],
+    url='https://git.e-science.pl/rbaszak230509/81a_RBaszak_Package',
+    author='Rafal Baszak',
+    author_email='rbaszak230509@e-science.pl'
+)
