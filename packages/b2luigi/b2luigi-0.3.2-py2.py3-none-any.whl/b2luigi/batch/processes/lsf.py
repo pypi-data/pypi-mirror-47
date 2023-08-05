deec2d541f38661c56cd0322f336c79d4a21c0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:50 +0000

    Add package b2luigi/0.3.2/b2luigi-0.3.2-py2.py3-none-any.whl

---
+++ packages/b2luigi/b2luigi-0.3.2-py2.py3-none-any.whl/b2luigi/batch/processes/lsf.py
@@ -0,0 +1,102 @@
+import json
+import re
+import subprocess
+
+from b2luigi.batch.processes import BatchProcess, JobStatus
+from b2luigi.core.utils import get_log_file_dir
+
+from cachetools import TTLCache
+
+
+class BatchJobStatusCache(TTLCache):
+    def __init__(self):
+        super(BatchJobStatusCache, self).__init__(maxsize=1000, ttl=20)
+
+    def _ask_for_job_status(self, job_id=None):
+        if job_id:
+            output = subprocess.check_output(["bjobs", "-json", "-o", "jobid stat", str(job_id)])
+        else:
+            output = subprocess.check_output(["bjobs", "-json", "-o", "jobid stat"])
+        output = output.decode()
+        output = json.loads(output)["RECORDS"]
+
+        for record in output:
+            self[record["JOBID"]] = record["STAT"]
+
+    def __missing__(self, job_id):
+        # First, ask for all jobs
+        self._ask_for_job_status(job_id=None)
+        if job_id in self:
+            return self[job_id]
+
+        # Then, ask specifically for this job
+        self._ask_for_job_status(job_id=job_id)
+        if job_id in self:
+            return self[job_id]
+
+        raise KeyError
+
+
+_batch_job_status_cache = BatchJobStatusCache()
+
+
+class LSFProcess(BatchProcess):
+    """
+    Reference implementation of the batch process for an LSF batch system.
+
+    We assume that the batch system shares a file system with the submission node you
+    are currently working on (or at least the current folder is also available there with the same path).
+    We also assume that we can run the same python interpreter there by just copying
+    the current environment and calling it from the same path.
+    Both requirements are fulfilled by a "normal" LSF setup, so you do not keep those in mind typically.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._batch_job_id = None
+
+    def get_job_status(self):
+        assert self._batch_job_id
+
+        try:
+            job_status = _batch_job_status_cache[self._batch_job_id]
+        except KeyError:
+            return JobStatus.aborted
+
+        if job_status == "DONE":
+            return JobStatus.successful
+        elif job_status == "EXIT":
+            return JobStatus.aborted
+
+        return JobStatus.running
+
+    def start_job(self):
+        prefix = ["bsub", "-env all"]
+
+        try:
+            prefix += ["-q", self.task.queue]
+        except AttributeError:
+            pass
+
+        log_file_dir = get_log_file_dir(self.task)
+        stderr_log_file = log_file_dir + "stderr"
+        stdout_log_file = log_file_dir + "stdout"
+
+        prefix += ["-eo", stderr_log_file, "-oo", stdout_log_file]
+
+        output = subprocess.check_output(prefix + self.task_cmd, env=self.task_env)
+        output = output.decode()
+
+        # Output of the form Job <72065926> is submitted to default queue <s>.
+        match = re.search(r"<[0-9]+>", output)
+        if not match:
+            raise RuntimeError("Batch submission failed with output " + output)
+
+        self._batch_job_id = match.group(0)[1:-1]
+
+    def kill_job(self):
+        if not self._batch_job_id:
+            return
+
+        subprocess.check_call(["bkill", self._batch_job_id], stdout=subprocess.DEVNULL)
---
+++ packages/b2luigi/b2luigi-0.3.2-py2.py3-none-any.whl/b2luigi/core/settings.py
@@ -0,0 +1,102 @@
+import json
+import os
+import contextlib
+
+# The global object hosting the current settings
+_current_global_settings = {}
+
+
+def get_setting(key, default=None):
+    """
+    ``b2luigi`` adds a settings management to ``luigi`` 
+    and also uses it at various places.
+
+    With this function, you can get the current value of
+    a specific setting with the given key.
+    If there is no setting defined with this name,
+    either the default is returned or, if you did not 
+    supply any default, a value error is raised.
+
+    For information on how settings are set, please 
+    see :obj:`set_setting`.
+    Settings can be of any type, but are mostly strings.
+
+    Parameters:
+        key (:obj:`str`): The name of the parameter to query.
+        default (optional): If there is no setting which the name, 
+            either return this default or if it is not set, 
+            raise a ValueError.
+    """
+    try:
+        return _current_global_settings[key]
+    except KeyError:
+        for settings_file in _setting_file_iterator():
+            try:
+                with open(settings_file, "r") as f:
+                    j = json.load(f)
+                    return j[key]
+            except KeyError:
+                pass
+
+    if default is None:
+        raise ValueError(f"No settings found for {key}!")
+    return default
+
+
+def set_setting(key, value):
+    """
+    There are two possibilities to set a setting with a given name:
+
+    * Either you use this function and supply the key and the value.
+      The setting is then defined globally for all following calls
+      to :obj:`get_setting` with the specific key.
+    * Or you add a file called ``settings.json`` the the current
+      working directory *or any folder above that*.
+      In the json file, you need to supply a key and a value for each
+      setting you want to have, e.g::
+
+        {
+            "result_path": "results",
+            "some_setting": "some_value"
+        }
+
+      By looking also in the parent folders for setting files, you can
+      define project settings in a top folder and specific settings
+      further down in your local folders.
+
+    """
+    _current_global_settings[key] = value
+
+
+def clear_setting(key):
+    """
+    Clear the setting with the given key
+    """
+    try:
+        del _current_global_settings[key]
+    except KeyError:
+        pass
+
+
+def _setting_file_iterator():
+    path = os.getcwd()
+
+    while True:
+        json_file = os.path.join(path, "settings.json")
+        if os.path.exists(json_file):
+            yield json_file
+
+        path = os.path.split(path)[0]
+        if path == "/":
+            break
+
+
+@contextlib.contextmanager
+def with_new_settings():
+    global _current_global_settings
+    old_settings = _current_global_settings.copy()
+    _current_global_settings = {}
+
+    yield
+
+    _current_global_settings = old_settings.copy()
