a7a3ca1b0dde3dbfe092866798e5b08cf06de484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:41 +0000

    Add package iscc-cli/0.3.0/iscc-cli-0.3.0.tar.gz

---
+++ packages/iscc-cli/iscc-cli-0.3.0.tar.gz/iscc-cli-0.3.0/iscc_cli/cli.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import click
+from iscc_cli import __version__, init, gen, batch, sim
+from click_default_group import DefaultGroup
+
+
+@click.group(cls=DefaultGroup, default="gen", default_if_no_args=False)
+@click.version_option(version=__version__, message="ISCC CLI - %(version)s")
+def cli():
+    pass
+
+
+cli.add_command(init.init)
+cli.add_command(gen.gen)
+cli.add_command(batch.batch)
+cli.add_command(sim.sim)
+
+
+if __name__ == "__main__":
+    cli()
