59e86b6f3f76975d0fbb41bedf6a7a5b16dd282b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package cappy239/0.0.7/cappy239-0.0.7.tar.gz

---
+++ packages/cappy239/cappy239-0.0.7.tar.gz/cappy239-0.0.7/cappy239/cappy239.py
@@ -0,0 +1,12 @@
+import numpy as np
+from math import ceil, log2, sqrt
+
+# class Cappy239(object):
+# def __init__(self): 
+#     print('Instance created...')
+
+def welcome():
+    print('Funcionando...')
+
+def welcome2():
+    print('2 Funcionando...')
