fd1eac3478619d9348554b9133ed48e60bc0caa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:38 +0000

    Add package PyShow/0.0.1/PyShow-0.0.1.tar.gz

---
+++ packages/PyShow/PyShow-0.0.1.tar.gz/PyShow-0.0.1/setup.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Ram Rachum and collaborators.
+# This program is distributed under the MIT license.
+import setuptools
+import re
+
+
+def read_file(filename):
+    with open(filename) as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='PyShow',
+    version='0.0.1',
+    author='Ram Rachum',
+    author_email='ram@rachum.com',
+    description="",
+    packages=[]
+)
