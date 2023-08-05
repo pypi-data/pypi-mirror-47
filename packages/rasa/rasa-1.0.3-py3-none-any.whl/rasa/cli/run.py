f2c6f4a44c48b3234863e89e760b77a43ebf223a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:02 +0000

    Add package rasa/1.0.3/rasa-1.0.3-py3-none-any.whl

---
+++ packages/rasa/rasa-1.0.3-py3-none-any.whl/rasa/cli/run.py
@@ -0,0 +1,70 @@
+import argparse
+import logging
+import os
+from typing import List
+
+from rasa.cli.arguments import run as arguments
+from rasa.cli.utils import get_validated_path
+from rasa.constants import (
+    DEFAULT_ACTIONS_PATH,
+    DEFAULT_CREDENTIALS_PATH,
+    DEFAULT_ENDPOINTS_PATH,
+    DEFAULT_MODELS_PATH,
+)
+
+logger = logging.getLogger(__name__)
+
+
+# noinspection PyProtectedMember
+def add_subparser(
+    subparsers: argparse._SubParsersAction, parents: List[argparse.ArgumentParser]
+):
+    run_parser = subparsers.add_parser(
+        "run",
+        parents=parents,
+        conflict_handler="resolve",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        help="Starts a Rasa server with your trained model.",
+    )
+    run_parser.set_defaults(func=run)
+
+    run_subparsers = run_parser.add_subparsers()
+    sdk_subparser = run_subparsers.add_parser(
+        "actions",
+        parents=parents,
+        conflict_handler="resolve",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        help="Runs the action server.",
+    )
+    sdk_subparser.set_defaults(func=run_actions)
+
+    arguments.set_run_arguments(run_parser)
+    arguments.set_run_action_arguments(sdk_subparser)
+
+
+def run_actions(args: argparse.Namespace):
+    import rasa_sdk.__main__ as sdk
+    import sys
+
+    args.actions = args.actions or DEFAULT_ACTIONS_PATH
+
+    # insert current path in syspath so module is found
+    sys.path.insert(1, os.getcwd())
+    path = args.actions.replace(".", os.sep) + ".py"
+    _ = get_validated_path(path, "action", DEFAULT_ACTIONS_PATH)
+
+    sdk.main_from_args(args)
+
+
+def run(args: argparse.Namespace):
+    import rasa.run
+
+    args.model = get_validated_path(args.model, "model", DEFAULT_MODELS_PATH)
+    args.endpoints = get_validated_path(
+        args.endpoints, "endpoints", DEFAULT_ENDPOINTS_PATH, True
+    )
+    args.credentials = get_validated_path(
+        args.credentials, "credentials", DEFAULT_CREDENTIALS_PATH, True
+    )
+
+    rasa.run(**vars(args))
