aa4b1e76e1a38e6dabd66a7ced8b83755596be59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:41 +0000

    Add package launchpadtools/0.7.1/launchpadtools-0.7.1.tar.gz

---
+++ packages/launchpadtools/launchpadtools-0.7.1.tar.gz/launchpadtools-0.7.1/launchpadtools/__about__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+__version__ = "0.7.1"
+__author__ = "Nico Schlömer"
+__author_email__ = "nico.schloemer@gmail.com"
+__status__ = "Development Status :: 4 - Beta"
+__license__ = "License :: OSI Approved :: MIT License"
