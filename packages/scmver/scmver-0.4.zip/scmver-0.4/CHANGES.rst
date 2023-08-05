baf4c83099e45821ee862c8be419ad75ece913bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:55 +0000

    Add package scmver/0.4/scmver-0.4.zip

---
+++ packages/scmver/scmver-0.4.zip/scmver-0.4/CHANGES.rst
@@ -0,0 +1,49 @@
+scmver Changelog
+================
+
+Version 0.4
+-----------
+
+Release date: 2019-05-31
+
+* Add functions to retrieve versions of VCSes.
+
+  * ``bazaar.version``
+  * ``git.version``
+  * ``mercurial.version``
+  * ``subversion.version``
+
+* Add ``--bzr-tag`` option to scmver script.
+* Add support for Unicode branches and tags.
+* Add support for Fossil.
+* Improve support for ``.hg_archival.txt`` of Mercurial.
+* Make tag detection of Subversion more robust.
+
+
+Version 0.3
+-----------
+
+Release date: 2019-04-01
+
+* Add support for Bazaar.
+* Improve branch detection of Git.
+* Improve change detection of Git.
+* Fix root detection of Subversion.
+
+
+Version 0.2
+-----------
+
+Release date: 2019-03-21
+
+* Add support for Subversion.
+* Do not abbreviate revisions of Git.
+* Add scmver script.
+
+
+Version 0.1
+-----------
+
+Release date: 2019-02-28
+
+* Initial release.
---
+++ packages/scmver/scmver-0.4.zip/scmver-0.4/tests/test_util.py
@@ -0,0 +1,40 @@
+#
+# test_util
+#
+#   Copyright (c) 2019 Akinori Hattori <hattya@gmail.com>
+#
+#   SPDX-License-Identifier: MIT
+#
+
+import sys
+
+from scmver import util
+from base import SCMVerTestCase
+
+
+class UtilTestCase(SCMVerTestCase):
+
+    def test_exec(self):
+        for args in (
+            (sys.executable, r'-V'),
+            (sys.executable, u'-V'),
+        ):
+            with self.subTest(args=args):
+                out, err = util.exec_(args)
+                ver = 'Python {}.{}.{}'.format(*sys.version_info)
+                if sys.version_info[0] == 2:
+                    self.assertEqual(out, '')
+                    self.assertEqual(err.strip(), ver)
+                else:
+                    self.assertEqual(out.strip(), ver)
+                    self.assertEqual(err, '')
+
+        cmd = 'import sys; getattr(sys.stdout, "buffer", sys.stdout).write(u"\\U0001d70b = 3.14".encode("utf-8"))'
+        out, err = util.exec_((sys.executable, '-c', cmd), encoding='utf-8')
+        self.assertEqual(out, u'\U0001d70b = 3.14')
+        self.assertEqual(err, '')
+
+    def test_which(self):
+        sh = 'sh' if sys.platform != 'win32' else 'cmd'
+        self.assertNotEqual(util.which(sh), sh)
+        self.assertIsNone(util.which('__scmver.util__'))
