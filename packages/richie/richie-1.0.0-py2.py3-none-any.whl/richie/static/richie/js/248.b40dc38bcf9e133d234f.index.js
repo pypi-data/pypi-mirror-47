15a81967a08267f763da4845901fabc2e95af708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:27 +0000

    Add package richie/1.0.0/richie-1.0.0-py2.py3-none-any.whl

---
+++ packages/richie/richie-1.0.0-py2.py3-none-any.whl/richie/static/richie/js/248.b40dc38bcf9e133d234f.index.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[248],{444:function(n,w){}}]);
+//# sourceMappingURL=248.b40dc38bcf9e133d234f.index.js.map
---
+++ packages/richie/richie-1.0.0-py2.py3-none-any.whl/richie/static/richie/js/249.b40dc38bcf9e133d234f.index.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[249],{445:function(n,w){}}]);
+//# sourceMappingURL=249.b40dc38bcf9e133d234f.index.js.map
