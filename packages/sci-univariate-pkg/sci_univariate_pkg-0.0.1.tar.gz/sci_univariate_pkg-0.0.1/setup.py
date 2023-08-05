3ad1966bb2647b5c1f22cc64cfd82b9aa2b59c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:27 +0000

    Add package sci-univariate-pkg/0.0.1/sci_univariate_pkg-0.0.1.tar.gz

---
+++ packages/sci-univariate-pkg/sci_univariate_pkg-0.0.1.tar.gz/sci_univariate_pkg-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sci_univariate_pkg",
+    version="0.0.1",
+    author="gurdit singh",
+    author_email="gurdit.singh@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gurditsingh/FirstPython",
+    packages=setuptools.find_packages(),
+	 install_requires=[
+        "texttable >= 1.6.1",
+	],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
