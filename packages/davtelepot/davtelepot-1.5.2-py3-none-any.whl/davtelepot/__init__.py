37d05cf4447236cb2f4c627f40a1f4401b716955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:14 +0000

    Add package davtelepot/1.5.2/davtelepot-1.5.2-py3-none-any.whl

---
+++ packages/davtelepot/davtelepot-1.5.2-py3-none-any.whl/davtelepot/__init__.py
@@ -0,0 +1,14 @@
+"""Information about this package.
+
+See custombot.py for information about Bot class.
+"""
+
+__author__ = "Davide Testa"
+__email__ = "davte@libero.it"
+__credits__ = "Marco Origlia"
+__license__ = "GNU General Public License v3.0"
+__version__ = "1.5.2"
+__maintainer__ = "Davide Testa"
+__contact__ = "t.me/davte"
+
+from davtelepot.custombot import Bot
