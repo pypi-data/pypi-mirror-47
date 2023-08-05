3c14f333dfbc741cbab3e1feba087b58898c3742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:42 +0000

    Add package TPool/1.2/TPool-1.2.tar.gz

---
+++ packages/TPool/TPool-1.2.tar.gz/TPool-1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    # get rid of the first three lines in the readme
+    long_description = "\n".join(long_description.split('\n')[3:])
+
+setuptools.setup(
+    name="TPool",
+    version="1.2",
+    author="Ahmad Alobaid",
+    author_email="aalobaid@fi.upm.es",
+    description="Thread Pool for Python 2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oeg-upm/PPool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Operating System"
+    ],
+)
