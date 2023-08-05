648120f060b4e68e8f5ba97a4a6e44a1dd73831d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:33 +0000

    Add package distributions-edx6/0.1/distributions_edx6-0.1.tar.gz

---
+++ packages/distributions-edx6/distributions_edx6-0.1.tar.gz/distributions_edx6-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_edx6',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_edx6'],
+      zip_safe=False)
