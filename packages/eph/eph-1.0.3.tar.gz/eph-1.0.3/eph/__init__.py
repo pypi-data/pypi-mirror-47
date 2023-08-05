c6a367569ec7df65cddd608fbea3eb1feb5c8549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:03 +0000

    Add package eph/1.0.3/eph-1.0.3.tar.gz

---
+++ packages/eph/eph-1.0.3.tar.gz/eph-1.0.3/eph/__init__.py
@@ -0,0 +1,7 @@
+"""eph package aims to provide useful classes, functions and tools
+to *retrieve*, *represent* and *manipulate* ephemerides.
+
+"""
+
+from .interface import JplReq, JplRes
+from .shortcuts import *
