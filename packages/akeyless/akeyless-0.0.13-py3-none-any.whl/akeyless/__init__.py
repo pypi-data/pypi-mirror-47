5ebf51e4d37220d1a1684c43845847dd50f7f7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:28 +0000

    Add package akeyless/0.0.13/akeyless-0.0.13-py3-none-any.whl

---
+++ packages/akeyless/akeyless-0.0.13-py3-none-any.whl/akeyless/__init__.py
@@ -0,0 +1,7 @@
+import akeyless.exceptions
+
+__version__ = "0.0.13"
+
+from akeyless.config import AkeylessClientConfig
+from akeyless.client import AkeylessClient
+from akeyless.admin_client import AkeylessAdminClient
