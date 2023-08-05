f1f23e6bc96ebacc44774ef0967a38a303204f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:44 +0000

    Add package dataset-mount/1.10/dataset_mount-1.10-py3-none-any.whl

---
+++ packages/dataset-mount/dataset_mount-1.10-py3-none-any.whl/dataset_mount/mount.py
@@ -0,0 +1,21 @@
+from pyspark import SparkContext
+sc =SparkContext.getOrCreate()
+
+import IPython
+dbutils = IPython.get_ipython().user_ns["dbutils"]
+
+def mount(role_arn, bucket_path, mount_path):
+    #try:
+       sc._jsc.hadoopConfiguration().set("fs.s3a.credentialsType", "AssumeRole")
+       sc._jsc.hadoopConfiguration().set("fs.s3a.stsAssumeRole.arn", role_arn)
+       print(sc._jsc.hadoopConfiguration().get("fs.s3a.stsAssumeRole.arn") == role_arn)
+       dbutils.fs.mount(
+            "s3a://{}".format(bucket_path),
+            mount_path,
+            extra_configs = {
+                "fs.s3a.credentialsType" : "AssumeRole",
+                "fs.s3a.stsAssumeRole.arn" : role_arn
+            }
+       )
+    #except:
+    #   print("already mounted")
