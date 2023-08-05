b2f6b0ee166961d9fe0abfc91ae023441471d408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package observed/0.5.3/observed-0.5.3.tar.gz

---
+++ packages/observed/observed-0.5.3.tar.gz/observed-0.5.3/setup.py
@@ -0,0 +1,39 @@
+import re
+import setuptools
+
+
+README_FILENAME = "README.md"
+VERSION_FILENAME = "observed.py"
+VERSION_RE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+
+
+# Get version information
+with open(VERSION_FILENAME, "r") as version_file:
+    mo = re.search(VERSION_RE, version_file.read(), re.M)
+
+if mo:
+    version = mo.group(1)
+else:
+    msg = "Unable to find version string in %s." % (version_file,)
+    raise RuntimeError(msg)
+
+# Get description information
+with open(README_FILENAME, "r") as description_file:
+    long_description = description_file.read()
+
+setuptools.setup(
+    name="observed",
+    version=version,
+    author="Daniel Sank",
+    author_email="sank.daniel@gmail.com",
+    description="Observer pattern for functions and bound methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanielSank/observed",
+    py_modules=["observed"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
