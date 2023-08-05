91d0ec2ac962d9aaba83e1be72648ab48525c893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:21 +0000

    Add package genewrappers/0.0.0.3/genewrappers-0.0.0.3.tar.gz

---
+++ packages/genewrappers/genewrappers-0.0.0.3.tar.gz/genewrappers-0.0.0.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="genewrappers",
+    version="0.0.0.3",
+    packages=find_packages(),
+    include_package_data=True,
+    author="Adam Koziol",
+    author_email="adam.koziol@canada.ca",
+    url="https://github.com/adamkoziol/genewrappers",
+)
