530375e5074221be2b7462bbef81fb9b44b85e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:53 +0000

    Add package paperspace/0.2.0a3/paperspace-0.2.0a3.tar.gz

---
+++ packages/paperspace/paperspace-0.2.0a3.tar.gz/paperspace-0.2.0a3/paperspace/cli/common.py
@@ -0,0 +1,70 @@
+import functools
+import getpass
+
+import click
+from click_didyoumean import DYMMixin
+from click_help_colors import HelpColorsGroup
+
+api_key_option = click.option(
+    "--apiKey",
+    "api_key",
+    help="API key to use this time only",
+)
+
+
+def del_if_value_is_none(dict_):
+    """Remove all elements with value == None"""
+    for key, val in list(dict_.items()):
+        if val is None:
+            del dict_[key]
+
+
+class ClickGroup(DYMMixin, HelpColorsGroup):
+    def group(self, *args, **kwargs):
+        aliases = []
+        _args = args
+        if args and isinstance(args[0], list):
+            # we have a list so create group aliases
+            aliases = args[0][1:]
+            _args = [args[0][0]] + list(args[1:])
+
+        if 'alias' in kwargs:
+            aliases.append(kwargs.pop('alias'))
+
+        def decorator(f):
+            cmd = click.group(*_args, **kwargs)(f)
+            self.add_command(cmd)
+            for alias in set(aliases):
+                alias_cmd = click.group(alias, **kwargs)(f)
+                self.add_command(alias_cmd)
+                alias_cmd.commands = cmd.commands
+            return cmd
+
+        return decorator
+
+
+def prompt_for_secret(prompt):
+    def callback_fun(ctx, param, value):
+        if value is None:
+            value = getpass.getpass(prompt)
+
+        return value
+
+    return callback_fun
+
+
+def deprecated(version="1.0.0"):
+    deprecated_invoke_notice = """DeprecatedWarning: \nWARNING: This command will not be included in version %s .
+For more information, please see:
+
+https://docs.paperspace.com
+If you depend on functionality not listed there, please file an issue.""" % version
+
+    def new_invoke(self, ctx):
+        click.echo(click.style(deprecated_invoke_notice, fg='red'), err=True)
+        super(type(self), self).invoke(ctx)
+
+    def decorator(f):
+        f.invoke = functools.partial(new_invoke, f)
+
+    return decorator
