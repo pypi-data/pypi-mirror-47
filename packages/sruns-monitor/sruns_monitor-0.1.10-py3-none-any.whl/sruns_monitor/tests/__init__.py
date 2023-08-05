ce79896ce262de003d0f65a6ce3006bee16e36ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:24 +0000

    Add package sruns-monitor/0.1.10/sruns_monitor-0.1.10-py3-none-any.whl

---
+++ packages/sruns-monitor/sruns_monitor-0.1.10-py3-none-any.whl/sruns_monitor/tests/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+###
+# Nathaniel Watson
+# nathanielwatson@stanfordhealthcare.org
+###
+
+import os
+
+WATCH_DIR = os.path.join(os.path.dirname(__file__), "SEQ_RUNS")
+COMPLETED_RUNS_DIR = os.path.join(os.path.dirname(__file__), "COMPLETED_RUNS")
+TMP_DIR = os.path.join(os.path.dirname(__file__), "TMP")
+DATA_DIR = os.path.join(os.path.dirname(__file__), "DATA")
