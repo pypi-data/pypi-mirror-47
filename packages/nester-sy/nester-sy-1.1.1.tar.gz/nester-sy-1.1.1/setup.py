9a1a15b73158e88952b24d7d21b2717d4874249d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:03 +0000

    Add package nester-sy/1.1.1/nester-sy-1.1.1.tar.gz

---
+++ packages/nester-sy/nester-sy-1.1.1.tar.gz/nester-sy-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester-sy',
+        version         = '1.1.1',
+        py_modules      = ['nester'],
+        author          = 'ShenY',
+        author_email    = 'sy0626@163.com',
+        url             = 'http://www.163.com',
+        description     = 'A simple printer of nested lists added tab'     
+    )
