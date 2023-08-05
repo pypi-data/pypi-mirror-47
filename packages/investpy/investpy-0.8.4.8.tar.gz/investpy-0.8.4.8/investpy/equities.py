9c3e99f1fb36c9669fe7da9947580684a5c124fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:18 +0000

    Add package investpy/0.8.4.8/investpy-0.8.4.8.tar.gz

---
+++ packages/investpy/investpy-0.8.4.8.tar.gz/investpy-0.8.4.8/investpy/equities.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+
+# Copyright 2018-2019 Alvaro Bartolome
+# See LICENSE for details.
+
+__author__ = "Alvaro Bartolome <alvarob96@usal.es>"
+
+import time
+
+import pandas as pd
+import pkg_resources
+import requests
+from lxml.html import fromstring
+
+from investpy import user_agent as ua
+
+
+def get_equity_names():
+    """
+    This function retrieves all the available equities to retrieve data from.
+    All the equities available can be found at: https://es.investing.com/equities/spain
+
+    Returns
+    -------
+        :returns a dictionary containing all the equities information
+    """
+
+    params = {
+        "noconstruct": "1",
+        "smlID": "10119",
+        "sid": "",
+        "tabletype": "price",
+        "index_id": "all"
+    }
+
+    head = {
+        "User-Agent": ua.get_random(),
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept": "text/html",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+    }
+
+    url = "https://es.investing.com/equities/StocksFilter"
+
+    req = requests.get(url, params=params, headers=head)
+
+    if req.status_code != 200:
+        raise ConnectionError("ERR#015: error " + str(req.status_code) + ", try again later.")
+
+    root_ = fromstring(req.text)
+    path_ = root_.xpath(".//table[@id='cross_rate_markets_stocks_1']"
+                        "/tbody"
+                        "/tr")
+
+    results = list()
+
+    if path_:
+        for elements_ in path_:
+            id_ = elements_.get('id').replace('pair_', '')
+
+            for element_ in elements_.xpath('.//a'):
+                tag_ = element_.get('href').replace('/equities/', '')
+                full_name_ = element_.get('title').replace(' (CFD)', '')
+
+                try:
+                    isin_ = get_isin_code(tag_)
+                except (ConnectionError, IndexError):
+                    isin_ = None
+
+                data = {
+                    "name": element_.text,
+                    "full_name": full_name_.rstrip(),
+                    "tag": tag_,
+                    "isin": isin_,
+                    "id": id_
+                }
+
+                results.append(data)
+
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'es', 'equities.csv'))
+    file = pkg_resources.resource_filename(resource_package, resource_path)
+
+    df = pd.DataFrame(results)
+    df.to_csv(file, index=False)
+
+    return results
+
+
+def get_isin_code(info):
+    """
+    This is an additional function that adds data to the equities pandas.DataFrame.
+    Added data in this case, are the ISIN codes of every company in order to identify it.
+
+    Returns
+    -------
+        :returns a str that contains the ISIN code of the specified equity
+    """
+
+    url = "https://es.investing.com/equities/" + info
+
+    head = {
+        "User-Agent": ua.get_random(),
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept": "text/html",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+    }
+
+    req = requests.get(url, headers=head, timeout=5)
+
+    if req.status_code != 200:
+        raise ConnectionError("ERR#015: error " + str(req.status_code) + ", try again later.")
+
+    root_ = fromstring(req.text)
+    path_ = root_.xpath(".//div[contains(@class, 'overViewBox')]"
+                        "/div[@id='quotes_summary_current_data']"
+                        "/div[@class='right']"
+                        "/div")
+
+    for p in path_:
+        try:
+            if p.xpath("span[not(@class)]")[0].text_content().__contains__('ISIN'):
+                try:
+                    code = p.xpath("span[@class='elp']")[0].text_content().rstrip()
+                    time.sleep(.5)
+
+                    return code
+                except IndexError:
+                    raise IndexError("ERR#017: isin code unavailable or not found.")
+            else:
+                continue
+        except IndexError:
+            raise IndexError("ERR#017: isin code unavailable or not found.")
+
+    return None
+
+
+def get_equities():
+    """
+    This function retrieves all the available equities and returns a pandas.DataFrame of them all.
+    All the available equities can be found at: https://es.investing.com/equities/spain
+
+    Returns
+    -------
+        :returns a pandas.DataFrame with all the available equities to retrieve data from
+    """
+
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'es', 'equities.csv'))
+    if pkg_resources.resource_exists(resource_package, resource_path):
+        equities = pd.read_csv(pkg_resources.resource_filename(resource_package, resource_path))
+    else:
+        equities = pd.DataFrame(get_equity_names())
+
+    if equities is None:
+        raise IOError("ERR#001: equities list not found or unable to retrieve.")
+    else:
+        return equities
+
+
+def list_equities():
+    """
+    This function retrieves all the available equities and returns a list of each one of them.
+    All the available equities can be found at: https://es.investing.com/equities/spain
+
+    Returns
+    -------
+        :returns a list with all the available equities to retrieve data from
+    """
+
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'es', 'equities.csv'))
+    if pkg_resources.resource_exists(resource_package, resource_path):
+        equities = pd.read_csv(pkg_resources.resource_filename(resource_package, resource_path))
+    else:
+        equities = pd.DataFrame(get_equity_names())
+
+    if equities is None:
+        raise IOError("ERR#001: equities list not found or unable to retrieve.")
+    else:
+        return equities['name'].tolist()
---
+++ packages/investpy/investpy-0.8.4.8.tar.gz/investpy-0.8.4.8/investpy/etfs.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+
+# Copyright 2018-2019 Alvaro Bartolome
+# See LICENSE for details.
+
+__author__ = "Alvaro Bartolome <alvarob96@usal.es>"
+
+import json
+
+import pandas as pd
+import pkg_resources
+import requests
+from lxml.html import fromstring
+
+from investpy import user_agent as ua
+
+
+def get_etf_names():
+    """
+    This function retrieves all the available etfs to retrieve data from.
+    All the available etfs available can be found at: https://es.investing.com/etfs/spain-etfs
+
+    Returns
+    -------
+        :returns a dictionary containing all the etfs information
+    """
+
+    head = {
+        "User-Agent": ua.get_random(),
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept": "text/html",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+    }
+
+    url = "https://es.investing.com/etfs/spain-etfs"
+
+    req = requests.get(url, headers=head, timeout=5)
+
+    if req.status_code != 200:
+        raise ConnectionError("ERR#015: error " + str(req.status_code) + ", try again later.")
+
+    root_ = fromstring(req.text)
+    path_ = root_.xpath(".//table[@id='etfs']"
+                        "/tbody"
+                        "/tr")
+
+    results = list()
+
+    if path_:
+        for elements_ in path_:
+            id_ = elements_.get('id').replace('pair_', '')
+            symbol = elements_.xpath(".//td[contains(@class, 'symbol')]")[0].get('title')
+
+            nested = elements_.xpath(".//a")[0]
+            info = nested.get('href').replace('/etfs/', '')
+
+            if symbol:
+                data = {
+                    "name": nested.text,
+                    "symbol": symbol,
+                    "tag": info,
+                    "id": id_
+                }
+            else:
+                data = {
+                    "name": nested.text,
+                    "symbol": "undefined",
+                    "tag": info,
+                    "id": id_
+                }
+
+            results.append(data)
+
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'es', 'etfs.csv'))
+    file = pkg_resources.resource_filename(resource_package, resource_path)
+
+    df = pd.DataFrame(results)
+    df.to_csv(file, index=False)
+
+    return results
+
+
+def get_etfs():
+    """
+    This function retrieves all the available etfs and returns a pandas.DataFrame of them all.
+    All the available etfs can be found at: https://es.investing.com/etfs/spain-etfs
+
+    Returns
+    -------
+        :returns a pandas.DataFrame with all the available etfs to retrieve data from
+    """
+
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'es', 'etfs.csv'))
+    if pkg_resources.resource_exists(resource_package, resource_path):
+        etfs = pd.read_csv(pkg_resources.resource_filename(resource_package, resource_path))
+    else:
+        etfs = pd.DataFrame(get_etf_names())
+
+    if etfs is None:
+        raise IOError("ERR#009: etf list not found or unable to retrieve.")
+    else:
+        return etfs
+
+
+def list_etfs():
+    """
+    This function retrieves all the available etfs and returns a list of each one of them.
+    All the available etfs can be found at: https://es.investing.com/etfs/spain-etfs
+
+    Returns
+    -------
+        :returns a list with all the available etfs to retrieve data from
+    """
+
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'es', 'etfs.csv'))
+    if pkg_resources.resource_exists(resource_package, resource_path):
+        etfs = pd.read_csv(pkg_resources.resource_filename(resource_package, resource_path))
+    else:
+        etfs = pd.DataFrame(get_etf_names())
+
+    if etfs is None:
+        raise IOError("ERR#009: etf list not found or unable to retrieve.")
+    else:
+        return etfs['name'].tolist()
+
+
+def dict_etfs(columns=None, as_json=False):
+    """
+    This function retrieves all the available etfs and returns a dictionary with the specified columns.
+    Available columns are: 'id', 'name', 'symbol' and 'tag'
+    All the available etfs can be found at: https://es.investing.com/etfs/spain-etfs
+
+    Returns
+    -------
+        :returns a dictionary that contains all the available etf values specified in the columns
+    """
+
+    if columns is None:
+        columns = ['id', 'name', 'symbol', 'tag']
+    else:
+        if not isinstance(columns, list):
+            raise ValueError("ERR#020: specified columns argument is not a list, it can just be list type.")
+
+    if not isinstance(as_json, bool):
+        raise ValueError("ERR#002: as_json argument can just be True or False, bool type.")
+
+    resource_package = __name__
+    resource_path = '/'.join(('resources', 'es', 'etfs.csv'))
+    if pkg_resources.resource_exists(resource_package, resource_path):
+        etfs = pd.read_csv(pkg_resources.resource_filename(resource_package, resource_path))
+    else:
+        etfs = pd.DataFrame(get_etf_names())
+
+    if etfs is None:
+        raise IOError("ERR#009: etf list not found or unable to retrieve.")
+
+    if not all(column in etfs.columns.tolist() for column in columns):
+        raise ValueError("ERR#021: specified columns does not exist, available columns are <id, name, symbol, tag>")
+
+    if as_json:
+        return json.dumps(etfs[columns].to_dict(orient='records'))
+    else:
+        return etfs[columns].to_dict(orient='records')
