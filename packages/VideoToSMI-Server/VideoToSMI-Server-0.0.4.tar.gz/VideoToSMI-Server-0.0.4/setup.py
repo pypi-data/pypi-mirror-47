5b9c8407aa4caa99f7059978e04c49433b8b26b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package VideoToSMI-Server/0.0.4/VideoToSMI-Server-0.0.4.tar.gz

---
+++ packages/VideoToSMI-Server/VideoToSMI-Server-0.0.4.tar.gz/VideoToSMI-Server-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='VideoToSMI-Server',
+      version='0.0.4',
+      url='https://github.com/Sotaneum/VideoToSMI-Server',
+      author='Donggun LEE',
+      author_email='gnyotnu39@gmail.com',
+      description='Create a smi file in Web based on the video',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      zip_safe=False,
+      setup_requires=['deepgeo','videotosmi','confighelper'],
+      classifiers=[
+          'License :: OSI Approved :: MIT License'
+      ]
+     )
