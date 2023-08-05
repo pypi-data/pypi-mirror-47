1d4c27099e0bb6ed7d17adab77452639c7da4317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:59 +0000

    Add package production_rest_client/1.9.5/production_rest_client-1.9.5.tar.gz

---
+++ packages/production_rest_client/production_rest_client-1.9.5.tar.gz/production_rest_client-1.9.5/production_rest_client/rest_client.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+# pylint: disable=wrong-import-position, relative-import
+import sys
+import os
+import requests
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+from resources.testsuite_resource import TestSuiteResource
+from resources.testcase_resource import TestCaseResource
+from resources.operation_resource import OperationResource
+from resources.state_resource import StateResource
+from resources.remote_resource import RemoteResource
+
+
+class RestClient(object):
+
+    def __init__(self, host, port=5000, time_out=5):
+        __session = requests.Session()
+        self.test_suite = TestSuiteResource(host, port, __session, time_out=time_out)
+        self.test_case = TestCaseResource(host, port, __session, time_out=time_out)
+        self.operation = OperationResource(host, port, __session, time_out=time_out)
+        self.state = StateResource(host, port, __session, time_out=time_out)
+        self.remote = RemoteResource(host, port, self.test_case, self.state)
+
+if __name__ == '__main__':
+    RC = RestClient("172.29.130.138", time_out=5)
+    # #print(RC.state.get_state())
+    # #print(RC.test_suite.get_test_lists("fio"))
+    # # print(RC.test_case.list("fio"))
+    # # print(RC.remote.run("test_fio_windows:TestFioWindows.test_rand_mix_rw_70_30", "async"))
+    # print(RC.test_case.get_async_result("9cd1r0nbc2"))
+    # print(RC.test_suite.get_async_result("9cd1r0nbc2"))
+    # # print(RC.test_case.stop_tests())
