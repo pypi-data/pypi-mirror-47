ab9121bec45405a2aeb2d0c11da2b0b491e5abb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:27 +0000

    Add package dual-probability-byEmily/0.2/dual_probability_byEmily-0.2.tar.gz

---
+++ packages/dual-probability-byEmily/dual_probability_byEmily-0.2.tar.gz/dual_probability_byEmily-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dual_probability_byEmily',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      packages=['dual_probability_byEmily'],
+      author = "Emily Xia",
+   zip_safe=False)
