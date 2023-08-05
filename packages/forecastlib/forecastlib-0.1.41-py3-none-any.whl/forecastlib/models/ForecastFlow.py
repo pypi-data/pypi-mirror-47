209530976c3b661b5bfbe70db06942f0fe2b8ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:42 +0000

    Add package forecastlib/0.1.41/forecastlib-0.1.41-py3-none-any.whl

---
+++ packages/forecastlib/forecastlib-0.1.41-py3-none-any.whl/forecastlib/models/ForecastFlow.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+class ForecastFlow(object):
+    def __init__(self, classificator, model_map):
+        self.classificator = classificator
+        self.model_map = model_map
+
+    def predict(self, x):
+        classified = self.classify(x.copy())
+
+        frames = []
+        for k, v in self.model_map.items():
+            group = classified[classified["CLASSIFICATION"] == k]
+            if not group.empty:
+                group.loc["PREDICTED"] = v.predict(group)
+                frames.append(group["PREDICTED"])
+
+        concatenated = pd.concat(frames)
+        concatenated = concatenated.sort_index(axis=0)
+
+        return concatenated
+
+    def classify(self, x: pd.DataFrame):
+        classification = self.classificator.classify(x)
+        x["CLASSIFICATION"] = classification
+        return x
