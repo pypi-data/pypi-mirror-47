55210d1d1a3ee01180826f55ebd2e98e458d232c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:50 +0000

    Add package logcolor2/0.1.6/logcolor2-0.1.6.tar.gz

---
+++ packages/logcolor2/logcolor2-0.1.6.tar.gz/logcolor2-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='logcolor2',
+    version='0.1.6',
+    author='Whilliam sun',
+    author_email='syw1894@163.com',
+    packages=['logcolor2'],
+    url='http://pypi.python.org/pypi/log_color2/',
+    license='LICENSE.txt',
+    description='test',
+    #long_description=open('README.md').read(),
+)
