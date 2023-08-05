f80ca949fc1995b9f7e0c7b51201ae97671dcaf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:30 +0000

    Add package dueros-bot/3.0.0/dueros-bot-3.0.0.tar.gz

---
+++ packages/dueros-bot/dueros-bot-3.0.0.tar.gz/dueros-bot-3.0.0/dueros/directive/Display/template/ListTemplate4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- encoding=utf-8 -*-
+
+# description:
+# author:jack
+# create_time: 2018/9/17
+
+"""
+    纵向列表模板基类
+"""
+from dueros.directive.Display.template.ListTemplate import ListTemplate
+from dueros.directive.Display.template.ListTemplateItem import ListTemplateItem
+from dueros.directive.Display.tag.HotTag import HotTag
+
+class ListTemplate4(ListTemplate):
+
+    def __init__(self):
+        super(ListTemplate4, self).__init__('ListTemplate4')
+
+
+
+if __name__ == '__main__':
+
+
+
+    pass
