fd43ac41fe61c3dad0ccd00d69d0ec373112ebc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:19 +0000

    Add package bugbug/0.0.36/bugbug-0.0.36.tar.gz

---
+++ packages/bugbug/bugbug-0.0.36.tar.gz/bugbug-0.0.36/scripts/trainer.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import lzma
+import os
+import shutil
+from logging import INFO, basicConfig, getLogger
+from urllib.request import urlretrieve
+
+from bugbug import model
+from bugbug.models import get_model_class
+
+basicConfig(level=INFO)
+logger = getLogger(__name__)
+
+BASE_URL = "https://index.taskcluster.net/v1/task/project.relman.bugbug.data_{}.latest/artifacts/public"
+
+
+class Trainer(object):
+    def decompress_file(self, path):
+        with lzma.open(f"{path}.xz", "rb") as input_f:
+            with open(path, "wb") as output_f:
+                shutil.copyfileobj(input_f, output_f)
+
+    def compress_file(self, path):
+        with open(path, "rb") as input_f:
+            with lzma.open(f"{path}.xz", "wb") as output_f:
+                shutil.copyfileobj(input_f, output_f)
+
+    def download_db(self, db_type):
+        logger.info(f"Downloading {db_type} database")
+        url = BASE_URL.format(db_type)
+        urlretrieve(f"{url}/{db_type}.json.xz", "data/{db_type}.json.xz")
+        logger.info("Decompressing {db_type} database")
+        self.decompress_file(f"data/{db_type}.json")
+
+    def go(self, model_name):
+        # Download datasets that were built by bugbug_data.
+        os.makedirs("data", exist_ok=True)
+
+        model_class = get_model_class(model_name)
+
+        if model.BugModel in model_class.__bases__:
+            self.download_db("bugs")
+
+        if model.CommitModel in model_class.__bases__:
+            self.download_db("commits")
+
+        logger.info(f"Training *{model_name}* model")
+
+        model_obj = model_class()
+        model_obj.train()
+
+        logger.info(f"Training done")
+
+        model_file_name = f"{model_name}model"
+        assert os.path.exists(model_file_name)
+        self.compress_file(model_file_name)
+
+        logger.info(f"Model compressed")
+
+
+def main():
+    description = "Train the models"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("model", help="Which model to train.")
+
+    args = parser.parse_args()
+
+    retriever = Trainer()
+    retriever.go(args.model)
