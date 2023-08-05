8d2c4365dffb57ab3d47429e4f8b96facf4566cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package shiv/0.0.48/shiv-0.0.48.tar.gz

---
+++ packages/shiv/shiv-0.0.48.tar.gz/shiv-0.0.48/README.md
@@ -0,0 +1,116 @@
+[![PyPI](https://img.shields.io/pypi/v/shiv.svg)](https://pypi.python.org/pypi/shiv)
+[![Build Status](https://travis-ci.org/linkedin/shiv.svg?branch=master)](https://travis-ci.org/linkedin/shiv)
+[![AppVeyor Status](https://ci.appveyor.com/api/projects/status/vb9yht30n0iuy4y9?svg=true)](https://ci.appveyor.com/project/sixninetynine/shiv)
+[![Coverage Status](https://coveralls.io/repos/github/linkedin/shiv/badge.svg)](https://coveralls.io/github/linkedin/shiv)
+[![Documentation Status](https://readthedocs.org/projects/shiv/badge/?version=latest)](http://shiv.readthedocs.io/en/latest/?badge=latest)
+[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
+[![Supported](https://img.shields.io/pypi/pyversions/shiv.svg)](https://pypi.python.org/pypi/shiv)
+
+![snake](https://github.com/linkedin/shiv/raw/master/logo.png)
+
+# shiv
+shiv is a command line utility for building fully self-contained Python zipapps as outlined in [PEP 441](https://www.python.org/dev/peps/pep-0441/), but with all their dependencies included!
+
+shiv's primary goal is making distributing Python applications fast & easy.
+
+Full documentation can be found [here](http://shiv.readthedocs.io/en/latest/).
+
+### sys requirements
+
+- python3.6+
+- linux/osx/windows
+
+### how to
+
+shiv has a few command line options of its own and accepts almost all options passable to `pip install`.
+
+##### simple cli example
+
+Creating an executable of pipenv with shiv:
+
+```sh
+$ shiv -c pipenv -o ~/bin/pipenv pipenv pew
+$ ~/bin/pipenv --version
+pipenv, version 2018.05.18
+```
+
+##### complex example involving a wheel cache
+
+Creating an interactive executable with a downloaded wheel of boto:
+
+```sh
+$ python3 -m pip download boto
+Collecting boto
+  File was already downloaded /tmp/tmp.iklsO1qyd3/boto-2.48.0-py2.py3-none-any.whl
+Successfully downloaded boto
+$ shiv -o boto.pyz --find-links . --no-index boto
+ shiv! 🔪
+Collecting boto
+Installing collected packages: boto
+Successfully installed boto-2.48.0
+ done
+$ ./boto.pyz
+Python 3.6.1 (default, Apr 19 2017, 21:58:41)
+[GCC 4.8.5 20150623 (Red Hat 4.8.5-4)] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+(InteractiveConsole)
+>>> import boto
+>>>
+```
+
+### installing
+
+You can install shiv via `pip` / `pypi`
+
+```sh
+pip install shiv
+```
+
+You can even create a pyz _of_ shiv _using_ shiv!
+
+```sh
+python3 -m venv .
+source bin/activate
+pip install shiv
+shiv -c shiv -o shiv shiv
+```
+
+### developing
+
+We'd love contributions! Getting bootstrapped to develop is easy:
+
+```sh
+git clone git@github.com:linkedin/shiv.git
+cd shiv
+python3 setup.py venv
+. activate
+python3 setup.py develop
+```
+
+Don't forget to run and write tests:
+
+```sh
+pip install tox
+tox
+```
+
+### gotchas
+
+Zipapps created with shiv are not guaranteed to be cross-compatible with other architectures. For example, a `pyz`
+ file built on a Mac may only work on other Macs, likewise for RHEL, etc. This usually only applies to zipapps that have C extensions in their dependencies. If all your dependencies are pure python, then chances are the `pyz` _will_ work on other platforms. Just something to be aware of.
+
+Zipapps created with shiv *will* extract themselves into `~/.shiv`, unless overridden via
+`SHIV_ROOT`. If you create many utilities with shiv, you may want to occasionally clean this
+directory.
+
+---
+
+### acknowledgements
+
+Similar projects:
+
+* [PEX](https://github.com/pantsbuild/pex)
+* [pyzzer](https://pypi.org/project/pyzzer/#description)
+* [superzippy](https://github.com/brownhead/superzippy)
+
+Logo by Juliette Carvalho
---
+++ packages/shiv/shiv-0.0.48.tar.gz/shiv-0.0.48/setup.py
@@ -0,0 +1,140 @@
+import sys
+
+if sys.version_info < (3, 6):
+    print('\nshiv requires at least Python 3.6!')
+    sys.exit(1)
+
+import os
+import re
+import setuptools
+import venv
+
+from pathlib import Path
+from setuptools.command import easy_install
+
+install_requires = [
+    'click>=6.7,!=7.0',
+    'pip>=9.0.3',
+    'setuptools',
+]
+
+extras_require = {
+    ":python_version<'3.7'": ["importlib_resources>=0.4"]
+}
+
+if int(setuptools.__version__.split('.')[0]) < 18:
+    extras_require = {}
+    if sys.version_info < (3, 7):
+        install_requires.append('importlib_resources>=0.4')
+
+# The following template and classmethod are copied from
+# fast entry points, Copyright (c) 2016, Aaron Christianson
+# https://github.com/ninjaaron/fast-entry_point
+
+TEMPLATE = '''\
+# -*- coding: utf-8 -*-
+# EASY-INSTALL-ENTRY-SCRIPT: '{3}','{4}','{5}'
+__requires__ = '{3}'
+import re
+import sys
+from {0} import {1}
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit({2}())'''
+
+
+@classmethod
+def get_args(cls, dist, header=None):
+    """Overrides easy_install.ScriptWriter.get_args
+
+    This method avoids using pkg_resources to map a named entry_point
+    to a callable at invocation time.
+    """
+    if header is None:
+        header = cls.get_header()
+    spec = str(dist.as_requirement())
+    for type_ in 'console', 'gui':
+        group = type_ + '_scripts'
+        for name, ep in dist.get_entry_map(group).items():
+            # ensure_safe_name
+            if re.search(r'[\\/]', name):
+                raise ValueError("Path separators not allowed in script names")
+            script_text = TEMPLATE.format(
+                ep.module_name,
+                ep.attrs[0],
+                '.'.join(ep.attrs),
+                spec,
+                group,
+                name,
+            )
+            args = cls._get_script_args(type_, name, header, script_text)
+            for res in args:
+                yield res
+
+# patch in the fast-entry_points classmethod
+easy_install.ScriptWriter.get_args = get_args
+
+# fast entry points end.
+
+
+class Venv(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        """Abstract method that is required to be overwritten"""
+
+    def finalize_options(self):
+        """Abstract method that is required to be overwritten"""
+
+    def run(self):
+        venv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'venv', 'shiv')
+        print('Creating virtual environment in {}'.format(venv_path))
+        venv.main(args=[venv_path])
+        print(
+            'Linking `activate` to top level of project.\n'
+            'To activate, simply run `source activate`.'
+        )
+        try:
+            os.symlink(
+                Path(venv_path, 'bin', 'activate'),
+                Path(Path(__file__).absolute().parent, 'activate'),
+            )
+        except FileExistsError:
+            pass
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='shiv',
+    version='0.0.48',
+    description="A command line utility for building fully self contained Python zipapps.",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author="Loren Carvalho",
+    author_email="lcarvalho@linkedin.com",
+    url="https://github.com/linkedin/shiv",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=install_requires,
+    extras_require=extras_require,
+    entry_points={
+        'console_scripts': [
+            'shiv = shiv.cli:main',
+            'shiv-info = shiv.info:main',
+        ],
+    },
+    include_package_data=True,
+    cmdclass={'venv': Venv},
+    license='BSD License',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
