09d79fe1f8c50261eba2bb011eab696771ae8d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package flake8-continuation/0.0.0/flake8-continuation-0.0.0.tar.gz

---
+++ packages/flake8-continuation/flake8-continuation-0.0.0.tar.gz/flake8-continuation-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="flake8-continuation",
+    version="0.0.0",
+    author="Placeholder",
+    author_email="corpeng@planet.com",
+    description="placeholder",
+    packages=setuptools.find_packages(),
+)
