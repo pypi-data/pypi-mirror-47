b3181a22fd5a6b2d7900feded4c20a697e215e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:47 +0000

    Add package smurf/2019.6.3/smurf-2019.6.3-py2.py3-none-any.whl

---
+++ packages/smurf/smurf-2019.6.3-py2.py3-none-any.whl/smurf/metadata.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of smurf. See LICENSE file for full copyright and licensing details.
+
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the 'UNIX name' for the project.
+package = 'smurf'
+project = 'string morping, for smurfing from a string to another'
+
+version = "2019.06.03"
+
+description =project
+authors = ['Erdal Yildiz']
+authors_string = ', '.join(authors)
+emails = ['erdalyildiz1974@gmail.com']
+license = 'GPL-3.0'
+copyright = '2019 ' + authors_string
+url = 'https://github.com/e7dal/smurf'
+
