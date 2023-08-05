2338995b6426d7004da258e426f33f1ffa787b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:00 +0000

    Add package vlac/0.1/vlac-0.1.tar.gz

---
+++ packages/vlac/vlac-0.1.tar.gz/vlac-0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='vlac',
+    author='Maarten Grootendorst',
+    author_email='maartengrootendorst@gmail.com',
+    url="https://github.com/MaartenGr/VLAC",
+    packages=['vlac'],
+    description='Tool for creating document features',
+    install_requires=['numpy', 'sklearn'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='0.1',
+    license='MIT',
+)
