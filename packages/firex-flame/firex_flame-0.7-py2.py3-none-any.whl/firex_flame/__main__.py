dd1fc390b74b535e7f0e9207b3734b975672ef27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:42 +0000

    Add package firex-flame/0.7/firex_flame-0.7-py2.py3-none-any.whl

---
+++ packages/firex-flame/firex_flame-0.7-py2.py3-none-any.whl/firex_flame/__main__.py
@@ -0,0 +1,77 @@
+import argparse
+import eventlet
+import logging
+import os
+import signal
+import sys
+from threading import Timer
+
+from firex_flame.main_app import run_flame
+
+logger = logging.getLogger(__name__)
+eventlet.monkey_patch()
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', help='Port for starting the web server on', type=int, default=8000)
+    parser.add_argument('--uid', help='Unique identifier for the represented FireX run.')
+    parser.add_argument('--logs_dir', help='Logs directory.', default=None)
+    parser.add_argument('--chain', help='Chain of the run.', default=None)
+    parser.add_argument('--recording', help='A file containing the recording of celery events.', default=None)
+    parser.add_argument('--central_server', help='A central web server from which the UI and logs can be served.',
+                        default=None)
+    # TODO: could validate either rec file exists or broker is supplied.
+    parser.add_argument('--broker', help='Celery broker.', default=None)
+    parser.add_argument('--timeout', help='Maximum lifetime of this service, in seconds', type=int,
+                        default=60 * 60 * 24 * 2)
+    return parser.parse_args()
+
+
+def _sigterm_handler(_, __):
+    logger.info('SIGTERM detected, shutting down')
+    sys.exit(0)
+
+
+def _exit_on_timeout():
+    logger.info("Exiting on timeout")
+    sys.exit(0)
+
+
+def _config_logging(root_logs_dir):
+    flame_logs_dir = os.path.join(root_logs_dir, 'debug', 'flame_server')
+    os.makedirs(flame_logs_dir, exist_ok=True)
+    logging.basicConfig(
+        filename=os.path.join(flame_logs_dir, 'flame.log'),
+        format='[%(asctime)s][%(levelname)s][%(name)s]: %(message)s',
+        level=logging.DEBUG,
+    )
+    # This module is very noisy (logs all data sent), so turn up the level.
+    logging.getLogger('engineio.server').setLevel(logging.WARNING)
+
+
+def _create_run_metadata(cli_args):
+    return {
+        'uid': cli_args.uid,
+        'logs_dir': cli_args.logs_dir,
+        'central_server': cli_args.central_server,
+        'chain': cli_args.chain,
+        'central_documentation_url': 'http://www.firexapp.com/',
+    }
+
+
+def main():
+    args = _parse_args()
+    _config_logging(args.logs_dir)
+
+    signal.signal(signal.SIGTERM, _sigterm_handler)
+    t = Timer(args.timeout, _exit_on_timeout)
+    try:
+        t.start()
+        logger.info('Starting Flame Server with args: %s' % args)
+        run_flame(args.broker, args.port, _create_run_metadata(args), args.recording)
+        t.cancel()
+    except Exception as e:
+        logger.exception(e)
+    finally:
+        logger.info("Shutting down Flame server.")
---
+++ packages/firex-flame/firex_flame-0.7-py2.py3-none-any.whl/firex_flame/api.py
@@ -0,0 +1,102 @@
+"""
+Flask API module for interacting with celery tasks.
+"""
+
+import time
+import logging
+
+from flask import jsonify
+
+from firex_flame.event_aggregator import slim_tasks_by_uuid, INCOMPLETE_STATES
+
+logger = logging.getLogger(__name__)
+
+
+def create_socketio_task_api(sio_server, event_aggregator, run_metadata):
+
+    @sio_server.on('send-graph-state')
+    def emit_frontend_tasks_by_uuid(sid):
+        """ Send the full state."""
+        sio_server.emit('graph-state', slim_tasks_by_uuid(event_aggregator.tasks_by_uuid), room=sid)
+
+    @sio_server.on('send-graph-fields')
+    def emit_task_fields_by_uuid(sid, fields):
+        """ Send the requested fields for all tasks."""
+        response = {uuid: {f: v for f, v in task.items() if f in fields}
+                    for uuid, task in event_aggregator.tasks_by_uuid.items()}
+        sio_server.emit('graph-fields', response, room=sid)
+
+    @sio_server.on('send-run-metadata')
+    def emit_run_metadata(sid):
+        """ Get static run-level data."""
+        response = {
+            'uid': run_metadata['uid'],
+            'logs_dir': run_metadata['logs_dir'],
+            'root_uuid': event_aggregator.root_uuid,
+            'chain': run_metadata['chain'],
+            'centralServer': run_metadata['central_server'],
+            'central_documentation_url': run_metadata['central_documentation_url'],
+        }
+        sio_server.emit('run-metadata', response, room=sid)
+
+    @sio_server.on('send-task-details')
+    def emit_detailed_tasks(sid, uuids):
+        """Get the desired task structure
+
+        Arguments:
+            sid: The session ID to use.
+            uuid(str): The uuid of the desired task to get details for.
+        """
+        if isinstance(uuids, str):
+            uuid = uuids
+            response = event_aggregator.tasks_by_uuid.get(uuid, None)
+            sio_server.emit('task-details-' + uuid, response, room=sid)
+        else:
+            if not isinstance(uuids, list):
+                response = []
+            else:
+                response = [event_aggregator.tasks_by_uuid.get(u, None) for u in uuids]
+            sio_server.emit('task-details', response, room=sid)
+
+
+def create_rest_task_api(web_app, tasks):
+
+    @web_app.route('/api/tasks')
+    def all_tasks_by_uuid():
+        return jsonify(slim_tasks_by_uuid(tasks))
+
+
+def create_revoke_api(sio_server, celery_app, tasks):
+
+    @sio_server.on('revoke-task')
+    def socket_revoke_task(sid, uuid):
+        response = _revoke_task(uuid)
+        socket_event = 'revoke-success' if response else 'revoke-failed'
+        sio_server.emit(socket_event, room=sid)
+
+    def _wait_task_complete(task):
+        check_timeout = 5
+        check = 1
+        while task['state'] in INCOMPLETE_STATES:
+            time.sleep(1)
+            if check > check_timeout:
+                break
+            else:
+                check += 1
+
+    def _revoke_task(uuid):
+        if uuid not in tasks:
+            return False
+
+        # Get the task instance
+        task = tasks[uuid]
+
+        # Try to revoke the task
+        if task['state'] in INCOMPLETE_STATES:
+            celery_app.control.revoke(uuid, terminate=True)
+
+        # Wait for the task to become revoked
+        _wait_task_complete(task)
+
+        # If the task was successfully revoked, return true
+        return task['state'] == 'task-revoked'
