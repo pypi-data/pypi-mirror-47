ef0bca519095bafeeeec6861f387d34461f11add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package hackoregon-sandbox/0.1.21/hackoregon_sandbox-0.1.21.tar.gz

---
+++ packages/hackoregon-sandbox/hackoregon_sandbox-0.1.21.tar.gz/hackoregon_sandbox-0.1.21/api/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from api import views
+from api.views import AllSweepsV02ViewSet
+
+
+router = DefaultRouter()
+router.register(r'Tags', views.TagViewSet)
+router.register(r'Layers', views.LayerViewSet)
+router.register(r'Packages', views.PackageViewSet)
+router.register(r'AllSweepsV02', AllSweepsV02ViewSet)
+router.register(r'RlisNeighborhoods', views.RlisNeighborhoodsViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
