d8b2a27b99ca086ffded4903fc83bf99b9f41ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:39 +0000

    Add package hashdial/1.0.3/hashdial-1.0.3.tar.gz

---
+++ packages/hashdial/hashdial-1.0.3.tar.gz/hashdial-1.0.3/README.rst
@@ -0,0 +1,12 @@
+hashdial
+========
+
+Installation
+------------
+
+``pip install hashdial``
+
+Usage
+-----
+
+See `the documentation <http://py-hashdial.readthedocs.io/en/latest/>`__.
---
+++ packages/hashdial/hashdial-1.0.3.tar.gz/hashdial-1.0.3/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from os import path
+
+from setuptools import find_packages
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="hashdial",
+    version="1.0.3",
+    description="A module implementing hash based decision making",
+    long_description="""Implements, through hashing, decision making that is deterministic on input, but probabilistic across a set of inputs.
+
+For example, suppose a set of components in a distributed system wish to emit a log entry for 1% of requests - but each
+component should log the *same* 1% of requests, they could do so as such::
+
+    if hashdial.decide(request.id, 0.01):
+        log_request(request)
+
+Take a look at `the documentation <http://py-hashdial.readthedocs.io/en/latest/>`__ for more.
+""",  # noqa
+    long_description_content_type="text/x-rst",
+    url="https://github.com/scode/py-hashdial",
+    author="Peter Schuller",
+    author_email="peter.schuller@infidyne.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords="hashdial hashing decision",
+    packages=find_packages(exclude=["docs", "scripts", "tests"]),
+    python_requires=">3.4",
+    install_requires=[
+        "typing ; python_version<'3.5'",
+    ],
+    extras_require={
+        # see requirements-dev.in/requirements-dev.txt
+    },
+    project_urls={
+        "Documentation": "http://py-hashdial.readthedocs.io/en/latest/",
+        "Source": "https://github.com/scode/py-hashdial",
+    },
+)
