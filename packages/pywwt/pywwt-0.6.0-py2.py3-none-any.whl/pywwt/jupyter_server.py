5c0b3c390156ebd5624f5d863e2649577ce7037a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package pywwt/0.6.0/pywwt-0.6.0-py2.py3-none-any.whl

---
+++ packages/pywwt/pywwt-0.6.0-py2.py3-none-any.whl/pywwt/jupyter_server.py
@@ -0,0 +1,81 @@
+import os
+import json
+import mimetypes
+from hashlib import md5
+from tornado import web
+from notebook.utils import url_path_join
+from notebook.base.handlers import IPythonHandler
+
+__all__ = ['load_jupyter_server_extension']
+
+
+STATIC_DIR = os.path.join(os.path.dirname(__file__), 'nbextension', 'static')
+CONFIG = os.path.expanduser('~/.pywwt')
+
+
+class WWTFileHandler(IPythonHandler):
+
+    def get(self, filename):
+
+        filename = os.path.basename(filename)
+
+        # First we check if this is a standard file in the static directory
+        if os.path.exists(os.path.join(STATIC_DIR, filename)):
+            path = os.path.join(STATIC_DIR, filename)
+        else:
+            # If not, we open the config file which should contain a JSON
+            # dictionary with filenames and paths.
+            if not os.path.exists(CONFIG):
+                raise web.HTTPError(404)
+            with open(CONFIG) as f:
+                config = json.load(f)
+            if filename in config['paths']:
+                path = config['paths'][filename]
+            else:
+                raise web.HTTPError(404)
+
+        # Do our best to set an appropriate Content-Type.
+        self.set_header('Content-Type', mimetypes.guess_type(filename)[0])
+
+        with open(path, 'rb') as f:
+            content = f.read()
+
+        self.finish(content)
+
+
+def serve_file(path, extension=''):
+
+    if not os.path.exists(path):
+        raise ValueError("Path {0} does not exist".format(path))
+
+    hash = md5(path.encode('utf-8')).hexdigest() + extension
+
+    with open(CONFIG) as f:
+        config = json.load(f)
+
+    if hash not in config['paths']:
+
+        config['paths'][hash] = os.path.abspath(path)
+
+        with open(CONFIG, 'w') as f:
+            json.dump(config, f)
+
+    return '/wwt/' + hash
+
+
+def load_jupyter_server_extension(nb_server_app):
+
+    web_app = nb_server_app.web_app
+    host_pattern = '.*$'
+
+    if not os.path.exists(CONFIG):
+        config = {'paths': {}}
+        with open(CONFIG, 'w') as f:
+            json.dump(config, f)
+
+    mimetypes.add_type('image/fits','.fits')
+    mimetypes.add_type('image/fits','.fts')
+    mimetypes.add_type('image/fits','.fit')
+
+    route_pattern = url_path_join(web_app.settings['base_url'], '/wwt/(.*)')
+    web_app.add_handlers(host_pattern, [(route_pattern, WWTFileHandler)])
