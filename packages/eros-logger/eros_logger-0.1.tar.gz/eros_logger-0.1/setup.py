7b07c4a11946ae9216bb7438e0dba9dc04b7515e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:20 +0000

    Add package eros-logger/0.1/eros_logger-0.1.tar.gz

---
+++ packages/eros-logger/eros_logger-0.1.tar.gz/eros_logger-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eros_logger',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/sagarborse/logger',
+      author='Sagar Borse',
+      author_email='sagarborse90@gmail.com',
+      license='MIT',
+      packages=['logger'],
+      zip_safe=False)
