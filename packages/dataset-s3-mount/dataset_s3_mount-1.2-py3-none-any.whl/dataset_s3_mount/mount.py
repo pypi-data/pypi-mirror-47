9fbf633e157390cf2b1ce7924c1302943c88c77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package dataset-s3-mount/1.2/dataset_s3_mount-1.2-py3-none-any.whl

---
+++ packages/dataset-s3-mount/dataset_s3_mount-1.2-py3-none-any.whl/dataset_s3_mount/mount.py
@@ -0,0 +1,19 @@
+class dataset:
+
+    def __init_(self):
+        pass
+    def mount(role_arn, bucket_path, mount_path):
+        try:
+            sc._jsc.hadoopConfiguration().set("fs.s3a.credentialsType", "AssumeRole")
+            sc._jsc.hadoopConfiguration().set("fs.s3a.stsAssumeRole.arn", role_arn)
+            print(sc._jsc.hadoopConfiguration().get("fs.s3a.stsAssumeRole.arn") == role_arn)
+            dbutils.fs.mount(
+                "s3a://{}".format(bucket_path),
+                mount_path,
+                extra_configs = {
+                    "fs.s3a.credentialsType" : "AssumeRole",
+                    "fs.s3a.stsAssumeRole.arn" : role_arn
+                }
+            )
+        except:
+            print("already mounted")
