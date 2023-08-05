a780d2480d7d4b055c5248a5f8b14d88a875231d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:43 +0000

    Add package aimmo/0.21.1b4168/aimmo-0.21.1.b4168.tar.gz

---
+++ packages/aimmo/aimmo-0.21.1.b4168.tar.gz/aimmo-0.21.1.b4168/aimmo/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from django.http import JsonResponse
+
+
+class GameSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    status = serializers.CharField(max_length=1)
+    settings = serializers.SerializerMethodField("get_settings_as_dict")
+
+    def get_settings_as_dict(self, game):
+        return game.settings_as_dict()
