07591ff9f69101bb3128b7ebf9aa11ef1d2ca730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:09 +0000

    Add package gwf/1.5.1/gwf-1.5.1.tar.gz

---
+++ packages/gwf/gwf-1.5.1.tar.gz/gwf-1.5.1/src/gwf/cli.py
@@ -0,0 +1,109 @@
+import logging
+import os
+import os.path
+from pkg_resources import iter_entry_points
+
+import click
+from click_plugins import with_plugins
+
+from . import __version__
+from .conf import config
+from .backends import list_backends
+from .exceptions import ConfigurationError
+from .utils import ColorFormatter, ensure_dir
+
+logger = logging.getLogger(__name__)
+
+
+BASIC_FORMAT = "%(message)s"
+
+ADVANCED_FORMAT = "%(levelname)s:%(name)s:%(message)s"
+
+LOGGING_FORMATS = {
+    "warning": BASIC_FORMAT,
+    "info": BASIC_FORMAT,
+    "debug": ADVANCED_FORMAT,
+    "error": BASIC_FORMAT,
+}
+
+VERBOSITY_LEVELS = ["warning", "debug", "info", "error"]
+
+
+def get_level(level):
+    return getattr(logging, level.upper())
+
+
+def configure_logging(level_name, formatter_cls):
+    fmt = LOGGING_FORMATS[level_name]
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter_cls(fmt=fmt))
+
+    root = logging.getLogger()
+    root.addHandler(handler)
+    root.setLevel(get_level(level_name))
+
+
+def _validate_choice(key, value, choices):
+    if value not in choices:
+        msg = 'Invalid value "{}" for key "{}", must be one of: {}.'
+        raise ConfigurationError(msg.format(value, key, ", ".join(choices)))
+
+
+@config.validator("backend")
+def validate_backend(value):
+    return _validate_choice("backend", value, list_backends())
+
+
+@config.validator("verbose")
+def validate_verbose(value):
+    return _validate_choice("verbose", value, VERBOSITY_LEVELS)
+
+
+@config.validator("no_color")
+def validate_no_color(value):
+    choices = ("true", "yes", "false", "no")
+    if value not in (True, False):
+        msg = 'Invalid value "{}" for key "no_color", must be one of: {}.'
+        raise ConfigurationError(msg.format(value, ", ".join(choices)))
+
+
+@with_plugins(iter_entry_points("gwf.plugins"))
+@click.group(context_settings={"obj": {}})
+@click.version_option(version=__version__)
+@click.option("-f", "--file", default="workflow.py:gwf", help="Workflow/obj to load.")
+@click.option(
+    "-b",
+    "--backend",
+    type=click.Choice(list_backends()),
+    default=config["backend"],
+    help="Backend used to run workflow.",
+)
+@click.option(
+    "-v",
+    "--verbose",
+    type=click.Choice(VERBOSITY_LEVELS),
+    default=config["verbose"],
+    help="Verbosity level.",
+)
+@click.option(
+    "--no-color/--use-color",
+    default=config["no_color"],
+    help="Enable or disable output colors.",
+)
+@click.pass_context
+def main(ctx, file, backend, verbose, no_color):
+    """A flexible, pragmatic workflow tool.
+
+    See help for each command using the `--help` flag for that command:
+
+        gwf status --help
+
+    Shows help for the status command.
+    """
+    ensure_dir(os.path.join(".gwf"))
+
+    formatter_cls = logging.Formatter if no_color else ColorFormatter
+    configure_logging(level_name=verbose, formatter_cls=formatter_cls)
+
+    ctx.obj = {"file": file, "backend": backend}
---
+++ packages/gwf/gwf-1.5.1.tar.gz/gwf-1.5.1/src/gwf/conf.py
@@ -0,0 +1,84 @@
+import json
+from collections import ChainMap
+
+
+CONFIG_DEFAULTS = {"verbose": "info", "backend": "local", "no_color": False}
+
+
+class FileConfig:
+    def __init__(self, path, data=None):
+        self.path = path
+
+        self._data = ChainMap({}, data)
+        self._validators = {}
+
+    def validator(self, key):
+        """Register a configuration key validator function."""
+
+        def _inner(func):
+            self._validators[key] = func
+
+        return _inner
+
+    def _validate_value(self, key, value):
+        if key in self._validators:
+            self._validators[key](value)
+
+    def get(self, key, default=None):
+        value = self._data.get(key, default)
+        self._validate_value(key, value)
+        return value
+
+    def __getitem__(self, key):
+        value = self._data[key]
+        self._validate_value(key, value)
+        return value
+
+    def __setitem__(self, key, value):
+        self._validate_value(key, value)
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        del self._data[key]
+
+    def __len__(self):
+        return len(self._data)
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def dump(self):
+        """Dump the configuration to disk."""
+        with open(self.path, "w") as config_file:
+            json.dump(dict(self._data), config_file, indent=4, sort_keys=True)
+
+    @classmethod
+    def load(cls, path, data=None):
+        """Load configuration from a file.
+
+        Reads configuration from `file` and returns a :class:`Config` instance
+        with the configuration. The `defaults` will be merged into the
+        configuration.
+
+        :param path str: Path to the configuration file.
+        :param defaults dict: A set of defaults to merge into the configuration.
+        """
+        try:
+            with open(path) as config_file:
+                file_config = json.load(config_file)
+        except FileNotFoundError:
+            file_config = {}
+
+        if data is None:
+            data = {}
+        else:
+            data = dict(data)
+        data.update(file_config)
+        return cls(path=path, data=data)
+
+
+def config_from_path(path):
+    return FileConfig.load(path, data=CONFIG_DEFAULTS)
+
+
+config = config_from_path(".gwfconf.json")
