4eb321473b03ab83e575582c34cdbafb717e026c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package urlifyhelper/0.2/urlifyhelper-0.2.tar.gz

---
+++ packages/urlifyhelper/urlifyhelper-0.2.tar.gz/urlifyhelper-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='urlifyhelper',
+    version='0.2',
+    author='KamilKujawa',
+    packages=['urlifyhelper'],
+    url='https://github.com/KamilKujawa/example-lib',
+    license='MIT',
+)
