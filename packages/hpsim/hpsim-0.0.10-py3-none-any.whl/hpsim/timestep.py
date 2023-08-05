7c576d24bcf466d6474142bc8ee54c121ea98ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:33 +0000

    Add package hpsim/0.0.10/hpsim-0.0.10-py3-none-any.whl

---
+++ packages/hpsim/hpsim-0.0.10-py3-none-any.whl/hpsim/timestep.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 19:36:44 2019
+
+@author: hogbobson
+"""
+
+class timestep:
+    def __init__(self):
+        pass
+        
+    def constant_dt(value):
+        return value
+    
+    def courant():
+        pass
