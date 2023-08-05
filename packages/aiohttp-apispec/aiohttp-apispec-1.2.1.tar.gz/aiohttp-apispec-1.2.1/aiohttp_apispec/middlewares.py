c43cd8352f9c485205a763092a243a19253120b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package aiohttp-apispec/1.2.1/aiohttp-apispec-1.2.1.tar.gz

---
+++ packages/aiohttp-apispec/aiohttp-apispec-1.2.1.tar.gz/aiohttp-apispec-1.2.1/aiohttp_apispec/middlewares.py
@@ -0,0 +1,40 @@
+from aiohttp import web
+
+from .utils import issubclass_py37fix
+
+
+@web.middleware
+async def validation_middleware(request: web.Request, handler) -> web.Response:
+    """
+    Validation middleware for aiohttp web app
+
+    Usage:
+
+    .. code-block:: python
+
+        app.middlewares.append(validation_middleware)
+
+
+    """
+    orig_handler = request.match_info.handler
+    if not hasattr(orig_handler, "__schemas__"):
+        if not issubclass_py37fix(orig_handler, web.View):
+            return await handler(request)
+        sub_handler = getattr(orig_handler, request.method.lower(), None)
+        if sub_handler is None:
+            return await handler(request)
+        if not hasattr(sub_handler, "__schemas__"):
+            return await handler(request)
+        schemas = sub_handler.__schemas__
+    else:
+        schemas = orig_handler.__schemas__
+    kwargs = {}
+    for schema in schemas:
+        data = await request.app["_apispec_parser"].parse(
+            schema["schema"], request, locations=schema["locations"]
+        )
+        if data:
+            kwargs.update(data)
+    kwargs.update(request.match_info)
+    request[request.app["_apispec_request_data_name"]] = kwargs
+    return await handler(request)
