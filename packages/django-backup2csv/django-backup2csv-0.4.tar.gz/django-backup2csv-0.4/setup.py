36ca647907c6befd1b26120f75c0fdc84198cb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:11 +0000

    Add package django-backup2csv/0.4/django-backup2csv-0.4.tar.gz

---
+++ packages/django-backup2csv/django-backup2csv-0.4.tar.gz/django-backup2csv-0.4/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-backup2csv',
+    version='0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A simple Django app to backup and restore your models data.',
+    long_description=README,
+    url='https://gitlab.com/mmandrille/django-backup2csv',
+    author='Marcos Federico Mandrille',
+    author_email='mmandrille@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
