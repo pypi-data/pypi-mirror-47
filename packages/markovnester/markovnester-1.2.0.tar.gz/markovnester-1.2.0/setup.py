2d5cfbd3d4e7a53817510e11347dbf27396f6376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:02 +0000

    Add package markovnester/1.2.0/markovnester-1.2.0.tar.gz

---
+++ packages/markovnester/markovnester-1.2.0.tar.gz/markovnester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'markovnester',
+	version		= '1.2.0',
+	py_modules	= ['markovnester'],
+	author		= 'markovojvodic84',
+	author_email	= 'markovojvodic84@gmail.com',
+	url		= 'http://ludvigcomputers.com',
+	description	= 'A simple printer of nested lists',
+     )
