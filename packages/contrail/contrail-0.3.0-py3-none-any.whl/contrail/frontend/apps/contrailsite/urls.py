fbf70467de29e500e677bdd0f938731afdfeaf31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:23 +0000

    Add package contrail/0.3.0/contrail-0.3.0-py3-none-any.whl

---
+++ packages/contrail/contrail-0.3.0-py3-none-any.whl/contrail/frontend/apps/contrailsite/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('price/', price_view, name='price'),
+    path('instance/', instance_view, name='instance'),
+    path('historygraph/', history_graph_view, name='historygraph'),
+    path('help/', HelpView.as_view(), name='help'),
+    path('storage/', storage_view, name='storage'),
+]
