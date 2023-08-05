9e30c6feb3b5b6b87b931d74513fa4dc4ba1cea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:48 +0000

    Add package Mastodon.py/1.4.3/Mastodon.py-1.4.3.tar.gz

---
+++ packages/Mastodon.py/Mastodon.py-1.4.3.tar.gz/Mastodon.py-1.4.3/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+test_deps = [
+    'pytest', 
+    'pytest-runner', 
+    'pytest-cov', 
+    'vcrpy', 
+    'pytest-vcr', 
+    'pytest-mock', 
+    'requests-mock'
+]
+
+webpush_deps = [
+    'http_ece>=1.0.5',
+    'cryptography>=1.6.0',
+]
+
+extras = {
+      "test": test_deps + webpush_deps,
+      "webpush": webpush_deps,
+}
+
+setup(name='Mastodon.py',
+      version='1.4.3',
+      description='Python wrapper for the Mastodon API',
+      packages=['mastodon'],
+      install_requires=[
+          'requests>=2.4.2', 
+          'python-dateutil', 
+          'six', 
+          'pytz',
+          'python-magic',
+          'decorator>=4.0.0', 
+          'blurhash>=1.1.3',
+      ],
+      tests_require=test_deps,
+      extras_require=extras,
+      url='https://github.com/halcy/Mastodon.py',
+      author='Lorenz Diener',
+      author_email='lorenzd+mastodonpypypi@gmail.com',
+      license='MIT',
+      keywords='mastodon api microblogging',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Communications',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
