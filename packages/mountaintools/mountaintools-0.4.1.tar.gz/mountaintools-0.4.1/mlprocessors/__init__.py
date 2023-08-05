d567feb360badfca454b48b11e7ed99bb656c749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:11 +0000

    Add package mountaintools/0.4.1/mountaintools-0.4.1.tar.gz

---
+++ packages/mountaintools/mountaintools-0.4.1.tar.gz/mountaintools-0.4.1/mlprocessors/__init__.py
@@ -0,0 +1,19 @@
+from .core import *
+from .registry import *
+from .validators import *
+from .executebatch import executeBatch, configComputeResource, configComputeResources
+from .computeresourceserver import ComputeResourceServer
+from .computeresourceclient import ComputeResourceClient
+from .mountainjob import MountainJob
+from .shellscript import ShellScript
+from .localcomputeresource import LocalComputeResource
+from .temporarydirectory import TemporaryDirectory
+
+__all__ = [
+    "Input", "Output",
+    "Parameter", "StringParameter", "IntegerParameter", "FloatParameter",
+    "Processor",
+    "registry", "register_processor", "ProcessorRegistry",
+    "Validator", "ValueValidator", "RegexValidator", "FileExtensionValidator", "FileExistsValidator",
+    "executeBatch", "MountainJob"
+]
---
+++ packages/mountaintools/mountaintools-0.4.1.tar.gz/mountaintools-0.4.1/mlprocessors/execute.py
@@ -0,0 +1,17 @@
+from .createjobs import createJob
+import mtlogging
+
+
+def execute(
+    proc,
+    _container=None,
+    _use_cache=True,
+    _skip_failing=None,
+    _force_run=None,
+    _keep_temp_files=None,
+    _label=None,
+    **kwargs
+):
+    job = createJob(proc, _container=_container, _use_cache=_use_cache, _skip_failing=_skip_failing, _force_run=_force_run, _keep_temp_files=_keep_temp_files, _label=_label, _verbose=False, **kwargs)
+    result = job.execute()
+    return result
