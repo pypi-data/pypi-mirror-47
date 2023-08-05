e9b5c01bcf597c72f7fa2815df0f449f84d82680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:46 +0000

    Add package m-stats/0.1.2/m_stats-0.1.2.tar.gz

---
+++ packages/m-stats/m_stats-0.1.2.tar.gz/m_stats-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+  name = 'm_stats',
+  packages = ['m_stats'],
+  version = '0.1.2',
+  license='MIT',
+  description = 'A statistics and probability library for Python',
+  author = 'Miguel Angel Velez',
+  author_email = '',
+  url = 'https://github.com/mvelezg99/m_stats',
+  download_url = 'https://github.com/mvelezg99/m_stats/archive/0.1.2.tar.gz',
+  keywords = ['Python', 'statistics', 'probability', 'm_stats'],
+  install_requires=[
+          'numpy',
+          'm_stats',
+          'scipy',
+          'matplotlib'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
