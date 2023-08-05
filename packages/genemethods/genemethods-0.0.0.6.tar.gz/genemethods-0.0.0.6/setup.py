e4c02b59d83fc9cac0ea5ba9a7d9494920fe323c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:47 +0000

    Add package genemethods/0.0.0.6/genemethods-0.0.0.6.tar.gz

---
+++ packages/genemethods/genemethods-0.0.0.6.tar.gz/genemethods-0.0.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="genemethods",
+    version="0.0.0.6",
+    packages=find_packages(),
+    author="Adam Koziol",
+    author_email="adam.koziol@canada.ca",
+    url="https://github.com/OLC-Bioinformatics/genemethods",
+)
