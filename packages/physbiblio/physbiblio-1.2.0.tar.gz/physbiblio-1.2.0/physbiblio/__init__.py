c19f8753613edafcdbbf62c19b3b5bad379e9570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:08 +0000

    Add package physbiblio/1.2.0/physbiblio-1.2.0.tar.gz

---
+++ packages/physbiblio/physbiblio-1.2.0.tar.gz/physbiblio-1.2.0/physbiblio/__init__.py
@@ -0,0 +1,28 @@
+"""The package that contains all the modules used by the PhysBiblio,
+including the `gui` subpackage.
+
+This file is part of the physbiblio package.
+"""
+__author__ = "Stefano Gariazzo"
+__email__ = "stefano.gariazzo@gmail.com"
+
+__version__ = "1.2.0"
+__version_date__ = "04/06/2019"
+
+__all__ = [
+    "bibtexWriter",
+    "cli",
+    "config",
+    "databaseCore",
+    "database",
+    "errors",
+    "export",
+    "inspireStats",
+    "parseAccents",
+    "pdf",
+    "tablesDef",
+    "view",
+    "tests",
+    "gui",
+    "webimport",
+]
