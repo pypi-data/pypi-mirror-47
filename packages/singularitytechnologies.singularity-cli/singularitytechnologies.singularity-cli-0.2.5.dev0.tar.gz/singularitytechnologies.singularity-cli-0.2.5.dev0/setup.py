6bf58a6042cc5c1ffeac713a2fccb53b80b5553d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:57 +0000

    Add package singularitytechnologies.singularity-cli/0.2.5.dev0/singularitytechnologies.singularity-cli-0.2.5.dev0.tar.gz

---
+++ packages/singularitytechnologies.singularity-cli/singularitytechnologies.singularity-cli-0.2.5.dev0.tar.gz/singularitytechnologies.singularity-cli-0.2.5.dev0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+from singularity import __version__
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='singularitytechnologies.singularity-cli',
+    version=__version__,
+    author='Sam Lacey',
+    author_email='sam.lacey@singularity-technologies.io',
+    license='MIT',
+    description='A CLI to run common singularity tasks.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/singularitydigitaltechnologies/singularity-cli',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'asyncio',
+        'docopt',
+        'singularitytechnologies.singularitytechnologiesapi==0.2.4dev'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    entry_points='''
+        [console_scripts]
+        singularity-cli=singularity.singularity:main
+    ''',
+)
