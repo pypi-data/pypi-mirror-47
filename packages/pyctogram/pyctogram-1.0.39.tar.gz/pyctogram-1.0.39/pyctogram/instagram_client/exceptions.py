a60ce7c7645db12478ca36290796f480452c68d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:40 +0000

    Add package pyctogram/1.0.39/pyctogram-1.0.39.tar.gz

---
+++ packages/pyctogram/pyctogram-1.0.39.tar.gz/pyctogram-1.0.39/pyctogram/instagram_client/exceptions.py
@@ -0,0 +1,72 @@
+class InstagramException(BaseException):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __repr__(self):
+        return str(self.msg)
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class InstagramNoneResponse(InstagramException):
+    pass
+
+
+class InstagramNot2XX(InstagramException):
+    def __init__(self, msg, status_code):
+        super().__init__(msg)
+        self.status_code = status_code
+
+    def __repr__(self):
+        return str(f'{self.msg} (status = {self.status_code}')
+
+
+class InstagramWrongJsonStruct(InstagramException):
+    def __init__(self):
+        super().__init__('wrong json structure')
+
+
+class InstagramEmptyBody(InstagramException):
+    def __init__(self):
+        super().__init__('empty body')
+
+
+class InstagramNotJson(InstagramException):
+    pass
+
+
+class InstagramFailer(InstagramException):
+    pass
+
+
+class InstagramDidntChangeTheStatus(InstagramException):
+    pass
+
+
+class InstagramUserRestricred(InstagramNot2XX):
+    user_restricted_msg = 'user restricted'
+
+
+class InstagramSpamDetected(InstagramNot2XX):
+    feedback_required_message = 'feedback_required'
+
+
+class InsragramCheckpointRequired(InstagramNot2XX):
+    checkpoint_required_message = 'checkpoint_required'
+
+    def __init__(self, msg, status_code, checkpoint_url):
+        super().__init__(msg, status_code)
+        self.checkpoint_url = checkpoint_url
+
+
+class InstagramChallengeRequired(InstagramNot2XX):
+    challenge_required_message = 'challenge_required'
+
+    def __init__(self, msg, status_code, challenge_url):
+        super().__init__(msg, status_code)
+        self.challenge_url = challenge_url
+
+
+class VideoTooShort(RuntimeError):
+    pass
