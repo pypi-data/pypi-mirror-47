c159b0b2901f2d866510f643c6a1109cdc741e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:21 +0000

    Add package fiveoneone/0.5.0/fiveoneone-0.5.0.tar.gz

---
+++ packages/fiveoneone/fiveoneone-0.5.0.tar.gz/fiveoneone-0.5.0/setup.py
@@ -0,0 +1,36 @@
+"""
+Five One One
+-------------
+
+A python API to consume transit data from http://511.org.
+"""
+
+from setuptools import setup
+
+requirements = ("requests==2.22.0", )
+
+setup(
+    name='fiveoneone',
+    version='0.5.0',
+    license='MIT',
+    author='Ramiro Berrelleza',
+    author_email='rberrelleza@gmail.com',
+    description='A python API to consume transit data from http://511.org.',
+    url='https://github.com/rberrelleza/511-transit',
+    packages=['fiveoneone'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=requirements,
+    entry_points={
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
