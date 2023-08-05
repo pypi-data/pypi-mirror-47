1275f4768b835bd8ded664c39744ae983fc4b54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:29 +0000

    Add package distributions-mmbader/1.0/distributions_mmbader-1.0.tar.gz

---
+++ packages/distributions-mmbader/distributions_mmbader-1.0.tar.gz/distributions_mmbader-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_mmbader',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_mmbader'],
+      author='Michael Bader',
+      zip_safe=False)
