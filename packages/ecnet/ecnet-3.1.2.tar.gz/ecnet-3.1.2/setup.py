390a69f3f1868d8fadc304c2b8941bfb55dbab16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:00 +0000

    Add package ecnet/3.1.2/ecnet-3.1.2.tar.gz

---
+++ packages/ecnet/ecnet-3.1.2.tar.gz/ecnet-3.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ecnet',
+    version='3.1.2',
+    description='UMass Lowell Energy and Combustion Research Laboratory Neural'
+                ' Network Software',
+    url='http://github.com/tjkessler/ecnet',
+    author='Travis Kessler, Hernan Gelaf-Romer, Sanskriti Sharma',
+    author_email='travis.j.kessler@gmail.com,'
+                 ' Hernan_Gelafromer@student.uml.edu,'
+                 ' Sanskriti_Sharma@student.uml.edu',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'colorlogging',
+        'ditto-lib',
+        'ecabc',
+        'keras',
+        'numpy',
+        'pubchempy',
+        'pyyaml',
+        'sklearn',
+        'tensorflow'
+    ],
+    package_data={
+        'ecnet': [
+            'tools/PaDEL-Descriptor/*',
+            'tools/PaDEL-Descriptor/lib/*',
+            'tools/PaDEL-Descriptor/license/*'
+        ]
+    },
+    zip_safe=False
+)
