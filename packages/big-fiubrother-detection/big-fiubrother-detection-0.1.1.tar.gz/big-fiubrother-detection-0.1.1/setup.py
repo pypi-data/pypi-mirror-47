d701e2d31f00c734066e72fafcd3ad75e35673cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:21 +0000

    Add package big-fiubrother-detection/0.1.1/big-fiubrother-detection-0.1.1.tar.gz

---
+++ packages/big-fiubrother-detection/big-fiubrother-detection-0.1.1.tar.gz/big-fiubrother-detection-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='big-fiubrother-detection',
+    version='0.1.1',
+    description='Big Fiubrother Face Detection application',
+    license="GPLv3",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    scripts=[],
+    author='Eduardo Neira, Gabriel Gayoso',
+    author_email='gabriel.gayoso@fi.uba.ar',
+    packages=['big_fiubrother_detection'],
+    url= 'https://github.com/BigFiuBrother/big-fiubrother-detection',
+    install_requires=['opencv-python', 'numpy', 'caffe'],
+    include_package_data=True,
+)
