577c66853ca548cf06e08b57120fc5eaaa746362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:28 +0000

    Add package silver-surfer-models/0.0.23.dev0/silver_surfer_models-0.0.23.dev0-py3-none-any.whl

---
+++ packages/silver-surfer-models/silver_surfer_models-0.0.23.dev0-py3-none-any.whl/silver_surfer_models/resources/InstitutionProbability/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Text,
+    DateTime,
+    UniqueConstraint, Boolean, Float)
+from datetime import datetime
+
+from ...database import Base
+
+
+class InstitutionProbabilityModel(Base):
+    __tablename__ = 'institution_probabilities'
+
+    id = Column(Integer, primary_key=True)
+
+    ptab_trial_num = Column(String(190), unique=True, nullable=False)
+    is_instituted = Column(Boolean)
+    probability_of_institution = Column(Float)
+    probability_of_no_institution = Column(Float)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
