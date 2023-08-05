2d2a64518eb74fb913603e13cb7d933709be334d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:21 +0000

    Add package treelab/1.2.2/treelab-1.2.2-py3-none-any.whl

---
+++ packages/treelab/treelab-1.2.2-py3-none-any.whl/treelab/config.py
@@ -0,0 +1,21 @@
+# # server
+workspace_id = 'wspf490d665de0ca450'
+# core 1
+core_id_1 = 'creff094b41b80a5020'
+view_id_1 = 'viwff095168e78ac334'
+table_id_1 = 'tblff094df96e00ba05'
+# core 2
+core_id_2 = 'creff094b41b80a5020'
+view_id_2 = 'viwff095abf730e2885'
+table_id_2 = 'tblff095808748c28df'
+
+# # local
+# workspace_id = 'wspb580a4b6028b19f5'
+# # core 1
+# core_id_1 = 'creb580a4b622831199'
+# view_id_1 = 'viwb580a4b6a6068005'
+# table_id_1 = 'tblb580a4b6688459f2'
+# # core 2
+# core_id_2 = 'creb580a4b622831199'
+# view_id_2 = 'viwb580a4b74902a242'
+# table_id_2 = 'tblb580a4b70d8cf590'
---
+++ packages/treelab/treelab-1.2.2-py3-none-any.whl/treelab/rxmq_treelab/channel.py
@@ -0,0 +1,56 @@
+from rx import Observable
+from rx import operators as op
+from rx.core import ConnectableObservable
+from rx.subjects import ReplaySubject
+import time
+
+from treelab.rxmq_treelab.rxmq_utils import compare_topics, find_subject_by_name
+
+"""
+Rxmq channel class
+"""
+
+
+class Channel:
+    def __init__(self):
+        self.subjects = {}
+        self.channel_bus: ReplaySubject = ReplaySubject()
+        self.channel_stream: ConnectableObservable = self.channel_bus.pipe(op.publish(),
+                                                                           op.replay(), op.ref_count())
+
+    def subject(self, name: str) -> ReplaySubject:
+        """
+        subject not from wait
+        :param name:
+        :return:
+        """
+        subj = find_subject_by_name(self.subjects, name)
+        if not subj:
+            subj = ReplaySubject()
+            subj.name = name
+            self.subjects[name] = subj
+            self.channel_bus.on_next(subj)
+        return subj
+
+    def subject_wait(self, name: str) -> ReplaySubject:
+        """
+        from wait
+        :param name:
+        :return:
+        """
+        subj = find_subject_by_name(self.subjects, name)
+        while not subj:
+            time.sleep(0.1)
+            subj = find_subject_by_name(self.subjects, name)
+        return subj
+
+    def observe(self, name: str) -> Observable:
+        """
+        Get an Observable for specific set of topics
+        :param name:
+        :return:
+        """
+        if all([c not in name for c in ['#', '*']]):
+            return self.subject_wait(name)
+
+        return self.channel_stream.pipe(op.filter(lambda obs: compare_topics(obs.name, name)), op.merge_all())
