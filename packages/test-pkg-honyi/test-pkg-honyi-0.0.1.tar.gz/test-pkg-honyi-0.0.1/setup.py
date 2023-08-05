2b469f336b77511f6d898e9d7ea9b9d9ccdbbfd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:45 +0000

    Add package test-pkg-honyi/0.0.1/test-pkg-honyi-0.0.1.tar.gz

---
+++ packages/test-pkg-honyi/test-pkg-honyi-0.0.1.tar.gz/test-pkg-honyi-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test-pkg-honyi",
+    version="0.0.1",
+    author="honyi",
+    author_email="18201245243@163.com",
+    description="A test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/supeanut/test_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
