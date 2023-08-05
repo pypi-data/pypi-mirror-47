0507e79b98eb3e7a1b520c3225bd0c736871e668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:47 +0000

    Add package geohashcx/0.1.3/geohashcx-0.1.3-py2.py3-none-any.whl

---
+++ packages/geohashcx/geohashcx-0.1.3-py2.py3-none-any.whl/geohash/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Geohash."""
+
+__author__ = """Aldrin Navarro"""
+__email__ = "aldrinnavarro16@gmail.com"
+__version__ = "__version__ = '0.1.3'"
+name = "geohash"
+
+from .geohash import geohash_decode, geohash_encode
