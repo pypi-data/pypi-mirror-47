1bf7e44546e9287b00a317c12263cfd962d4e4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:06 +0000

    Add package ezhashlib/1.1.0/ezhashlib-1.1.0.tar.gz

---
+++ packages/ezhashlib/ezhashlib-1.1.0.tar.gz/ezhashlib-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import os
+this_directory = os.getcwd()
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='ezhashlib',
+      version='1.1.0',
+      description='Convenience wrapper around the hashlib library',
+      long_description_content_type="text/markdown",
+      long_description=long_description,
+      url='https://github.com/joshm12345/ezhashlib',
+      author='Josh Morrow',
+      author_email='joshmorrow12@icloud.com',
+      license='MIT',
+      packages=['ezhashlib'],
+      zip_safe=False)
