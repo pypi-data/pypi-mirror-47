b8afb77fa31bb2fa8e506154e5c4be338622a404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:34 +0000

    Add package abc123abc/0.0.0/abc123abc-0.0.0.tar.gz

---
+++ packages/abc123abc/abc123abc-0.0.0.tar.gz/abc123abc-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="abc123abc",
+    version="0.0.0",
+    author="Placeholder",
+    author_email="corpeng@planet.com",
+    description="placeholder",
+    packages=setuptools.find_packages(),
+)
