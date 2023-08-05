c0b41d6d7003e45df86ac328857605866c0dd074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:05 +0000

    Add package distributions-gau-bin/0.1/distributions_gau_bin-0.1.tar.gz

---
+++ packages/distributions-gau-bin/distributions_gau_bin-0.1.tar.gz/distributions_gau_bin-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_gau_bin',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gau_bin'],
+      zip_safe=False)
