c9a93c1e830036d4a41e40ccfd958924d54ebc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package keras-unet/0.0.5/keras-unet-0.0.5.tar.gz

---
+++ packages/keras-unet/keras-unet-0.0.5.tar.gz/keras-unet-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='keras-unet',
+      version='0.0.5',
+      description='U-Net model for semantic segmentation implementation in Keras + useful utility functions for semantic segmentation tasks',
+      url='http://github.com/karolzak/keras-unet',
+      author='Karol Zak',
+      author_email='karol.zak@hotmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires = ['keras'])
