bb5446e04bc8e03403a57dccab8271d0fd06bf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package wagtail-guide/1.0.4/wagtail_guide-1.0.4.tar.gz

---
+++ packages/wagtail-guide/wagtail_guide-1.0.4.tar.gz/wagtail_guide-1.0.4/setup.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env python
+"""
+Installs the Wagtail Guide plugin.
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='wagtail_guide',
+      version='1.0.4',
+      description='Adds functionality to add and edit a CMS guide page for editors.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/kevinhowbrook/wagtailguide',
+      author='Kevin Howbrook - Torchbox',
+      author_email='kevin.howbrook@torchbox.com',
+      license='MIT',
+      packages=find_packages(exclude=['tests*']),
+      include_package_data=True,
+      install_requires=[
+          'wagtail>=2.4',
+      ])
---
+++ packages/wagtail-guide/wagtail_guide-1.0.4.tar.gz/wagtail_guide-1.0.4/wagtail_guide/wagtail_hooks.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
+from wagtail.admin.menu import MenuItem
+from wagtail.core import hooks
+
+from . import views
+
+
+@hooks.register('register_admin_urls')
+def register_admin_urls():
+    return [
+        url(r'^editorguide/$', views.index, name='wagtaileditorguide'),
+    ]
+
+
+@hooks.register('register_admin_menu_item')
+def register_editor_guide_menu_item():
+    return MenuItem(
+        _('Editor guide'),
+        reverse('wagtaileditorguide'),
+        classnames='icon icon-help',
+        order=1000
+    )
+
+
+@hooks.register("insert_global_admin_css", order=100)
+def global_admin_css():
+    """Add /static/css/wagtailguide.css to the wagtail admin area."""
+    return format_html('<link rel="stylesheet" href="{}">', static("css/wagtailguide.css"))
