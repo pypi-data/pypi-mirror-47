9f40a23e814bb6f0fc83ea05ecc4d81eadcad145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:31 +0000

    Add package rpi-automator/0.0.4/rpi_automator-0.0.4.tar.gz

---
+++ packages/rpi-automator/rpi_automator-0.0.4.tar.gz/rpi_automator-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rpi_automator",
+    version="0.0.4",
+    author="Ryan Aviles",
+    author_email="ryan.aviles@gmail.com",
+    description="Raspberry Pi automation tool",
+    long_description="rpi_automator",
+    url="https://github.com/raviles/rpi_automator",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    scripts=[
+        'bin/rpi_automator'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Other OS"
+    ],
+)
