a679e433585a3ec7e371b309eb08b4f47595e981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:44 +0000

    Add package LMIPy/0.1.19/LMIPy-0.1.19.tar.gz

---
+++ packages/LMIPy/LMIPy-0.1.19.tar.gz/LMIPy-0.1.19/LMIPy/__init__.py
@@ -0,0 +1,9 @@
+from .lmipy import Vocabulary, Metadata, Widget
+from .layer import Layer
+from .dataset import Dataset
+from .geometry import Geometry
+from .collection import Collection
+from .table import Table
+from pkg_resources import get_distribution
+
+__version__ = get_distribution('LMIPy').version
