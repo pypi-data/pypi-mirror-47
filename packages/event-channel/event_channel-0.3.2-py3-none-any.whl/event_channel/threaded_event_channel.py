b31cff8ba203790191705ef0c50f92eb62371c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:03 +0000

    Add package event-channel/0.3.2/event_channel-0.3.2-py3-none-any.whl

---
+++ packages/event-channel/event_channel-0.3.2-py3-none-any.whl/event_channel/threaded_event_channel.py
@@ -0,0 +1,25 @@
+import threading
+
+from .event_channel import EventChannel
+
+
+class ThreadedEventChannel(EventChannel):
+    def __init__(self):
+        super(ThreadedEventChannel, self).__init__()
+
+    def publish(self, event, *args, **kwargs):
+        threads = []
+        if event in self.subscribers.keys():
+            for callback in self.subscribers[event]:
+                threads.append(threading.Thread(
+                  target=callback,
+                  args=args,
+                  kwargs=kwargs
+                ))
+            for th in threads:
+                th.start()
+
+            for th in threads:
+                th.join()
+
+channel = ThreadedEventChannel()
