8085f06a8cec3bd600212cc44f1ee2d6d92bed71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:43 +0000

    Add package pyhasse.fuzzy/0.1.1/pyhasse.fuzzy-0.1.1-py2.py3-none-any.whl

---
+++ packages/pyhasse.fuzzy/pyhasse.fuzzy-0.1.1-py2.py3-none-any.whl/pyhasse/fuzzy/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+""" Module
+
+acm -- Analysis of conflicts 
+(Antichain: a subset of the set of objects, mutually incomparable) .
+"""
+
+from pyhasse.fuzzy.calc import Fuzzy
+
+name = "pyhasse.fuzzy"
+
+__author__ = 'Rainer Bruggemann'
+__email__ = 'rainer.bruggemann@pyhasse.org'
+__version__ = '0.1.1'
