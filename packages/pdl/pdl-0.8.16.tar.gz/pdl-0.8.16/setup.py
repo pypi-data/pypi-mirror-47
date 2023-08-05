9dc4ddedd911640cfdceeba65150e3a7a2db3d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package pdl/0.8.16/pdl-0.8.16.tar.gz

---
+++ packages/pdl/pdl-0.8.16.tar.gz/pdl-0.8.16/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pdl",
+    version="0.8.16",
+    author="Zero to singularity",
+    author_email="jan@zerotosingularity.com",
+    install_requires=['requests>=2.18.4'],
+    url="https://github.com/zerotosingularity/pdl",
+    description="Python Download Lib",
+    long_description="Easily download and explore public datasets.",
+    license="MIT",
+    packages=['pdl']
+)
