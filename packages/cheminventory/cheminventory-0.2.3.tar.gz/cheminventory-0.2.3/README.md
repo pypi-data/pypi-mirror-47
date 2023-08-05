42d3edad24a860eba4c3e36e4cbf77e7549ec16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:26 +0000

    Add package cheminventory/0.2.3/cheminventory-0.2.3.tar.gz

---
+++ packages/cheminventory/cheminventory-0.2.3.tar.gz/cheminventory-0.2.3/README.md
@@ -0,0 +1,23 @@
+# ChemInventory
+
+[![PyPI version](https://badge.fury.io/py/cheminventory.svg)](https://pypi.org/project/cheminventory/)
+
+An unofficial python SDK for [ChemInventory](https://www.cheminventory.net/). ChemInventory is used for keeping track of chemicals in a laboratory.
+
+## Usage
+
+```python
+ci = ChemInventory(email='joe@none.com', password='123456')
+#Search for a CAS number
+ci.search('75-65-0')
+```
+
+For more details look at the [tutorial](tutorial/tutorial.ipynb).
+
+## Installation
+
+`pip install cheminventory`
+
+## Authors
+
+This is an unofficial cheminventory package. ChemInventory and its namesake are not owned by me. 
---
+++ packages/cheminventory/cheminventory-0.2.3.tar.gz/cheminventory-0.2.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+setuptools.setup(
+    name="cheminventory",
+    version="0.2.3",
+    url="https://github.com/marcosfelt/cheminventory_python",
+
+    author="Kobi Felton",
+    author_email="kobi.c.f@gmail.com",
+
+    description="A CLI and SDK for ChemInventory",
+    # long_description_content_type='text/markdown',
+    # long_description=open('README.md').read(),
+    long_description="See Github",
+    packages=setuptools.find_packages(),
+
+    install_requires=['requests'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
