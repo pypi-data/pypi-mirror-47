43f47a9a4c9e518c9b04d825c41743e174a8045c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:27 +0000

    Add package joony-Nester/0.0.2/joony_Nester-0.0.2.tar.gz

---
+++ packages/joony-Nester/joony_Nester-0.0.2.tar.gz/joony_Nester-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name	= 'joony_Nester',
+	version	= '0.0.2',
+	py_modules= ['joony_Nester'],
+	author	= 'joony',
+	author_email = 'joony@narrowsoft.net',
+	url 	= 'http://www.narrowsoft.net',
+	description = 'A simple printer of nested lists',
+	)
