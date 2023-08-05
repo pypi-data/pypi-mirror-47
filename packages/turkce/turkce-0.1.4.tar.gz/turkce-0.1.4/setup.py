fa58bb1c32fdafd67c69e9261b261b77b304d574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:46 +0000

    Add package turkce/0.1.4/turkce-0.1.4.tar.gz

---
+++ packages/turkce/turkce-0.1.4.tar.gz/turkce-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'turkce',
+    packages = ['.'],
+    version = '0.1.4',
+    description = 'turkceAI',
+    author = '@talhatarik',
+    author_email = 'ttarikkucuk@gmail.com',
+    url = 'https://github.com/turkceAI/python',
+    download_url = 'https://codeload.github.com/turkceAI/python/tar.gz/master',
+    keywords = ['turkce', 'nlp'],
+    classifiers = [],
+)
