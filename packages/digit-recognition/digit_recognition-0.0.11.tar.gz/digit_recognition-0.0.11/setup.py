4f83663e18a78379d8774388696880c963c8223b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:01 +0000

    Add package digit-recognition/0.0.11/digit_recognition-0.0.11.tar.gz

---
+++ packages/digit-recognition/digit_recognition-0.0.11.tar.gz/digit_recognition-0.0.11/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="digit_recognition",
+    version="0.0.11",
+    py_modules=['digit_recognition'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Maximilian Mittenbuhler",
+    author_email="max.mittenbuhler@student.uva.nl",
+    description="Neural network for digit recognition",
+    url="https://github.com/Mittenbuhler/Neural_Network.git",
+    install_requires=['numpy', 'idx2numpy', 'urllib3', 'Pillow', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'install_network = digit_recognition:install_network',
+            'digit_recognition = digit_recognition:run_gui'
+        ]
+    },
+)
+
+# 'numpy', 'urllib3', , 'PIL', 'idx2numpy'
