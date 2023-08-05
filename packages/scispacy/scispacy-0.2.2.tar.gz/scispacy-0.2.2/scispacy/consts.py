fcebfaf5d5fe8d3810bf96c371dfc5b38123311f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:25 +0000

    Add package scispacy/0.2.2/scispacy-0.2.2.tar.gz

---
+++ packages/scispacy/scispacy-0.2.2.tar.gz/scispacy-0.2.2/scispacy/consts.py
@@ -0,0 +1,19 @@
+from typing import List
+
+ABBREVIATIONS: List[str] = ["sec.",
+                            "secs.",
+                            "Sec.",
+                            "Secs.",
+                            "fig.",
+                            "figs.",
+                            "Fig.",
+                            "Figs.",
+                            "eq.",
+                            "eqs.",
+                            "Eq.",
+                            "Eqs.",
+                            "no.",
+                            "nos.",
+                            "No.",
+                            "Nos.",
+                            "al."]
