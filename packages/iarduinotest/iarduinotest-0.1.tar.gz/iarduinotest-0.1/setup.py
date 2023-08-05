da811dbddd55497a638952ebb964be49a58970d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:36 +0000

    Add package iarduinotest/0.1/iarduinotest-0.1.tar.gz

---
+++ packages/iarduinotest/iarduinotest-0.1.tar.gz/iarduinotest-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+        name = 'iarduinotest',
+        packages = ['iarduinotest'],
+        version = '0.1',
+        description = 'test lib for future exploration',
+        author = 'Dron Distortion',
+        author_email = 'dron@b0ga.net',
+        url = 'https://github.com/drondistortion/iarduinotest',
+        download_url = 'https://github.com/drondistortion/iarduinotest/tarball/0.1',
+        keywords = ['test', 'python', 'iarduino'],
+        classifiers = [],
+)
