ed928cc3e02a54f04ee83a3c0a7525f52891ca95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:09 +0000

    Add package voila/0.0.13/voila-0.0.13.tar.gz

---
+++ packages/voila/voila-0.0.13.tar.gz/voila-0.0.13/voila/__init__.py
@@ -0,0 +1,17 @@
+#############################################################################
+# Copyright (c) 2018, Voila Contributors                                    #
+#                                                                           #
+# Distributed under the terms of the BSD 3-Clause License.                  #
+#                                                                           #
+# The full license is in the file LICENSE, distributed with this software.  #
+#############################################################################
+
+from ._version import __version__  # noqa
+
+
+def _jupyter_nbextension_paths():
+    return [dict(
+        section="notebook",
+        src="static",
+        dest="voila",
+        require="voila/nbextension")]
