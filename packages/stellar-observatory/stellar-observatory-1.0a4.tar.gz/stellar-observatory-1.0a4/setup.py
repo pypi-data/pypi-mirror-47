15d7cadf436a83e880b8b095177c6477928d1571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:31 +0000

    Add package stellar-observatory/1.0a4/stellar-observatory-1.0a4.tar.gz

---
+++ packages/stellar-observatory/stellar-observatory-1.0a4.tar.gz/stellar-observatory-1.0a4/setup.py
@@ -0,0 +1,27 @@
+"""setup"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="stellar-observatory",
+    version="1.0a4",
+    author="André Gaul",
+    author_email="andre@gaul.io",
+    description="Python package for analyzing the Stellar network",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrenarchy/stellar-observatory",
+    packages=setuptools.find_packages(),
+    python_requires="~=3.5",
+    install_requires=[
+        "requests~=2.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research"
+    ],
+)
