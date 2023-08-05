3dda3c78c7961f8a1cf53739c55f90f093b4f237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:29 +0000

    Add package wordweaver/0.0.1/wordweaver-0.0.1.tar.gz

---
+++ packages/wordweaver/wordweaver-0.0.1.tar.gz/wordweaver-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import wordweaver
+
+setup(
+    name='wordweaver',
+    python_requires='>=3.6',
+    version=wordweaver.VERSION,
+    long_description='WordWeaver',
+    packages=['wordweaver',
+              'wordweaver.buildtools',
+              'wordweaver.static',
+              'wordweaver.fst',
+              'wordweaver.fst.utils',
+              'wordweaver.resources'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['flask', 'flask_restful', 'flask_cors']
+)
