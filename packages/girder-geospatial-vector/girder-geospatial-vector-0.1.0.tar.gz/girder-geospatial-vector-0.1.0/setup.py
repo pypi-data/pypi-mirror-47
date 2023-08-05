5897b8194c443e93ab9d80346aad966e87f4d6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:31 +0000

    Add package girder-geospatial-vector/0.1.0/girder-geospatial-vector-0.1.0.tar.gz

---
+++ packages/girder-geospatial-vector/girder-geospatial-vector-0.1.0.tar.gz/girder-geospatial-vector-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='girder-geospatial-vector',
+    version='0.1.0',
+    description='Support for vector data types in the girder-geospatial package',
+    url='https://github.com/OpenGeoscience/girder_geospatial',
+    maintainer='Kitware, Inc.',
+    maintainer_email='kitware@kitware.com',
+    entry_points={
+        'geometa.types': [
+            'vector=geometa_vector.schema:handler'
+        ]
+    },
+    packages=[
+        'geometa_vector'
+    ],
+    install_requires=[
+        'girder-geospatial'
+    ]
+)
