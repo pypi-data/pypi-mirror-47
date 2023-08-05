9ccabee422cd54f96583d6107bf17120376f5a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:07 +0000

    Add package opedia/0.2.71/opedia-0.2.71.tar.gz

---
+++ packages/opedia/opedia-0.2.71.tar.gz/opedia-0.2.71/opedia/climatology.py
@@ -0,0 +1,20 @@
+import sys
+sys.dont_write_bytecode = True
+import db
+
+def isClimatology(tableName, varName=None):
+    res = db.isClimatology(tableName)
+    '''
+    query = "SELECT Climatology FROM tblVariables "
+    query = query + "JOIN tblDatasets ON [tblVariables].Dataset_ID=[tblDatasets].ID "    
+    query = query + "WHERE Table_Name='%s' AND Short_Name='%s' "    
+    query = query % (tableName, varName)
+    df = db.dbFetch(query)
+    res = df.Climatology[0]
+    '''
+    return 
+
+def timeToMonth(dt):
+    # assumption: dt is string and has the following format:  YYYY-MM-dd ....
+    month = int(dt.split('-')[1])
+    return month
