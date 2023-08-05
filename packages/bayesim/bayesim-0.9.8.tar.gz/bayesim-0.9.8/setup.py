0b04f0edc718cd529e3f91dece74512b9ca18d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:37 +0000

    Add package bayesim/0.9.8/bayesim-0.9.8.tar.gz

---
+++ packages/bayesim/bayesim-0.9.8.tar.gz/bayesim-0.9.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+setup(name='bayesim',
+      version='0.9.8',
+      description='Fast model fitting via Bayesian inference',
+      author='Rachel Kurchin, Giuseppe Romano',
+      author_email='rkurchin@mit.edu, romanog@mit.edu',
+      classifiers=['Programming Language :: Python :: 3.6'],
+      long_description=open('README.rst').read(),
+      install_requires=['pandas',
+                        'deepdish',
+                        'numpy',
+                        'scipy',
+                        'matplotlib',
+                        'joblib'
+                         ],
+      dependency_links=['https://github.com/slwatkins/deepdish/tarball/master#egg=deepdish-0.3.4'],
+      license='GPLv2',
+      packages = ['bayesim'],
+      entry_points = {
+     'console_scripts': [
+      'bayesim=bayesim.__main__:main'],
+      },
+      zip_safe=False)
