7aa8f376857b67f577dce9dc6ba2cd9434ed48b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:02 +0000

    Add package NonCipher/1.0/NonCipher-1.0.tar.gz

---
+++ packages/NonCipher/NonCipher-1.0.tar.gz/NonCipher-1.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'NonCipher',
+  packages = ["NonCipher"],
+  version = '1.0',
+  license='Apache 2.0',
+  description = 'XOR Algorithm with hashing, wow!',
+  author = 'NonSense',
+  author_email = 'valerastatilko@gmail.com',
+  url = 'https://github.com/NotStatilko/NonCipher',
+  download_url = 'https://github.com/NotStatilko/NonCipher/archive/1.0.tar.gz',
+  keywords = ['Python', 'Cipher', 'Hashing', 'XOR'],
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Security :: Cryptography',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ]
+)
