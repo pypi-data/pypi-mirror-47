07643d499e019fc8113e7be08f3ab46c2b8f9e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:24 +0000

    Add package quaerere-base-common/0.1.0/quaerere-base-common-0.1.0.tar.gz

---
+++ packages/quaerere-base-common/quaerere-base-common-0.1.0.tar.gz/quaerere-base-common-0.1.0/quaerere_base_common/schema.py
@@ -0,0 +1,24 @@
+__all__ = ['BaseSchema']
+
+from marshmallow import Schema, post_load
+
+
+class BaseSchema(Schema):
+    """Base class for use in schema'd objects
+
+    Subclasses :any:Schema from marshmallow and provides """
+    model_class = None
+
+    @post_load
+    def make_model(self, data):
+        """Return an instance of :py:attr:model_class
+
+        If :py:attr:model_class is :any:None, the data passed is
+        returned unchanged
+
+        :param data: Instance data
+        :return:
+        """
+        if self.model_class is None:
+            return data
+        return self.model_class(**data)
