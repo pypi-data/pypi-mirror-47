e4e119af8616c5ca94b2218f8d8cf20faae1c606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:57 +0000

    Add package rocketgram/1.1/rocketgram-1.1-py3-none-any.whl

---
+++ packages/rocketgram/rocketgram-1.1-py3-none-any.whl/rocketgram/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2015-2019 by Vd.
+# This file is part of Rocketgram, the modern Telegram bot framework.
+# Rocketgram is released under the MIT License (see LICENSE).
+
+
+from contextlib import suppress
+
+with suppress(ModuleNotFoundError):
+    import uvloop
+
+    uvloop.install()
+
+from . import context
+from . import tools
+from .bot import Bot
+from .connectors import *
+from .errors import *
+from .executors import *
+from .keyboards import *
+from .middlewares import *
+from .requests import *
+from .routers import *
+from .types import *
+from .update import *
+from .version import version
---
+++ packages/rocketgram/rocketgram-1.1-py3-none-any.whl/rocketgram/context.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2015-2019 by Vd.
+# This file is part of Rocketgram, the modern Telegram bot framework.
+# Rocketgram is released under the MIT License (see LICENSE).
+
+
+from contextvars import ContextVar
+from typing import TYPE_CHECKING, List, Optional
+
+if TYPE_CHECKING:
+    from .bot import Bot
+    from .update import Update, Message, Chat, User
+    from .requests import Request
+    from .executors import Executor
+
+current_executor = ContextVar('current_executor')
+current_bot = ContextVar('current_bot')
+current_webhook_requests = ContextVar('current_webhook_requests')
+
+current_update = ContextVar('current_update')
+current_message = ContextVar('current_message')
+current_chat = ContextVar('current_chat')
+current_user = ContextVar('current_user')
+
+
+def executor() -> Optional['Executor']:
+    """Returns Executor object for current request."""
+
+    return current_executor.get()
+
+
+def bot() -> Optional['Bot']:
+    """Returns current Bot object."""
+
+    return current_bot.get()
+
+
+def update() -> Optional['Update']:
+    """Returns Update object for current request."""
+    return current_update.get()
+
+
+def message() -> Optional['Message']:
+    """Returns Message object for current request."""
+
+    return current_message.get()
+
+
+def chat() -> Optional['Chat']:
+    """Returns Chat object for current request."""
+
+    return current_chat.get()
+
+
+def user() -> Optional['User']:
+    """Returns User object for current request."""
+
+    return current_user.get()
+
+
+def webhook_request(request: 'Request'):
+    current_webhook_requests.get().append(request)
+
+
+def get_webhook_requests() -> List['Request']:
+    try:
+        return current_webhook_requests.get()
+    except LookupError:
+        lst = list()
+        current_webhook_requests.set(lst)
+        return lst
