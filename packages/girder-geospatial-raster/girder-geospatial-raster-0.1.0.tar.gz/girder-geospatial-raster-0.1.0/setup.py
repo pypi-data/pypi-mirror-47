cb3262b3a31ad164ed3b557613bf296939a5c798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:31 +0000

    Add package girder-geospatial-raster/0.1.0/girder-geospatial-raster-0.1.0.tar.gz

---
+++ packages/girder-geospatial-raster/girder-geospatial-raster-0.1.0.tar.gz/girder-geospatial-raster-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='girder-geospatial-raster',
+    version='0.1.0',
+    description='Support for raster data types in the girder-geospatial package',
+    url='https://github.com/OpenGeoscience/girder_geospatial',
+    maintainer='Kitware, Inc.',
+    maintainer_email='kitware@kitware.com',
+    entry_points={
+        'geometa.types': [
+            'raster=geometa_raster.schema:handler'
+        ]
+    },
+    packages=[
+        'geometa_raster'
+    ],
+    install_requires=[
+        'girder-geospatial'
+    ]
+)
