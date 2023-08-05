3e6fbf4ecd8a87d082353f7bb0b885bb61577f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:45 +0000

    Add package blackcurve/0.3.0/blackcurve-0.3.0.tar.gz

---
+++ packages/blackcurve/blackcurve-0.3.0.tar.gz/blackcurve-0.3.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blackcurve",
+    version="0.3.0",
+    author="BlackCurve LTD.",
+    author_email="george.rowberry@blackcurve.com",
+    description="An easy to use python interface for BlackCurve's API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blackcurve/BlackCurve-API",
+    packages=setuptools.find_packages(exclude=['tests']),
+    install_requires=[
+      'requests'
+    ],
+    classifiers=(
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
