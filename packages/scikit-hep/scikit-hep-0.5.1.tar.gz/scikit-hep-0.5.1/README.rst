89f488d25333bead11ad55800074691a0df5a08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package scikit-hep/0.5.1/scikit-hep-0.5.1.tar.gz

---
+++ packages/scikit-hep/scikit-hep-0.5.1.tar.gz/scikit-hep-0.5.1/README.rst
@@ -0,0 +1,81 @@
+
+Scikit-HEP
+==========
+
+.. image:: https://img.shields.io/pypi/v/scikit-hep.svg
+  :alt: PyPI
+  :target: https://pypi.python.org/pypi/scikit-hep
+
+.. image:: https://zenodo.org/badge/DOI/10.5281/zenodo.1043949.svg
+  :target: https://doi.org/10.5281/zenodo.1043949
+
+.. image:: https://travis-ci.org/scikit-hep/scikit-hep.svg
+   :target: https://travis-ci.org/scikit-hep/scikit-hep
+
+.. image:: https://coveralls.io/repos/github/scikit-hep/scikit-hep/badge.svg?branch=master
+   :target: https://coveralls.io/github/scikit-hep/scikit-hep?branch=master
+
+Python versions supported:
+
+.. image:: https://img.shields.io/badge/python-2.7-blue.svg
+  :target: https://badge.fury.io/py/scikit-hep
+
+.. image:: https://img.shields.io/badge/python-3.4-blue.svg
+  :target: https://badge.fury.io/py/scikit-hep
+
+.. image:: https://img.shields.io/badge/python-3.5-blue.svg
+  :target: https://badge.fury.io/py/scikit-hep
+
+.. image:: https://img.shields.io/badge/python-3.6-blue.svg
+  :target: https://badge.fury.io/py/scikit-hep
+
+
+Project info
+------------
+
+The Scikit-HEP project (http://scikit-hep.org/) is a community-driven and community-oriented project
+with the aim of providing Particle Physics at large with a Python package containing core and common tools.
+The project started in Autumn 2016 and its packages are actively developed and maintained.
+
+For what concerns the project grand structure, it should be seen as a toolset rather than a toolkit.
+It is not just about providing common tools for the community.
+It is also about improving on discoverability of utility packages and projects.
+
+Getting in touch
+................
+
+There are various ways to
+`get in touch <http://scikit-hep.org/get-in-touch.html>`_
+with project admins and/or users and developers.
+
+scikit-hep package
+------------------
+
+This ``scikit-hep package`` is under redesign, in order to become a metapackage for the project.
+
+Installation
+.............
+
+Install ``scikit-hep`` like any other Python package:
+
+.. code-block:: bash
+
+   pip install scikit-hep
+
+or similar (use ``--user``, ``virtualenv``, etc. if you wish).
+
+Strict dependencies
+...................
+
+- `Python <http://docs.python-guide.org/en/latest/starting/installation/>`_ (2.7+, 3.4+)
+- `Numpy <https://scipy.org/install.html>`_: the numerical library for Python (1.11.0+)
+- `matplotlib <https://matplotlib.org/>`_: a popular Python 2D plotting library (> 2.0.0 , < 2.1)
+
+
+Documentation
+.............
+
+The project documentation is hosted on the `Scikit-HEP website`_, and the source code
+is in the ``doc/`` folder.
+
+.. _Scikit-HEP website: http://scikit-hep.org/
---
+++ packages/scikit-hep/scikit-hep-0.5.1.tar.gz/scikit-hep-0.5.1/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# Licensed under a 3-clause BSD style license, see LICENSE.
+import sys
+import os
+
+# try to use setuptools if installed
+from setuptools import setup, find_packages
+from setuputils import read, find_version
+
+
+# General information on the project
+PROJECT_NAME = 'scikit-hep'
+AUTHOR = 'the scikit-hep developers'
+AUTHOR_EMAIL = 'scikit-hep-admins@googlegroups.com'
+DESCRIPTION = 'Toolset of interfaces and tools for Particle Physics.'
+URL = 'https://github.com/scikit-hep/scikit-hep/'
+LICENSE = 'new BSD'
+VERSION = find_version('skhep/__init__.py')
+
+# Specification of minimal versions of required dependencies
+PYTHON_REQUIRES = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4'
+NUMPY_MIN_VERSION = '1.11.0'
+
+# Prevent setup from trying to create hard links
+# which are not allowed on AFS between directories.
+# This is a hack to force copying.
+try:
+    del os.link
+except AttributeError:
+    pass
+
+LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
+# setup.py can be called from outside the scikit-hep directory
+os.chdir(LOCAL_PATH)
+sys.path.insert(0, LOCAL_PATH)
+
+INSTALL_REQUIRES = [
+    'hepunits',
+    'numpy>={0};python_version>="2.7"'.format(NUMPY_MIN_VERSION),
+    'pandas<0.21;python_version=="3.4.*"',
+    'pandas;python_version>="2.7"',
+    'matplotlib>2.0.0;python_version>="2.7"',
+]
+
+TESTS_REQUIRE = [
+    'pytest>3.0;python_version>="2.7"'
+]
+
+SETUP_REQUIRES = [
+    'pytest-runner'
+]
+
+setup(
+    name=PROJECT_NAME,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read('README.rst'),
+    url=URL,
+    license=LICENSE,
+    packages=find_packages(),
+    py_modules=['setuputils'],
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    setup_requires=SETUP_REQUIRES,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'License :: OSI Approved',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: C++',
+        'Programming Language :: Cython',
+        'Development Status :: 4 - Beta',
+    ]
+)
