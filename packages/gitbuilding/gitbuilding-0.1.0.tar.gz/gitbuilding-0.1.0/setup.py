ecad90e68ad31e8e9e37a872680bf7233ab79037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package gitbuilding/0.1.0/gitbuilding-0.1.0.tar.gz

---
+++ packages/gitbuilding/gitbuilding-0.1.0.tar.gz/gitbuilding-0.1.0/setup.py
@@ -0,0 +1,30 @@
+ 
+__author__ = 'Julian Stirling'
+
+from setuptools import setup, find_packages
+import sys
+if sys.version_info[0] == 2:
+    sys.exit("Sorry, Python 2 is not supported")
+
+
+setup(name = 'gitbuilding',
+      version = '0.1.0',
+      description = 'For documenting and open source hardware project with minimal effort, so you can stop writing and git building.',
+      long_description = 'For documenting and open source hardware project with minimal effort, so you can stop writing and git building.',
+      author = 'Julian Stirling',
+      author_email = 'julian@julianstirling.co.uk',
+      packages = find_packages(),
+      package_data={'gitbuilding': ['static/*.*','static/Logo/*.*']},
+      keywords = ['Documentation','Hardware'],
+      zip_safe = True,
+      url = 'https://gitlab.com/bath_open_instrumentation_group/git-building',
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.6'
+          ],
+      install_requires=['argparse','regex','pyyaml','flask'],
+      python_requires=">=3.6",
+      entry_points = {'console_scripts': ['gitbuilding = gitbuilding.__main__:main']},
+      )
+
