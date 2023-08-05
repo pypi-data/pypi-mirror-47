ae392dcfddb9ba3809da82f3431878026ae399ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:40 +0000

    Add package apian/0.2.6/apian-0.2.6.tar.gz

---
+++ packages/apian/apian-0.2.6.tar.gz/apian-0.2.6/src/apian/api.py
@@ -0,0 +1,26 @@
+"""
+This file contains a hack to make swagger-ui work from behind a load balancer.
+It contains no application specific code and could have been a library.
+"""
+from flask_restplus import Api
+from flask_restplus.api import SwaggerView
+
+
+class CustomAPI(Api):
+    """
+    Hack to serve swagger.json from the /api prefix.  This is a requirement for
+    serving swagger-ui from behind a path-routing load balancer without
+    additional proxies.
+    """
+
+    def _register_specs(self, app_or_blueprint):
+        if self._add_specs:
+            endpoint = str('specs')
+            self._register_view(
+                app_or_blueprint,
+                SwaggerView,
+                self.default_namespace,
+                '{}/swagger.json'.format(self.prefix),
+                endpoint=endpoint,
+                resource_class_args=(self, ))
+            self.endpoints.add(endpoint)
