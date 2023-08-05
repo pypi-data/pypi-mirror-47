230e14cb0913d221b8cd947b2f2d96297b360286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:03 +0000

    Add package tziota/1.1/tziota-1.1.zip

---
+++ packages/tziota/tziota-1.1.zip/tziota-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+        name="tziota",
+        version="1.1",
+        description="",
+        author="jdh99",
+        url="",
+        packages=find_packages(),
+    )
