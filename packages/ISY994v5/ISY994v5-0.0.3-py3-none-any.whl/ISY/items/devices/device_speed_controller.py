7982351fc66892ef2244c2037eb703bfc4076723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:00 +0000

    Add package ISY994v5/0.0.3/ISY994v5-0.0.3-py3-none-any.whl

---
+++ packages/ISY994v5/ISY994v5-0.0.3-py3-none-any.whl/ISY/items/devices/device_speed_controller.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+
+from .device_base import Device_Base
+
+
+class Device_Speed_Controller(Device_Base):
+
+    def __init__(self, container, name ,speeds=['off','low','medium','high']):
+        Device_Base.__init__(self,container,'speed_controller',name)
+
+        self.speeds = speeds
+
+        self.add_property('speed','unknown') 
+
+    def set_speed(self,speed):
+        pass
+
