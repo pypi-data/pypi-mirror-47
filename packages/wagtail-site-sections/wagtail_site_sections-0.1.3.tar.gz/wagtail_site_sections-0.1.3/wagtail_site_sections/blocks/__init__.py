6ae15a497cad5db7fbcf00518e2c74ae30be37dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:33 +0000

    Add package wagtail-site-sections/0.1.3/wagtail_site_sections-0.1.3.tar.gz

---
+++ packages/wagtail-site-sections/wagtail_site_sections-0.1.3.tar.gz/wagtail_site_sections-0.1.3/wagtail_site_sections/blocks/__init__.py
@@ -0,0 +1,14 @@
+from .blocks import HeroSectionBlock, TeamMemberBlock, TeamSectionBlock, CarouselImageBlock, CarouselSectionBlock # noqa
+from .blocks import FaqBlock, FaqSectionBlock, TestimonialBlock, TestimonialSectionBlock # noqa
+
+# noqa avoids unused imports in __init__.py bug with flake8
+
+
+# Simply add this to any existing list of streamfield blocks in a content panel to enable creation of sections
+section_blocks = [
+    ('hero_section', HeroSectionBlock()),
+    ('team_section', TeamSectionBlock()),
+    ('carousel_section', CarouselSectionBlock()),
+    ('faq_section', FaqSectionBlock()),
+    ('testimonial_section', TestimonialSectionBlock()),
+]
