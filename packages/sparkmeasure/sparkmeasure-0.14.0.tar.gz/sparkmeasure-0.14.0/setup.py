97216f107b848d0816a97747c978dc7ecd662e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:29 +0000

    Add package sparkmeasure/0.14.0/sparkmeasure-0.14.0.tar.gz

---
+++ packages/sparkmeasure/sparkmeasure-0.14.0.tar.gz/sparkmeasure-0.14.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+description = 'Python API for sparkMeasure, a tool for performance troubleshooting of Apache Spark workloads'
+
+long_description = """SparkMeasure is a tool for performance troubleshooting of Apache Spark workloads.  
+It simplifies the collection and analysis of Spark performance metrics. The bulk of sparkMeasure is written in Scala.
+This package contains the Python API for sparkMeasure and is intended to work in conjunction with PySpark.
+Use from python command line or in Jupyter notebook environments, or as a tool to instrument Python code running Spark workloads.  
+**[Link to sparkMeasure GitHub page and documentation](https://github.com/lucacanali/sparkMeasure)**"""
+
+setup(name='sparkmeasure',
+    version='0.14.0',
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Luca Canali',
+    author_email='luca.canali@cern.ch',
+    url='https://github.com/lucacanali/sparkMeasure',
+    license='Apache License, Version 2.0',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    python_requires='>=2.7',
+    install_requires=[],
+    classifiers=[
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: Apache Software License',
+    'Intended Audience :: Developers',
+    'Development Status :: 4 - Beta',
+    ]
+    )
---
+++ packages/sparkmeasure/sparkmeasure-0.14.0.tar.gz/sparkmeasure-0.14.0/sparkmeasure/taskmetrics.py
@@ -0,0 +1,52 @@
+"""sparkmeasure is a tool for performance troubleshooting of Apache Spark workloads.
+It simplifies the collection and analysis of Spark task metrics data.
+See details at https://github.com/LucaCanali/sparkMeasure
+"""
+
+from __future__ import print_function
+
+"""TaskMetrics class provides the API to collect and process task metrics data aggregated by task execution
+This is a finer granularity than StageMetrics and potentially collects much more data.
+This is a Python wrapper class to the corresponding Scala class of sparkMeasure.
+"""
+class TaskMetrics:
+    def __init__(self, sparksession):
+        self.sparksession = sparksession
+        self.sc = self.sparksession.sparkContext
+        self.taskmetrics = self.sc._jvm.ch.cern.sparkmeasure.TaskMetrics(self.sparksession._jsparkSession, False)
+
+    def begin(self):
+        self.taskmetrics.begin()
+
+    def end(self):
+        self.taskmetrics.end()
+
+    def report(self):
+        return self.taskmetrics.report()
+
+    def print_report(self):
+        print(self.report())
+
+    def print_accumulables(self):
+        none # not yet implemented
+
+    def runandmeasure(self, env, codetorun):
+        self.begin()
+        exec(codetorun, env)
+        self.end()
+        self.print_report()
+
+    def create_taskmetrics_DF(self, viewname="PerfTaskMetrics"):
+        df = self.taskmetrics.createTaskMetricsDF(viewname)
+        # convert the returned Java object to a Python Dataframe
+        from pyspark.sql.dataframe import DataFrame
+        return DataFrame(df, self.sparksession)
+
+    def aggregate_taskmetrics_DF(self, viewname="PerfTaskMetrics"):
+        df = self.taskmetrics.aggregateTaskMetrics(viewname)
+        # convert the returned Java object to a Python Dataframe
+        from pyspark.sql.dataframe import DataFrame
+        return DataFrame(df, self.sparksession)
+
+    def save_data(self, df, filepathandname, fileformat):
+        df.repartition(1).write.format(fileformat).save(filepathandname)
