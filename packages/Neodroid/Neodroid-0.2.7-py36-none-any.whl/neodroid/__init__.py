dbb3a7f21005cfc116d471f2aad3aa4aa22ddc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package Neodroid/0.2.7/Neodroid-0.2.7-py36-none-any.whl

---
+++ packages/Neodroid/Neodroid-0.2.7-py36-none-any.whl/neodroid/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .api import *
+from .api_wrappers import *
+from neodroid.version import get_version
+
+__author__ = 'cnheider'
+__version__ = get_version()
+__doc__ = r'''
+.. module:: neodroid
+   :platform: Unix, Windows
+   :synopsis: An API for communicating with a Unity Game process.
+
+.. moduleauthor:: Christian Heider Nielsen <christian.heider@alexandra.dk>
+
+
+'''
---
+++ packages/Neodroid/Neodroid-0.2.7-py36-none-any.whl/neodroid/utilities/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'cnheider'
+
+from .launcher import *
+from .neodroid_standard_modules import *
+from .reaction_factories import *
+from .transformations import *
