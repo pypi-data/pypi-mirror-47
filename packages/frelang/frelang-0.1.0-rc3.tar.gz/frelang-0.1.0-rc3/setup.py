baafc24cb4f9e21675700c117b88c8e2863c93c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:39 +0000

    Add package frelang/0.1.0-rc3/frelang-0.1.0-rc3.tar.gz

---
+++ packages/frelang/frelang-0.1.0-rc3.tar.gz/frelang-0.1.0-rc3/setup.py
@@ -0,0 +1,17 @@
+import os
+
+from distutils.core import setup
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    setup(
+        name='frelang',
+        description='Interpreted, statically typed, simple, flexible, embeddable language.',
+        version='0.1.0-rc3',
+        author='Gustavo Ramos Rehermann (Gustavo6046)',
+        author_email='rehermann6046@gmail.com',
+        license='The MIT License',
+
+        packages=['fre'],
+        long_description=f.read()
+    )
