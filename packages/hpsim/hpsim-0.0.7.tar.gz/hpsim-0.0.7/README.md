db212d96cb6ca7144a5a6d4d2a1e96edd6d8cd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:24 +0000

    Add package hpsim/0.0.7/hpsim-0.0.7.tar.gz

---
+++ packages/hpsim/hpsim-0.0.7.tar.gz/hpsim-0.0.7/README.md
@@ -0,0 +1,5 @@
+# hpsim
+A (hopefully) high performance, modular simulator for everything from molecules to galaxy clusters.
+
+v0.0.1:
+Trying to setup the package. Internally I'm at 0.3.2 - I hope to have the package reach the same point soon.
---
+++ packages/hpsim/hpsim-0.0.7.tar.gz/hpsim-0.0.7/hpsim/ntgrtr.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 28 19:24:16 2019
+
+@author: hogbobson
+"""
+
+class ntgrtr:
+    def __init__(self):
+        pass
+    
+    def n_squared(self, ensemble):
+        pass
