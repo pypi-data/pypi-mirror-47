165a8bdc3e471007c9f947ae81bca54432c3d011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:18 +0000

    Add package evopy/0.2/evopy-0.2.tar.gz

---
+++ packages/evopy/evopy-0.2.tar.gz/evopy-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='evopy',
+      version='0.2',
+      description='Evolutionary Strategies made simple',
+      url='http://github.com/evopy/evopy',
+      author='evopy',
+      author_email='info@gandreadis.com',
+      license='MIT',
+      packages=['evopy'],
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      install_requires=[
+          "numpy",
+          "nose",
+          "pylint",
+          "parameterized",
+          "sphinx",
+          "sphinx-rtd-theme",
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ])
