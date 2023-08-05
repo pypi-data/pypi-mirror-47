54906cdc5db7fd1a6f394ace980d726cb062a841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:34 +0000

    Add package turkce/0.1.3/turkce-0.1.3.tar.gz

---
+++ packages/turkce/turkce-0.1.3.tar.gz/turkce-0.1.3/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='turkce',
+    version='0.1.3',
+    packages=['pack',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='turkceAI',
+)
