045f44f8c2c0195539506a610100a1a5a6e1bcbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:40 +0000

    Add package cfn-review-bot/1!20190529.233234/cfn_review_bot-1!20190529.233234-py3-none-any.whl

---
+++ packages/cfn-review-bot/cfn_review_bot-1!20190529.233234-py3-none-any.whl/cfn_review_bot/error.py
@@ -0,0 +1,6 @@
+'''
+Defines `Error`, the base class for all exceptions generated in this package.
+'''
+
+class Error(Exception):
+  pass
---
+++ packages/cfn-review-bot/cfn_review_bot-1!20190529.233234-py3-none-any.whl/cfn_review_bot/schema/stack.py
@@ -0,0 +1,30 @@
+'''
+Schema for configuring a CloudFormation stack and its deployment targets.
+'''
+
+import schema
+
+from . import aws, util
+
+
+StackTarget = schema.Or(
+  str,
+  {
+    'name': str,
+    # overrides regions defined in the target
+    'region': util.OneOrMany(str),
+  },
+)
+
+StackSchema = schema.Schema({
+  'template': util.OneOrMany(StackTarget),
+  schema.Optional('name'): str,
+  schema.Optional('target'): util.OneOrMany(str),
+
+  # overrides regions defined in the target
+  schema.Optional('region'): util.OneOrMany(aws.Region),
+
+  schema.Optional('capability', default=[]): util.OneOrMany(str),
+  schema.Optional('parameter', default={}): {str: str},
+  schema.Optional('tag', default={}): {str: str},
+}, name='Stack Description')
