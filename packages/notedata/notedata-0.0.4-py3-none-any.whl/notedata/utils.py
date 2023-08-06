e065808bfa05a2d49e4d8da4077f21d47365919d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:20 +0000

    Add package notedata/0.0.4/notedata-0.0.4-py3-none-any.whl

---
+++ packages/notedata/notedata-0.0.4-py3-none-any.whl/notedata/utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/06/05 10:35
+# @Author  : niuliangtao
+# @Site    : 
+# @File    : utils.py
+# @Software: PyCharm
+import os
+import pycurl
+
+__all__ = ['utils']
+
+data_root = '/content/tmp/'
+
+
+def download_file(url, path):
+    if not os.path.exists(path):
+        print("downloading from " + url + " to " + path)
+        with open(path, 'wb') as f:
+            c = pycurl.Curl()
+            c.setopt(pycurl.URL, url)
+            c.setopt(pycurl.WRITEDATA, f)
+            c.perform()
+            c.close()
+        print('download success')
+
+
+def exists(path):
+    return os.path.exists(path)
