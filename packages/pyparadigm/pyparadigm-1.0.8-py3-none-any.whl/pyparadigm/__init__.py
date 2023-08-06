bb7744e242130e0f6d9924303abeb865c9fff4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:08 +0000

    Add package pyparadigm/1.0.8/pyparadigm-1.0.8-py3-none-any.whl

---
+++ packages/pyparadigm/pyparadigm-1.0.8-py3-none-any.whl/pyparadigm/__init__.py
@@ -0,0 +1,20 @@
+"""A package to build psychological Paradigms. Based on PyGame """
+__version__ = '1.0.8'
+
+
+from .eventlistener import EventConsumerInfo, EventListener, Handler,\
+    MouseProxy, MOUSE_LEFT, MOUSE_MIDDLE, MOUSE_RIGHT, MOUSE_SCROL_FW,\
+    MOUSE_SCROL_BW, is_left_click, is_key_press
+from .misc import init, display, empty_surface, slide_show, process_char,\
+    rgba
+from .surface_composition import Border, Circle, Fill, LLItem, LinLayout,\
+    Line, Margin, Overlay, Padding,\
+    RectangleShaper, Surface, Cross, Font,\
+    GridLayout, Text, compose
+
+try:
+    __import__("numpy")
+    __import__("matplotlib")
+    from .extras import  mat_to_surface, apply_color_map, to_24bit_gray
+except ImportError:
+    pass
