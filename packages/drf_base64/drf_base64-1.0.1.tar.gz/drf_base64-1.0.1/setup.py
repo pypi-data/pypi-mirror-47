dee44a600b911134566bef4d7f14a5ef78ec566a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:56 +0000

    Add package drf_base64/1.0.1/drf_base64-1.0.1.tar.gz

---
+++ packages/drf_base64/drf_base64-1.0.1.tar.gz/drf_base64-1.0.1/setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import setup
+
+try:
+    import pypandoc
+
+
+    README = pypandoc.convert_file('README.md', 'rst')
+except ImportError:
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+        README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='drf_base64',
+    version='1.0.1',
+    packages=['drf_base64'],
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='DRF serializers to handle base64-encoded files',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://bitbucket.org/levit_scs/drf_base64',
+    author='LevIT SCS',
+    author_email='info@levit.be',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'Django>=1.8,<2.0',
+        'djangorestframework<4.0.0',
+    ]
+)
