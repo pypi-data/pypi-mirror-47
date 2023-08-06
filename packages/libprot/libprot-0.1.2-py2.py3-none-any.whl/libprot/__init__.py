cc09a5e32ea63ecffa4ab54dca78378ff06fbce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:09 +0000

    Add package libprot/0.1.2/libprot-0.1.2-py2.py3-none-any.whl

---
+++ packages/libprot/libprot-0.1.2-py2.py3-none-any.whl/libprot/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for libprot."""
+from prody import confProDy
+
+__author__ = """Nate Guerin"""
+__email__ = 'nate@textpla.in'
+__version__ = '0.1.2'
+
+confProDy(verbosity='none')
