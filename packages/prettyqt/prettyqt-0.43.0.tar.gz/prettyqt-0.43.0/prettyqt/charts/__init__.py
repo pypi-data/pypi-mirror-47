a893f5f11127721558239c8c9c95c828faebf294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:49 +0000

    Add package prettyqt/0.43.0/prettyqt-0.43.0.tar.gz

---
+++ packages/prettyqt/prettyqt-0.43.0.tar.gz/prettyqt-0.43.0/prettyqt/charts/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""charts module
+"""
+
+from .scatterseries import ScatterSeries
+from .lineseries import LineSeries
+from .chart import Chart
+from .chartview import ChartView
+
+__all__ = ["ChartView", "Chart", "LineSeries", "ScatterSeries"]
---
+++ packages/prettyqt/prettyqt-0.43.0.tar.gz/prettyqt-0.43.0/prettyqt/core/mimedata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtCore
+
+from prettyqt import core
+
+
+QtCore.QMimeData.__bases__ = (core.Object,)
+
+
+class MimeData(QtCore.QMimeData):
+
+    def set_data(self, mime_type, data):
+        self.setData(mime_type, QtCore.QByteArray(data.encode()))
