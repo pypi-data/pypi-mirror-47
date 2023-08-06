3b4cee8bea3cb2a64d39c5375c8d222e4b153fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:51 +0000

    Add package om-probability/0.1/om_probability-0.1.tar.gz

---
+++ packages/om-probability/om_probability-0.1.tar.gz/om_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='om_probability',
+      version='0.1',
+      description='Gaussian om_probability',
+      packages=['om_probability'],
+      zip_safe=False)
