704f0de5d3d3452773b39bfd5a96841568375750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:06 +0000

    Add package toonapilib/3.2.4/toonapilib-3.2.4.tar.gz

---
+++ packages/toonapilib/toonapilib-3.2.4.tar.gz/toonapilib-3.2.4/toonapilib/configuration.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# File: configuration.py
+"""A place to store the configuration."""
+
+
+STATE_CACHING_SECONDS = 300
+THERMOSTAT_STATE_CACHING_SECONDS = 3600
+
+STATES = {0: 'Comfort',
+          1: 'Home',
+          2: 'Sleep',
+          3: 'Away',
+          4: 'Holiday',
+          5: 'Unknown'}
+
+BURNER_STATES = {0: 'off',
+                 1: 'on',
+                 2: 'water_heating',
+                 3: 'pre_heating'}
+
+PROGRAM_STATES = {0: 'off',
+                  1: 'on',
+                  2: 'manual'}
