cfcb71725ffc7138df0796b39f9b857dcab1f873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:31 +0000

    Add package JT-Techfield/0.0.5/JT-Techfield-0.0.5.tar.gz

---
+++ packages/JT-Techfield/JT-Techfield-0.0.5.tar.gz/JT-Techfield-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JT-Techfield",
+    version="0.0.5",
+    author="Jacob Thompson",
+    author_email="jacob.thompson@techfield.us",
+    description="My regression module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent",
+    ],
+	install_requires=[
+		"numpy", 
+		"matplotlib"
+		], 
+	python_requires='>=3'
+)
