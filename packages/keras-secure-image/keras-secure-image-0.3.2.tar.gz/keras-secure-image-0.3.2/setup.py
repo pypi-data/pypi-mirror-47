3bb45993274abe2e40ad8176f3c31891bb007abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:35 +0000

    Add package keras-secure-image/0.3.2/keras-secure-image-0.3.2.tar.gz

---
+++ packages/keras-secure-image/keras-secure-image-0.3.2.tar.gz/keras-secure-image-0.3.2/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+
+long_description = '''
+This package is an add on library on top of keras or tensorflow.
+The aim of this package is to provide privacy of images by enabling the model to be trained on encrypted images.
+
+Read the documentation at: https://github.com/akshaybahadur21/keras-secure-image
+
+For a detailed code overview on colab, see:
+https://colab.research.google.com/drive/1D7ziODKrzL0QK-GuI7BH9f9Fk7U3tfUW
+
+keras-secure-image is compatible with Python 2.7-3.6
+and is distributed under the MIT license.
+
+'''
+
+setup(
+  name = 'keras-secure-image',         
+  version = '0.3.2',      
+  license='MIT',       
+  long_description = long_description,
+  description='Add on library for deep learning on encrypted images',
+  author = 'Akshay Bahadur',                   
+  author_email = 'akshaybahadur21@gmail.com',      
+  url = 'https://github.com/akshaybahadur21',   
+  download_url = 'https://github.com/akshaybahadur21/keras-secure-image/archive/v0.3.tar.gz',
+  keywords = ['KERAS', 'SECURITY', 'PRIVACY', 'DEEP LEARNING'],   
+  install_requires=[            
+          'numpy',
+          'Pillow',
+	  'scikit-image'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      	
+
+    'Intended Audience :: Developers',      	
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',   
+
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
