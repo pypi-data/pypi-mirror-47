a07dec2835a3ddfb96552bb9569e5edda25c9357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:51 +0000

    Add package random-password-generator-cli/1.0.0/random-password-generator-cli-1.0.0.tar.gz

---
+++ packages/random-password-generator-cli/random-password-generator-cli-1.0.0.tar.gz/random-password-generator-cli-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from random_password_generator import __name__, __version__, __description__, __url__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=__name__,
+    version=__version__,
+    url=__url__,
+    license="MIT",
+    description=__description__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Valerio Preti",
+    author_email="valerio.preti.rpg@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click>=7.0",
+        "requests>=2.0"
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "rpg = random_password_generator.rpg:rpg",
+        ],
+    },
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
