08b34ec3842d9dc5503f5934be5fce5ef441f792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:41 +0000

    Add package royalnet/5.0a23/royalnet-5.0a23-py3-none-any.whl

---
+++ packages/royalnet/royalnet-5.0a23-py3-none-any.whl/royalnet/web/royalprints/__init__.py
@@ -0,0 +1,8 @@
+"""Some Royalprints that can be used with the Royalnet Flask server."""
+
+from .home import rp as rp_home
+from .wikiview import rp as rp_wikiview
+from .tglogin import rp as rp_tglogin
+from .docs import rp as rp_docs
+
+__all__ = ["rp_home", "rp_wikiview", "rp_tglogin", "rp_docs"]
