d0aac7b3f2df5bda8457c434df839645757cff69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:59 +0000

    Add package discountr-client/0.0.6/discountr_client-0.0.6-py3-none-any.whl

---
+++ packages/discountr-client/discountr_client-0.0.6-py3-none-any.whl/discountr/models.py
@@ -0,0 +1,96 @@
+import json
+
+
+class BaseObject(dict):
+    serialized_keys = []
+
+    def to_json(self):
+        temp = {}
+        for key in self.serialized_keys:
+            temp[key] = getattr(self, key)
+        return json.dumps(temp)
+
+
+class Brand(BaseObject):
+    serialized_keys = [
+        'name',
+        'slug'
+    ]
+
+    @property
+    def slug(self):
+        return self.name
+
+    @property
+    def name(self):
+        return self.get('brand_name')
+
+
+class Category(BaseObject):
+    serialized_keys = [
+        'name',
+        'slug'
+    ]
+
+    @property
+    def slug(self):
+        return self.get('slug')
+
+    @property
+    def name(self):
+        return self.get('name')
+
+
+class Price(BaseObject):
+    serialized_keys = [
+        'value',
+        'created_at',
+        'product_id'
+    ]
+
+    @property
+    def value(self):
+        return self.get('value')
+
+    @property
+    def created_at(self):
+        return self.get('created_at')
+
+    @property
+    def product_id(self):
+        return self.get('product_id')
+
+
+class Product(BaseObject):
+    serialized_keys = [
+        'name',
+        'slug',
+        'code',
+        'brand_id',
+        'category_id',
+        'price_id'
+    ]
+
+    @property
+    def slug(self):
+        return self.get('slug')
+
+    @property
+    def name(self):
+        return self.get('name')
+
+    @property
+    def code(self):
+        return self.get('code')
+
+    @property
+    def brand_id(self):
+        return self.get('brand_id')
+
+    @property
+    def category_id(self):
+        return self.get('category_id')
+
+    @property
+    def price(self) -> Price:
+        return Price(self)
