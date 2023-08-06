78f429d61a8de978e809457c102aaefc7fe8e930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:04 +0000

    Add package geophysics-analysis/0.0.5/geophysics_analysis-0.0.5.tar.gz

---
+++ packages/geophysics-analysis/geophysics_analysis-0.0.5.tar.gz/geophysics_analysis-0.0.5/README.md
@@ -0,0 +1,7 @@
+# geophysics_analysis
+A collection of functions for the analysis of geophysical fields.
+
+Ver 0.0.5
+
+### What's New:
+* ga.write_netCDF now can account for 3D (surface/TOA) as well as 4D Variables
---
+++ packages/geophysics-analysis/geophysics_analysis-0.0.5.tar.gz/geophysics_analysis-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geophysics_analysis",
+    version="0.0.5",
+    author="Michael Needham",
+    author_email="m.needham@colostate.edu",
+    description="A collection of functions for the analysis of geophysical fields",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
