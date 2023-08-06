a3bffcba78aadd13ec4572ea8f1f33b399b3c510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:39 +0000

    Add package maup/0.5/maup-0.5.tar.gz

---
+++ packages/maup/maup-0.5.tar.gz/maup-0.5/maup/__init__.py
@@ -0,0 +1,7 @@
+from .assign import assign
+from .indexed_geometries import IndexedGeometries
+from .intersections import intersections, prorate
+from .adjacencies import adjacencies
+
+__version__ = "0.5"
+__all__ = ["assign", "IndexedGeometries", "intersections", "prorate", "adjacencies"]
