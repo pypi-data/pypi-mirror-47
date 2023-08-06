8eb7f3f1e649bd90237c25b41578e389c80d1999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:12 +0000

    Add package pyFaceTrace/0.0.4/pyFaceTrace-0.0.4.tar.gz

---
+++ packages/pyFaceTrace/pyFaceTrace-0.0.4.tar.gz/pyFaceTrace-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: Microsoft',
+    'Programming Language :: Python :: 3.6',
+    #'Topic :: Internet :: Proxy Servers',
+    #'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+]
+
+setup(
+    name = 'pyFaceTrace',      
+    version = '0.0.4',
+    packages=find_packages(),
+    install_requires=[],
+    author = 'funny4875',        
+    author_email = 'funny4875@gmail.com',
+    url = 'https://github.com/funny4875/pyFaceTrace',
+	description = 'face trace tool',
+	license='MIT',
+	include_package_data=True,
+    )
