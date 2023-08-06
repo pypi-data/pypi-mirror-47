d316d394ced27ee333e2baf2ca941d71204aa7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:31 +0000

    Add package zhulong/5.1.28/zhulong-5.1.28-py3-none-any.whl

---
+++ packages/zhulong/zhulong-5.1.28-py3-none-any.whl/zhulong/util/data.py
@@ -0,0 +1,62 @@
+zhulong_diqu_dict = {
+        'shandong': ['anqiu', 'binzhou', 'dezhou', 'dongying', 'feicheng', 'heze', 'jiaozhou', 'jinan', 'jining', 'laiwu',
+                     'leling', 'liaocheng', 'linqing', 'linyi', 'pingdu', 'qingdao', 'qufu', 'rizhao', 'rongcheng',
+                     'rushan', 'shandong', 'taian', 'tengzhou', 'weifang', 'weihai', 'xintai', 'yantai', 'yucheng',
+                     'zaozhuang', 'zibo', 'zoucheng'],
+        'hubei': ['dangyang', 'enshi', 'huanggang', 'huangshi', 'jingmen', 'lichuan', 'shiyan', 'suizhou', 'wuhan',
+                  'xiangyang', 'yichang', 'yidu', 'xiaogan'],
+        'hainan': ['danzhou', 'dongfang', 'haikou', 'hainan', 'qionghai', 'sansha', 'sanya'],
+        'jiangsu': ['changshu', 'changzhou', 'danyang', 'dongtai', 'huaian', 'jiangsu', 'jiangyin', 'kunshan',
+                    'lianyungang', 'nanjing', 'nantong', 'suqian', 'suzhou', 'taizhou', 'wuxi', 'xinyi', 'xuzhou',
+                    'yancheng', 'yangzhou', 'yizheng', 'zhangjiagang', 'zhenjiang'],
+        'jilin': ['baicheng', 'baishan', 'changchun', 'jilin', 'jilinshi', 'liaoyuan', 'siping', 'songyuan', 'tonghua'],
+        'guangdong': ['guangzhou', 'heyuan', 'huizhou', 'jiangmen', 'jieyang', 'lianzhou', 'meizhou', 'nanxiong',
+                      'shaoguan', 'shenzhen', 'sihui', 'yingde', 'yunfu', 'zhanjiang', 'zhaoqing', 'zhongshan', 'zhuhai'
+            , "dongguan", "qingyuan", "chaozhou", "shantou", "shanwei", "foshan", "yangjiang", "maoming", "guangdong"],
+        'neimenggu': ['baotou', 'bayannaoer', 'chifeng', 'eeduosi', 'huhehaote', 'hulunbeier', 'manzhouli', 'neimenggu',
+                      'tongliao', 'wuhai', 'wulanchabu', 'xilinguolemeng', 'xinganmeng', 'alashan'],
+        'fujian': ['fujian', 'fuqing', 'fuzhou', 'jianou', 'longyan', 'nanan', 'nanping', 'ningde', 'putian', 'quanzhou',
+                   'sanming', 'shaowu', 'wuyishan', 'xiamen', 'yongan', 'zhangzhou'], 'qinghai': ['qinghai', 'xining'],
+        'chongqing': ['yongchuan','chongqing'],
+        'shanxi': ['chenzhou', 'shanxi', 'weinan', 'xian', 'xianyang', 'yanan'],
+        'xinjiang': ['beitun', 'kezhou', 'wulumuqi', 'xinjiang', 'akesu'],
+        'ningxia': ['guyuan', 'ningxia', 'yinchuan'],
+        'jiangxi': ['dexing', 'fengcheng', 'fuzhou', 'ganzhou', 'gaoan', 'jian', 'jiangxi', 'jingdezhen', 'jingdezhen2', 'jinggangshan',
+                    'lushan', 'nanchang', 'ruichang', 'ruijin', 'shangrao', 'xinyu', 'yichun', 'yingtan', 'zhangshu'],
+        'henan': ['anyang', 'dengfeng', 'gongyi', 'hebi', 'jiaozhuo', 'jiyuan', 'jiyuan1', 'kaifeng', 'linzhou', 'luohe',
+                  'luoyang', 'mengzhou', 'nanyang', 'pingdingshan', 'puyang', 'qinyang', 'ruzhou', 'sanmenxia', 'shangqiu',
+                  'weihui', 'wugang', 'xinmi', 'xinxiang', 'xinyang', 'xinzheng', 'xuchang', 'yanshi', 'yongcheng',
+                  'zhengzhou', 'zhoukou', 'zhumadian'],
+        'shanxi1': ['shanxi', 'shanxi2'],
+        'xizang': ['lasa', 'rikaze', 'xizang'],
+        'sichuan': ['bazhong', 'chengdu', 'chongzhou', 'dazhou', 'deyang', 'dujiangyan', 'guangan', 'guanghan',
+                    'guangyuan', 'jiangyou', 'jianyang', 'leshan', 'longchang', 'luzhou', 'meishan', 'mianyang',
+                    'mianyang1', 'mianyang2', 'nanchong', 'neijiang', 'panzhihua', 'pengzhou', 'qionglai', 'shifang',
+                    'sichuan', 'sichuan2', 'suining', 'wanyuan', 'yaan', 'yibin'],
+        'guangxi': ['baise', 'beihai', 'chongzuo', 'fangchenggang', 'guangxi', 'guigang', 'guilin', 'hechi', 'hezhou',
+                    'laibin', 'liuzhou', 'nanning', 'qinzhou', 'wuzhou'],
+        'hunan': ['changde', 'changsha', 'chenzhou', 'hengyang', 'huaihua', 'liling', 'liuyang', 'loudi', 'hunan', 'shaoyang',
+                  'xiangtan', 'yiyang', 'yongzhou', 'yuanjiang', 'yueyang', 'zhangjiajie', 'zhuzhou'],
+        'zhejiang': ['cixi', 'dongyang', 'hangzhou', 'huzhou', 'jiaxing', 'jinhua', 'linhai', 'lishui', 'longquan',
+                     'ningbo', 'pinghu', 'quzhou', 'ruian', 'shaoxing', 'shengzhou', 'taizhou', 'tongxiang', 'wenling',
+                     'wenzhou', 'yiwu', 'yueqing', 'yuhuan', 'zhejiang', 'zhoushan', 'zhuji'],
+        'yunnan': ['baoshan', 'chuxiong', 'dali', 'kunming', 'lijiang', 'lincang', 'puer', 'tengchong', 'wenshan',
+                   'yunnan', 'yuxi', 'zhaotong'],
+        'heilongjiang': ['daqing', 'haerbin_gcjs', 'haerbin_zfcg', 'hegang', 'heilongjiang', 'qiqihaer', 'yichun'],
+        'liaoning': ['anshan', 'beizhen', 'benxi', 'chaoyang', 'dalian', 'dandong', 'donggang', 'fushun', 'fuxin',
+                     'haicheng', 'huludao', 'jinzhou', 'liaoning', 'liaoyang', 'panjin', 'shenyang', 'tieling', 'yingkou'],
+        'anhui': ['anqing', 'bengbu', 'bozhou', 'chaohu', 'chizhou', 'chuzhou', 'fuyang', 'hefei', 'huaibei', 'huainan',
+                  'huangshan', 'luan', 'maanshan', 'suzhou', 'tongling', 'wuhu', 'xuancheng'],
+        'guizhou': ["anshun", "bijie", "guiyang", "liupanshui", "shenghui", "tongren", "shenghui2", "zunyi", ],
+        'gansu': ["baiyin", "dingxi", "gansu", "jiayuguan", "jiuquan", "lanzhou", "longnan", "pingliang", "qingyang", "tianshui", "wuwei",
+                  "zhangye", ]
+
+    }
+
+# q, m = 0, 0
+# for i in zhulong_diqu_dict:
+#     m += 1
+#     for k in zhulong_diqu_dict[i]:
+#         print(k)
+#         q += 1
+# print(q, m)
