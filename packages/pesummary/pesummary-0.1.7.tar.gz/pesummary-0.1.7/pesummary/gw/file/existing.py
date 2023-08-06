ec301ee0ecaae2eeb9c1616b20bb27b7260d744c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package pesummary/0.1.7/pesummary-0.1.7.tar.gz

---
+++ packages/pesummary/pesummary-0.1.7.tar.gz/pesummary-0.1.7/pesummary/gw/file/existing.py
@@ -0,0 +1,96 @@
+# Copyright (C) 2018  Charlie Hoy <charlie.hoy@ligo.org>
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from pesummary.core.file.existing import ExistingFile
+from pesummary.core.file.one_format import load_recusively
+
+
+class GWExistingFile(ExistingFile):
+    """This class handles the existing posterior_samples.h5 file
+
+    Parameters
+    ----------
+    parser: argparser
+        The parser containing the command line arguments
+
+    Attributes
+    ----------
+    existing_file: str
+        the path to the existing posterior_samples.h5 file
+    existing_approximants: list
+        list of approximants that have been used in the previous analysis
+    existing_labels: list
+        list of labels that have been used in the previous analysis
+    existing_samples: nd list
+        nd list of samples stored for each approximant used in the previous
+        analysis
+    """
+    def __init__(self, existing_webdir):
+        self.existing = existing_webdir
+        self.existing_data = []
+
+    def _grab_data_from_dictionary(self, dictionary):
+        """
+        """
+        labels = list(dictionary["posterior_samples"].keys())
+        existing_structure = {
+            i: j for i in labels for j in
+            dictionary["posterior_samples"]["%s" % (i)].keys()}
+        labels = list(existing_structure.keys())
+
+        parameter_list, sample_list, approx_list, inj_list = [], [], [], []
+        for num, i in enumerate(labels):
+            p = [j for j in dictionary["posterior_samples"]["%s" % (i)]["parameter_names"]]
+            s = [j for j in dictionary["posterior_samples"]["%s" % (i)]["samples"]]
+            if "injection_data" in dictionary.keys():
+                inj = [j for j in dictionary["injection_data"]["%s" % (i)]["injection_values"]]
+                inj_list.append(inj)
+            if isinstance(p[0], bytes):
+                parameter_list.append([j.decode("utf-8") for j in p])
+            else:
+                parameter_list.append([j for j in p])
+            sample_list.append(s)
+            psd, cal, config = None, None, None
+            if "config_file" in dictionary.keys():
+                config, = load_recusively("config_file", dictionary)
+            if "psds" in dictionary.keys():
+                psd, = load_recusively("psds", dictionary)
+            if "calibration_envelope" in dictionary.keys():
+                cal, = load_recusively("calibration_envelope", dictionary)
+            if "approximant" in dictionary.keys():
+                approx_list.append(dictionary["approximant"]["%s" % (i)])
+            else:
+                approx_list.append(None)
+        return labels, parameter_list, sample_list, psd, cal, config, approx_list, inj_list
+
+    @property
+    def existing_psd(self):
+        return self.existing_data[3]
+
+    @property
+    def existing_calibration(self):
+        return self.existing_data[4]
+
+    @property
+    def existing_config(self):
+        return self.existing_data[5]
+
+    @property
+    def existing_approximant(self):
+        return self.existing_data[6]
+
+    @property
+    def existing_injection(self):
+        return self.existing_data[7]
---
+++ packages/pesummary/pesummary-0.1.7.tar.gz/pesummary-0.1.7/setup.py
@@ -0,0 +1,95 @@
+# Copyright (C) 2018  Charlie Hoy <charlie.hoy@ligo.org>
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from setuptools import setup
+import subprocess
+
+version = "0.1.7"
+
+
+def full_description():
+    """Get the full readme
+    """
+    with open("README.md", "r") as f:
+        readme = f.read()
+    return readme
+
+
+def write_version_file(version):
+    """Add the version number and the git hash to the file
+    'pesummary.__init__.py'
+
+    Parameters
+    ----------
+    version: str
+        the release version of the code that you are running
+    """
+    try:
+        git_log = subprocess.check_output(
+            ["git", "log", "-1", "--pretty=format:%h"]).decode("utf-8")
+    except Exception as e:
+        print("Unable to obtain git version information, because %s" % (e))
+        git_log = ""
+
+    with open("pesummary/.version", "w") as f:
+        f.writelines(["%s %s" % (version, git_log)])
+    return ".version"
+
+
+readme = full_description()
+version_file = write_version_file(version)
+
+setup(name='pesummary',
+      version=version,
+      description='Python package to produce summary pages for Parameter '
+                  'estimation codes',
+      author='Charlie Hoy',
+      author_email='charlie.hoy@ligo.org',
+      url='https://git.ligo.org/lscsoft/pesummary',
+      download_url='https://git.ligo.org/lscsoft/pesummary',
+      install_requires=[
+          'numpy>=1.15.4',
+          'h5py',
+          'matplotlib',
+          'corner',
+          'tables>=3.4.4',
+          'deepdish',
+          'pandas',
+          'pygments',
+          'pluggy>=0.9.0',
+          'astropy>=2.0.9',
+          'lalsuite',
+          'configparser'],
+      include_package_data=True,
+      packages=['pesummary', 'pesummary.core', 'pesummary.core.webpage',
+                'pesummary.core.plots', 'pesummary.core.file',
+                'pesummary.gw', 'pesummary.gw.file',
+                'pesummary.gw.plots', 'pesummary.utils', 'cli'],
+      package_data={
+          'pesummary': [version_file],
+          'pesummary.core': ['js/*.js', 'css/*.css'],
+          'pesummary.gw.plots': ['cylon.csv'],
+      },
+      entry_points={
+          'console_scripts': [
+              'summaryconvert=pesummary.gw.file.one_format:main',
+              'summarypages=cli.summarypages:main',
+              'summaryplots=cli.summaryplots:main']},
+      classifiers=[
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6"],
+      license='MIT',
+      long_description=readme,
+      long_description_content_type='text/markdown')
