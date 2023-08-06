b46ef2a988e8654f61936dd6daaf6b551fed5189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:30 +0000

    Add package pyixnetwork/1.6/pyixnetwork-1.6.tar.gz

---
+++ packages/pyixnetwork/pyixnetwork-1.6.tar.gz/pyixnetwork-1.6/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import print_function
+from setuptools import setup
+import io
+
+import ixnetwork
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+install_requires = [r for r in required if r and r[0] != '#' and not r.startswith('git')]
+
+long_description = read('README.txt')
+
+
+setup(
+    name='pyixnetwork',
+    version=ixnetwork.__version__,
+    url='https://github.com/shmir/PyIxNetwork/',
+    license='Apache Software License',
+    author='Yoram Shamir',
+    install_requires=install_requires,
+    tests_require=['pytest'],
+    author_email='yoram@ignissoft.com',
+    description='Python OO API package to automate Ixia IxNetwork traffic generator',
+    long_description=long_description,
+    packages=['ixnetwork', 'ixnetwork.test', 'ixnetwork.api'],
+    include_package_data=True,
+    platforms='any',
+    test_suite='ixnetwork.test',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Testing :: Traffic Generation'],
+)
