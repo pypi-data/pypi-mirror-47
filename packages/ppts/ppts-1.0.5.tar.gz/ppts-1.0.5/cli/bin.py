56628d5847aea9819646245a23bcf26a2ffafec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:02 +0000

    Add package ppts/1.0.5/ppts-1.0.5.tar.gz

---
+++ packages/ppts/ppts-1.0.5.tar.gz/ppts-1.0.5/cli/bin.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask import jsonify
+from parse import parseUser, parsePages
+
+app = Flask(__name__)
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'demo.ppts')
+
+with open(filename, 'r') as file:
+    text = file.read()
+
+userinfo = parseUser(text)
+pages = parsePages(text)
+
+
+@app.route("/")
+def user():
+    return str(pages)
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
