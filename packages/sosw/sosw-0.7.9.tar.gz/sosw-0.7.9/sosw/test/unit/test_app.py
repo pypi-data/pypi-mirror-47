2a07fe411e2403bc5d414ce8d7b7d8ed31646241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:41 +0000

    Add package sosw/0.7.9/sosw-0.7.9.tar.gz

---
+++ packages/sosw/sosw-0.7.9.tar.gz/sosw-0.7.9/sosw/test/unit/test_app.py
@@ -0,0 +1,114 @@
+import boto3
+import os
+import unittest
+
+from unittest import mock
+from unittest.mock import MagicMock
+
+from sosw.app import Processor, get_lambda_handler
+from sosw.components.sns import SnsManager
+from sosw.components.siblings import SiblingsManager
+
+
+os.environ["STAGE"] = "test"
+os.environ["autotest"] = "True"
+
+
+class app_UnitTestCase(unittest.TestCase):
+    TEST_CONFIG = {'test': True}
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        try:
+            del (os.environ['AWS_LAMBDA_FUNCTION_NAME'])
+        except:
+            pass
+
+
+    @mock.patch("boto3.client")
+    def test_app_init(self, mock_boto_client):
+        Processor(custom_config=self.TEST_CONFIG)
+        self.assertTrue(True)
+
+
+    @mock.patch("boto3.client")
+    def test_app_init__fails_without_custom_config(self, mock_boto_client):
+        self.assertRaises(RuntimeError, Processor)
+
+
+    @mock.patch("boto3.client")
+    def test_app_init__with_some_clients(self, mock_boto_client):
+        custom_config = {
+            'init_clients': ['Sns', 'Siblings'],
+            'siblings_config': {
+                "test": True
+            }
+        }
+
+        processor = Processor(custom_config=custom_config)
+        self.assertIsInstance(getattr(processor, 'sns_client'), SnsManager,
+                              "SnsManager was not initialized. Probably boto3 sns instead of it.")
+        self.assertIsNotNone(getattr(processor, 'siblings_client'))
+
+
+    @mock.patch("boto3.client")
+    def test_app_init__boto_and_components_custom_clients(self, mock_boto_client):
+        custom_config = {
+            'init_clients': ['dynamodb', 'Siblings'],
+            'siblings_config': {
+                "test": True
+            }
+        }
+
+        processor = Processor(custom_config=custom_config)
+        self.assertIsInstance(getattr(processor, 'siblings_client'), SiblingsManager)
+
+        # Clients of boto3 will not be exactly of same type (something dynamic in boto3), so we can't compare classes.
+        # Let us assume that checking the class_name is enough for this test.
+        self.assertEqual(str(type(getattr(processor, 'dynamodb_client'))), str(type(boto3.client('dynamodb'))))
+
+
+    @mock.patch("boto3.client")
+    def test_app_init__with_some_invalid_client(self, mock_boto_client):
+        custom_config = {
+            'init_clients': ['NotExists']
+        }
+        Processor(custom_config=custom_config)
+        mock_boto_client.assert_called_with('not_exists')
+
+
+    @mock.patch("sosw.app.get_config")
+    def test_app_calls_get_config(self, mock_ssm):
+
+        mock_ssm.return_value = {'mock': 'called'}
+        os.environ['AWS_LAMBDA_FUNCTION_NAME'] = 'test_func'
+
+        Processor(custom_config=self.TEST_CONFIG)
+        mock_ssm.assert_called_once_with('test_func_config')
+
+
+    # @unittest.skip("https://github.com/bimpression/sosw/issues/40")
+    # def test__account(self):
+    #     raise NotImplementedError
+    #
+    #
+    # @unittest.skip("https://github.com/bimpression/sosw/issues/40")
+    # def test__region(self):
+    #     raise NotImplementedError
+
+
+    def test_lambda_handler(self):
+
+        class Child(Processor):
+            def __call__(self, event):
+                return event.get('k')
+
+        lambda_handler = get_lambda_handler(Child, self.TEST_CONFIG)
+        self.assertIsNotNone(lambda_handler)
+
+        result = lambda_handler(event={'k': 'success'}, context={})
+        self.assertEqual(result, 'success')
