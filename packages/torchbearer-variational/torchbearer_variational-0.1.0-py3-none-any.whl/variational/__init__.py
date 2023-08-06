2336200f89b48bc63a3a0112a210a64c820d3ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:08 +0000

    Add package torchbearer-variational/0.1.0/torchbearer_variational-0.1.0-py3-none-any.whl

---
+++ packages/torchbearer-variational/torchbearer_variational-0.1.0-py3-none-any.whl/variational/__init__.py
@@ -0,0 +1,44 @@
+"""
+Distributions
+------------------------------------
+
+..  automodule:: variational.distributions
+        :members:
+        :undoc-members:
+
+Divergences
+------------------------------------
+
+..  automodule:: variational.divergence
+        :members:
+        :undoc-members:
+
+Auto-Encoding
+------------------------------------
+
+..  automodule:: variational.auto_encoder
+        :members:
+        :undoc-members:
+
+Datasets
+------------------------------------
+
+..  automodule:: variational.datasets
+        :members:
+        :undoc-members:
+
+Visualisation
+------------------------------------
+
+..  automodule:: variational.visualisation
+        :members:
+        :undoc-members:
+"""
+
+from .version import __version__
+
+from .auto_encoder import *
+from .distributions import *
+from .divergence import *
+from .datasets import *
+from .visualisation import *
