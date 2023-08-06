cc49b43be51373ee075434a58fd2155b9e4e7a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:39 +0000

    Add package pymedphys-logfiles/0.9.0/pymedphys_logfiles-0.9.0-py3-none-any.whl

---
+++ packages/pymedphys-logfiles/pymedphys_logfiles-0.9.0-py3-none-any.whl/pymedphys_logfiles/_install_requires.py
@@ -0,0 +1,11 @@
+install_requires = [
+    "attrs",
+    "matplotlib",
+    "numpy >= 1.12",
+    "pandas",
+    "pymedphys_databases >= 0.9.0, < 0.10.0",
+    "pymedphys_fileformats >= 0.9.0, < 0.10.0",
+    "pymedphys_mudensity >= 0.9.0, < 0.10.0",
+    "pymedphys_utilities >= 0.9.0, < 0.10.0",
+    "python_dateutil"
+]
