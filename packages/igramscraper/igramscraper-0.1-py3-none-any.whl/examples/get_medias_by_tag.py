501e33f4f7a491e2735f08b223d790a01168c80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:07 +0000

    Add package igramscraper/0.1/igramscraper-0.1-py3-none-any.whl

---
+++ packages/igramscraper/igramscraper-0.1-py3-none-any.whl/examples/get_medias_by_tag.py
@@ -0,0 +1,16 @@
+from context import Instagram # pylint: disable=no-name-in-module
+
+instagram = Instagram.with_credentials('username', 'password', 'path/to/cache/folder')
+instagram.login()
+
+medias = instagram.get_medias_by_tag('instagood', count=20)
+
+for media in medias:
+    print(media)
+    print('Account info:')
+    account = media.owner
+    print('Id', account.identifier)
+    # print('Username', account.username)
+    # print('Full Name', account.full_name)
+    # print('Profile Pic Url', account.get_profile_picture_url_hd())
+    print('--------------------------------------------------')
