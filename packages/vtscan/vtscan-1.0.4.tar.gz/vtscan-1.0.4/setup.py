f51eec3b55d90a02bcc97ae88975c5dc91d94857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:12 +0000

    Add package vtscan/1.0.4/vtscan-1.0.4.tar.gz

---
+++ packages/vtscan/vtscan-1.0.4.tar.gz/vtscan-1.0.4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import os
+from vtscan import vtscan
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+s = setup(
+	name='vtscan',
+	version=vtscan.__version__,
+	license='MIT',
+	description='Utility to scan for malicious files using the VirusTotal API.',
+	long_description=read("README.md"),
+	long_description_content_type='text/markdown',
+	keywords="security,scanner",
+	url='https://github.com/prahladyeri/vtscan',
+	packages=find_packages(),
+	#scripts=['./config.json'],
+	#package_data={'vtscan': ['config.json']},
+	include_package_data=True,
+	entry_points={
+		"console_scripts": [
+			"vtscan = vtscan.vtscan:main",
+		],
+	},
+	install_requires=['requests'],
+	author='Prahlad Yeri',
+	author_email='prahladyeri@yahoo.com',
+	)
