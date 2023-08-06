5d5f53ce9bf692173d35fd0db02d17f9fcad052e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:12 +0000

    Add package trepr/0.1.0.dev57/trepr-0.1.0.dev57-py3-none-any.whl

---
+++ packages/trepr/trepr-0.1.0.dev57-py3-none-any.whl/trepr/__init__.py
@@ -0,0 +1,25 @@
+"""
+trepr package
+
+Package for reproducible and traceable analysis of trepr data.
+
+Available modules
+-----------------
+:mod:`trepr.dataset`
+    Organise dataset, consisting of data and metadata, and the metadata.
+:mod:`trepr.io`
+    Import trepr raw data in Freiburg Speksim format including its metadata.
+:mod:`trepr.processing`
+    Process data in datasets.
+:mod:`trepr.analysis`
+    Analyse data in datasets.
+:mod:`trepr.plotting`
+    Plot data in datasets.
+:mod:`trepr.fitpy_interface`
+    Call FitPy to fit datasets.
+:mod:`trepr.report`
+    Create reports with given datasets.
+
+
+
+"""
