8ecda04c9e91ccb9a2f87a1cee814fd6de34909f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:48 +0000

    Add package TDXLib/0.1.3/TDXLib-0.1.3.tar.gz

---
+++ packages/TDXLib/TDXLib-0.1.3.tar.gz/TDXLib-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='TDXLib',
+    description='a python library for interacting with the TeamDynamix Web API',
+    version='0.1.3',
+    packages=['tdxlib'],
+    url='https://github.com/cedarville-university/tdxlib',
+    license='GNU General Public License v3.0',
+    long_description='This module provides options for programmatically affectin TDX objects, including tickets, '
+                     'people, accounts, assets, and groups.',
+    install_requires=['python-dateutil','requests'],
+    python_requires='>=3.6'
+)
