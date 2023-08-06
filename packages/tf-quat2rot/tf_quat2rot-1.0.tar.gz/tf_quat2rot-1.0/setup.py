6ed1bdfb70a62f9035a80fe754589fed1096627a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:34 +0000

    Add package tf-quat2rot/1.0/tf_quat2rot-1.0.tar.gz

---
+++ packages/tf-quat2rot/tf_quat2rot-1.0.tar.gz/tf_quat2rot-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='tf_quat2rot',
+      version='1.0',
+      description='Convert between quaternion and rotation matrices in tensorflow.',
+      url='http://github.com/risteon/tf_quat2rot',
+      author='Christoph Rist',
+      author_email='c.rist@posteo.de',
+      license='MIT',
+      packages=['tf_quat2rot'],
+      zip_safe=False,
+      install_requires=['tensorflow'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      python_requires='>=3.5',
+      )
