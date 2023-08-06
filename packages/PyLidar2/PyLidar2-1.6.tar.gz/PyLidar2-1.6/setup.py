5d89437810b4575513c3aa19123d905b973f7df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:22 +0000

    Add package PyLidar2/1.6/PyLidar2-1.6.tar.gz

---
+++ packages/PyLidar2/PyLidar2-1.6.tar.gz/PyLidar2-1.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+with open("D:\Github\PyLidar2\README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyLidar2",
+    python_requires="<3",
+    version="1.6",
+    author="Lakshman mallidi",
+    author_email="lakshman.mallidi@gmail.com",
+    description="Library for Lidar. Currently supports YdLidar(X4,G4) from http://www.ydlidar.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lakshmanmallidi/PyLidar2",
+    packages=['PyLidar2'],
+    install_requires=[
+        'pyserial',
+        'enum'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+   
