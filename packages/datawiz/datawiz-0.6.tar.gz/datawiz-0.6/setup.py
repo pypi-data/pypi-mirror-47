07fbc55ac008dc21291afaeec10b1cca3c4f85db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:24 +0000

    Add package datawiz/0.6/datawiz-0.6.tar.gz

---
+++ packages/datawiz/datawiz-0.6.tar.gz/datawiz-0.6/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'datawiz',         
+  packages = ['datawiz'],   
+  version = '0.6',             
+  description = 'DataWiz helps new data learners, hobbyist and industry practitioners write Machine Learning code faster by doing the heavy-lifting such as data cleaning, data transformation, preparation and more! ',   # Give a short description about your library
+  author = 'Koye Sodipo',                   
+  author_email = 'koye.sodipo@gmail.com',      
+  #url = 'https://github.com/ksodipo/DataWiz',   
+  #download_url = 'https://github.com/ksodipo/DataWiz/archive/v0.5-beta.tar.gz',    
+  keywords = ['Data Engineering', 'data preparation', 'data science', 'data cleaning'],   
+  install_requires=[            
+          'numpy',
+          'pandas',
+          'sklearn',
+          'scipy'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',  
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
