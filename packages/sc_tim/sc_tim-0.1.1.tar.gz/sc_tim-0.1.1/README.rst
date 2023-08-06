d5da1ccbca60c8ed56adcb3cc472a49c10e46f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:08 +0000

    Add package sc_tim/0.1.1/sc_tim-0.1.1.tar.gz

---
+++ packages/sc_tim/sc_tim-0.1.1.tar.gz/sc_tim-0.1.1/README.rst
@@ -0,0 +1,5 @@
+Introduction:
+
+A convenient tool for marker detection based on single cell RNA-seq data.
+
+See usage at https://github.com/Frank-Orwell/scTIM
---
+++ packages/sc_tim/sc_tim-0.1.1.tar.gz/sc_tim-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+ 
+setup(
+		name="sc_tim",
+		version="0.1.1",
+		author="Zhanying Feng",
+		author_email="zyfeng@amss.ac.cn",
+		description="scTIM is a convenient tool for cell-type indicative marker detection based on single cell RNA-seq data",
+		long_description=open("README.rst").read(),
+		license="AMSS",
+		url="https://github.com/Frank-Orwell/scTIM",
+		packages=find_packages(),
+		install_requires=[
+			"numpy",
+			],
+		classifiers=[
+			"Programming Language :: Python",
+			"Programming Language :: Python :: 3",
+			"Programming Language :: Python :: 3.5",
+		],
+)
+
