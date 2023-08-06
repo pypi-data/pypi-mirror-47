47c560c132ac992717286063140c4d9fe9bcf3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:34 +0000

    Add package chitti/0.1.1/chitti-0.1.1.tar.gz

---
+++ packages/chitti/chitti-0.1.1.tar.gz/chitti-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='chitti',
+     version='0.1.1',
+     author="Raja Mohan Reddy",
+     author_email="rajamohanrdy@gmail.com",
+     description="Python utility packages",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/raja-1996",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
