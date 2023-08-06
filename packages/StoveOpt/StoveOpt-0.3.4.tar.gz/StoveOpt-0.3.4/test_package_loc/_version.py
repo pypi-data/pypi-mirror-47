b3ee0433d0b22ba203599750380e2008f22e90f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:42 +0000

    Add package StoveOpt/0.3.4/StoveOpt-0.3.4.tar.gz

---
+++ packages/StoveOpt/StoveOpt-0.3.4.tar.gz/StoveOpt-0.3.4/test_package_loc/_version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 10:22:45 2019
+
+@author: Lee
+"""
+__version_info__ = (0, 3, 3)
+__version__ = '.',join(map(str,__version_info__[:3]))
+if len(__version_info__) == 4:
+    __version__+=__version_info[-1]
+    
+    
