06da01328781de93d4ee4accc99bafdb2da06f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:12 +0000

    Add package gitutils/1.3.0/gitutils-1.3.0.tar.gz

---
+++ packages/gitutils/gitutils-1.3.0.tar.gz/gitutils-1.3.0/branch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+report on git repos not on the expected branch e.g. 'master'
+"""
+from argparse import ArgumentParser
+from gitutils import findbranch
+from pathlib import Path
+import asyncio
+import os
+
+
+async def find_branch(branch: str, path: Path):
+    path = Path(path).expanduser()
+
+    async for b in findbranch(branch, path):
+        print(b[0], ' => ', b[1])
+
+
+def main():
+    p = ArgumentParser()
+    p.add_argument('codepath', help='path to code root', nargs='?', default='~/code')
+    p.add_argument('mainbranch', nargs='?',
+                   default='master', help='name of your main branch')
+    P = p.parse_args()
+
+    if os.name == 'nt':
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+    asyncio.run(find_branch(P.mainbranch, P.codepath))
+
+
+if __name__ == '__main__':
+    main()
