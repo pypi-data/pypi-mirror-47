c52480dd35b418e16bdcbd0f5db165c37f07e2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:00 +0000

    Add package NativDebugging/35/NativDebugging-35.tar.gz

---
+++ packages/NativDebugging/NativDebugging-35.tar.gz/NativDebugging-35/setup.py
@@ -0,0 +1,41 @@
+# python setup.py bdist_wheel
+
+import sys
+if sys.platform.lower().startswith('win'):
+    packagesNames = ['NativDebugging', 'NativDebugging/Win32']
+    packagesDirs = {'NativDebugging' : 'src', 'NativDebugging/Win32' : 'src/Win32'}
+    ExtraDataFiles = [('Lib\\site-packages\\NativDebugging\\Win32', (
+        'src/Win32/memReaderAMD64.exe',
+        'src/Win32/memReaderIa64.exe',
+        'src/Win32/memReaderx86.exe',
+        'src/Win32/pythonGateAMD64.dll',
+        'src/Win32/pythonGatex86.dll'))]
+else:
+    packagesNames = ['NativDebugging', 'NativDebugging/Linux', 'NativDebugging/Unix']
+    packagesDirs = { \
+            'NativDebugging' : 'src', \
+            'NativDebugging/Linux' : 'src/Linux',\
+            'NativDebugging/Unix' : 'src/Unix'}
+    ExtraDataFiles = []
+packagesNames.append('NativDebugging/File')
+packagesNames.append('NativDebugging/MemoryDump')
+packagesNames.append('NativDebugging/Patterns')
+packagesDirs['NativDebugging/File'] = 'src/File'
+packagesDirs['NativDebugging/MemoryDump'] = 'src/MemoryDump'
+packagesDirs['NativDebugging/Patterns'] = 'src/Patterns'
+
+from setuptools import setup
+setup(
+        name = 'NativDebugging',
+        version = '35',
+        description = 'Debugging tools for many platforms',
+        author = 'Assaf Nativ',
+        author_email = 'Nativ.Assaf@gmail.com',
+        packages = packagesNames,
+        package_dir = packagesDirs,
+        url = 'https://github.com/assafnativ/NativDebugging',
+        keywords = ['debugger', 'memory', 'patterns', 'research', 'lowlevel', 'native'],
+        license = "LGPLv3",
+        data_files = [('Lib\\\site-packages', ('NativDebugging.pth',))] + ExtraDataFiles)
+
+
