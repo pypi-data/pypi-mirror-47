9d50241b17e04499ad88c0608e1ba9b7a49de36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:07 +0000

    Add package jMathLib/3/jMathLib-3.tar.gz

---
+++ packages/jMathLib/jMathLib-3.tar.gz/jMathLib-3/jMathLib/basicMathLib.py
@@ -0,0 +1,24 @@
+def add(First_num, Second_num):
+    out=First_num+Second_num
+    return out
+def subtract(Starting_num, Num_to_subtract_with):
+    out=Starting_num-Num_to_subtract_with
+    return out
+def multiply(First_num, Second_num):
+    out=First_num*Second_num
+    return out
+def divide(Starting_num, Num_to_divide_with):
+    out=Starting_num/Num_to_divide_with
+    return out
+def remainder(Starting_num, Num_to_divide_with):
+    out=Starting_num%Num_to_divide_with
+    return out
+def floorDivision(Starting_num, Num_to_divide_with):
+    out=Starting_num//Num_to_divide_with
+    return out
+def exponent(Starting_num, Exponent_num):
+    out=Starting_num**Exponent_num
+    return out
+def squareRoot(Num_to_get_square_root_of):
+    Num_to_get_square_root_of**(0.5)
+    return out
