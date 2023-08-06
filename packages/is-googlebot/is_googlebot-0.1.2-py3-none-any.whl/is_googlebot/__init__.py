1813e2df298c71caa5f04e63e91ff0c5f2df8061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:59 +0000

    Add package is-googlebot/0.1.2/is_googlebot-0.1.2-py3-none-any.whl

---
+++ packages/is-googlebot/is_googlebot-0.1.2-py3-none-any.whl/is_googlebot/__init__.py
@@ -0,0 +1,42 @@
+import re
+import socket
+from functools import lru_cache
+
+
+# __all__ = 'is_googlebot', 'gethostbyaddr', 'gethostbyname', 'test',
+GOOGLEBOT_RE = re.compile(r'''\b(google|googlebot)\b''', re.I)
+GOOGLEBOT_DOMAINS = {'googlebot', 'google'}
+
+
+@lru_cache(4096)
+def gethostbyaddr(ip_address):
+    try:
+        return socket.gethostbyaddr(ip_address)[0]
+    except (socket.herror, socket.error):
+        return False
+
+
+@lru_cache(4096)
+def gethostbyname(hostname):
+    return socket.gethostbyname(hostname)
+
+
+def is_googlebot(ip_address, user_agent=None):
+    if user_agent is not None and not GOOGLEBOT_RE.search(user_agent):
+        return False
+
+    host = gethostbyaddr(ip_address)
+    if host is False:
+        return False
+
+    host_parts = host.split('.')
+    try:
+        if host_parts[-1] != 'com' or host_parts[-2] not in GOOGLEBOT_DOMAINS:
+            return False
+    except IndexError:
+        return False
+
+    return gethostbyname(host) == ip_address
+
+
+test = is_googlebot
