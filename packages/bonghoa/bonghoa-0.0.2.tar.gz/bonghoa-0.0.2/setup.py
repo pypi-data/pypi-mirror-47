d3a20447dec86fc776b7439af61b17e37149a1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:30 +0000

    Add package bonghoa/0.0.2/bonghoa-0.0.2.tar.gz

---
+++ packages/bonghoa/bonghoa-0.0.2.tar.gz/bonghoa-0.0.2/setup.py
@@ -0,0 +1,20 @@
+
+import setuptools
+
+setuptools.setup(
+     name='bonghoa',  
+     version='0.0.2',
+     scripts=['bonghoa'] ,
+     author="Bong Hoa",
+     author_email="nguyendieuhoa15@gmail.com",
+     description="test project",
+     long_description="what is it",
+   long_description_content_type="text/markdown",
+     url="https://github.com/bonghoa15/bong_proj1",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
