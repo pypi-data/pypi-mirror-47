fd77c7bc508cfa6b2975b54761e6462f5c55a17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package mmodes/0.2.1/mmodes-0.2.1.tar.gz

---
+++ packages/mmodes/mmodes-0.2.1.tar.gz/mmodes-0.2.1/mmodes/__init__.py
@@ -0,0 +1,8 @@
+from .mmodes import NoBiomassException
+from .mmodes import NotIntegratorError
+from .mmodes import dModel
+from .mmodes import dMetabolite
+from .mmodes import Consortium
+
+__all__ = ["NoBiomassException", "NotIntegratorError", "dModel", "dMetabolite", "Consortium", "io"]
+__version__ = "0.2.0"
