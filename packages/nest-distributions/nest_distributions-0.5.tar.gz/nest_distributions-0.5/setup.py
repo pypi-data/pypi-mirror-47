dac7acb0d182f0cde1d1f084d6b7379225f5ae29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:42 +0000

    Add package nest-distributions/0.5/nest_distributions-0.5.tar.gz

---
+++ packages/nest-distributions/nest_distributions-0.5.tar.gz/nest_distributions-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nest_distributions',
+      version='0.5',
+      description='Gaussian and Binomial distributions',
+      packages=['nest_distributions'],
+      author = 'Sachin Rai',
+      author_email = 'raisachin416@gmail.com',
+      zip_safe=False)
