9b1ab4c8fce53155de0ee5567cc47bf1ba51e085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:21 +0000

    Add package zato-vault-client/1.4/zato-vault-client-1.4.tar.gz

---
+++ packages/zato-vault-client/zato-vault-client-1.4.tar.gz/zato-vault-client-1.4/setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2019 Zato Source s.r.o. https://zato.io
+
+Licensed under LGPLv3, see LICENSE.txt for terms and conditions.
+"""
+
+# Part of Zato - ESB, SOA, REST, APIs and Cloud Integrations in Python
+# https://zato.io
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os, sys
+from setuptools import setup, find_packages
+
+version = '1.4'
+
+LONG_DESCRIPTION = """"""
+
+package_dir = b'src' if sys.version_info.major == 2 else 'src'
+
+setup(
+      name = 'zato-vault-client',
+      version = version,
+
+      author = 'Zato Source s.r.o.',
+      author_email = 'info@zato.io',
+      url = 'https://zato.io/docs',
+      description = 'A convenience Vault client for Zato-related projects ',
+      long_description = LONG_DESCRIPTION,
+      platforms = ['OS Independent'],
+      license = 'GNU Lesser General Public License v3 (LGPLv3)',
+
+      package_dir = {'':package_dir},
+      packages = find_packages(package_dir),
+
+      namespace_packages = ['zato'],
+      install_requires = [
+          'hvac',
+          'six',
+        ],
+
+      zip_safe = False,
+
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Topic :: Communications',
+        'Topic :: Education :: Testing',
+        'Topic :: Internet',
+        'Topic :: Internet :: Proxy Servers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities',
+        ],
+)
