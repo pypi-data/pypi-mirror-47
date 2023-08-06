f12e10c6c93885948b1a3a4e7f9ef7fa9be357ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:55 +0000

    Add package g85-sentry-auth-oidc/2.0.2/g85-sentry-auth-oidc-2.0.2.tar.gz

---
+++ packages/g85-sentry-auth-oidc/g85-sentry-auth-oidc-2.0.2.tar.gz/g85-sentry-auth-oidc-2.0.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+install_requires = [
+    'sentry>=7.0.0',
+    'requests<2.19.0,>=2.18.4',
+]
+
+tests_require = [
+    'flake8>=3.5',
+]
+
+setup(
+    name='g85-sentry-auth-oidc',
+    version='2.0.2',
+    author='Max Wittig',
+    author_email='max.wittig@siemens.com',
+    url='https://www.getsentry.com',
+    description='OpenID Connect authentication provider for Sentry',
+    long_description=readme,
+    license='Apache 2.0',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'tests': tests_require},
+    include_package_data=True,
+    entry_points={
+        'sentry.apps': [
+            'sentry_auth_oidc = sentry_auth_oidc',
+        ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ],
+)
