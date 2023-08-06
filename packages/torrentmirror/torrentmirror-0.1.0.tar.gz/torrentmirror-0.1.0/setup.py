fd53979282757bf9687b18b49e6f121f36bb452b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:20 +0000

    Add package torrentmirror/0.1.0/torrentmirror-0.1.0.tar.gz

---
+++ packages/torrentmirror/torrentmirror-0.1.0.tar.gz/torrentmirror-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['torrentmirror']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['delver>=0.1.6,<0.2.0',
+ 'docopt>=0.6.2,<0.7.0',
+ 'robobrowser>=0.5.3,<0.6.0',
+ 'tabulate>=0.8.3,<0.9.0']
+
+entry_points = \
+{'console_scripts': ['torrentmirror = torrentmirror:main']}
+
+setup_kwargs = {
+    'name': 'torrentmirror',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'David Francos',
+    'author_email': 'opensource@davidfrancos.net',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
