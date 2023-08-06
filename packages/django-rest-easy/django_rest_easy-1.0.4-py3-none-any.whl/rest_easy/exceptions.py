8e5861a16d5d52898c781faf34b16ba2ab1c457f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:23 +0000

    Add package django-rest-easy/1.0.4/django_rest_easy-1.0.4-py3-none-any.whl

---
+++ packages/django-rest-easy/django_rest_easy-1.0.4-py3-none-any.whl/rest_easy/exceptions.py
@@ -0,0 +1,11 @@
+"""
+This module contains exceptions native to django-rest-easy.
+"""
+
+__all__ = ['RestEasyException']
+
+
+class RestEasyException(Exception):
+    """
+    Default django-rest-easy exception class.
+    """
