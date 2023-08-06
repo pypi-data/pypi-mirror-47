aa9233bf876c976a34b48b4df3ae2ea275862a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:03 +0000

    Add package dvt/0.0.6/dvt-0.0.6.tar.gz

---
+++ packages/dvt/dvt-0.0.6.tar.gz/dvt-0.0.6/dvt/aggregate/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import warnings
+import os
+
+from . import core
+from . import cut
+
+__version__ = "0.1.0"
---
+++ packages/dvt/dvt-0.0.6.tar.gz/dvt-0.0.6/dvt/annotate/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from . import core
+from . import diff
+from . import embed
+from . import face
+from . import png
+
+__version__ = "0.1.0"
