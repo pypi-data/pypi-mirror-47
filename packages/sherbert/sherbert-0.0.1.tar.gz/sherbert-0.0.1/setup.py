4f084ce99c6bcbdc40e25c9adb7409b9bd109ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:09 +0000

    Add package sherbert/0.0.1/sherbert-0.0.1.tar.gz

---
+++ packages/sherbert/sherbert-0.0.1.tar.gz/sherbert-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'sherbert',
+  packages = ['sherbert'],
+  version = '0.0.1',
+  description = 'sherbert',
+  long_description = '',
+  author = '',
+  license = '',
+  url = 'https://github.com/alvations/sherbert',
+  keywords = [],
+  classifiers = [],
+  install_requires = ['six', 'click', 'joblib', 'tqdm'],
+)
