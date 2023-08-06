c92b4e5f23cb8849d8b4231ad158b0a22e102a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:05 +0000

    Add package hg-evolve/9.0.0/hg-evolve-9.0.0.tar.gz

---
+++ packages/hg-evolve/hg-evolve-9.0.0.tar.gz/hg-evolve-9.0.0/hgext3rd/evolve/metadata.py
@@ -0,0 +1,11 @@
+# define Mercurial extension metadata for evolution
+#
+# Copyright 2017 Pierre-Yves David <pierre-yves.david@ens-lyon.org>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+__version__ = '9.0.0'
+testedwith = '4.5.2 4.6.2 4.7 4.8 4.9 5.0'
+minimumhgversion = '4.5'
+buglink = 'https://bz.mercurial-scm.org/'
