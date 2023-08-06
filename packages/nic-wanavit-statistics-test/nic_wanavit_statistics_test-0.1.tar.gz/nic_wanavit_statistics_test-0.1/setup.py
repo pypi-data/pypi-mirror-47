e80f2a81f73f57dad5dc28012cdd7a776f115378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:33 +0000

    Add package nic-wanavit-statistics-test/0.1/nic_wanavit_statistics_test-0.1.tar.gz

---
+++ packages/nic-wanavit-statistics-test/nic_wanavit_statistics_test-0.1.tar.gz/nic_wanavit_statistics_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nic_wanavit_statistics_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nic_wanavit_statistics_test'],
+      zip_safe=False)
