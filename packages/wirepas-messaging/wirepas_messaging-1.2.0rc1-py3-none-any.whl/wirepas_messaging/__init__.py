be1444e9b12eb458be4ade8d642138f50b278f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:43 +0000

    Add package wirepas-messaging/1.2.0rc1/wirepas_messaging-1.2.0rc1-py3-none-any.whl

---
+++ packages/wirepas-messaging/wirepas_messaging-1.2.0rc1-py3-none-any.whl/wirepas_messaging/__init__.py
@@ -0,0 +1,33 @@
+# Wirepas Oy
+
+from . import gateway
+from . import nanopb
+
+try:
+    from . import wpe
+except ImportError:
+    print("Could not import WPE handles")
+
+try:
+    from . import wnt
+except ImportError:
+    print("Could not import WPE handles")
+
+
+from google.protobuf.internal import api_implementation
+
+warning_msg = """
+***********************************************************************
+* WARNING:
+*     You are using the pure python protobuf implementation.
+*     For better results, please consider using the cpp version.
+*     For more information please consult:
+*     https://github.com/protocolbuffers/protobuf/tree/master/python
+***********************************************************************
+"""
+if api_implementation._default_implementation_type == "python":
+    print(warning_msg)
+
+__title__ = "wirepas_messaging"
+__version__ = "1.2.0rc1"
+
