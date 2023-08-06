af9839f0f42ec7e44355de070d8f309bdc0d18a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:31 +0000

    Add package unix-ar/0.2/unix_ar-0.2.tar.gz

---
+++ packages/unix-ar/unix_ar-0.2.tar.gz/unix_ar-0.2/setup.py
@@ -0,0 +1,34 @@
+import io
+import os
+from setuptools import setup
+
+
+# pip workaround
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+# Need to specify encoding for PY3, which has the worst unicode handling ever
+with io.open('README.md', encoding='utf-8') as fp:
+    description = fp.read()
+setup(name='unix_ar',
+      version='0.2',
+      py_modules=['unix_ar'],
+      python_requires='~=3.6',
+      description="AR file handling",
+      author="Remi Rampin",
+      author_email='remirampin@gmail.com',
+      maintainer="Alan Justino",
+      maintainer_email='alan.justino@yahoo.com.br',
+      url='https://github.com/getninjas/unix_ar',
+      long_description=description,
+      long_description_content_type='text/markdown',
+      license='BSD',
+      keywords=['ar', 'archive', 'unix', 'deb'],
+      classifiers=[ # yapf: nolint
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+          'Topic :: System :: Archiving',
+          'Topic :: Utilities'])
