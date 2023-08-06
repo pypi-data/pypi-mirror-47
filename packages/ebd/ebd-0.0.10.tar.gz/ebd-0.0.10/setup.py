a0caf3de2e8f017c1c934837056c746b67ee7b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:29 +0000

    Add package ebd/0.0.10/ebd-0.0.10.tar.gz

---
+++ packages/ebd/ebd-0.0.10.tar.gz/ebd-0.0.10/setup.py
@@ -0,0 +1,68 @@
+import setuptools
+import setuptools.command
+import setuptools.command.install
+import setuptools.command.develop
+import os
+
+
+def get_rc_candidate():
+    home = os.path.expanduser("~")
+    print("home: {}".format(home))
+    bashrc = os.path.join(home, ".bashrc")
+    if os.path.exists(bashrc):
+        return bashrc
+    profile = os.path.join(home, ".profile")
+    if os.path.exists(profile):
+        return profile
+
+
+def setup_odus():
+    rc = get_rc_candidate()
+    if rc is not None:
+        with open(rc) as file:
+            print("rc file {} content:".format(rc))
+            print(file.read())
+    else:
+        print("no suitable rc found")
+
+
+class PostInstallCommand(setuptools.command.install.install):
+    def run(self):
+        print("install command")
+        setup_odus()
+        setuptools.command.install.install.run(self)
+
+
+class PostDevelopCommand(setuptools.command.develop.develop):
+    def run(self):
+        print("develop command")
+        setuptools.command.develop.develop.run(self)
+
+
+with open("README.md") as file:
+    long_description = file.read()
+
+with open("VERSION") as file:
+    version = file.read()
+
+setuptools.setup(
+    name="ebd",
+    version=version,
+    author="Fiely Teach",
+    author_email="admin@admin.ru",
+    description="Wrapper for LFSM-enabled systems.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    cmdclass={
+        "install": PostInstallCommand,
+        "develop": PostDevelopCommand
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
