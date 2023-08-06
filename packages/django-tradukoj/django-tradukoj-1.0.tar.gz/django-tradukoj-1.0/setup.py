e587ffc9ded668edef49f1ee53ebe67e7173833c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:03 +0000

    Add package django-tradukoj/1.0/django-tradukoj-1.0.tar.gz

---
+++ packages/django-tradukoj/django-tradukoj-1.0.tar.gz/django-tradukoj-1.0/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='django-tradukoj',
+    version='1.0',
+    packages=['tradukoj'],
+    description='A django IETF\'s BCP 47 DB-based translation system',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Develatio Technologies S.L.',
+    author_email='contacto@develat.io',
+    url='https://github.com/develatio/django-tradukoj/',
+    license='BSD',
+    install_requires=['Django>=2.0', 'langcodes >= 1.4.1'])
