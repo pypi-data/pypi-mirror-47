0c2f3d8ba8fb467f6634fc6e96f0dc1abc31e08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:35 +0000

    Add package webviz-subsurface/0.0.7/webviz-subsurface-0.0.7.tar.gz

---
+++ packages/webviz-subsurface/webviz-subsurface-0.0.7.tar.gz/webviz-subsurface-0.0.7/tests/test_parameter_distribution.py
@@ -0,0 +1,37 @@
+import mock
+import dash
+import pandas as pd
+from pytest_dash.wait_for import wait_for_element_by_css_selector
+from webviz_config.common_cache import cache
+from webviz_config.containers import ParameterDistribution
+
+# mocked functions
+get_parameters = 'webviz_subsurface.containers'\
+                '._parameter_distribution.get_parameters'
+
+
+def test_parameter_dist(dash_threaded):
+
+    app = dash.Dash(__name__)
+    app.css.config.serve_locally = True
+    app.scripts.config.serve_locally = True
+    app.config.suppress_callback_exceptions = True
+    cache.init_app(app.server)
+    driver = dash_threaded.driver
+    container_settings = {'scratch_ensembles': {'iter-0': ''}}
+    ensembles = ['iter-0']
+
+    with mock.patch(get_parameters) as mock_parameters:
+        mock_parameters.return_value = pd.read_csv('tests/data/parameters.csv')
+
+        p = ParameterDistribution(app, container_settings, ensembles)
+
+        app.layout = p.layout
+        dash_threaded(app)
+
+        my_component = wait_for_element_by_css_selector(
+                           driver,
+                           f'#{p.ens_matrix_id}'
+                       )
+        if not my_component.text.startswith('iter-0'):
+            raise AssertionError()
