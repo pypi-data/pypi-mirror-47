e1c1e0eac8b0f40a7b2b0663f2523048128739fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:31 +0000

    Add package jirap/0.0.2/jirap-0.0.2.tar.gz

---
+++ packages/jirap/jirap-0.0.2.tar.gz/jirap-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jirap",
+    version="0.0.2",
+    author="Xavier Mabras",
+    author_email="xavier.mabras@protonmail.ch",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xafardero/jirap",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
