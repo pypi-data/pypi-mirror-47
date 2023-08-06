f50e6459400024f9f5c59eaefc32897a8219812b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:41 +0000

    Add package idebug/0.0.15/idebug-0.0.15.tar.gz

---
+++ packages/idebug/idebug-0.0.15.tar.gz/idebug-0.0.15/idebug/__init__.py
@@ -0,0 +1,11 @@
+
+import os
+PKG_PATH = os.path.dirname(os.path.abspath(__file__))
+PJT_PATH = os.path.dirname(PKG_PATH)
+
+from .DataStructure import *
+from .etc import *
+from .html import *
+from .iinspect import *
+from .mongo import *
+from .performance import *
