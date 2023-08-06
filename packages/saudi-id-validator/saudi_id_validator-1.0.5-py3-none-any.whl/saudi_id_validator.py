56737ad13f235e46d7e504da8bd0e7df220813a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:05 +0000

    Add package saudi-id-validator/1.0.5/saudi_id_validator-1.0.5-py3-none-any.whl

---
+++ packages/saudi-id-validator/saudi_id_validator-1.0.5-py3-none-any.whl/saudi_id_validator.py
@@ -0,0 +1,17 @@
+def validate(id_number: int) -> bool:
+    """Validate format and checksum of the identity number.
+
+    :param id_number: Saudi Arabian identity number
+    :type id_number: int
+    """
+
+    digits = [int(d) for d in str(id_number)]
+
+    if len(digits) != 10 or digits[0] not in [1, 2]:
+        return False
+
+    odd_sum = sum(digits[-1::-2])
+    even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
+    checksum = (odd_sum + even_sum) % 10
+
+    return bool(checksum == 0)
