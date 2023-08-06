46994d4e6abd34c798f434ad38931292a86660c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:08 +0000

    Add package pythanos/0.1/pythanos-0.1.tar.gz

---
+++ packages/pythanos/pythanos-0.1.tar.gz/pythanos-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythanos',
+      version='0.1',
+      description='The aim of this package is to create a pipeline for data analysis and further extends to machine learning',
+      url='http://github.com/umar895/pythanos',
+      author='Umar',
+      author_email='m.umar.hameed@outlook.com',
+      license='MIT',
+      packages=['pythanos'],
+      zip_safe=False)
