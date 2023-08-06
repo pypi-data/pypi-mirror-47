4469c366fbe4b096e9d53cae488debe17c21ed02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:49 +0000

    Add package django-lazycrud/1.5.4/django-lazycrud-1.5.4.tar.gz

---
+++ packages/django-lazycrud/django-lazycrud-1.5.4.tar.gz/django-lazycrud-1.5.4/lazycrud/utils.py
@@ -0,0 +1,45 @@
+import types
+from datetime import datetime, date
+
+from django.utils import formats
+from django.utils.timezone import localtime
+from django.conf import settings
+
+def formatDate(dt):
+    if dt is None:
+        return 'n.d.'
+    return formats.date_format(dt, "SHORT_DATE_FORMAT")
+
+def formatDateTime(dt):
+    if dt is None:
+        return 'n.d.'
+    if settings.USE_TZ:
+        dt = localtime(dt)
+    return formats.date_format(dt, "SHORT_DATETIME_FORMAT")
+
+def fieldlabel(obj, key):
+    try:
+        return obj._meta.get_field(key).verbose_name.strip().capitalize() # strip serve per forzare l'esecuzione di ugettext_lazy
+    except:
+        try:
+            return getattr(obj, '%s_label' % key)()
+        except:
+            return key.capitalize().replace('_', ' ')
+
+def fieldvalue(obj, key):
+    try:
+        return getattr(obj, 'get_%s_display' % key)()
+    except:
+        try:
+            ret = getattr(obj, key)
+        except:
+            ret = None
+        if type(ret) == types.MethodType:
+            ret = ret()
+        if ret is None:
+            ret = ''
+        elif isinstance(ret, datetime):
+            ret = formatDateTime(ret)
+        elif isinstance(ret, date):
+            ret = formatDate(ret)
+        return ret
