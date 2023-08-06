4fee0ae58dd93eb643d2666d66f84f4dcd0d5438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:37 +0000

    Add package pyBigParser/2.1/pyBigParser-2.1.tar.gz

---
+++ packages/pyBigParser/pyBigParser-2.1.tar.gz/pyBigParser-2.1/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+	
+setuptools.setup(
+    name='pyBigParser',
+    version='2.1',
+    author='Nelson Carrasquel',
+    author_email='carrasquel@outlook.com',
+	description="Math parser for simple and compound strings math expressions evaluations",
+	long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    packages=['pybigparser',],
+    license='BSD License',
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
