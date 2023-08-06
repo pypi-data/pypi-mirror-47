7686a97aa18dc9a0ef45dd3e3c4ded976ca19841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:56 +0000

    Add package markyp-bootstrap4/0.1906.0/markyp-bootstrap4-0.1906.0.tar.gz

---
+++ packages/markyp-bootstrap4/markyp-bootstrap4-0.1906.0.tar.gz/markyp-bootstrap4-0.1906.0/test/test_navbars.py
@@ -0,0 +1,25 @@
+from markyp_bootstrap4.navbars import *
+
+def test_navbar_text():
+    pass
+
+def test_ExpandPoint():
+    pass
+
+def test_Theme():
+    pass
+
+def test_brand():
+    pass
+
+def test_collapse():
+    pass
+
+def test_navbar():
+    pass
+
+def test_navbar_nav():
+    pass
+
+def test_navbar_toggler():
+    pass
