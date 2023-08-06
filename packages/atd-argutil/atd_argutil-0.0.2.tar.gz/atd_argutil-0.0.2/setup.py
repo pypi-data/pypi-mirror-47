9bb069026072c9ec8dbcc746b5b4d35851f5feeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:24 +0000

    Add package atd-argutil/0.0.2/atd_argutil-0.0.2.tar.gz

---
+++ packages/atd-argutil/atd_argutil-0.0.2.tar.gz/atd_argutil-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atd_argutil",
+    version="0.0.2",
+    author="City of Austin",
+    author_email="transportation.data@austintexas.gov",
+    description="Reusable argument parser for Python scripts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cityofaustin/atd-utils-args",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta", 
+    ),
+)
+
