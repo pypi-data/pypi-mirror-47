b763c2a7f5d0c3c58ec973cf8b887b288aa78761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:25 +0000

    Add package django-privacy-killer/0.2.0/django-privacy-killer-0.2.0.tar.gz

---
+++ packages/django-privacy-killer/django-privacy-killer-0.2.0.tar.gz/django-privacy-killer-0.2.0/CHANGELOG.rst
@@ -0,0 +1,24 @@
+Change log
+==========
+
+`Next version`_
+~~~~~~~~~~~~~~~
+
+`0.2`_ (2019-06-12)
+~~~~~~~~~~~~~~~~~~~
+
+- Changed the Google Analytics snippet to always anonymize IPs by
+  default.
+- Dropped compatibility with Python 2.
+- Added Django 2.1, 2.2 and Django 3.0 to the matrix.
+- Reformatted the code using black.
+
+
+`0.1`_ (2017-08-29)
+~~~~~~~~~~~~~~~~~~~
+
+- Initial public version.
+
+.. _0.1: https://github.com/matthiask/django-privacy-killer/commit/89bb93c5cdba
+.. _0.2: https://github.com/matthiask/django-privacy-killer/compare/0.1...0.2
+.. _Next version: https://github.com/matthiask/django-privacy-killer/compare/0.2...master
---
+++ packages/django-privacy-killer/django-privacy-killer-0.2.0.tar.gz/django-privacy-killer-0.2.0/privacy_killer/templatetags/privacy_killer.py
@@ -0,0 +1,42 @@
+import re
+
+from django import template
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.html import mark_safe
+
+
+register = template.Library()
+
+
+SNIPPETS = [
+    (
+        r"^GTM-",
+        "privacy_killer/google_tag_manager_head.html",
+        "privacy_killer/google_tag_manager_body.html",
+    ),
+    (r"^UA-", "privacy_killer/google_analytics_head.html", None),
+]
+
+
+def _snippets(index):
+    parts = []
+    for id in getattr(settings, "PRIVACY_KILLER_IDS", ()):
+        for snippet in SNIPPETS:
+            if re.search(snippet[0], id) and snippet[index]:
+                parts.append(render_to_string(snippet[index], {"id": id}))
+                break
+        else:
+            # TODO Emit a warning.
+            pass
+    return mark_safe("".join(parts))
+
+
+@register.simple_tag
+def privacy_killer_head():
+    return _snippets(1)
+
+
+@register.simple_tag
+def privacy_killer_body():
+    return _snippets(2)
