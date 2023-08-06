6c444511886071d7d378e6da40919bfbf179da1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:06 +0000

    Add package alita-session/0.1.3/alita-session-0.1.3.tar.gz

---
+++ packages/alita-session/alita-session-0.1.3.tar.gz/alita-session-0.1.3/alita_session/mongo.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import copy
+import asyncio
+import motor.motor_asyncio
+from datetime import datetime
+from alita_session.base import *
+from dateutil.relativedelta import relativedelta
+
+
+class SessionManager(SessionInterface):
+    _client = None
+    _db = None
+    _collection = None
+    _database = None
+
+    async def get_session_collection(self):
+        if not self._collection:
+            loop = asyncio.get_event_loop()
+            _config = copy.copy(self.client_config)
+            self._database = _config.pop('db', 'default')
+            self._client = motor.motor_asyncio.AsyncIOMotorClient(io_loop=loop, **_config)
+            self._db = self._client[self._database]
+            self._collection = self._db[self.session_table_name]
+        return self._collection
+
+    async def delete(self, session_key):
+        collection = await self.get_session_collection()
+        await collection.remove({'session_key': session_key})
+
+    async def exists(self, session_key):
+        collection = await self.get_session_collection()
+        return await collection.find_one({'session_key': session_key})
+
+    async def get(self, session_key):
+        collection = await self.get_session_collection()
+        return await collection.find_one({
+            'session_key': session_key,
+            'expire_date': {'$gt': datetime.now()}
+        })
+
+    async def save(self, request):
+        data = self.get_session_data(request.session)
+        session = await self.exists(data['session_key'])
+        collection = await self.get_session_collection()
+        if not session:
+            await collection.insert_one(data)
+        else:
+            if self.must_save:
+                update_data = {
+                    'session_data': data['session_data']
+                }
+                if self.auto_postpone:
+                    update_data['expire_date'] = data['expire_date']
+                await collection.update_one({
+                    'session_key': data['session_key']
+                }, {
+                    '$set': update_data
+                })
+            else:
+                raise UpdateError
+
+    async def clear_expired(self, months=1):
+        collection = await self.get_session_collection()
+        _date = datetime.now() + relativedelta(months=-months)
+        await collection.remove({'expire_date': {'$lt': _date}})
---
+++ packages/alita-session/alita-session-0.1.3.tar.gz/alita-session-0.1.3/alita_session/redis.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import asyncio_redis
+from alita_session.base import *
+
+
+class RedisSessionManager(SessionInterface):
+    _pool = None
+    _pool_size = 10
+
+    async def get_redis_pool(self):
+        raise NotImplementedError
+
+    async def delete(self, session_key):
+        pool = await self.get_redis_pool()
+        await pool.delete(session_key)
+
+    async def exists(self, session_key):
+        pool = await self.get_redis_pool()
+        return await pool.get(session_key)
+
+    def get_session_data(self, session, seconds=None):
+        return dict(
+            key=self.get_session_key(session),
+            value=self.encode(session),
+            seconds=seconds or self.get_expiry_age(session)
+        )
+
+    async def get(self, session_key):
+        pool = await self.get_redis_pool()
+        return await pool.get(session_key)
+
+    async def save(self, request):
+        pool = await self.get_redis_pool()
+        session_key = self.get_session_key(request.session)
+        if not await self.exists(session_key):
+            session_data = self.get_session_data(request.session)
+        else:
+            seconds = None if self.auto_postpone else await pool.ttl(session_key)
+            session_data = self.get_session_data(request.session, seconds)
+        await pool.setex(**session_data)
+
+
+class SessionManager(RedisSessionManager):
+    async def get_redis_pool(self):
+        self.client_config.setdefault("poolsize", self._pool_size)
+        if not self._pool:
+            loop = asyncio.get_event_loop()
+            self._pool = await asyncio_redis.Pool.create(
+                loop=loop, **self.client_config)
+        return self._pool
