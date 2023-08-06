f8ffaccb87f5dff9f5e9ac891b4608f48d5a62eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:30 +0000

    Add package quicksect/0.2.1/quicksect-0.2.1.tar.gz

---
+++ packages/quicksect/quicksect-0.2.1.tar.gz/quicksect-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools.extension import Extension
+from setuptools import setup, find_packages
+
+extensions = [Extension("quicksect", ["src/quicksect.pyx"])]
+
+setup(version='0.2.1',
+	  name='quicksect',
+      description="fast, simple interval intersection",
+      long_description=open('README.rst').read(),
+      author="Brent Pedersen",
+      author_email="bpederse@gmail.com",
+      packages=find_packages(),
+      setup_requires=['cython'],
+      install_requires=['cython'],
+      ext_modules=extensions,
+      test_suite='nose.collector',
+      tests_require='nose',
+)
