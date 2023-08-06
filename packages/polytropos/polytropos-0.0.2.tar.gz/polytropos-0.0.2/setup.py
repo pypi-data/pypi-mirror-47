8e4d4a0dc22c59810434519e8d29b97608b128b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:16 +0000

    Add package polytropos/0.0.2/polytropos-0.0.2.tar.gz

---
+++ packages/polytropos/polytropos-0.0.2.tar.gz/polytropos-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polytropos",
+    version="0.0.2",
+    license='agpl-3.0',
+    url='https://github.com/borenstein/polytropos',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'scipy',
+        'numpy',
+        'pytest',
+        'glom',
+        'pytest-repeat',
+        'dacite',
+        'pyyaml',
+    ],
+    download_url='https://github.com/borenstein/polytropos/archive/0.0.2.tar.gz',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
