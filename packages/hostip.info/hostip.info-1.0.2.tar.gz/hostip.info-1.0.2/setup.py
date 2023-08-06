3751e84790ab578836989acac85220905c67b53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:53 +0000

    Add package hostip.info/1.0.2/hostip.info-1.0.2.tar.gz

---
+++ packages/hostip.info/hostip.info-1.0.2.tar.gz/hostip.info-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='hostip.info',
+      version='1.0.2',
+      description='Object oriented wrapper for hostip.info',
+      long_description='An object oriented wrapper for hostip.info\'s API',
+      author='sl4v',
+      author_email='iamsl4v@protonmail.com',
+      url='https://github.com/sl4vkek/hostip.info',
+      packages=['hostip'],
+      install_requires=['requests'],
+      license="WTFPL"
+)
