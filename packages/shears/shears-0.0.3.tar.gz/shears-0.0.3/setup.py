3be522aaef4fb6be40b8b64f6a0ee5613a7bf4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:50 +0000

    Add package shears/0.0.3/shears-0.0.3.tar.gz

---
+++ packages/shears/shears-0.0.3.tar.gz/shears-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup (
+  name='shears',
+  version='0.0.3',
+  packages=['shears'],
+  keywords = ['machine-vision', 'computer-vision', 'image-processing', 'data-collection'],
+  description='Extract illustrations from book page scans',
+  url='https://github.com/yaledhlab/shears',
+  author='Douglas Duhaime',
+  author_email='douglas.duhaime@gmail.com',
+  license='MIT',
+  install_requires=[
+    'glob2>=0.6',
+    'matplotlib>=2.0.0',
+    'scikit-image>=0.15.0',
+    'scikit-learn>=0.20.3',
+    'scipy>=1.2.1',
+    'six>=1.11.0',
+  ],
+)
