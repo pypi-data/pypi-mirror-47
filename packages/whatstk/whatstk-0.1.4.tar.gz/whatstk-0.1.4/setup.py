ddd4d720238ed9da1ddb9f966ef054b997a3ee65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:40 +0000

    Add package whatstk/0.1.4/whatstk-0.1.4.tar.gz

---
+++ packages/whatstk/whatstk-0.1.4.tar.gz/whatstk-0.1.4/setup.py
@@ -0,0 +1,33 @@
+"""setup script"""
+from setuptools import setup, find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+    requirements = [req.strip() for req in f.readlines()]
+
+
+setup(
+    name='whatstk',
+    version="0.1.4",
+    description="Parser and analytics tools for WhatsApp group chats",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/lucasrodes/whatstk',
+    author='Lucas Rodes-Guirao',
+    author_email='hi@lcsrg.me',
+    license='MIT',
+    install_requires=requirements,
+    packages=find_packages('.'),
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
