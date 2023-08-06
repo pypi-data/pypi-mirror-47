dc6ced8877b8858910f52016658461a8444841aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:09 +0000

    Add package zac-pyutils/1.52/zac_pyutils-1.52.tar.gz

---
+++ packages/zac-pyutils/zac_pyutils-1.52.tar.gz/zac_pyutils-1.52/zac_pyutils/ExqUtils.py
@@ -0,0 +1,55 @@
+import logging
+import datetime
+import itertools
+from collections import deque
+
+INFO = logging.INFO
+WARN = logging.WARN
+ERROR = logging.ERROR
+DEBUG = logging.DEBUG
+
+
+def load_file_as_iter(path):
+    with open(path, "r+") as f:
+        for i in f:
+            yield i
+
+
+def padding(tokens_inp, pad="__PAD__"):
+    pad_len = max([len(i) for i in tokens_inp])
+    return [(i+[pad]*pad_len)[:pad_len] for i in tokens_inp]
+
+
+def zprint(message):
+    new_m = "|{}| {}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), message)
+    print(new_m)
+
+
+def get_logger(log_file):
+    logger = logging.Logger("logger to {}".format(log_file))
+    hdlr = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(levelname)s %(lineno)s %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    return logger
+
+
+def log2file(message, logger, level=logging.INFO, verbose=False):
+    new_m = "|{}| {}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), message)
+    logger.log(level, new_m)
+    if verbose: print(new_m)
+
+
+def map_on_iter(iter_item, function, chunk_size=100):
+    dq = deque()
+    while True:
+        target = list(itertools.islice(iter_item, chunk_size))
+        if len(target) > 0:
+            dq.append(function(target))
+        else:
+            break
+    return flat(list(dq))
+
+
+def flat(inp_list):
+    return [item for sublist in inp_list for item in sublist]
