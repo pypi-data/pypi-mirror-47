99656ea72b2343c780852e3c0161acfef7bcf0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:26 +0000

    Add package tf_assist/0.1/tf_assist-0.1.tar.gz

---
+++ packages/tf_assist/tf_assist-0.1.tar.gz/tf_assist-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tf_assist',   #pip install tf_assist
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/aniketmaurya/tf_assist',
+      author='Aniket Maurya',
+      author_email='aniketmaurya.ai@gmail.com',
+      license='MIT',
+      packages=['assist'],
+      zip_safe=False)
