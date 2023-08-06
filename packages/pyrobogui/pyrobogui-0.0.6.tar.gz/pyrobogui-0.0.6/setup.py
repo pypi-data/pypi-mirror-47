f6525d1391bb9413f34940cf18388eac156f94fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:32 +0000

    Add package pyrobogui/0.0.6/pyrobogui-0.0.6.tar.gz

---
+++ packages/pyrobogui/pyrobogui-0.0.6.tar.gz/pyrobogui-0.0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+#python setup.py bdist_wheel sdist
+#cd dist 
+#twine upload *
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup (
+	name="pyrobogui",
+	version="0.0.6",
+	description="Wrapper around pyautogui for automating mouse and keyboard - plus some new functions",
+	url="https://github.com/ClimenteA/pyrobogui",
+	author="Climente Alin",
+	author_email="climente.alin@gmail.com",
+	license='MIT',
+	py_modules=["pyrobogui"],
+	install_requires=[
+          'pyautogui',
+		  'opencv-python',
+		  'numpy',
+		  'pyperclip',
+	],
+	packages=find_packages(),
+	long_description=long_description,
+    long_description_content_type="text/markdown",
+	package_dir={"":"src"}
+)
