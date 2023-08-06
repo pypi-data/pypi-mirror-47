a43bc7cb203c71d96d8a02318aa6fa15b5050ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:06 +0000

    Add package newchanic/1.0.2/newchanic-1.0.2-py3-none-any.whl

---
+++ packages/newchanic/newchanic-1.0.2-py3-none-any.whl/newchanic/physics.py
@@ -0,0 +1,24 @@
+from typing import Tuple, List
+
+from .utils import Number
+
+
+class Particle:
+    def __init__(self, mass: Number, position: List[Number], velocity: List[Number]):
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        self.is_alive = True
+
+    def apply_force(self, force: Tuple[Number, Number]):
+        for i in range(len(force)):
+            self.velocity[i] += force[i] / self.mass
+
+    def exist(self):
+        for i in range(len(self.position)):
+            self.position[i] += self.velocity[i]
+
+
+class Law:
+    def compute_force(self, particle: Particle, other_particle: Particle) -> Tuple[Number, Number]:
+        raise NotImplementedError
