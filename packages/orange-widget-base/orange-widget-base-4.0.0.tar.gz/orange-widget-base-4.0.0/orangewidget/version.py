9a832d9e92ba605648b142a04ae3599065058f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:40 +0000

    Add package orange-widget-base/4.0.0/orange-widget-base-4.0.0.tar.gz

---
+++ packages/orange-widget-base/orange-widget-base-4.0.0.tar.gz/orange-widget-base-4.0.0/orangewidget/version.py
@@ -0,0 +1,10 @@
+# THIS FILE IS GENERATED FROM ORANGE-WIDGET-BASE SETUP.PY
+short_version = '4.0.0'
+version = '4.0.0'
+full_version = '4.0.0'
+git_revision = 'ccb107a8c704a95b9a6d067c5a678227c6db4d3e'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
