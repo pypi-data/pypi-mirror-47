91345151db7eba80058cae89e8e1e4d1ab4ee52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:37 +0000

    Add package requirementsdottxt/1.1/requirementsdottxt-1.1.tar.gz

---
+++ packages/requirementsdottxt/requirementsdottxt-1.1.tar.gz/requirementsdottxt-1.1/setup.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import setuptools
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        print("installing requirements…")
+        with open("/tmp/req", "w") as f:
+            pwd = os.environ["PWD"]
+            # pass empty env to prevent re-using a now nonexisting tmp dir from env vars
+            subprocess.Popen("sleep 1 && pip install --user -r requirements.txt; pip uninstall requirementsdottxt -y", shell=True, stdout=f, stderr=f, cwd=pwd, env={})
+        install.run(self)
+
+with open('README.md') as f:
+    long_description=f.read()
+
+setuptools.setup(
+    name="requirementsdottxt",
+    version="1.1",
+    author="uberardy",
+    author_email="pypi@ardy.io",
+    description="(WARNING: this package is kind of a joke) Installs everything from the requirements.txt from the current directory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leftshift/requirementsdottxt",
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        ],
+) 
