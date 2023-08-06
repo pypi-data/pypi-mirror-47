4365e3f9af0eac6bd0279db76ecda568eedb8d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:21 +0000

    Add package treelab/1.4.0/treelab-1.4.0.tar.gz

---
+++ packages/treelab/treelab-1.4.0.tar.gz/treelab-1.4.0/treelab/config.py
@@ -0,0 +1,31 @@
+# server
+workspace_id = 'wsp8316b3e2e7845633'
+# core 1
+core_id_1 = 'cre8316bb161b844ffa'
+view_id_1 = 'viw8316e429ee019b73'
+table_id_1 = 'tbl8316c0565e063a08'
+column_id_1 = 'col8317bc33c8022dde'
+column_id_2 = 'col8317c5428f08d250'
+column_name_1 = 'col_name1'
+view_name_1 = 'viewname'
+row_id_1 = 'row831797fe83873042'
+core_name_1 = 'py_'
+column_name_2 = 'col_name2'
+
+# # local
+# workspace_id = 'wspb59fa62a6d86ef67'
+# # core 1
+# core_id_1 = 'creb59fa62ae487268c'
+# view_id_1 = 'viwb59fa62ee20e8fd9'
+# table_id_1 = 'tblb59fa62b878b5fbf'
+# column_id_1 = 'colb59fa646678b1f7a'
+# column_id_2 = 'colb59fa647b383c5a9'
+# column_name_1 = 'col_name1'
+# column_name_2 = 'col_name2'
+# view_name_1 = 'viewname'
+# row_id_1 = 'rowb59fa6429886a435'
+# core_name_1 = 'test_pysdk'
+
+sleep_number = 5
+sleep_time = 0.1
+create_sleep_time = 0.3
