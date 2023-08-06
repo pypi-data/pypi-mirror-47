9ecbb3ddc7f36c53af392c40fa050c9d4f43fa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:26 +0000

    Add package botframework-connector/4.4.0b2/botframework-connector-4.4.0b2.tar.gz

---
+++ packages/botframework-connector/botframework-connector-4.4.0b2.tar.gz/botframework-connector-4.4.0b2/setup.py
@@ -0,0 +1,46 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+import os
+from setuptools import setup
+
+NAME = "botframework-connector"
+VERSION = os.environ["packageVersion"] if "packageVersion" in os.environ else "4.4.0b1"
+REQUIRES = [
+    "msrest>=0.6.6",
+    "requests>=2.8.1",
+    "cryptography>=2.1.4",
+    "PyJWT>=1.5.3",
+    "botbuilder-schema>=4.4.0b2"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Microsoft Bot Framework Bot Builder SDK for Python.",
+    author='Microsoft',
+    url="https://www.github.com/Microsoft/botbuilder-python",
+    keywords=["BotFrameworkConnector", "bots","ai", "botframework", "botbuilder"],
+    install_requires=REQUIRES,
+    packages=["botframework.connector",
+              "botframework.connector.auth",
+              "botframework.connector.async_mixin",
+              "botframework.connector.operations",
+              "botframework.connector.models",
+              "botframework.connector.aio",
+              "botframework.connector.aio.operations_async",
+              "botframework.connector.token_api",
+              "botframework.connector.token_api.aio",
+              "botframework.connector.token_api.models",
+              "botframework.connector.token_api.operations"
+    ],
+    include_package_data=True,
+    long_description="Microsoft Bot Framework Bot Builder SDK for Python.",
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ]
+)
