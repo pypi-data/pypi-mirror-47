ff7cc2ebe7fc29b832681ca15711ecf1e2df7e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:57 +0000

    Add package netlookup/1.0.0/netlookup-1.0.0.tar.gz

---
+++ packages/netlookup/netlookup-1.0.0.tar.gz/netlookup-1.0.0/setup.py
@@ -0,0 +1,43 @@
+
+from setuptools import setup, find_packages
+from netlookup.version import __version__
+
+setup(
+    name='netlookup',
+    keywords='network subnet lookup utilities',
+    description='python tools to look up information about networks',
+    author='Ilkka Tuohela',
+    author_email='hile@iki.fi',
+    url='https://github.com/hile/netlookup/',
+    version=__version__,
+    license='PSF',
+    packages=find_packages(),
+    python_requires='>3.6.0',
+    entry_points={
+        'console_scripts': [
+            'netlookup=netlookup.bin.netlookup:main',
+        ],
+    },
+    install_requires=(
+        'dnspython',
+        'netaddr',
+        'requests',
+        'systematic',
+    ),
+    tests_require=(
+        'pytest',
+        'pytest-runner',
+        'pytest-datafiles',
+    ),
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+        'Topic :: System',
+        'Topic :: System :: Systems Administration',
+    ],
+)
