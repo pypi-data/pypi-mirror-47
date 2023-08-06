518e552686304da40e714bbe828909e10905683b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:19 +0000

    Add package iar-tokenizer/1.0.9/iar_tokenizer-1.0.9.tar.gz

---
+++ packages/iar-tokenizer/iar_tokenizer-1.0.9.tar.gz/iar_tokenizer-1.0.9/README.md
@@ -0,0 +1,6 @@
+# IAR Tokenizer
+
+The IAR (Iván Arias Rodríguez) Tokenizer is a tokenizer developed mainly for Spanish. It is able to divide a text in paragraphs, those in 
+sentences, and each sentence in a list of tokens.
+
+More information to be added in the future...
---
+++ packages/iar-tokenizer/iar_tokenizer-1.0.9.tar.gz/iar_tokenizer-1.0.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="iar_tokenizer",
+                 version="1.0.9",
+                 author="Ivan Arias Rodriguez",
+                 author_email="ivan.arias.rodriguez@gmail.com",
+                 description="A tokenizer focused on Spanish language.",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/ivansiiito/iar_tokenizer",
+                 packages=setuptools.find_packages(),
+                 classifiers=["Programming Language :: Python :: 3",
+                              "License :: OSI Approved :: MIT License",
+                              "Operating System :: OS Independent",
+                              ],
+                 install_requires=['nltk'],
+                 )
