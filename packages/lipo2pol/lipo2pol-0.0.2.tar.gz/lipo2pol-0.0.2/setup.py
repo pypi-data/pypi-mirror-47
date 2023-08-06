d4282e5e88db8436dcba57f4c6004e06e04d86bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:24 +0000

    Add package lipo2pol/0.0.2/lipo2pol-0.0.2.tar.gz

---
+++ packages/lipo2pol/lipo2pol-0.0.2.tar.gz/lipo2pol-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lipo2pol",
+    version="0.0.2",
+    author="Fevzi Das",
+    author_email="fevzidas@gmail.com",
+    description="Converts LineString and Point data in Geojson format to Polygon data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
