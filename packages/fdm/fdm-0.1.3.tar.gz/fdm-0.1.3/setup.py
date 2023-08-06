cd4b923864a9abdec8763699391907f4b8cd52df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package fdm/0.1.3/fdm-0.1.3.tar.gz

---
+++ packages/fdm/fdm-0.1.3.tar.gz/fdm-0.1.3/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from setuptools import find_packages, setup
+
+requirements = ['numpy>=1.16']
+
+setup(packages=find_packages(exclude=['docs']),
+      install_requires=requirements,
+      include_package_data=True)
