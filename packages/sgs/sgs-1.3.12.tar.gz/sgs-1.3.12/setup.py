68930b566ffe645c952246279fcd91ac545b819f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:30 +0000

    Add package sgs/1.3.12/sgs-1.3.12.tar.gz

---
+++ packages/sgs/sgs-1.3.12.tar.gz/sgs-1.3.12/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+__version__ = '1.3.12'
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name = 'sgs',
+    packages = ['sgs'],
+    install_requires = [
+        'jinja2>=2.9.5',
+        'lxml>=4',
+        'numpy>= 1.1'
+        'pandas>=0.22.0',
+        'requests>=2.18.4'
+    ],
+    version = __version__,
+    description = 'Python wrapper para o webservice do SGS - Sistema Gerenciador de Series Temporais do Banco Central do Brasil.',
+    long_description=readme,
+    author = 'Rafael Alves Ribeiro',
+    author_email = 'rafael.alves.ribeiro@gmail.com',
+    url = 'https://github.com/rafpyprog/pySGS',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+)
