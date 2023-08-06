e6c6d437b8df83b1653cb9b74e495620af06cb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:07 +0000

    Add package xyscript/0.2.4.8/xyscript-0.2.4.8.tar.gz

---
+++ packages/xyscript/xyscript-0.2.4.8.tar.gz/xyscript-0.2.4.8/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import sys
+def main():
+    print("进入入口主程序")
+    pass
+
+def test():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ packages/xyscript/xyscript-0.2.4.8.tar.gz/xyscript-0.2.4.8/xyscript/extension.py
@@ -0,0 +1,13 @@
+#-*- encoding:utf-8 -*-
+import os, sys, re
+
+
+
+if __name__ == "__main__":
+    pass
+    # print(Extension().get_current_version_des())
+    # print(Extension().get_location_version())
+    # Extension().check_version()
+    # dirname, filename = os.path.split(os.path.abspath(sys.argv[0])) 
+    # print(dirname)
+    
