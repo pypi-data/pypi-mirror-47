a39dcc122192b3bf57df857574e746278c2d39a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package pypdfplot/0.3.7/pypdfplot-0.3.7.tar.gz

---
+++ packages/pypdfplot/pypdfplot-0.3.7.tar.gz/pypdfplot-0.3.7/setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup
+
+version = '0.3.7'
+
+
+try:
+    #Building the README.rst
+    files = []
+    doc_folder = 'docs/source/'
+    
+    with open(doc_folder + 'index.rst','r') as f:
+        line = f.readline()
+        while line == '\n':
+            line = f.readline()
+        
+        while line != '\n':
+            line = f.readline()
+
+        while line == '\n':
+            line = f.readline()
+        
+        while line != '\n':
+            line = f.readline()
+        
+        for line in f:
+            if line != '\n':
+                files.append(line.strip()+'.rst')
+
+    doc = ''
+    for fname in files:
+        with open(doc_folder + fname,'r') as f:
+           doc += f.read()
+           doc += '\n'
+
+    img_link = 'https://pypdfplot.readthedocs.io/en/latest/_images/'
+    doc = doc.replace('.. image:: _static/',
+                      '.. image:: ' + img_link)
+
+    with open('README.rst','w') as f:
+        f.write(doc)
+except:
+    # User installation
+    with open('README.rst','r') as f:
+        doc = f.read()
+
+setup(name='pypdfplot',
+      version = version,
+      description="Saves plots as PDF with embedded generating script",
+      author='Dirk van den Bekerom',
+      author_email='dcmvdbekerom@gmail.com',
+      license='MIT',
+      #url = 'https://github.com/dcmvdbekerom/pypdfplot',
+      packages=['pypdfplot'],
+      install_requires=['matplotlib','PyPDF4'],
+      project_urls={
+        'Documentation': 'https://pypdfplot.readthedocs.io/',
+        'GitHub': 'https://github.com/dcmvdbekerom/pypdfplot'}, 
+      zip_safe=False,
+      classifiers = [
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "Programming Language :: Python",
+          "Topic :: Multimedia :: Graphics",
+          "Topic :: Scientific/Engineering :: Visualization"],
+      long_description=doc,
+      long_description_content_type='text/x-rst')
