bd30cb8fbf80e730f414cae45816315362bed2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:30 +0000

    Add package royalnet/5.0a21/royalnet-5.0a21.tar.gz

---
+++ packages/royalnet/royalnet-5.0a21.tar.gz/royalnet-5.0a21/royalnet/__init__.py
@@ -0,0 +1,5 @@
+from . import audio, bots, commands, database, network, utils, error, web
+
+version = "5.0a21"
+
+__all__ = ["audio", "bots", "commands", "database", "network", "utils", "error", "web"]
