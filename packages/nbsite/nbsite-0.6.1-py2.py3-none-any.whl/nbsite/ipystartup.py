1fb747cdd69f53c2a908a8a3167bc2a02154483f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:22 +0000

    Add package nbsite/0.6.1/nbsite-0.6.1-py2.py3-none-any.whl

---
+++ packages/nbsite/nbsite-0.6.1-py2.py3-none-any.whl/nbsite/ipystartup.py
@@ -0,0 +1,16 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import matplotlib as mpl
+mpl.use('agg')
+
+import holoviews.plotting.widgets as hw
+hw.NdWidget.export_json=True
+hw.NdWidget.json_load_path = './'
+hw.NdWidget.json_save_path = './'
+del hw
+
+import holoviews.plotting.mpl as hmpl
+hmpl.MPLPlot.fig_alpha = 0
+del hmpl
+
