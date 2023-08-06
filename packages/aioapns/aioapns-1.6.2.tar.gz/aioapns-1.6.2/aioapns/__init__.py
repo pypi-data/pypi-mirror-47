e567a2281b8e5ebc0f556efc31b48f8b6f822087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:41 +0000

    Add package aioapns/1.6.2/aioapns-1.6.2.tar.gz

---
+++ packages/aioapns/aioapns-1.6.2.tar.gz/aioapns-1.6.2/aioapns/__init__.py
@@ -0,0 +1,5 @@
+from aioapns.client import APNs
+from aioapns.common import NotificationRequest, PRIORITY_NORMAL, PRIORITY_HIGH
+
+
+__all__ = ['APNs', 'NotificationRequest', 'PRIORITY_NORMAL', 'PRIORITY_HIGH']
