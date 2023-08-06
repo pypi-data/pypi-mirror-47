38bdb7c7715a91030bfecce6783c1310ac44b30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:54 +0000

    Add package django-widget-tweaks/1.4.5/django-widget-tweaks-1.4.5.tar.gz

---
+++ packages/django-widget-tweaks/django-widget-tweaks-1.4.5.tar.gz/django-widget-tweaks-1.4.5/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+version = '1.4.5'
+
+setup(
+    name='django-widget-tweaks',
+    version=version,
+    author='Mikhail Korobov',
+    author_email='kmike84@gmail.com',
+    url='https://github.com/jazzband/django-widget-tweaks',
+    description='Tweak the form field rendering in templates, not in python-level form definitions.',
+    long_description=open('README.rst').read() + "\n\n" + open('CHANGES.rst').read(),
+    license='MIT license',
+    requires=['django (>= 1.8)'],
+    packages=['widget_tweaks', 'widget_tweaks.templatetags'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
