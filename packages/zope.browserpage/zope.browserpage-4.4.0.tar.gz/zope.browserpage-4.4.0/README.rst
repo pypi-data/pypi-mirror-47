b222d2506410b8bbba7c1ae197cbda912d2df18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:34 +0000

    Add package zope.browserpage/4.4.0/zope.browserpage-4.4.0.tar.gz

---
+++ packages/zope.browserpage/zope.browserpage-4.4.0.tar.gz/zope.browserpage-4.4.0/README.rst
@@ -0,0 +1,34 @@
+======================
+ ``zope.browserpage``
+======================
+
+.. image:: https://img.shields.io/pypi/v/zope.browserpage.svg
+        :target: https://pypi.python.org/pypi/zope.browserpage/
+        :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/zope.browserpage.svg
+        :target: https://pypi.org/project/zope.browserpage/
+        :alt: Supported Python versions
+
+.. image:: https://travis-ci.org/zopefoundation/zope.browserpage.svg?branch=master
+        :target: https://travis-ci.org/zopefoundation/zope.browserpage
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.browserpage/badge.svg?branch=master
+        :target: https://coveralls.io/github/zopefoundation/zope.browserpage?branch=master
+
+.. note::
+
+   This package is at present not reusable without depending on a large
+   chunk of the Zope Toolkit and its assumptions. It is maintained by the
+   `Zope Toolkit project <http://docs.zope.org/zopetoolkit/>`_.
+
+This package provides ZCML directives for configuring browser views.
+More specifically it defines the following ZCML directives:
+
+- ``browser:page``
+- ``browser:pages``
+- ``browser:view``
+
+These directives also support menu item registration for pages, when
+``zope.browsermenu`` package is installed. Otherwise, they simply ignore
+the menu attribute.
---
+++ packages/zope.browserpage/zope.browserpage-4.4.0.tar.gz/zope.browserpage-4.4.0/setup.py
@@ -0,0 +1,100 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""zope.browserpage setup
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+long_description = (read('README.rst') + '\n\n' + read('CHANGES.rst'))
+
+
+def alltests():
+    import sys
+    import unittest
+    # use the zope.testrunner machinery to find all the
+    # test suites we've put under ourselves
+    import zope.testrunner.find
+    import zope.testrunner.options
+    here = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+    args = sys.argv[:]
+    defaults = ["--test-path", here]
+    options = zope.testrunner.options.get_options(args, defaults)
+    suites = list(zope.testrunner.find.find_suites(options))
+    return unittest.TestSuite(suites)
+
+
+TESTS_REQUIRE = [
+    'zope.browsermenu',
+    'zope.testing',
+    'zope.testrunner',
+]
+
+setup(
+    name='zope.browserpage',
+    version='4.4.0',
+    url='https://github.com/zopefoundation/zope.browserpage',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope :: 3',
+    ],
+    description='ZCML directives for configuring browser views for Zope.',
+    long_description=long_description,
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope'],
+    install_requires=[
+        'setuptools',
+        'zope.tal >= 4.2.0',
+        'zope.pagetemplate',
+        'zope.component >= 3.7',
+        'zope.configuration',
+        'zope.interface',
+        'zope.publisher >= 3.8',
+        'zope.schema',
+        'zope.security',
+        'zope.traversing',
+        ],
+    extras_require={
+        'menu': [
+            'zope.browsermenu',
+        ],
+        'test': TESTS_REQUIRE,
+    },
+    tests_require=TESTS_REQUIRE,
+    test_suite='__main__.alltests',
+    include_package_data=True,
+    zip_safe=False,
+)
