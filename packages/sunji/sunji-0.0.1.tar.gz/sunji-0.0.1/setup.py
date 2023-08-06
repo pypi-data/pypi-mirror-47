9f4e9c815157e311ad6c25c2eead13c8c2174fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:21 +0000

    Add package sunji/0.0.1/sunji-0.0.1.tar.gz

---
+++ packages/sunji/sunji-0.0.1.tar.gz/sunji-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "sunji",
+    version = "0.0.1",
+    packages = find_packages(),
+    descripion = "This is a demo",
+    author = "lian.he",
+    author_email = "327842846@qq.com",
+    url = "https://github.com/hlzy/mutl_repo",
+    )
