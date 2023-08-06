0eb1af0a436c419f66970d578e6c39c4bf96dc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:01 +0000

    Add package bioeval/1.1/bioeval-1.1.tar.gz

---
+++ packages/bioeval/bioeval-1.1.tar.gz/bioeval-1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from distutils.core import setup
+
+__author__ = 'Aleksandar Savkov'
+
+setup(name='bioeval',
+      version='1.1',
+      description='BIO and BEISO evaluation library',
+      author='Aleksandar Savkov',
+      author_email='aleksandar@savkov.eu',
+      url='https://www.github.com/asavkov/bioeval/',
+      packages=find_packages()
+     )
