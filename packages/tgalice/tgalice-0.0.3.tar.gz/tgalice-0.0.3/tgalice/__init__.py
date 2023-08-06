4ce142bdf77346c9ed73dc212ce427f2ddb2a013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:07 +0000

    Add package tgalice/0.0.3/tgalice-0.0.3.tar.gz

---
+++ packages/tgalice/tgalice-0.0.3.tar.gz/tgalice-0.0.3/tgalice/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from tgalice import dialog_connector, session_storage, flask_server
+from tgalice.nlu import basic_nlu
+from tgalice.dialog_manager import base
