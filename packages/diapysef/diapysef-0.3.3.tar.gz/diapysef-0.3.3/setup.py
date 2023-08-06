abb2b7c73832834fa97cfdb74d0aeac7b6494fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:12 +0000

    Add package diapysef/0.3.3/diapysef-0.3.3.tar.gz

---
+++ packages/diapysef/diapysef-0.3.3.tar.gz/diapysef-0.3.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='diapysef',
+      version='0.3.3',
+      description='Analysis and visualization of DIA PASEF data',
+      long_description=readme(),
+      author='Max Frank, Annie Ha, Hannes Roest',
+      author_email='hannes.rost@utoronto.ca',
+      license='MIT',
+      packages=['diapysef'],
+      install_requires=[
+          'pandas',
+          'numpy',
+          'matplotlib',
+          'statsmodels',
+          'pyopenms',
+          'patsy'],
+      package_data={
+          'diapysef': ['data/*']
+      },
+      scripts=['scripts/get_dia_windows.py',
+               'scripts/annotate_mq_ionmobility.py',
+               'scripts/plot_dia_windows.py',
+               'scripts/convertTDFtoMzML.py',
+               'scripts/create_library.py'],
+      zip_safe=False)
