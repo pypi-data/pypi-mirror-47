3a977da77810fbc0f235475edb500eb07206584d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:45 +0000

    Add package mountaintools/0.5.0/mountaintools-0.5.0.tar.gz

---
+++ packages/mountaintools/mountaintools-0.5.0.tar.gz/mountaintools-0.5.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+pkg_name = "mountaintools"
+
+# NOTE: you should install this project in development mode
+# > python setup.py develop
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.5.0",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="Tools for reproducible scientific research",
+    packages=setuptools.find_packages(),
+    scripts=['bin/kb-cat', 'bin/kb-ls', 'bin/kb-download', 'bin/kb-snapshot', 'bin/kb-resolve-key-path', 'bin/kb-find', 'bin/compute-resource-start', 'bin/kachery-token'],
+    install_requires=[
+        'matplotlib', 'requests', 'ipython', 'simple-crypt', 'python-dotenv'
+        #        'matplotlib','requests','ipython','simple-crypt','python-dotenv', 'asyncio', 'nest_asyncio', 'aiohttp'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    )
+)
