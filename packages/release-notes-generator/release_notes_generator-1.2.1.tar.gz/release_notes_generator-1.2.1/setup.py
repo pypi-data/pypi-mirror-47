5206497670c6c2cb603257f81ab5da1e718ba582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:20 +0000

    Add package release-notes-generator/1.2.1/release_notes_generator-1.2.1.tar.gz

---
+++ packages/release-notes-generator/release_notes_generator-1.2.1.tar.gz/release_notes_generator-1.2.1/setup.py
@@ -0,0 +1,87 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='release_notes_generator',
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.2.1',
+
+    description=(
+        'A Python package that parses a specific PR template given a '
+        'list of PRs and generates release notes form them.'),
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # The project's main homepage.
+    url='https://github.counsyl.com/lims/release_note_generator',
+
+    # Author details
+    author='mmentele',
+    author_email='mmentele@counsyl.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='releases notes',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['graphqlclient<=0.2.0', 'click<7', 'pygithub'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'release_notes = release_notes_generator.cli:release_notes',
+        ],
+    },
+)
