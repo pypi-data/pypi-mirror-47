a4a71ac0085592a38939018a2afac91cc22ebf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:32 +0000

    Add package django-latest-tweets/0.4.6/django_latest_tweets-0.4.6-py2.py3-none-any.whl

---
+++ packages/django-latest-tweets/django_latest_tweets-0.4.6-py2.py3-none-any.whl/latest_tweets/management/commands/latest_tweets_auth.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from twitter.oauth_dance import oauth_dance
+
+
+def get_auth_tokens(stdout):
+    oauth_token, oauth_secret = oauth_dance(
+        app_name='django-latest-tweets',
+        consumer_key=settings.TWITTER_CONSUMER_KEY,
+        consumer_secret=settings.TWITTER_CONSUMER_SECRET)
+
+    stdout.write("\nNow add the following lines to your settings.py:\n\n")
+    stdout.write("TWITTER_OAUTH_TOKEN = '%s'\n" % (oauth_token,))
+    stdout.write("TWITTER_OAUTH_SECRET = '%s'\n\n" % (oauth_secret,))
+
+
+class Command(BaseCommand):
+    help = 'Generate OAuth tokens/secret needed for Twitter'
+
+    def handle(self, **options):
+        get_auth_tokens(self.stdout)
---
+++ packages/django-latest-tweets/django_latest_tweets-0.4.6-py2.py3-none-any.whl/latest_tweets/models.py
@@ -0,0 +1,96 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Hashtag(models.Model):
+    text = models.CharField(max_length=140, unique=True)
+
+    class Meta:
+        ordering = ('text',)
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return 'https://twitter.com/hashtag/%s' % (self.text,)
+
+
+@python_2_unicode_compatible
+class Tweet(models.Model):
+    tweet_id = models.BigIntegerField('Tweet ID', unique=True)
+    user = models.CharField(max_length=15, db_index=True)
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=250)
+    html = models.TextField('HTML')
+    hashtags = models.ManyToManyField(Hashtag, blank=True)
+    favorite_count = models.PositiveIntegerField(default=0)
+    retweet_count = models.PositiveIntegerField(default=0)
+    retweeted_username = models.CharField(max_length=20, blank=True)
+    retweeted_name = models.CharField(max_length=50, blank=True)
+    retweeted_tweet_id = models.BigIntegerField('Retweeted tweet ID', null=True, blank=True)
+    is_reply = models.BooleanField(default=False, db_index=True)
+    created = models.DateTimeField(db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '@%s - %s' % (self.user, self.text)
+
+    def get_absolute_url(self):
+        return 'https://twitter.com/%s/status/%s' % (self.user, self.tweet_id)
+
+    def user_url(self):
+        return 'https://twitter.com/%s' % (self.user,)
+
+    def retweeted_user_url(self):
+        if self.retweeted_username:
+            return 'https://twitter.com/%s' % (self.retweeted_username,)
+        else:
+            return None
+
+
+@python_2_unicode_compatible
+class Photo(models.Model):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name='photos')
+    photo_id = models.BigIntegerField('Photo ID')
+    text = models.CharField(max_length=250)
+    text_index = models.PositiveIntegerField(db_index=True)
+    url = models.URLField('URL')
+    media_url = models.URLField('Media URL')
+    large_width = models.PositiveIntegerField()
+    large_height = models.PositiveIntegerField()
+    image_file = models.ImageField(upload_to='latest_tweets/photo', blank=True)
+
+    class Meta:
+        ordering = ('tweet', 'text_index')
+        unique_together = (
+            ('tweet', 'photo_id'),
+        )
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return self.url
+
+    def large_url(self):
+        return '%s:large' % (self.media_url,)
+
+
+@python_2_unicode_compatible
+class Like(models.Model):
+    user = models.CharField(max_length=15)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-tweet',)
+        unique_together = (
+            ('user', 'tweet'),
+        )
+
+    def __str__(self):
+        return '%s' % (self.tweet,)
