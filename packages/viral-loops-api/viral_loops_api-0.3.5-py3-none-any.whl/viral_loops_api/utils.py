f22a5b18db7136aad4ac47571d6e1cffedc4d384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:38 +0000

    Add package viral-loops-api/0.3.5/viral_loops_api-0.3.5-py3-none-any.whl

---
+++ packages/viral-loops-api/viral_loops_api-0.3.5-py3-none-any.whl/viral_loops_api/utils.py
@@ -0,0 +1,12 @@
+from viral_loops_api.exceptions import InvalidChoiceException
+
+
+def validate_enum(value, possible):
+  """
+  Throws an exception if a given value is outside the list of possible values.
+  Returns the value if it is a valid choice.
+  """
+  if value not in possible:
+    raise InvalidChoiceException(f'{value} not in {possible}')
+
+  return value
