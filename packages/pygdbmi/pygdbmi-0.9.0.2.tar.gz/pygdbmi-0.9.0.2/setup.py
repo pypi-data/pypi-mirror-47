636fe8893833f3ebb7ea54c78db1ac7276fe9a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:16 +0000

    Add package pygdbmi/0.9.0.2/pygdbmi-0.9.0.2.tar.gz

---
+++ packages/pygdbmi/pygdbmi-0.9.0.2.tar.gz/pygdbmi-0.9.0.2/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#  pip install twine
+
+import io
+import os
+import re
+from setuptools import find_packages, setup
+from codecs import open
+
+EXCLUDE_FROM_PACKAGES = ["tests"]
+CURDIR = os.path.abspath(os.path.dirname(__file__))
+README = io.open("README.md", "r", encoding="utf-8").read()
+
+with open("pygdbmi/__init__.py", "r") as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
+    ).group(1)
+
+
+setup(
+    name="pygdbmi",
+    version=version,
+    author="Chad Smith",
+    author_email="grassfedcode@gmail.com",
+    description="Parse gdb machine interface output with Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cs01/pygdbmi",
+    license="MIT",
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data=True,
+    keywords=["gdb", "python", "machine-interface", "parse", "frontend"],
+    scripts=[],
+    entry_points={},
+    extras_require={
+        "dev": [
+            'black;python_version>="3.6"',
+            'pdoc;python_version>="3.6"',
+            "flake8==3.5.0",
+            "collective.checkdocs==0.2",
+            'pdoc3;python_version>="3.6"',
+        ]
+    },
+    zip_safe=False,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
