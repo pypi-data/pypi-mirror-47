6373f332c007e624a7eaabb9161d37cec29a577e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:23 +0000

    Add package easytello/0.0.6/easytello-0.0.6.tar.gz

---
+++ packages/easytello/easytello-0.0.6.tar.gz/easytello-0.0.6/easytello/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'easytello'
+__author__ = 'Ezra Fielding'
+__liscence__ = 'MIT'
+__copyright__ = 'Copyright 2019 Ezra Fielding'
+__version__ = '0.0.6'
+__all__ = ['tello', 'stats']
+
+from .tello import Tello
+from .stats import Stats
+import easytello.tello_video
