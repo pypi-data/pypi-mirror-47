f3f7a2a645a2275c8df474ce1cdc253605743a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:04 +0000

    Add package wmbook/0.0.1/wmbook-0.0.1.tar.gz

---
+++ packages/wmbook/wmbook-0.0.1.tar.gz/wmbook-0.0.1/wmbook/__init__.py
@@ -0,0 +1,35 @@
+"""
+wmbook AI开发日志 模板
+"""
+import sys
+
+name = "wmbook"
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: "X.Y.dev" or "X.Y.devN" where N is an integer.
+# "X.Y.dev0" is the canonical version of "X.Y.dev"
+#
+__version__ = "0.0.1"
+
+try:
+    # 此变量是由生成过程在 _builtins_ 中注入的。它用于在未生成二进制文件时启用 wmbook 子包的导入
+    __SKLEARN_SETUP__
+except NameError:
+    __SKLEARN_SETUP__ = False
+
+if __SKLEARN_SETUP__:
+    sys.stderr.write("在生成过程中部分导入 wmbook\n")
+else:
+    from .utils._show_versions import show_versions
---
+++ packages/wmbook/wmbook-0.0.1.tar.gz/wmbook-0.0.1/wmbook/test/test_aidevlog.py
@@ -0,0 +1,5 @@
+import wmbook
+
+
+def test_wmbook_name():
+    assert wmbook.name == "wmbook"
