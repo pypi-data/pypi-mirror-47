1838f7772582e6b6a9da891ba46afd08e15c1432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:45 +0000

    Add package pretix-servicefees/1.3.0/pretix-servicefees-1.3.0.tar.gz

---
+++ packages/pretix-servicefees/pretix-servicefees-1.3.0.tar.gz/pretix-servicefees-1.3.0/pretix_servicefees/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_servicefees'
+    verbose_name = 'pretix Service Fees'
+
+    class PretixPluginMeta:
+        name = ugettext_lazy('pretix Service Fees')
+        author = 'Raphael Michel'
+        description = ugettext_lazy('This plugin allows to charge a service fee on all non-free orders.')
+        visible = True
+        version = '1.3.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_servicefees.PluginApp'
