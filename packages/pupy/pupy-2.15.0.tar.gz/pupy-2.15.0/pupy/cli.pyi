ec7b5b3a11209b95347fc99f56dd3b54dc95c352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:09 +0000

    Add package pupy/2.15.0/pupy-2.15.0.tar.gz

---
+++ packages/pupy/pupy-2.15.0.tar.gz/pupy-2.15.0/pupy/cli.pyi
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+
+from typing import Any
+from typing import Optional
+
+def unescaped_str(arg_str: Any): ...
+
+parser: Any
+
+def main(args: Optional[Any] = ...) -> None: ...
---
+++ packages/pupy/pupy-2.15.0.tar.gz/pupy-2.15.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['pupy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pupy',
+    'version': '2.15.0',
+    'description': 'pretty useful python',
+    'long_description': None,
+    'author': 'jessekrubin',
+    'author_email': 'jessekrubin@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
