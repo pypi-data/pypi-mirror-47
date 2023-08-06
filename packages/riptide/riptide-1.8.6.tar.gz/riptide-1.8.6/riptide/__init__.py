d356c3373f14e50c7b7c213241eb46b769e3a0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:47 +0000

    Add package riptide/1.8.6/riptide-1.8.6.tar.gz

---
+++ packages/riptide/riptide-1.8.6.tar.gz/riptide-1.8.6/riptide/__init__.py
@@ -0,0 +1,27 @@
+
+from __future__ import absolute_import, print_function
+
+import sys
+import copy
+import time
+import numpy
+import cobra
+import pandas
+import bisect
+import symengine
+from cobra.util import solver
+from optlang.symbolics import Zero
+from cobra.manipulation.delete import remove_genes
+from cobra.flux_analysis import flux_variability_analysis
+
+from riptide import riptide
+
+# Create a class to house riptide output data
+class riptideClass:
+    def __init__(self):
+        self.model = 'NULL'
+        self.fluxes = 'NULL'
+        self.flux_type = 'NULL'
+        self.quantile_range = 'NULL'
+        self.linear_coefficient_range = 'NULL'
+        self.fraction_of_optimum = 'NULL'
