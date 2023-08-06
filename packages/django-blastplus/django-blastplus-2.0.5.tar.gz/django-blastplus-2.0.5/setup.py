e7eb5e8d35d4f3275158a7ccd83ea96ab990c5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:02 +0000

    Add package django-blastplus/2.0.5/django-blastplus-2.0.5.tar.gz

---
+++ packages/django-blastplus/django-blastplus-2.0.5.tar.gz/django-blastplus-2.0.5/setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-blastplus',
+    version='2.0.5',
+    packages=['blastplus'],
+    include_package_data=True,
+    license='MIT License',
+    description='A simple Django app to conduct web-based blast+ local alignment search.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/michal-stuglik/django-blastplus',
+    author='Michal Stuglik',
+    author_email='stuglik@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
