a81c8c303e616d9eec10dacd1d5ced1fd2f7667d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:46 +0000

    Add package puflib/0.2.14/puflib-0.2.14-py3-none-any.whl

---
+++ packages/puflib/puflib-0.2.14-py3-none-any.whl/puflib/__init__.py
@@ -0,0 +1,9 @@
+"""PUFlib is a library for emulating physically unclonable functions."""
+
+import numpy as np
+from .architectures import *
+from .util import *
+from .version import get_version
+
+
+__version__ = get_version()
