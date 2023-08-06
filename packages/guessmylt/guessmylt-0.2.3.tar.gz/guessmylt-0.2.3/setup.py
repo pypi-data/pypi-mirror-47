df991fd0357df49f8e5cfc006967d4b3fe9b856e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:40 +0000

    Add package guessmylt/0.2.3/guessmylt-0.2.3.tar.gz

---
+++ packages/guessmylt/guessmylt-0.2.3.tar.gz/guessmylt-0.2.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='guessmylt',
+    version='0.2.3',
+
+    description='An efficient way to guess the library type of RNA-reads',
+
+    url='https://github.com/NBISweden/GUESSmyLT',
+    author='Hampus Olin, Erik Berner-Wik, Caitlin Vigetun Haughey, Lisa Klasson, Jacques Dainat',
+
+    license='GPL-3.0',
+    packages=find_packages(),
+
+    install_requires=['biopython==1.67', 'bcbio-gff==0.6.4', 'pysam>=0.13.0', 'snakemake==5.4.*'],
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': ['GUESSmyLT = GUESSmyLT.GUESSmyLT:main',
+        'GUESSmyLT-example = GUESSmyLT.test.example:main',
+        ],
+    },
+)
