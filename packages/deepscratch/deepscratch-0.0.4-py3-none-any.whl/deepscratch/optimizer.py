6c6ce01e7b6dc1d3937c33e822026ca01ce1e4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:18 +0000

    Add package deepscratch/0.0.4/deepscratch-0.0.4-py3-none-any.whl

---
+++ packages/deepscratch/deepscratch-0.0.4-py3-none-any.whl/deepscratch/optimizer.py
@@ -0,0 +1,19 @@
+class Optimizer:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class SGD(Optimizer):
+    def __init__(self, layers, lr=0.001, momentum=0.1):
+        self.lr = lr
+        self.momentum = momentum
+        self.layers = list(layers)
+
+    def update(self):
+        for layer in self.layers:
+            if layer.trainable:
+                for param, grad in zip(layer.parameters(), layer.dparameters()):
+                    param -= self.lr * grad
