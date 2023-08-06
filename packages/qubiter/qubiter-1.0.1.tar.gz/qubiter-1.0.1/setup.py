f33eda37c6dac28b41afb345ca7d5e48823c08bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:05 +0000

    Add package qubiter/1.0.1/qubiter-1.0.1.tar.gz

---
+++ packages/qubiter/qubiter-1.0.1.tar.gz/qubiter-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="qubiter",
+    version="1.0.1",
+    author="qubiter contributors",
+    keywords = ('quantum compiler'),
+    author_email="rrtucci@gmail.com",
+    description="Python tools for reading, writing, compiling, simulating quantum computer circuits.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/artiste-qb-net/qubiter",
+    packages=find_packages(),
+    #include_package_data=True,
+    package_data={
+        '': ['*.txt', '*.pdf', '*.ipynb', '*.md', '*.tex', '*.sty'],
+        'qubiter': ['quantum_CSD_compiler/*.md'],
+    },	
+    data_files=[('qubiter', ['go_to_qubiter_inner_notebooks.ipynb'])],
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
