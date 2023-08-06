cb3c1d52b20061b32fefca3c39f5937b03e2f715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:27 +0000

    Add package awswrangler/0.0b3/awswrangler-0.0b3-py36,py37-none-any.whl

---
+++ packages/awswrangler/awswrangler-0.0b3-py36,py37-none-any.whl/awswrangler/exceptions.py
@@ -0,0 +1,18 @@
+class UnsupportedType(Exception):
+    pass
+
+
+class UnsupportedFileFormat(Exception):
+    pass
+
+
+class UnsupportedWriteMode(Exception):
+    pass
+
+
+class InvalidArguments(Exception):
+    pass
+
+
+class RedshiftLoadError(Exception):
+    pass
