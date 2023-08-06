5b418558d6b3f5653fae5dc4dc8e78a479d96d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:53 +0000

    Add package finex/0.0.1/finex-0.0.1-py2.py3-none-any.whl

---
+++ packages/finex/finex-0.0.1-py2.py3-none-any.whl/finex/__init__.py
@@ -0,0 +1,17 @@
+"""
+Python Ecosystem for Finance
+~~~~~~~~~~~~
+
+This package contains code to:
+
+* Calculate main financials
+* Calculate financial ratios
+"""
+
+name = "finex"
+
+
+from finex.ratios import *
+from finex.financials import *
+
+__author__ = 'Mehmet Dogan'
