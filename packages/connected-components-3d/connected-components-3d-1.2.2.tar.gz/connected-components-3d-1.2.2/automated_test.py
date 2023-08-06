e03e7544af84723dae9e00113140009fb4fa65ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:01 +0000

    Add package connected-components-3d/1.2.2/connected-components-3d-1.2.2.tar.gz

---
+++ packages/connected-components-3d/connected-components-3d-1.2.2.tar.gz/connected-components-3d-1.2.2/automated_test.py
@@ -0,0 +1,390 @@
+import pytest
+
+import cc3d
+import numpy as np
+
+TEST_TYPES = [
+  np.int8, np.int16, np.int32, np.int64,
+  np.uint8, np.uint16, np.uint32, np.uint64,
+]
+
+def gt_c2f(gt):
+  f_gt = np.copy(gt)
+  mx = np.max(gt) + 1
+  f_gt[ f_gt == 2 ] = mx
+  f_gt[ f_gt == 3 ] = 2
+  f_gt[ f_gt == mx ] = 3
+  return f_gt
+
+def test_2d_square():
+  def test(order, ground_truth):
+    for dtype in TEST_TYPES:
+      input_labels = np.zeros( (16,16), dtype=dtype, order=order )
+      input_labels[:8,:8] = 8
+      input_labels[8:,:8] = 9
+      input_labels[:8,8:] = 10
+      input_labels[8:,8:] = 11
+
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      
+      print(order)
+      print(output_labels)
+
+      assert np.all(output_labels == ground_truth.astype(dtype))
+
+  ground_truth = np.array([
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+  ])
+
+  test('C', ground_truth)
+  test('F', ground_truth.T)
+
+def test_2d_rectangle():
+  def test(order, ground_truth):
+    for dtype in TEST_TYPES:
+      input_labels = np.zeros( (16,13,1), dtype=dtype, order=order )
+      input_labels[:8,:8,:] = 8
+      input_labels[8:,:8,:] = 9
+      input_labels[:8,8:,:] = 10
+      input_labels[8:,8:,:] = 11
+
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      print(output_labels.shape)
+      output_labels = output_labels[:,:,0]
+
+      print(output_labels)
+
+      assert np.all(output_labels == ground_truth.astype(dtype))
+
+  ground_truth = np.array([
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+  ])
+
+  test('C', ground_truth)
+  test('F', gt_c2f(ground_truth))
+
+def test_2d_cross():
+  def test(order, ground_truth):
+    for dtype in TEST_TYPES:
+      input_labels = np.zeros( (17,17), dtype=dtype, order=order)
+      input_labels[:] = 1
+      input_labels[:,8] = 0
+      input_labels[8,:] = 0
+
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      print(output_labels)
+
+      assert np.all(output_labels == ground_truth)
+
+      input_labels[9:,9:] = 2
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      output_labels = output_labels[:,:]
+      assert np.all(output_labels == ground_truth)
+
+  ground_truth = np.array([
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3, 3, 3, 0, 4, 4, 4, 4, 4, 4, 4, 4],
+  ], dtype=np.uint32)
+
+  test('C', ground_truth)
+  test('F', gt_c2f(ground_truth))
+
+
+def test_2d_diagonals():
+  input_labels = np.array([
+    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  ], dtype=np.uint32)
+
+  ground_truth = np.array([
+    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 0, 0, 1, 0, 0, 3, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 1, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0],
+    [2, 0, 2, 0, 0, 0, 4, 4, 0, 0, 4, 4, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 4, 4, 0, 0, 0, 0, 0],
+    [0, 0, 5, 0, 6, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+    [0, 5, 0, 0, 0, 6, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0],
+  ], dtype=np.uint32)
+
+  output_labels = cc3d.connected_components(input_labels)
+  print(output_labels)
+  assert np.all(output_labels == ground_truth)
+
+def test_2d_cross_with_intruder():
+  def test(order, ground_truth):
+    input_labels = np.zeros( (5,5), dtype=np.uint8, order=order)
+    input_labels[:] = 1
+    input_labels[:,2] = 0
+    input_labels[2,:] = 0
+    input_labels[3:,3:] = 2
+    input_labels[3,3] = 1
+
+    output_labels = cc3d.connected_components(input_labels).astype(np.uint8)
+    assert np.all(output_labels == ground_truth)
+
+  ground_truth = np.array([
+    [1, 1, 0, 2, 2],
+    [1, 1, 0, 2, 2],
+    [0, 0, 0, 0, 0],
+    [3, 3, 0, 4, 5],
+    [3, 3, 0, 5, 5],
+  ], dtype=np.uint8)
+
+  test("C", ground_truth)
+  test("F", gt_c2f(ground_truth))
+
+def test_3d_all_different():
+  for order in ("C", "F"):
+    input_labels = np.arange(0, 100 * 99 * 98).astype(np.uint32)
+    input_labels = input_labels.reshape((100,99,98), order=order)
+
+    output_labels = cc3d.connected_components(input_labels)
+
+    assert np.unique(output_labels).shape[0] == 100*99*98
+    assert output_labels.shape == (100, 99, 98)
+
+def test_3d_cross():
+  def test(order, ground_truth):
+    print(order)
+    for dtype in TEST_TYPES:
+      print(dtype)
+      input_labels = np.zeros( (7,7,7), dtype=dtype, order=order )
+      input_labels[:] = 1
+      input_labels[:,3,:] = 0
+      input_labels[:,:,3] = 0
+
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      print(output_labels)
+      assert np.all(output_labels == ground_truth)
+
+      input_labels[:,4:,4:] = 2
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      assert np.all(output_labels == ground_truth)
+
+  ground_truth = np.array([
+     [[1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4]]], dtype=np.uint32)
+
+  test('C', ground_truth)
+  test('F', gt_c2f(ground_truth))
+
+def test_3d_cross_asymmetrical():
+  def test(order, ground_truth):
+    print(order)
+    for dtype in TEST_TYPES:
+      print(dtype)
+      input_labels = np.zeros( (7,7,8), dtype=dtype, order=order )
+      input_labels[:] = 1
+      input_labels[:,3,:] = 0
+      input_labels[:,:,3] = 0
+
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      print(output_labels)
+      assert np.all(output_labels == ground_truth)
+
+      input_labels[:,4:,4:] = 2
+      output_labels = cc3d.connected_components(input_labels).astype(dtype)
+      assert np.all(output_labels == ground_truth)
+
+  ground_truth = np.array([
+     [[1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4]],
+
+     [[1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [1, 1, 1, 0, 2, 2, 2, 2],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4],
+      [3, 3, 3, 0, 4, 4, 4, 4]],
+
+    ], dtype=np.uint32)
+
+  test('C', ground_truth)
+  test('F', gt_c2f(ground_truth))
+
+def test_512_cube_no_segfault_no_jitsu():
+  input_labels = np.arange(0, 512 ** 3).astype(np.uint64).reshape((512,512,512))
+  output_labels = cc3d.connected_components(input_labels)
+
+def test_max_labels_nonsensical():
+  input_labels = np.arange(0, 64 ** 3).astype(np.uint64).reshape((64,64,64))
+  real_labels = cc3d.connected_components(input_labels, max_labels=64*64*64)
+  zero_labels = cc3d.connected_components(input_labels, max_labels=0)
+  negative_labels = cc3d.connected_components(input_labels, max_labels=-50)
+
+  assert np.all(real_labels == zero_labels)
+  assert np.all(real_labels == negative_labels)
+
+def test_compare_scipy():
+  import scipy.ndimage.measurements
+
+  sx, sy, sz = 256, 256, 256
+  labels = np.random.randint(0,2, (sx,sy,sz), dtype=np.bool)
+
+  structure = [
+    [[1,1,1], [1,1,1], [1,1,1]],
+    [[1,1,1], [1,1,1], [1,1,1]],
+    [[1,1,1], [1,1,1], [1,1,1]]
+  ]
+
+  cc3d_labels = cc3d.connected_components(labels)
+  scipy_labels, wow = scipy.ndimage.measurements.label(labels, structure=structure)
+
+  print(cc3d_labels)
+  print(scipy_labels)
+
+  assert np.all(cc3d_labels == scipy_labels)
+
+
+# def test_sixty_four_bit():
+#   input_labels = np.ones((1626,1626,1626), dtype=np.uint8)
+#   cc3d.connected_components(input_labels, max_labels=0)  
---
+++ packages/connected-components-3d/connected-components-3d-1.2.2.tar.gz/connected-components-3d-1.2.2/cc3d.pyx
@@ -0,0 +1,166 @@
+"""
+Cython binding for connected components applied to 3D images
+with 26-connectivity and handling for multiple labels.
+
+Author: William Silversmith
+Affiliation: Seung Lab, Princeton Neuroscience Institute
+Date: August 2018
+"""
+
+from libc.stdlib cimport calloc, free
+from libc.stdint cimport (
+  int8_t, int16_t, int32_t, int64_t,
+  uint8_t, uint16_t, uint32_t, uint64_t,
+)
+from cpython cimport array 
+import array
+import sys
+
+from libcpp.vector cimport vector
+cimport numpy as cnp
+import numpy as np
+
+__VERSION__ = '1.2.2'
+
+cdef extern from "cc3d.hpp" namespace "cc3d":
+  cdef uint32_t* connected_components3d[T](
+    T* in_labels, 
+    int64_t sx, int64_t sy, int64_t sz,
+    int64_t max_labels, uint32_t* out_labels
+  )
+
+def connected_components(data, int64_t max_labels=-1):
+  """
+  Connected components applied to 3D images
+  with 26-connectivity and handling for multiple labels.
+
+  Required:
+    data: Input weights in a 2D or 3D numpy array. 
+  Optional:
+    max_labels (int): save memory by predicting the maximum
+      number of possible labels that might be output.
+      Defaults to number of voxels.
+  
+  Returns: 2D or 3D numpy array remapped to reflect
+    the connected components.
+  """
+  dims = len(data.shape)
+  assert dims in (1, 2, 3)
+
+  if data.size == 0:
+    return np.zeros(shape=(0,), dtype=np.uint32)
+
+  order = 'F' if data.flags['F_CONTIGUOUS'] else 'C'
+
+  while len(data.shape) < 3:
+    if order == 'C':
+      data = data[np.newaxis, ...]
+    else: # F
+      data = data[..., np.newaxis ]
+
+  if not data.flags['C_CONTIGUOUS'] and not data.flags['F_CONTIGUOUS']:
+    data = np.copy(data, order=order)
+
+  shape = list(data.shape)
+
+  # The default C order of 4D numpy arrays is (channel, depth, row, col)
+  # col is the fastest changing index in the underlying buffer. 
+  # fpzip expects an XYZC orientation in the array, namely nx changes most rapidly. 
+  # Since in this case, col is the most rapidly changing index, 
+  # the inputs to fpzip should be X=col, Y=row, Z=depth, F=channel
+  # If the order is F, the default array shape is fine.
+  if order == 'C':
+    shape.reverse()
+
+  cdef int sx = shape[0]
+  cdef int sy = shape[1]
+  cdef int sz = shape[2]
+
+  cdef uint8_t[:,:,:] arr_memview8u
+  cdef uint16_t[:,:,:] arr_memview16u
+  cdef uint32_t[:,:,:] arr_memview32u
+  cdef uint64_t[:,:,:] arr_memview64u
+  cdef int8_t[:,:,:] arr_memview8
+  cdef int16_t[:,:,:] arr_memview16
+  cdef int32_t[:,:,:] arr_memview32
+  cdef int64_t[:,:,:] arr_memview64
+
+  cdef uint64_t voxels = <uint64_t>sx * <uint64_t>sy * <uint64_t>sz
+  cdef cnp.ndarray[uint32_t, ndim=1] out_labels = np.zeros( (voxels,), dtype=np.uint32, order='C' )
+
+  if max_labels <= 0:
+    max_labels = voxels
+
+  dtype = data.dtype
+  
+  if dtype == np.uint64:
+    arr_memview64u = data
+    labels = connected_components3d[uint64_t](
+      &arr_memview64u[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  elif dtype == np.uint32:
+    arr_memview32u = data
+    labels = connected_components3d[uint32_t](
+      &arr_memview32u[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  elif dtype == np.uint16:
+    arr_memview16u = data
+    labels = connected_components3d[uint16_t](
+      &arr_memview16u[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  elif dtype in (np.uint8, np.bool):
+    arr_memview8u = data.astype(np.uint8)
+    labels = connected_components3d[uint8_t](
+      &arr_memview8u[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  elif dtype == np.int64:
+    arr_memview64 = data
+    labels = connected_components3d[int64_t](
+      &arr_memview64[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  elif dtype == np.int32:
+    arr_memview32 = data
+    labels = connected_components3d[int32_t](
+      &arr_memview32[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  elif dtype == np.int16:
+    arr_memview16 = data
+    labels = connected_components3d[int16_t](
+      &arr_memview16[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  elif dtype == np.int8:
+    arr_memview8 = data
+    labels = connected_components3d[int8_t](
+      &arr_memview8[0,0,0],
+      sx, sy, sz, max_labels,
+      <uint32_t*>&out_labels[0]
+    )
+  else:
+    raise TypeError("Type {} not currently supported.".format(dtype))
+
+  if dims == 3:
+    if order == 'C':
+      return out_labels.reshape( (sz, sy, sx), order=order)
+    else:
+      return out_labels.reshape( (sx, sy, sz), order=order)
+  elif dims == 2:
+    if order == 'C':
+      return out_labels.reshape( (sy, sx), order=order)
+    else:
+      return out_labels.reshape( (sx, sy), order=order)
+  else:
+    return out_labels.reshape( (sx), order=order)
