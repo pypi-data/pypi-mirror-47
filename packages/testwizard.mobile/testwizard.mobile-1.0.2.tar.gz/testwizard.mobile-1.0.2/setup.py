bb7613109ada43ba8ce02eff598e48a588176aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:16 +0000

    Add package testwizard.mobile/1.0.2/testwizard.mobile-1.0.2.tar.gz

---
+++ packages/testwizard.mobile/testwizard.mobile-1.0.2.tar.gz/testwizard.mobile-1.0.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.mobile",
+    version="1.0.2",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard for Mobile testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.mobile'],
+    install_requires=[
+        'testwizard.core==1.0.2',
+        'testwizard.testobjects-core==1.0.2',
+        'testwizard.commands-audio==1.0.2',
+        'testwizard.commands-mobile==1.0.2',
+        'testwizard.commands-video==1.0.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
