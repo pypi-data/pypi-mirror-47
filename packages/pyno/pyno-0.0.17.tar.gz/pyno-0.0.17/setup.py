108b2284eb6c44ff13596bfff84d6477ecceb076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:11 +0000

    Add package pyno/0.0.17/pyno-0.0.17.tar.gz

---
+++ packages/pyno/pyno-0.0.17.tar.gz/pyno-0.0.17/setup.py
@@ -0,0 +1,29 @@
+import re
+import ast
+from setuptools import setup
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+
+with open('pyno/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+
+setup(
+    name='pyno',
+    author='Jackie Vincent Larsen',
+    author_email='jackie.v.larsen@gmail.com',
+    version=version,
+    #url='https://github.com/jVinc/pyno',
+    description='A framework for building html, svg and other structured text in Python',
+    keywords=['web-development', 'web'],
+    packages=['pyno'],
+    install_requires=['beautifulsoup4'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ],
+)
