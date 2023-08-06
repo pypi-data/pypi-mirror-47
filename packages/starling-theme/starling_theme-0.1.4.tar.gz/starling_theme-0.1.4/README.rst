7c8ac0949b29ebd4fc8dff673763b87d0055c769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:53 +0000

    Add package starling-theme/0.1.4/starling_theme-0.1.4.tar.gz

---
+++ packages/starling-theme/starling_theme-0.1.4.tar.gz/starling_theme-0.1.4/README.rst
@@ -0,0 +1,39 @@
+=====================
+StARLinG Sphinx Theme
+=====================
+
+A Sphinx Theme for `StARLinG Lab <https://starling.utdallas.edu>`_ Documentation.
+
+There are already a lot of tools that support writing and building documentation with Sphinx. This component makes it easy to generate ``.html`` pages that meld seamlessly with `StARLinG's Webpage <https://github.com/starling-lab/starling.utdallas.edu>`_.
+
+Installation & Getting Started
+------------------------------
+
+1. Clone the repository
+
+.. code-block:: bash
+
+  $ git clone git@github.com:starling-lab/starling-theme.git
+  $ python setup.py develop
+
+2. This repository uses itself as a theme. Build the documentation with Sphinx and open it with a browser.
+
+.. code-block:: bash
+
+  $ cd documentation
+  $ make html
+  $ firefox build/html/index.html
+
+Project State & Current Goals
+-----------------------------
+
+This repository is in an *Alpha Phase*. The goal is to create a documentation theme which blends seamlessly with the StARLinG Website, but exactly how this may be achieved is up for debate.
+
+Open Problems are tracked through the `Issues Page <https://github.com/starling-lab/starling-theme/issues/>`_.
+
+License
+-------
+
+Copyright © 2018, Alexander L. Hayes
+
+Distributed under the terms of the MIT License. See the LICENSE at the base of the repository for details.
---
+++ packages/starling-theme/starling_theme-0.1.4.tar.gz/starling_theme-0.1.4/setup.py
@@ -0,0 +1,59 @@
+# MIT License
+
+# Copyright (c) 2018 Alexander L. Hayes
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+``setup.py`` for ``starling_theme``
+"""
+
+from codecs import open as open_
+from os import path
+from setuptools import setup
+
+from starling_theme import __version__
+
+HERE = path.abspath(path.dirname(__file__))
+with open_(path.join(HERE, "README.rst"), encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="starling_theme",
+    version=__version__,
+    license="MIT",
+    author="Alexander L. Hayes",
+    author_email="alexander@batflyer.net",
+    url="https://github.com/starling-lab/starling-theme/",
+    description="Sphinx Theme for the StARLinG Lab.",
+    long_description=LONG_DESCRIPTION,
+    packages=["starling_theme"],
+    package_data={"starling_theme": ["theme.conf", "*.html", "static/css/*.css"]},
+    include_package_data=True,
+    entry_points={"sphinx.html_themes": ["starling_theme = starling_theme"]},
+    install_requires=["sphinx"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Framework :: Sphinx",
+        "Framework :: Sphinx :: Theme",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Documentation",
+    ],
+)
