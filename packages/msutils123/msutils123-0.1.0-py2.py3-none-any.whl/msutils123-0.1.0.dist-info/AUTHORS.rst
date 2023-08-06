19dd78a351d77d1a4cc8fb7cc92e703884785994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:38 +0000

    Add package msutils123/0.1.0/msutils123-0.1.0-py2.py3-none-any.whl

---
+++ packages/msutils123/msutils123-0.1.0-py2.py3-none-any.whl/msutils123-0.1.0.dist-info/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Tuan Vu <tuanmanh@gmail.com>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/msutils123/msutils123-0.1.0-py2.py3-none-any.whl/utils_tools_for_musesoft/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Utils tools for Musesoft."""
+
+__author__ = """Tuan Vu"""
+__email__ = 'tuanmanh@gmail.com'
+__version__ = '0.1.0'
