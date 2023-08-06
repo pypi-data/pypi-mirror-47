16f8d928dfec7c06a4eefebc4e1e3b2249e616d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:03 +0000

    Add package clarusui/0.400/clarusui-0.400-py2.py3-none-any.whl

---
+++ packages/clarusui/clarusui-0.400-py2.py3-none-any.whl/clarusui/iframe.py
@@ -0,0 +1,14 @@
+from clarusui.layout import Element
+from clarusui import templates as tmp
+
+class IFrame(Element):
+    def __init__(self, response, **options):
+        super(IFrame, self).__init__(response, **options)
+        
+        if response is None:
+            raise ValueError("IFrame needs a content source URL")
+        self.add_custom_css({'height':'90vh', 'overflow':'hidden'})
+    
+    def toDiv(self):
+        return tmp.IFRAME_TEMPLATE.render(element=self)
+    
