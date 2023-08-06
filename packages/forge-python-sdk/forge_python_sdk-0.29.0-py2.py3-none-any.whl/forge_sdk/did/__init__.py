cb7a61a44b05327aba1d69aba3e7690decbb64da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:42 +0000

    Add package forge-python-sdk/0.29.0/forge_python_sdk-0.29.0-py2.py3-none-any.whl

---
+++ packages/forge-python-sdk/forge_python_sdk-0.29.0-py2.py3-none-any.whl/forge_sdk/did/__init__.py
@@ -0,0 +1,29 @@
+from forge_sdk.did.abt_did import AbtDid
+from forge_sdk.did.auth import *
+from forge_sdk.did.lib import *
+from forge_sdk.did.mobile import WalletResponse
+
+PREFIX = "did:abt:"
+
+
+def get_asset_address(itx):
+    """
+    Calculate asset address for provided CreateAssetTx
+
+    Args:
+        did_address(string): user's did address to create this asset
+        itx(:obj:`google.protobuf.any`): encoded createAssetTx
+
+    Returns:
+        asset_address(string)
+
+    Examples:
+        >>> from forge_sdk import protos, utils
+        >>> itx = protos.CreateAssetTx(data=utils.encode_to_any(None,b'123'))
+        >>> res = get_asset_address(itx)
+
+    """
+    data = itx.SerializeToString()
+    asset_did = AbtDid(role_type='asset')
+    asset_address = asset_did.pk_to_did(data).split(":")[-1]
+    return asset_address
