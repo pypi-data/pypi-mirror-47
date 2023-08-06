4e9f2ab087643833bebc32a0b7d2a685f1e526c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:34 +0000

    Add package SimpleDataTransport/1.4/SimpleDataTransport-1.4.tar.gz

---
+++ packages/SimpleDataTransport/SimpleDataTransport-1.4.tar.gz/SimpleDataTransport-1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='SimpleDataTransport',
+    version='1.4',
+    author="Raymond Tunstill",
+    author_email="ray.tunstill@live.co.uk",
+    description="Simple python 2/3 library for transporting data to a remote machine, applying transformations "
+                "and returning a response.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RaymondKirk/SimpleDataTransport",
+    packages=["SimpleDataTransport"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=['Flask', 'numpy', 'requests', 'jsonpickle'],
+)
