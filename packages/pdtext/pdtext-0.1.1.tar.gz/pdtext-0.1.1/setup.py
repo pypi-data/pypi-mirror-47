9cf5b6c1fd5920d97c6fe330ac9fe2e7e3d9d3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:45 +0000

    Add package pdtext/0.1.1/pdtext-0.1.1.tar.gz

---
+++ packages/pdtext/pdtext-0.1.1.tar.gz/pdtext-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pdtext",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=['numpy', 'matplotlib',
+                      'scikit-learn', 'pandas',
+                     ],
+    include_package_data=True,
+
+    author="Neal Caren",
+    author_email="neal.caren@gmail.com",
+    description=("Helper functions for working with text in pandas"),
+    license="BSD",
+    keywords="pandas ",
+    url="https://github.com/nealcaren/pdtext",
+)
