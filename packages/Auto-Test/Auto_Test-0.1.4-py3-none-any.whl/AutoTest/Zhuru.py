206a0fcc9b83aa1d359394875894b5dff2a75fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:48 +0000

    Add package Auto-Test/0.1.4/Auto_Test-0.1.4-py3-none-any.whl

---
+++ packages/Auto-Test/Auto_Test-0.1.4-py3-none-any.whl/AutoTest/Zhuru.py
@@ -0,0 +1,17 @@
+from FormCommon import *
+
+class Zhuru():
+    # student_phone = getRandomPhone()  # 创建学生手机号
+    # student_name = getRandomName()  # 创建学生姓名
+    # student_extraPhone = getRandomPhone()  # 创建学生次要手机号
+    def student_name(self):
+        return getRandomName()
+    def student_phone(self):
+        return getRandomPhone()
+    def student_extraPhone(self):
+        return getRandomPhone()
+
+#
+# if __name__ == '__main__':
+#     print(Zhuru().student_name())
+#     print(Zhuru().student_name())
