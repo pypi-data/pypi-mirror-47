73c100f8f4494eb14405b81a527c0f316b050924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:48 +0000

    Add package pynets/0.7.39/pynets-0.7.39-py2.py3-none-any.whl

---
+++ packages/pynets/pynets-0.7.39-py2.py3-none-any.whl/pynets/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for PyNets."""
+
+__author__ = """Derek Pisner"""
+__email__ = 'dpisner@utexas.edu'
+
+from .stats import *
+from .registration import *
+from .dmri import *
+from .fmri import *
+from .plotting import *
