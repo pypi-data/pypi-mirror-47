0049a3df2f6b8807cea0b9226bca6ae1f2666a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:32 +0000

    Add package reprobench/0.11.0.dev7/reprobench-0.11.0.dev7.tar.gz

---
+++ packages/reprobench/reprobench-0.11.0.dev7.tar.gz/reprobench-0.11.0.dev7/reprobench/core/worker.py
@@ -0,0 +1,81 @@
+import atexit
+import json
+from pathlib import Path
+
+import click
+import zmq
+from loguru import logger
+
+from reprobench.console.decorators import common, server_info
+from reprobench.core.events import (
+    RUN_FINISH,
+    RUN_INTERRUPT,
+    RUN_START,
+    RUN_STEP,
+    WORKER_JOIN,
+    WORKER_LEAVE,
+)
+from reprobench.utils import decode_message, import_class, send_event
+
+REQUEST_TIMEOUT = 15000
+
+
+class BenchmarkWorker:
+    def __init__(self, server_address):
+        self.server_address = server_address
+
+    def killed(self, run_id):
+        send_event(self.socket, RUN_INTERRUPT, run_id)
+        send_event(self.socket, WORKER_LEAVE)
+
+    def run(self):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.DEALER)
+        self.socket.connect(self.server_address)
+
+        send_event(self.socket, WORKER_JOIN)
+        run = decode_message(self.socket.recv())
+
+        self.run_id = run["id"]
+        atexit.register(self.killed, self.run_id)
+
+        tool = import_class(run["tool"])
+
+        if not tool.is_ready():
+            tool.setup()
+
+        context = {}
+        context["socket"] = self.socket
+        context["tool"] = tool
+        context["run"] = run
+        logger.info(f"Processing task: {run['id']}")
+
+        directory = Path(run["id"])
+        directory.mkdir(parents=True, exist_ok=True)
+
+        payload = dict(tool_version=tool.version(), run_id=self.run_id)
+        send_event(self.socket, RUN_START, payload)
+
+        for runstep in run["steps"]:
+            logger.debug(f"Running step {runstep['module']}")
+            step = import_class(runstep["module"])
+            config = json.loads(runstep["config"])
+            step.execute(context, config)
+            payload = {"run_id": self.run_id, "step": runstep["module"]}
+            send_event(self.socket, RUN_STEP, payload)
+
+        send_event(self.socket, RUN_FINISH, self.run_id)
+        atexit.unregister(self.killed)
+        send_event(self.socket, WORKER_LEAVE, self.run_id)
+
+
+@click.command("worker")
+@server_info
+@common
+def cli(server_address):
+    worker = BenchmarkWorker(server_address)
+    worker.run()
+
+
+if __name__ == "__main__":
+    cli()
