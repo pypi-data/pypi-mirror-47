af0e9e52dd25b354b54cc6a4cbd06b9cbc3bfdcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:56 +0000

    Add package PatrowlEnginesUtils/0.0.7/PatrowlEnginesUtils-0.0.7.tar.gz

---
+++ packages/PatrowlEnginesUtils/PatrowlEnginesUtils-0.0.7.tar.gz/PatrowlEnginesUtils-0.0.7/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup file."""
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PatrowlEnginesUtils',
+    version='0.0.7',
+    description='Common classes for PatrowlEngines',
+    url='https://github.com/Patrowl/PatrowlEnginesUtils',
+    author='Nicolas Mattiocco',
+    author_email='getsupport@patrowl.io',
+    license='AGPLv3',
+    packages=find_packages(),
+)
