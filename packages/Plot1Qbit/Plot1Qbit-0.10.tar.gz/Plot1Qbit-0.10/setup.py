75bc713196f0724927a80e3f6f67a8b7f6e61de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:32 +0000

    Add package Plot1Qbit/0.10/Plot1Qbit-0.10.tar.gz

---
+++ packages/Plot1Qbit/Plot1Qbit-0.10.tar.gz/Plot1Qbit-0.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup( name='Plot1Qbit',
+       version='0.10',
+       author='Kyle Mills',
+       author_email="kyle.mills@uoit.net",
+       description="Set matplotlib style to conform with 1QBit's brand policies",
+       packages=['plot1qbit'],
+       include_package_data=True
+    )
