b758dbf1ebfcfa0b322aee46f1730d21d81ac693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:28 +0000

    Add package martypy/1.3/martypy-1.3.tar.gz

---
+++ packages/martypy/martypy-1.3.tar.gz/martypy-1.3/setup.py
@@ -0,0 +1,63 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name="martypy",
+    version="1.3",
+    description="Client library for Marty the Robot by Robotical Ltd",
+    long_description=readme,
+    author="Robotical Ltd",
+    author_email="hello@robotical.io",
+    maintainer='Robotical Ltd',
+    maintainer_email='hello@robotical.io',
+    packages=['martypy'],
+    url='http://github.com/robotical/martypy',
+    license='Apache 2.0',
+    install_requires=[
+        'six>=1.12.0',
+        'requests>=2.22.0',
+    ],
+    keywords=[
+        'ros',
+        'robot',
+        'marty',
+        'marty the robot',
+        'robotical',
+    ],
+    classifiers=(
+        # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        #'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        #'Environment :: Web Environment',
+        #'Operating System :: POSIX',
+        'License :: OSI Approved :: Apache Software License',
+    )
+)
+
+# To Publish:
+# First, build a source distribution:
+# $ python setup.py sdist
+# Then upload this to PyPi (have ~/.pypirc exist)
+# $ twine upload dist/*
+
