cb2278b38aa474a3b91b9cb17e8d988fa24b27dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:09 +0000

    Add package tagui/0.0.2/tagui-0.0.2.tar.gz

---
+++ packages/tagui/tagui-0.0.2.tar.gz/tagui-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tagui',
+    version='0.0.2',
+    author='Tebel.Automation',
+    author_email='ken@tebel.org',
+    url='https://github.com/tebelorg/TagUI-Python',
+    license='LICENSE.txt',
+    description='Placeholder for TagUI Python package',
+    long_description=open('README.md').read(),
+)
