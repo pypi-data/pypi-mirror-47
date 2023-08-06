874fb94e34b706137712362fee6c767ed26b92dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:42 +0000

    Add package into-dbus-python/0.7/into-dbus-python-0.7.tar.gz

---
+++ packages/into-dbus-python/into-dbus-python-0.7.tar.gz/into-dbus-python-0.7/src/into_dbus_python/_xformer.py
@@ -0,0 +1,329 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Transforming Python basic types to Python dbus types.
+"""
+
+import functools
+
+import dbus
+
+from dbus_signature_pyparsing import Parser
+
+from ._errors import IntoDPValueError
+
+
+def _wrapper(func):
+    """
+    Wraps a generated function so that it catches all Type- and ValueErrors
+    and raises IntoDPValueErrors.
+
+    :param func: the transforming function
+    """
+
+    @functools.wraps(func)
+    def the_func(expr):
+        """
+        The actual function.
+
+        :param object expr: the expression to be xformed to dbus-python types
+        """
+        try:
+            return func(expr)
+        except (TypeError, ValueError) as err:
+            raise IntoDPValueError(expr, "expr", "could not be transformed") \
+               from err
+
+    return the_func
+
+
+class _ToDbusXformer(Parser):
+    """
+    Class which extends a Parser to yield a function that yields
+    a function that transforms a value in base Python types to a correct value
+    using dbus-python types.
+
+    Actually, it yields a pair, a function and a signature string. The
+    signature string is useful for stowing a signature in Array or Dictionary
+    types, as may be necessary if the Array or Dictionary is empty, and so
+    the type can not be inferred from the contents of the value.
+    """
+    # pylint: disable=too-few-public-methods
+
+    @staticmethod
+    def _variant_levels(level, variant):
+        """
+        Gets the level for the variant.
+
+        :param int level: the current variant level
+        :param int variant: the value for this level if variant
+
+        :returns: a level for the object and one for the function
+        :rtype: int * int
+        """
+        return (level + variant, level + variant) \
+           if variant != 0 else (variant, level)
+
+    def _handle_variant(self):
+        """
+        Generate the correct function for a variant signature.
+
+        :returns: function that returns an appropriate value
+        :rtype: ((str * object) or list)-> object
+        """
+
+        def the_func(a_tuple, variant=0):
+            """
+            Function for generating a variant value from a tuple.
+
+            :param a_tuple: the parts of the variant
+            :type a_tuple: (str * object) or list
+            :param int variant: object's variant index
+            :returns: a value of the correct type with correct variant level
+            :rtype: object * int
+            """
+            # pylint: disable=unused-argument
+            (signature, an_obj) = a_tuple
+            (func, sig) = self.COMPLETE.parseString(signature)[0]
+            assert sig == signature
+            (xformed, _) = func(an_obj, variant=variant + 1)
+            return (xformed, xformed.variant_level)
+
+        return (the_func, 'v')
+
+    @staticmethod
+    def _handle_array(toks):
+        """
+        Generate the correct function for an array signature.
+
+        :param toks: the list of parsed tokens
+        :returns: function that returns an Array or Dictionary value
+        :rtype: ((or list dict) -> ((or Array Dictionary) * int)) * str
+        """
+
+        if len(toks) == 5 and toks[1] == '{' and toks[4] == '}':
+            subtree = toks[2:4]
+            signature = ''.join(s for (_, s) in subtree)
+            [key_func, value_func] = [f for (f, _) in subtree]
+
+            def the_dict_func(a_dict, variant=0):
+                """
+                Function for generating a Dictionary from a dict.
+
+                :param a_dict: the dictionary to transform
+                :type a_dict: dict of (`a * `b)
+                :param int variant: variant level
+
+                :returns: a dbus dictionary of transformed values and level
+                :rtype: Dictionary * int
+                """
+                elements = \
+                   [(key_func(x), value_func(y)) for (x, y) in a_dict.items()]
+                level = 0 if elements == [] \
+                   else max(max(x, y) for ((_, x), (_, y)) in elements)
+                (obj_level, func_level) = \
+                   _ToDbusXformer._variant_levels(level, variant)
+                return (dbus.types.Dictionary(
+                    ((x, y) for ((x, _), (y, _)) in elements),
+                    signature=signature,
+                    variant_level=obj_level), func_level)
+
+            return (the_dict_func, 'a{' + signature + '}')
+
+        if len(toks) == 2:
+            (func, sig) = toks[1]
+
+            def the_array_func(a_list, variant=0):
+                """
+                Function for generating an Array from a list.
+
+                :param a_list: the list to transform
+                :type a_list: list of `a
+                :param int variant: variant level of the value
+                :returns: a dbus Array of transformed values and variant level
+                :rtype: Array * int
+                """
+                if isinstance(a_list, dict):
+                    raise IntoDPValueError(a_list, "a_list",
+                                           "is a dict, must be an array")
+                elements = [func(x) for x in a_list]
+                level = 0 if elements == [] else max(x for (_, x) in elements)
+                (obj_level, func_level) = \
+                   _ToDbusXformer._variant_levels(level, variant)
+
+                return (dbus.types.Array(
+                    (x for (x, _) in elements),
+                    signature=sig,
+                    variant_level=obj_level), func_level)
+
+            return (the_array_func, 'a' + sig)
+
+        raise IntoDPValueError(toks, "toks",
+                               "unexpected tokens")  # pragma: no cover
+
+    @staticmethod
+    def _handle_struct(toks):
+        """
+        Generate the correct function for a struct signature.
+
+        :param toks: the list of parsed tokens
+        :returns: function that returns an Array or Dictionary value
+        :rtype: ((list or tuple) -> (Struct * int)) * str
+        """
+        subtrees = toks[1:-1]
+        signature = ''.join(s for (_, s) in subtrees)
+        funcs = [f for (f, _) in subtrees]
+
+        def the_func(a_list, variant=0):
+            """
+            Function for generating a Struct from a list.
+
+            :param a_list: the list to transform
+            :type a_list: list or tuple
+            :param int variant: variant index
+            :returns: a dbus Struct of transformed values and variant level
+            :rtype: Struct * int
+            :raises IntoDPValueError:
+            """
+            if isinstance(a_list, dict):
+                raise IntoDPValueError(a_list, "a_list",
+                                       "must be a simple sequence, is a dict")
+            if len(a_list) != len(funcs):
+                raise IntoDPValueError(
+                    a_list,
+                    "a_list",
+                    "must have exactly %u items, has %u" % \
+                      (len(funcs), len(a_list))
+                )
+            elements = [f(x) for (f, x) in zip(funcs, a_list)]
+            level = 0 if elements == [] else max(x for (_, x) in elements)
+            (obj_level, func_level) = \
+                _ToDbusXformer._variant_levels(level, variant)
+            return (dbus.types.Struct(
+                (x for (x, _) in elements),
+                signature=signature,
+                variant_level=obj_level), func_level)
+
+        return (the_func, '(' + signature + ')')
+
+    @staticmethod
+    def _handle_base_case(klass, symbol):
+        """
+        Handle a base case.
+
+        :param type klass: the class constructor
+        :param str symbol: the type code
+        """
+
+        def the_func(value, variant=0):
+            """
+            Base case.
+
+            :param int variant: variant level for this object
+            :returns: a tuple of a dbus object and the variant level
+            :rtype: dbus object * int
+            """
+            (obj_level, func_level) = _ToDbusXformer._variant_levels(
+                0, variant)
+            return (klass(value, variant_level=obj_level), func_level)
+
+        return lambda: (the_func, symbol)
+
+    def __init__(self):
+        super(_ToDbusXformer, self).__init__()
+
+        self.BYTE.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.Byte, 'y'))
+        self.BOOLEAN.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.Boolean, 'b'))
+        self.INT16.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.Int16, 'n'))
+        self.UINT16.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.UInt16, 'q'))
+        self.INT32.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.Int32, 'i'))
+        self.UINT32.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.UInt32, 'u'))
+        self.INT64.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.Int64, 'x'))
+        self.UINT64.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.UInt64, 't'))
+        self.DOUBLE.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.Double, 'd'))
+        self.UNIX_FD.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.UnixFd, 'h'))
+        self.STRING.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.String, 's'))
+        self.OBJECT_PATH.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.ObjectPath, 'o'))
+        self.SIGNATURE.setParseAction(
+            _ToDbusXformer._handle_base_case(dbus.types.Signature, 'g'))
+
+        self.VARIANT.setParseAction(self._handle_variant)
+
+        self.ARRAY.setParseAction(_ToDbusXformer._handle_array)
+
+        self.STRUCT.setParseAction(_ToDbusXformer._handle_struct)
+
+
+_XFORMER = _ToDbusXformer()
+
+
+def xformers(sig):
+    """
+    Get the list of xformer functions for the given signature.
+
+    :param str sig: a signature
+    :returns: a list of xformer functions for the given signature.
+    :rtype: list of tuple of a function * str
+
+    Each function catches all TypeErrors it encounters and raises
+    corresponding IntoDPValueError exceptions.
+    """
+    return \
+       [(_wrapper(f), l) for (f, l) in \
+       _XFORMER.PARSER.parseString(sig, parseAll=True)]
+
+
+def xformer(signature):
+    """
+    Returns a transformer function for the given signature.
+
+    :param str signature: a dbus signature
+    :returns: a function to transform a list of objects to inhabit the signature
+    :rtype: (list of object) -> (list of object)
+    """
+
+    funcs = [f for (f, _) in xformers(signature)]
+
+    def the_func(objects):
+        """
+        Returns the a list of objects, transformed.
+
+        :param objects: a list of objects
+        :type objects: list of object
+
+        :returns: transformed objects
+        :rtype: list of object (in dbus types)
+        """
+        if len(objects) != len(funcs):
+            raise IntoDPValueError(
+                objects,
+                "objects",
+                "must have exactly %u items, has %u" % \
+                  (len(funcs), len(objects))
+            )
+        return [x for (x, _) in (f(a) for (f, a) in zip(funcs, objects))]
+
+    return the_func
---
+++ packages/into-dbus-python/into-dbus-python-0.7.tar.gz/into-dbus-python-0.7/tests/test_signature_parser.py
@@ -0,0 +1,290 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Test signature parsing.
+"""
+
+from os import environ
+from os import sys
+import string
+import unittest
+
+import dbus
+
+from dbus_signature_pyparsing import Parser
+
+from hypothesis import given
+from hypothesis import settings
+from hypothesis import strategies
+from hypothesis import HealthCheck
+
+from hs_dbus_signature import dbus_signatures
+
+from into_dbus_python import xformer
+from into_dbus_python import xformers
+from into_dbus_python import signature
+from into_dbus_python import IntoDPError
+
+settings.register_profile(
+    "tracing", deadline=None, suppress_health_check=[HealthCheck.too_slow])
+if sys.gettrace() is not None or environ.get('TRAVIS') is not None:
+    settings.load_profile("tracing")
+
+# Omits h, unix fd, because it is unclear what are valid fds for dbus
+SIGNATURE_STRATEGY = dbus_signatures(max_codes=20, blacklist="h")
+
+OBJECT_PATH_STRATEGY = strategies.builds(
+    '/'.__add__,
+    strategies.builds(
+        '/'.join,
+        strategies.lists(
+            strategies.text(
+                alphabet=[
+                    x for x in string.digits + string.ascii_uppercase +
+                    string.ascii_lowercase + '_'
+                ],
+                min_size=1,
+                max_size=10),
+            max_size=10)))
+
+
+class StrategyGenerator(Parser):
+    """
+    Generate a hypothesis strategy for generating objects for a particular
+    dbus signature which make use of base Python classes.
+    """
+    # pylint: disable=too-few-public-methods
+
+    @staticmethod
+    def _handle_array(toks):
+        """
+        Generate the correct strategy for an array signature.
+
+        :param toks: the list of parsed tokens
+        :returns: strategy that generates an array or dict as appropriate
+        :rtype: strategy
+        """
+
+        if len(toks) == 5 and toks[1] == '{' and toks[4] == '}':
+            return strategies.dictionaries(
+                keys=toks[2], values=toks[3], max_size=20)
+        if len(toks) == 2:
+            return strategies.lists(elements=toks[1], max_size=20)
+        raise ValueError("unexpected tokens")  # pragma: no cover
+
+    def __init__(self):
+        super(StrategyGenerator, self).__init__()
+
+        # pylint: disable=unnecessary-lambda
+        self.BYTE.setParseAction(
+            lambda: strategies.integers(min_value=0, max_value=255))
+        self.BOOLEAN.setParseAction(lambda: strategies.booleans())
+        self.INT16.setParseAction(
+            lambda: strategies.integers(min_value=-0x8000, max_value=0x7fff))
+        self.UINT16.setParseAction(
+            lambda: strategies.integers(min_value=0, max_value=0xffff))
+        self.INT32.setParseAction(
+            lambda: strategies.integers(min_value=-0x80000000, max_value=0x7fffffff))
+        self.UINT32.setParseAction(
+            lambda: strategies.integers(min_value=0, max_value=0xffffffff))
+        self.INT64.setParseAction(
+            lambda: strategies.integers(
+                min_value=-0x8000000000000000,
+                max_value=0x7fffffffffffffff))
+        self.UINT64.setParseAction(
+            lambda: strategies.integers(min_value=0, max_value=0xffffffffffffffff))
+        self.DOUBLE.setParseAction(lambda: strategies.floats())
+
+        self.STRING.setParseAction(lambda: strategies.text())
+        self.OBJECT_PATH.setParseAction(lambda: OBJECT_PATH_STRATEGY)
+        self.SIGNATURE.setParseAction(lambda: SIGNATURE_STRATEGY)
+
+        def _handle_variant():
+            """
+            Generate the correct strategy for a variant signature.
+
+            :returns: strategy that generates an object that inhabits a variant
+            :rtype: strategy
+            """
+            signature_strategy = dbus_signatures(
+                max_codes=5,
+                min_complete_types=1,
+                max_complete_types=1,
+                blacklist="h")
+            return signature_strategy.flatmap(
+                lambda x: strategies.tuples(strategies.just(x), self.COMPLETE.parseString(x)[0]))
+
+        self.VARIANT.setParseAction(_handle_variant)
+
+        self.ARRAY.setParseAction(StrategyGenerator._handle_array)
+
+        self.STRUCT.setParseAction(
+            # pylint: disable=used-before-assignment
+            lambda toks: strategies.tuples(*toks[1:-1]))
+
+
+STRATEGY_GENERATOR = StrategyGenerator().PARSER
+
+
+def _descending(dbus_object):
+    """
+    Verify levels of variant values always descend.
+
+    :param object dbus_object: a dbus object
+    :returns: None if there was a failure of the property, otherwise the level
+    :rtype: int or NoneType
+
+    None is a better choice than False, for 0, a valid variant level, is always
+    interpreted as False.
+    """
+    # pylint: disable=too-many-return-statements
+    if isinstance(dbus_object, dbus.Dictionary):
+        key_levels = [_descending(x) for x in dbus_object.keys()]
+        value_levels = [_descending(x) for x in dbus_object.values()]
+        if any(k is None for k in key_levels) or \
+           any(v is None for v in value_levels):
+            return None
+
+        max_key_level = max(key_levels) if key_levels != [] else 0
+        max_value_level = max(value_levels) if value_levels != [] else 0
+        max_level = max(max_key_level, max_value_level)
+
+        variant_level = dbus_object.variant_level
+        if variant_level == 0:
+            return max_level
+
+        return None if variant_level < max_level + 1 else variant_level
+
+    if isinstance(dbus_object, (dbus.Array, dbus.Struct)):
+        levels = [_descending(x) for x in dbus_object]
+        if any(l is None for l in levels):
+            return None
+
+        max_level = max(levels) if levels != [] else 0
+
+        variant_level = dbus_object.variant_level
+        if variant_level == 0:
+            return max_level
+
+        return None if variant_level < max_level + 1 else variant_level
+
+    return dbus_object.variant_level
+
+
+class ParseTestCase(unittest.TestCase):
+    """
+    Test parsing various signatures.
+    """
+
+    @given(dbus_signatures(
+        min_complete_types=1,
+        max_complete_types=1,
+        blacklist="h").flatmap(lambda s: strategies.tuples(
+            strategies.just(s),
+            STRATEGY_GENERATOR.parseString(s, parseAll=True)[0])))
+    @settings(deadline=None, suppress_health_check=[HealthCheck.too_slow])
+    def test_parsing(self, strat):
+        """
+        Test that parsing is always succesful.
+
+        Verify that the original signature corresponds to the signature
+        returned by the parser and to the signature of the generated value.
+
+        Verify that the variant levels always descend within the constructed
+        value.
+        """
+        (a_signature, base_type_object) = strat
+
+        (func, sig_synth) = xformers(a_signature)[0]
+        (value, level) = func(base_type_object)
+        sig_orig = dbus.Signature(a_signature)
+
+        self.assertEqual(sig_orig, sig_synth)
+
+        if 'v' not in sig_orig:
+            self.assertEqual(level, 0)
+        self.assertIsNotNone(_descending(value))
+        self.assertEqual(signature(value), sig_orig)
+
+    @given(dbus_signatures(
+        min_complete_types=1,
+        blacklist="h").map(lambda s: "(%s)" % s).flatmap(lambda s: strategies.tuples(
+            strategies.just(s),
+            STRATEGY_GENERATOR.parseString(s, parseAll=True)[0])))
+    @settings(
+        max_examples=10,
+        suppress_health_check=[HealthCheck.too_slow],
+        deadline=None)
+    def test_struct(self, strat):
+        """
+        Test exception throwing on a struct signature when number of items
+        is not equal to number of complete types in struct signature.
+        """
+        (sig, struct) = strat
+
+        xform = xformer(sig)
+
+        with self.assertRaises(IntoDPError):
+            xform([struct + (1, )])
+
+        with self.assertRaises(IntoDPError):
+            xform([struct[:-1]])
+
+    @given(dbus_signatures(blacklist="hbs", exclude_dicts=True))
+    @settings(max_examples=100)
+    def test_exceptions(self, sig):
+        """
+        Test that an exception is raised for a dict if '{' is blacklisted.
+
+        Need to also blacklist 'b' and 's', since dbus.String and dbus.Boolean
+        constructors can both convert a dict.
+        """
+
+        xform = xformer(sig)
+
+        with self.assertRaises(IntoDPError):
+            xform([{True: True}])
+
+    def test_bad_array_value(self):
+        """
+        Verify that passing a dict for an array will raise an exception.
+        """
+        with self.assertRaises(IntoDPError):
+            xformer('a(qq)')([dict()])
+
+    def test_variant_depth(self):
+        """
+        Verify that a nested variant has appropriate variant depth.
+        """
+        self.assertEqual(
+            xformer('v')([('v', ('v', ('b', False)))])[0].variant_level, 3)
+        self.assertEqual(
+            xformer('v')([('v', ('ab', [False]))])[0],
+            dbus.Array([dbus.Boolean(False)], signature='b', variant_level=2))
+        self.assertEqual(
+            xformer('av')([([('v', ('b', False))])])[0],
+            dbus.Array([dbus.Boolean(False, variant_level=2)], signature="v"))
+
+    @given(STRATEGY_GENERATOR.parseString('v', parseAll=True)[0])
+    @settings(max_examples=50)
+    def test_unpacking(self, value):
+        """
+        Test that signature unpacking works.
+        """
+        dbus_value = xformer('v')([value])[0]
+        unpacked = signature(dbus_value, unpack=True)
+        packed = signature(dbus_value)
+
+        self.assertEqual(packed, 'v')
+        self.assertFalse(unpacked.startswith('v'))
