07c7b5e72c78827c06848cea5dcd02f27ff33d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:17 +0000

    Add package tensorboardY/0.1.9/tensorboardY-0.1.9-py2.py3-none-any.whl

---
+++ packages/tensorboardY/tensorboardY-0.1.9-py2.py3-none-any.whl/tensorboardY/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. include:: ../README.md
+"""
+
+from .server import show
+from .widgets import Widget, Text, Image
+from .__version__ import __version__
+
+__all__ = ['show', 'Widget', 'Text', 'Image', '__version__']
