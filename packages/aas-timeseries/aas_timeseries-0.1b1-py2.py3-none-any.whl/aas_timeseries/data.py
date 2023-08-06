9895de8920e268e805283181f1ef24777b96609f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:28 +0000

    Add package aas-timeseries/0.1b1/aas_timeseries-0.1b1-py2.py3-none-any.whl

---
+++ packages/aas-timeseries/aas_timeseries-0.1b1-py2.py3-none-any.whl/aas_timeseries/data.py
@@ -0,0 +1,26 @@
+import uuid
+
+from astropy.units import Quantity, UnitsError, UnitBase
+
+
+class Data:
+
+    def __init__(self, time_series):
+        self.time_series = time_series
+        self.uuid = str(uuid.uuid4())
+        self.time_column = 'time'
+
+    def column_to_values(self, colname, unit):
+
+        # First make sure the column is a quantity
+        quantity = Quantity(self.time_series[colname], copy=False)
+
+        if quantity.unit.is_equivalent(unit):
+            return quantity.to_value(unit)
+        else:
+            raise UnitsError(f"Cannot convert the units '{quantity.unit}' of "
+                             f"column '{colname}' to the required units of "
+                             f"'{unit}'")
+
+    def unit(self, colname):
+        return Quantity(self.time_series[colname], copy=False).unit
