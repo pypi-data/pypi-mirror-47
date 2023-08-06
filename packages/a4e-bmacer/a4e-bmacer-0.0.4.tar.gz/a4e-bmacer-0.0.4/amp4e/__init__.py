076d4068c50475950f898f62c8834d51173f6d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:16 +0000

    Add package a4e-bmacer/0.0.4/a4e-bmacer-0.0.4.tar.gz

---
+++ packages/a4e-bmacer/a4e-bmacer-0.0.4.tar.gz/a4e-bmacer-0.0.4/amp4e/__init__.py
@@ -0,0 +1,9 @@
+print("hello")
+import requests
+
+class Computer:
+    def __init__(self):
+        self.foo = "bar"
+
+c = Computer()
+
---
+++ packages/a4e-bmacer/a4e-bmacer-0.0.4.tar.gz/a4e-bmacer-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="a4e-bmacer",
+    version="0.0.4",
+    author="Brandon Macer",
+    author_email="bmacer@cisco.com",
+    description="A package for using AMP for Endpoint API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/a4e",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
