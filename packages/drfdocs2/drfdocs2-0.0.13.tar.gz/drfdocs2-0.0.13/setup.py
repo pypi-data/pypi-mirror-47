3ac1292705663122dbda333b671b90bd732126d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:18 +0000

    Add package drfdocs2/0.0.13/drfdocs2-0.0.13.tar.gz

---
+++ packages/drfdocs2/drfdocs2-0.0.13.tar.gz/drfdocs2-0.0.13/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="drfdocs2",
+    version=__import__('rest_framework_docs').__version__,
+    author="Emmanouil Konstantinidis",
+    author_email="manos@iamemmanouil.com",
+    packages=find_packages(),
+    include_package_data=True,
+    url="http://www.drfdocs.com",
+    license='BSD',
+    description="Documentation for Web APIs made with Django 2.0 and DRF",
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
