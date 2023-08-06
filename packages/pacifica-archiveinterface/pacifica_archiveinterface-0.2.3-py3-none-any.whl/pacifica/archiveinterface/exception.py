8bb0201ffcd9621440ab716cbc99b514211c4289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:39 +0000

    Add package pacifica-archiveinterface/0.2.3/pacifica_archiveinterface-0.2.3-py3-none-any.whl

---
+++ packages/pacifica-archiveinterface/pacifica_archiveinterface-0.2.3-py3-none-any.whl/pacifica/archiveinterface/exception.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Module with the Archive Interface Error class."""
+
+
+class ArchiveInterfaceError(Exception):
+    """
+    ArchiveInterfaceError.
+
+    Basic exception class for this module.
+    Will be used to throw exceptions up to the top level of the application
+    """
+
+    pass
