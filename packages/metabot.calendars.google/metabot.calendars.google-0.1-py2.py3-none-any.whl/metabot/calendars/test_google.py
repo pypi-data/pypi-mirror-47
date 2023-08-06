0e2000b6dcad028242f7511be4ffd31296c42a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:58 +0000

    Add package metabot.calendars.google/0.1/metabot.calendars.google-0.1-py2.py3-none-any.whl

---
+++ packages/metabot.calendars.google/metabot.calendars.google-0.1-py2.py3-none-any.whl/metabot/calendars/test_google.py
@@ -0,0 +1,11 @@
+"""Tests for metabot.calendars.google."""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from metabot.calendars import google
+
+
+def test_import():
+    """Dummy test."""
+
+    assert google.Calendar
