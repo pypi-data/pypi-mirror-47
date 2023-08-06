1477558efc2fa4c68abdb074a007faded5be19b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:55 +0000

    Add package strawberry-graphql/0.11.0/strawberry-graphql-0.11.0.tar.gz

---
+++ packages/strawberry-graphql/strawberry-graphql-0.11.0.tar.gz/strawberry-graphql-0.11.0/strawberry/cli/__init__.py
@@ -0,0 +1,46 @@
+import click
+import sys
+
+import os
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+
+import importlib
+
+import uvicorn
+
+import hupper
+
+from strawberry.contrib.starlette import GraphQLApp, GraphQLSubscriptionApp
+
+
+@click.group()
+def run():
+    pass
+
+
+@run.command("server")
+@click.argument("module", type=str)
+@click.option("-h", "--host", default="0.0.0.0", type=str)
+@click.option("-p", "--port", default=8000, type=int)
+def server(module, host, port):
+    sys.path.append(os.getcwd())
+
+    reloader = hupper.start_reloader("strawberry.cli.run", verbose=False)
+
+    schema_module = importlib.import_module(module)
+
+    reloader.watch_files([schema_module.__file__])
+
+    app = Starlette(debug=True)
+
+    app.add_middleware(
+        CORSMiddleware, allow_headers=["*"], allow_origins=["*"], allow_methods=["*"]
+    )
+
+    app.add_route("/graphql", GraphQLApp(schema_module.schema))
+    app.add_websocket_route("/graphql", GraphQLSubscriptionApp(schema_module.schema))
+
+    print(f"Running strawberry on http://{host}:{port}/graphql 🍓")
+
+    uvicorn.run(app, host=host, port=port, log_level="error")
---
+++ packages/strawberry-graphql/strawberry-graphql-0.11.0.tar.gz/strawberry-graphql-0.11.0/strawberry/contrib/starlette/__init__.py
@@ -0,0 +1,2 @@
+from .app.graphql_app import GraphQLApp  # noqa
+from .app.graphql_ws_app import GraphQLSubscriptionApp  # noqa
