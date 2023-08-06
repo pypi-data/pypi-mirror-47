a7a693456843bf526ea881313bd462eb3fc12455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:39 +0000

    Add package robotframework-httplibrary3/0.6.0/robotframework-httplibrary3-0.6.0.tar.gz

---
+++ packages/robotframework-httplibrary3/robotframework-httplibrary3-0.6.0.tar.gz/robotframework-httplibrary3-0.6.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+CLASSIFIERS = """
+Programming Language :: Python
+Topic :: Software Development :: Testing
+"""[1:-1]
+
+from os.path import join, dirname
+long_description = open(join(dirname(__file__), 'README.rst',)).read()
+
+setup(
+    name='robotframework-httplibrary3',
+    version="0.6.0",
+    description='Robot Framework keywords for HTTP requests',
+    long_description="not working right now",
+    author='Filip Noetzel',
+    author_email='filip+rfhttplibrary@j03.de',
+    url='https://github.com/peritus/robotframework-httplibrary',
+    license='Beerware',
+    keywords='robotframework testing testautomation web http webtest',
+    platforms='any',
+    zip_safe=False,
+    classifiers=CLASSIFIERS.splitlines(),
+    package_dir={'': 'src'},
+    install_requires=['robotframework', 'webtest>=2.0', 'jsonpatch',
+                      'jsonpointer'],
+    packages=['HttpLibrary']
+)
+
