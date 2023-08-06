bdc5def7573a9fabd9af6c9a1808a6a3fe2e08aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:58 +0000

    Add package edx-lint/1.3.0/edx-lint-1.3.0.tar.gz

---
+++ packages/edx-lint/edx-lint-1.3.0.tar.gz/edx-lint-1.3.0/setup.py
@@ -0,0 +1,26 @@
+"""
+edx-lint
+========
+
+A collection of code quality tools:
+
+- A few pylint plugins to check for quality issues pylint misses.
+
+- A command-line tool to generate config files like pylintrc from a master
+  file (part of edx_lint), and a repo-specific tweaks file.
+
+"""
+
+import os
+
+from setuptools import setup
+
+# pbr does some things we don't need. Turn them off the only way pbr gives us.
+os.environ['SKIP_GENERATE_AUTHORS'] = '1'
+os.environ['SKIP_WRITE_GIT_CHANGELOG'] = '1'
+
+setup(
+    # to update the version (uses pbr), create a new version tag in the repo
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
+)
