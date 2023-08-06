b79284e57163819e1dd4f5b1299047c10bfb8af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:08 +0000

    Add package beetrack-uploader/0.0.3/beetrack_uploader-0.0.3.tar.gz

---
+++ packages/beetrack-uploader/beetrack_uploader-0.0.3.tar.gz/beetrack_uploader-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="beetrack_uploader",
+    version="0.0.3",
+    author="Nicolas Kipreos",
+    author_email="nicolas.kipreos@beetrack.com",
+    description="A simple and small wrapper for uploading files to Beetrack using Python scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nkipreos/BeetackUploader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
