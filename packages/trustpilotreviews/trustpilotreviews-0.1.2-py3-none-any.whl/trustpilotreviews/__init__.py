520b6c990b798afa2815cb8bb5242cce206a8293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:38 +0000

    Add package trustpilotreviews/0.1.2/trustpilotreviews-0.1.2-py3-none-any.whl

---
+++ packages/trustpilotreviews/trustpilotreviews-0.1.2-py3-none-any.whl/trustpilotreviews/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'Prayson W. Daniel'
+__copyright__ = 'Copyright 2018, Prayson W. Daniel'
+__credits__ = []
+__license__ = 'Creative Commons Attribution 3.0 Unported (CC BY 3.0): http://creativecommons.org/licenses/by/3.0/'
+__maintainer__ = 'Prayson W. Daniel'
+__version__ = '0.1.0'
+
+# See if we can access directly
+from .get_reviews import GetReviews
