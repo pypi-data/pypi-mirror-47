22aac524e71b9ec4c0009364ed98b272f72a6230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:35 +0000

    Add package blender-id-oauth-client/0.9.1/blender-id-oauth-client-0.9.1.tar.gz

---
+++ packages/blender-id-oauth-client/blender-id-oauth-client-0.9.1.tar.gz/blender-id-oauth-client-0.9.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['blender_id_oauth_client',
+ 'blender_id_oauth_client.management',
+ 'blender_id_oauth_client.management.commands',
+ 'blender_id_oauth_client.migrations']
+
+package_data = \
+{'': ['*'], 'blender_id_oauth_client': ['templates/blender_id_oauth_client/*']}
+
+install_requires = \
+['Django>=2.1,<3.0', 'requests-oauthlib>=1.0,<2.0']
+
+setup_kwargs = {
+    'name': 'blender-id-oauth-client',
+    'version': '0.9.1',
+    'description': 'Django app for using Blender ID as OAuth2 authentication service.',
+    'long_description': None,
+    'author': 'Sybren A. Stüvel',
+    'author_email': 'sybren@blender.studio',
+    'url': 'https://gitlab.com/blender-institute/blender-id-oauth-client',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
