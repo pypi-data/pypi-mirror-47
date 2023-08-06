b0dd3a6c609d3d319e0344ea27c7af5d9dac43af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:16 +0000

    Add package edc-model-admin/0.2.14/edc_model_admin-0.2.14-py3-none-any.whl

---
+++ packages/edc-model-admin/edc_model_admin-0.2.14-py3-none-any.whl/edc_model_admin/dashboard/model_admin_crf_dashboard_mixin.py
@@ -0,0 +1,18 @@
+from edc_fieldsets import FieldsetsModelAdminMixin
+from edc_visit_tracking.modeladmin_mixins import CrfModelAdminMixin
+
+from .model_admin_subject_dashboard_mixin import ModelAdminSubjectDashboardMixin
+
+
+class ModelAdminCrfDashboardMixin(
+    CrfModelAdminMixin, FieldsetsModelAdminMixin, ModelAdminSubjectDashboardMixin
+):
+
+    show_save_next = True
+    show_cancel = True
+
+    def get_subject_dashboard_url_kwargs(self, obj):
+        return dict(
+            subject_identifier=obj.subject_visit.subject_identifier,
+            appointment=str(obj.subject_visit.appointment.id),
+        )
---
+++ packages/edc-model-admin/edc_model_admin-0.2.14-py3-none-any.whl/edc_model_admin/templates_model_admin_mixin.py
@@ -0,0 +1,5 @@
+class TemplatesModelAdminMixin:
+
+    add_form_template = "edc_model_admin/admin/change_form.html"
+    change_form_template = "edc_model_admin/admin/change_form.html"
+    change_list_template = "edc_model_admin/admin/change_list.html"
