e0dbf574578d9d9447e4f7ea233011e9297b0783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:18 +0000

    Add package nbtimebar/0.2.1/nbtimebar-0.2.1.tar.gz

---
+++ packages/nbtimebar/nbtimebar-0.2.1.tar.gz/nbtimebar-0.2.1/README.md
@@ -0,0 +1,12 @@
+## Widget based progress bar for Jupyter (IPython Notebook)
+
+### Installation
+```bash
+pip install nbtimebar
+```
+
+### Usage
+```
+from nbtimebar import log_progress
+```
+
---
+++ packages/nbtimebar/nbtimebar-0.2.1.tar.gz/nbtimebar-0.2.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbtimebar",
+    version="0.2.1",
+    author="Maciej Rapacz",
+    author_email="parallel.bars@gmail.com",
+    description="Widget based progress bar for Jupyter (IPython Notebook)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrapacz/log-progress",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
