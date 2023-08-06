301230c4aad038b92026460d9ddf95ea4a734f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:32 +0000

    Add package moltransform/0.1.1/moltransform-0.1.1.tar.gz

---
+++ packages/moltransform/moltransform-0.1.1.tar.gz/moltransform-0.1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['moltransform']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.16,<2.0']
+
+setup_kwargs = {
+    'name': 'moltransform',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'Rodolfo Ferro',
+    'author_email': 'rodolfoferroperez@gmail.com',
+    'url': 'https://github.com/RodolfoFerro/moltransform',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
