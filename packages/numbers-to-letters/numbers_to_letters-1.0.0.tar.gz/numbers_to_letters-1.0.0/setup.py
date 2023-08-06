b1b3c1fd08750b9159efa890da049fa84aace13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:11 +0000

    Add package numbers-to-letters/1.0.0/numbers_to_letters-1.0.0.tar.gz

---
+++ packages/numbers-to-letters/numbers_to_letters-1.0.0.tar.gz/numbers_to_letters-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='numbers_to_letters',
+      version='1.0.0',
+      description='convert numbers int or float to letters',
+      url='',
+      author='Reyes2777',
+      author_email='jonathan.reyes2777@gmail.com',
+      license='MIT',
+      packages=['numbers_to_letters'],
+      zip_safe=False)
