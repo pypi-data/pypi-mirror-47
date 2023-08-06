d4ac10898af6ba2eab069e0a24cc2e92d5cd95cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package aws-creds/0.1.5/aws_creds-0.1.5-py3-none-any.whl

---
+++ packages/aws-creds/aws_creds-0.1.5-py3-none-any.whl/bin/run.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+import bin.util as util
+import bin.ui as ui
+import sys
+
+def main():
+    util.init_config()
+    subcommands = ["add", "rm", "ls", "login", "logout", "version", "status", "help"]
+
+    if len(sys.argv) >= 2:
+        command = sys.argv[1]
+
+        if command in subcommands:
+            method = getattr(ui, command)
+
+            if command == "help":
+                method(subcommands)
+            else:
+                method()
+        else:
+            ui.help(subcommands)
+    else:
+        ui.help(subcommands)
