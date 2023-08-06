7e7f01c4427668c0fb8e6c903cd5cac697db61e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:02 +0000

    Add package nudb/1.1.4/nudb-1.1.4.tar.gz

---
+++ packages/nudb/nudb-1.1.4.tar.gz/nudb-1.1.4/nudb/customErrorMessage.py
@@ -0,0 +1,13 @@
+custom_error_message = {
+    'FILE_NOT_EXISTS': '檔案不存在',
+    'WRONG_FORMAT': '資料格式錯誤',
+    'WRONG_FORMAT_PARAMETER': 'data_type 參數錯誤, 必須是 json 或 text',
+    'WRONG_SEARCH_FIELD_PARAMETER': 'search_field 參數錯誤, 必須是 rid 或 key',
+    'WRONG_UPDATE_METHOD_PARAMETER': 'update_method 參數錯誤, 必須是 replaceRecord 或 replaceField',
+    'MISSING_RECBEG_PARAMETER': '缺少 rec_beg 參數',
+    'MISSING_ID_PARAMETER': '缺少 id 參數',
+    'MISSING_DB_PARAMETER': '缺少 db 參數',
+    'MISSING_QUERY_PARAMETER': '缺少 query 參數',
+    'MISSING_DATA_PARAMETER': '缺少 data 參數',
+    'MISSING_FILE_PARAMETER': '缺少 file 參數'
+}
