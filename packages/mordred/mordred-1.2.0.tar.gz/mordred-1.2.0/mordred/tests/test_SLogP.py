908a1fcf82d04e33c83069ba78788ca121c5ad61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:25 +0000

    Add package mordred/1.2.0/mordred-1.2.0.tar.gz

---
+++ packages/mordred/mordred-1.2.0.tar.gz/mordred-1.2.0/mordred/tests/test_SLogP.py
@@ -0,0 +1,15 @@
+from rdkit import Chem
+from numpy.testing import assert_almost_equal
+
+from mordred.SLogP import SMR, SLogP
+
+
+def test_WildmanCrippen1():
+    mol = Chem.MolFromSmiles("Oc1ccccc1OC")
+    yield assert_almost_equal, SLogP()(mol), 1.4, 2
+    yield assert_almost_equal, SMR()(mol), 34.66, 2
+
+
+def test_WildmanCrippen2():
+    mol = Chem.MolFromSmiles("c1ccccc1c2ccccn2")
+    yield assert_almost_equal, SLogP()(mol), 2.75, 2
