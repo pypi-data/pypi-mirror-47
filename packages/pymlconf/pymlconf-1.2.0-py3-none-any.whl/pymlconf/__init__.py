12cb739a037e6c4e6145e6461384f2cf6a4508f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:24 +0000

    Add package pymlconf/1.2.0/pymlconf-1.2.0-py3-none-any.whl

---
+++ packages/pymlconf/pymlconf-1.2.0-py3-none-any.whl/pymlconf/__init__.py
@@ -0,0 +1,10 @@
+
+from .models import Mergable, MergableList, MergableDict, Root, \
+    ConfigurationNamespace, DeferredRoot
+
+from .errors import ConfigurationAlreadyInitializedError, \
+    ConfigurationNotInitializedError
+
+
+__version__ = '1.2.0'
+
