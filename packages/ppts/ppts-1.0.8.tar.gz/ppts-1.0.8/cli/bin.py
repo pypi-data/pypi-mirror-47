9cb3c3a2feafa2f0d944333f82c4a9ec29b240ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:44 +0000

    Add package ppts/1.0.8/ppts-1.0.8.tar.gz

---
+++ packages/ppts/ppts-1.0.8.tar.gz/ppts-1.0.8/cli/bin.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from flask import Flask
+from flask import jsonify
+from .parse import parseUser, parsePages
+
+print('============')
+print(sys.argv[0])
+print(sys.argv[1])
+
+app = Flask(__name__)
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, sys.argv[0])
+
+with open(filename, 'r') as file:
+    text = file.read()
+
+userinfo = parseUser(text)
+pages = parsePages(text)
+
+
+@app.route("/")
+def user():
+    return str(pages)
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ppts/ppts-1.0.8.tar.gz/ppts-1.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ppts',
+    version='1.0.8',
+    description='ppt with web',
+    author='simdd',
+    author_email='dev.simdd@gmail.com',
+    packages=find_packages(),
+    scripts=['cli/bin.py'],
+    entry_points={
+        'console_scripts': [
+            'ppts = cli:main',
+        ]
+    }
+)
