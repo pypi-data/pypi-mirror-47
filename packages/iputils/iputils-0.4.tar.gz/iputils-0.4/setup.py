76f84379362d2f910df7c805297fc359fb0ccc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:36 +0000

    Add package iputils/0.4/iputils-0.4.tar.gz

---
+++ packages/iputils/iputils-0.4.tar.gz/iputils-0.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# date:        2019/6/6
+# author:      he.zhiming
+#
+
+from __future__ import (absolute_import, unicode_literals)
+
+from setuptools import setup
+import codecs
+
+setup(
+    name="iputils",
+    version="0.4",
+    platforms="any",
+    url="https://github.com/hezhiming/iputils",
+    license="GPL",
+    author="he.zhiming",
+    author_email="he.zhiming@foxmail.com",
+    description="wrapper of ipaddress.py, easy to use",
+    long_description=codecs.open("README.md", encoding="utf-8", mode="r").read(),
+    long_description_content_type="text/markdown",
+    keywords="IPv4, IPv6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Testing",
+    ],
+)
