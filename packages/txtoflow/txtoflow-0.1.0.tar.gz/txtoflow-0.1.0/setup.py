6868eb76292e39c13aafca6c6d81b6a6ba2fa64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:26 +0000

    Add package txtoflow/0.1.0/txtoflow-0.1.0.tar.gz

---
+++ packages/txtoflow/txtoflow-0.1.0.tar.gz/txtoflow-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="txtoflow",
+    version="0.1.0",
+    author="Krishna",
+    author_email="krishna.vijay4444@gmail.com",
+    description="Library to generate flowcharts from pseudo code",
+    long_description=open('README.md').read(),
+    platforms=['any'],
+    license='MIT License',
+    long_description_content_type="text/markdown",
+    url="https://github.com/KrishKasula/txtoflow",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
