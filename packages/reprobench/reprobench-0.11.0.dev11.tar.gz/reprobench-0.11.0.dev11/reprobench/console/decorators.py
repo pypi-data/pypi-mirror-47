fea65d009624607a2d63d4da484ea095e7d554a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:52 +0000

    Add package reprobench/0.11.0.dev11/reprobench-0.11.0.dev11.tar.gz

---
+++ packages/reprobench/reprobench-0.11.0.dev11.tar.gz/reprobench-0.11.0.dev11/reprobench/console/decorators.py
@@ -0,0 +1,63 @@
+import functools
+import os
+import sys
+
+import click
+from loguru import logger
+
+
+def common(func):
+    @click.option("-q", "--quiet", is_flag=True)
+    @click.option(
+        "--verbose", "-v", "verbosity", count=True, default=0, help="Verbosity"
+    )
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        quiet = kwargs.pop("quiet")
+        verbosity = kwargs.pop("verbosity")
+
+        sys.path.append(os.getcwd())
+        logger.remove()
+
+        if not quiet:
+            verbosity_levels = ["INFO", "DEBUG", "TRACE"]
+            verbosity = min(verbosity, 2)
+            logger.add(sys.stderr, level=verbosity_levels[verbosity])
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def server_info(func):
+    @click.option("-a", "--address", default="tcp://127.0.0.1:31313", show_default=True)
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        server_address = kwargs.pop("address")
+        return func(server_address=server_address, *args, **kwargs)
+
+    return wrapper
+
+
+def use_tunneling(func):
+    @click.option("-h", "--host", required=False, help="[Tunneling] SSH Host")
+    @click.option(
+        "-p", "--port", help="[Tunneling] Remote server port for", default=31313
+    )
+    @click.option(
+        "-K",
+        "--key-file",
+        type=click.Path(exists=True),
+        help="[Tunneling] SSH private key file",
+        required=False,
+    )
+    @functools.wraps(func)
+    def wrapper(host, port, key_file, *args, **kwargs):
+        if host is not None:
+            return func(
+                tunneling=dict(host=host, port=port, key_file=key_file), *args, **kwargs
+            )
+
+        return func(tunneling=None, *args, **kwargs)
+
+    return wrapper
