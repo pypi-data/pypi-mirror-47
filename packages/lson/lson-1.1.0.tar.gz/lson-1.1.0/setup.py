2f2a00851628c24b0bec1d9418b8d82ba15d12b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:32 +0000

    Add package lson/1.1.0/lson-1.1.0.tar.gz

---
+++ packages/lson/lson-1.1.0.tar.gz/lson-1.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lson",
+    version="1.1.0",
+    author="Alex Broaddus",
+    author_email="alexbroaddus1@gmail.com",
+    description="Display directory structure in json format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    entry_points = {
+        'console_scripts': ['lson=lson.command_line:main']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
