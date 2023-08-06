b6d83945fd8ce74a49c78d875b2eb2036623fa59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:14 +0000

    Add package chs/1.1/chs-1.1.tar.gz

---
+++ packages/chs/chs-1.1.tar.gz/chs-1.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+name = 'chs'
+
+setup(
+  name = name,
+  packages = ['chs'],
+  version = '1.1',
+  license='MIT',
+  description = 'Play chess against the Stockfish engine in your terminal.',
+  author = 'Nick Zuber',
+  author_email = 'zuber.nicholas@gmail.com',
+  url = 'https://github.com/nickzuber/chs',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['chess', 'terminal', 'stockfish'],
+  install_requires=[
+    'python-chess',
+    'editdistance',
+  ],
+  entry_points={
+    'console_scripts': ['{} = chs.__main__:run'.format(name)],
+  }
+)
