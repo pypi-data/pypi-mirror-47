8e3ff0e44fb0df4f2f1c922852cc6546615648db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:39 +0000

    Add package AgentLogin/1.0.0/AgentLogin-1.0.0.tar.gz

---
+++ packages/AgentLogin/AgentLogin-1.0.0.tar.gz/AgentLogin-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AgentLogin",
+    version="1.0.0",
+    author="taozhengquan",
+    author_email="1483906080@qq.com",
+    description="A project for Third party login",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhengquantao/ProxyLogin",
+    # packages=setuptools.find_packages(),
+    packages=['AgentLogin'],
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
