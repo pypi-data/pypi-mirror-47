33fe07ff1a6c8b861a863a6c383443f6656655e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:32 +0000

    Add package critical-path/0.0.7/critical_path-0.0.7.tar.gz

---
+++ packages/critical-path/critical_path-0.0.7.tar.gz/critical_path-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='critical_path',
+      version='0.0.7',
+      description='Tools for adapting universal language models to specifc tasks',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ltskinner/critical-path-nlp',
+      author='ltskinner',
+      keywords='BERT google transformer squad SQuAD nlp',
+      license='Apache',
+      install_requires=['tensorflow'],
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      zip_safe=False)
