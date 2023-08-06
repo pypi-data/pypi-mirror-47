a70ae1a2caf938ab04d5baf98b21f80b5d9c75a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:53 +0000

    Add package peek-admin/1.3.1/peek-admin-1.3.1.tar.gz

---
+++ packages/peek-admin/peek-admin-1.3.1.tar.gz/peek-admin-1.3.1/peek_admin/build-web/src/plugin-home-links.ts
@@ -0,0 +1,93 @@
+// This file is auto generated, the git version is blank and .gitignored
+export const homeLinks = [
+    {
+        "name": "peek_plugin_pof_diagram", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_diagram", 
+        "title": "DMS Diagram"
+    }, 
+    {
+        "name": "peek_core_device", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_device", 
+        "title": "Device"
+    }, 
+    {
+        "name": "peek_plugin_diagram", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_diagram", 
+        "title": "Diagram"
+    }, 
+    {
+        "name": "peek_plugin_generic_diagram_menu", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_generic_diagram_menu", 
+        "title": "Diagram Generic Menu"
+    }, 
+    {
+        "name": "peek_plugin_docdb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_docdb", 
+        "title": "Document DB"
+    }, 
+    {
+        "name": "peek_core_email", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_email", 
+        "title": "Email"
+    }, 
+    {
+        "name": "peek_plugin_graphdb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_graphdb", 
+        "title": "Graph DB"
+    }, 
+    {
+        "name": "peek_plugin_livedb", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_livedb", 
+        "title": "Live DB"
+    }, 
+    {
+        "name": "peek_plugin_branch", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_branch", 
+        "title": "Model Branches"
+    }, 
+    {
+        "name": "peek_plugin_pof_graphdb_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_graphdb_loader", 
+        "title": "PoF Connectivity Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_diagram_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_diagram_loader", 
+        "title": "PoF Diagram Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_equipment_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_equipment_loader", 
+        "title": "PoF Equipment Loader"
+    }, 
+    {
+        "name": "peek_plugin_pof_livedb_loader", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_plugin_pof_livedb_loader", 
+        "title": "PoF Realtime Loader"
+    }, 
+    {
+        "name": "peek_core_search", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_search", 
+        "title": "Search"
+    }, 
+    {
+        "name": "peek_core_user", 
+        "pluginIconPath": null, 
+        "resourcePath": "/peek_core_user", 
+        "title": "Users"
+    }
+];
