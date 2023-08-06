f61087bb15b74be6e664521e3cb332d56cfcd924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:07 +0000

    Add package reprobench/0.11.0.dev1/reprobench-0.11.0.dev1-py3-none-any.whl

---
+++ packages/reprobench/reprobench-0.11.0.dev1-py3-none-any.whl/reprobench/core/observers.py
@@ -0,0 +1,89 @@
+from functools import lru_cache
+
+from peewee import fn
+
+from reprobench.core.base import Observer
+from reprobench.core.bootstrap.server import bootstrap
+from reprobench.core.db import Limit, Run, Step
+from reprobench.core.events import (
+    BOOTSTRAP,
+    RUN_FINISH,
+    RUN_INTERRUPT,
+    RUN_START,
+    RUN_STEP,
+    WORKER_JOIN,
+)
+from reprobench.utils import encode_message
+
+
+class CoreObserver(Observer):
+    SUBSCRIBED_EVENTS = (BOOTSTRAP, WORKER_JOIN, RUN_START, RUN_STEP, RUN_FINISH)
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def get_limits(cls):
+        return {l.key: l.value for l in Limit.select()}
+
+    @classmethod
+    def get_run(cls, run_id):
+        run = Run.get_by_id(run_id)
+
+        if run is None:
+            return None
+
+        run.status = Run.SUBMITTED
+        run.save()
+
+        last_step = run.last_step_id or 0
+
+        runsteps = Step.select().where(
+            (Step.category == Step.RUN) & (Step.id > last_step)
+        )
+        limits = cls.get_limits()
+        parameters = {p.key: p.value for p in run.parameter_group.parameters}
+
+        run_dict = dict(
+            id=run.id,
+            task=run.task_id,
+            tool=run.tool.module,
+            parameters=parameters,
+            steps=list(runsteps.dicts()),
+            limits=limits,
+        )
+
+        return run_dict
+
+    @classmethod
+    def get_pending_run_ids(cls):
+        last_step = (
+            Step.select(fn.MAX(Step.id)).where(Step.category == Step.RUN).scalar()
+        )
+        Run.update(status=Run.PENDING).where(
+            (Run.status < Run.DONE) | (Run.last_step_id != last_step)
+        ).execute()
+        pending_runs = Run.select(Run.id).where(Run.status == Run.PENDING)
+        return [r.id for r in pending_runs]
+
+    @classmethod
+    def handle_event(cls, event_type, payload, **kwargs):
+        reply = kwargs.pop("reply")
+        address = kwargs.pop("address")
+        observe_args = kwargs.pop("observe_args")
+
+        if event_type == BOOTSTRAP:
+            bootstrap(observe_args=observe_args, **payload)
+            run_ids = cls.get_pending_run_ids()
+            reply.send_multipart([address, encode_message(run_ids)])
+        elif event_type == WORKER_JOIN:
+            run = cls.get_run(payload)
+            reply.send_multipart([address, encode_message(run)])
+        elif event_type == RUN_INTERRUPT:
+            Run.update(status=Run.PENDING).where(Run.id == payload).execute()
+        elif event_type == RUN_START:
+            run_id = payload.pop("run_id")
+            Run.update(status=Run.RUNNING, **payload).where(Run.id == run_id).execute()
+        elif event_type == RUN_STEP:
+            step = Step.get(module=payload["step"])
+            Run.update(last_step=step).where(Run.id == payload["run_id"]).execute()
+        elif event_type == RUN_FINISH:
+            Run.update(status=Run.DONE).where(Run.id == payload).execute()
