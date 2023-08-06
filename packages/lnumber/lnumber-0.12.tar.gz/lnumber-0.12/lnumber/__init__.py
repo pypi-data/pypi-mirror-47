f0721a95c774d1d8ce1f5bc9e67bcc93dae774dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:02 +0000

    Add package lnumber/0.12/lnumber-0.12.tar.gz

---
+++ packages/lnumber/lnumber-0.12.tar.gz/lnumber-0.12/lnumber/__init__.py
@@ -0,0 +1,10 @@
+from ctypes import cdll, c_char, c_size_t
+from os import path
+
+lib = cdll.LoadLibrary(path.join(path.dirname(__file__),"./lnumber.so"))
+
+def lnumber(graph, verts):
+  global lib
+  return lib.laman_number(str(graph).encode("utf-8"), c_size_t(verts))
+
+#print lnumber(252590061719913632,12)
