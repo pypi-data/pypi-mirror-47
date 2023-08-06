a5b455e5ae1a297b9fd38602234eb0ea1266e2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:40 +0000

    Add package kuber/1.8.1/kuber-1.8.1.tar.gz

---
+++ packages/kuber/kuber-1.8.1.tar.gz/kuber-1.8.1/kuber/v1_12/__init__.py
@@ -0,0 +1,18 @@
+import datetime as _datetime
+
+from kuber import versioning
+
+# Information about the kubernetes API library version associated with this
+# package and kuber metadata around its creation and labeling within the
+# kuber library.
+KUBERNETES_VERSION = versioning.KubernetesVersion(
+    label='v1.12',
+    version='v1.12.8',
+    major='1',
+    minor='12',
+    patch='8',
+    pre_release='',
+    build='',
+    created_at=_datetime.datetime(2019, 6, 17),
+    commit_sha='a89f8c11a5f4f132503edbc4918c98518fd504e3'
+)
