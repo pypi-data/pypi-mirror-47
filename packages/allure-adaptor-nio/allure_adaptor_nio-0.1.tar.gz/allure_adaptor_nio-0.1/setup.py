4f8d6a1266fa0f661d0926b4e13a224bfeba9b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:08 +0000

    Add package allure-adaptor-nio/0.1/allure_adaptor_nio-0.1.tar.gz

---
+++ packages/allure-adaptor-nio/allure_adaptor_nio-0.1.tar.gz/allure_adaptor_nio-0.1/setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from setuptools import setup
+
+PACKAGE = "allure_adaptor_nio"
+VERSION = "0.1"
+
+install_requires = [
+    "lxml>=3.2.0",
+    "pytest>=2.7.3",
+    "namedlist",
+    "six>=1.9.0"
+]
+
+if sys.version_info < (3, 4):
+    install_requires.append("enum34")
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def main():
+    setup(
+        name=PACKAGE,
+        version=VERSION,
+        description=("change get_marker to get_closest_marker"),
+        author="",
+        packages=["allure"],
+        install_requires=install_requires
+    )
+
+
+if __name__ == '__main__':
+    main()
