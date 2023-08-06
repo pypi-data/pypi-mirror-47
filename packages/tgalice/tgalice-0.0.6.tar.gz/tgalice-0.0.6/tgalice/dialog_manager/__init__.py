5f481c65e246eaa267620dfafd1635e60ada18ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:24 +0000

    Add package tgalice/0.0.6/tgalice-0.0.6.tar.gz

---
+++ packages/tgalice/tgalice-0.0.6.tar.gz/tgalice-0.0.6/tgalice/dialog_manager/__init__.py
@@ -0,0 +1,6 @@
+from tgalice.dialog_manager.base import (
+    Context, Response, BaseDialogManager, CascadableDialogManager, CascadeDialogManager, GreetAndHelpDialogManager,
+    COMMANDS
+)
+from tgalice.dialog_manager.faq import FAQDialogManager
+from tgalice.dialog_manager.form_filling import FormFillingDialogManager
