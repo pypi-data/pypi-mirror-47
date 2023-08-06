da00aa30548ae2addb14cd8c32eb966c5c521555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:04 +0000

    Add package openpifpaf/0.7.0/openpifpaf-0.7.0.tar.gz

---
+++ packages/openpifpaf/openpifpaf-0.7.0.tar.gz/openpifpaf-0.7.0/openpifpaf/predict.py
@@ -0,0 +1,152 @@
+"""Predict poses for given images."""
+
+import argparse
+import glob
+import json
+import logging
+import os
+
+import numpy as np
+import torch
+
+from .network import nets
+from . import datasets, decoder, show, transforms
+
+
+def cli():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    nets.cli(parser)
+    decoder.cli(parser, force_complete_pose=False, instance_threshold=0.1, seed_threshold=0.5)
+    parser.add_argument('images', nargs='*',
+                        help='input images')
+    parser.add_argument('--glob',
+                        help='glob expression for input images (for many images)')
+    parser.add_argument('-o', '--output-directory',
+                        help=('Output directory. When using this option, make '
+                              'sure input images have distinct file names.'))
+    parser.add_argument('--show', default=False, action='store_true',
+                        help='show image of output overlay')
+    parser.add_argument('--output-types', nargs='+', default=['skeleton', 'json'],
+                        help='what to output: skeleton, keypoints, json')
+    parser.add_argument('--batch-size', default=1, type=int,
+                        help='processing batch size')
+    parser.add_argument('--long-edge', default=None, type=int,
+                        help='apply preprocessing to batch images')
+    parser.add_argument('--loader-workers', default=2, type=int,
+                        help='number of workers for data loading')
+    parser.add_argument('--disable-cuda', action='store_true',
+                        help='disable CUDA')
+    parser.add_argument('--figure-width', default=10.0, type=float,
+                        help='figure width')
+    parser.add_argument('--dpi-factor', default=1.0, type=float,
+                        help='increase dpi of output image by this factor')
+    group = parser.add_argument_group('logging')
+    group.add_argument('-q', '--quiet', default=False, action='store_true',
+                       help='only show warning messages or above')
+    group.add_argument('--debug', default=False, action='store_true',
+                       help='print debug messages')
+    args = parser.parse_args()
+
+    log_level = logging.INFO
+    if args.quiet:
+        log_level = logging.WARNING
+    if args.debug:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level)
+
+    # glob
+    if args.glob:
+        args.images += glob.glob(args.glob)
+    if not args.images:
+        raise Exception("no image files given")
+
+    # add args.device
+    args.device = torch.device('cpu')
+    args.pin_memory = False
+    if not args.disable_cuda and torch.cuda.is_available():
+        args.device = torch.device('cuda')
+        args.pin_memory = True
+
+    return args
+
+
+def main():
+    args = cli()
+
+    # load model
+    model, _ = nets.factory_from_args(args)
+    model = model.to(args.device)
+    processor = decoder.factory_from_args(args, model)
+
+    # data
+    preprocess = None
+    if args.long_edge:
+        preprocess = transforms.Compose([
+            transforms.RescaleAbsolute(args.long_edge),
+            transforms.CenterPad(args.long_edge),
+        ])
+    data = datasets.ImageList(args.images, preprocess=preprocess)
+    data_loader = torch.utils.data.DataLoader(
+        data, batch_size=args.batch_size, shuffle=False,
+        pin_memory=args.pin_memory, num_workers=args.loader_workers)
+
+    # visualizers
+    keypoint_painter = show.KeypointPainter(show_box=False)
+    skeleton_painter = show.KeypointPainter(show_box=False, color_connections=True,
+                                            markersize=1, linewidth=6)
+
+    for image_i, (image_paths, image_tensors, processed_images_cpu) in enumerate(data_loader):
+        images = image_tensors.permute(0, 2, 3, 1)
+
+        processed_images = processed_images_cpu.to(args.device, non_blocking=True)
+        fields_batch = processor.fields(processed_images)
+        pred_batch = processor.annotations_batch(fields_batch, debug_images=processed_images_cpu)
+
+        # unbatch
+        for image_path, image, processed_image_cpu, pred in zip(
+                image_paths,
+                images,
+                processed_images_cpu,
+                pred_batch):
+
+            if args.output_directory is None:
+                output_path = image_path
+            else:
+                file_name = os.path.basename(image_path)
+                output_path = os.path.join(args.output_directory, file_name)
+            logging.info('image %d: %s to %s', image_i, image_path, output_path)
+
+            processor.set_cpu_image(image, processed_image_cpu)
+            keypoint_sets, scores = processor.keypoint_sets_from_annotations(pred)
+
+            if 'json' in args.output_types:
+                with open(output_path + '.pifpaf.json', 'w') as f:
+                    json.dump([
+                        {'keypoints': np.around(kps, 1).reshape(-1).tolist(),
+                         'bbox': [np.min(kps[:, 0]), np.min(kps[:, 1]),
+                                  np.max(kps[:, 0]), np.max(kps[:, 1])]}
+                        for kps in keypoint_sets
+                    ], f)
+
+            if 'keypoints' in args.output_types:
+                with show.image_canvas(image,
+                                       output_path + '.keypoints.png',
+                                       show=args.show,
+                                       fig_width=args.figure_width,
+                                       dpi_factor=args.dpi_factor) as ax:
+                    keypoint_painter.keypoints(ax, keypoint_sets)
+
+            if 'skeleton' in args.output_types:
+                with show.image_canvas(image,
+                                       output_path + '.skeleton.png',
+                                       show=args.show,
+                                       fig_width=args.figure_width,
+                                       dpi_factor=args.dpi_factor) as ax:
+                    skeleton_painter.keypoints(ax, keypoint_sets, scores=scores)
+
+
+if __name__ == '__main__':
+    main()
