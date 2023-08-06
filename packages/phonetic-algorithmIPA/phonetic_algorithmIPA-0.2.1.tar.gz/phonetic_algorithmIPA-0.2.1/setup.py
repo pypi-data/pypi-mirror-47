edab1e0088807ac20f5e0aa336195d8ea3ec082b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:37 +0000

    Add package phonetic-algorithmIPA/0.2.1/phonetic_algorithmIPA-0.2.1.tar.gz

---
+++ packages/phonetic-algorithmIPA/phonetic_algorithmIPA-0.2.1.tar.gz/phonetic_algorithmIPA-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='phonetic_algorithmIPA',
+     version='0.2.1',
+     description='Phonenetic algorithm for IPA',
+     author='Anastasiya Kostyanitsyna, George Moroz',
+     packages=find_packages(),
+     include_package_data=True,
+     zip_safe=False
+)
