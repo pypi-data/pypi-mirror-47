d414cee13a293ce87216944fe76dd1bf97ddbf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:22 +0000

    Add package cuishuang/0.0.1/cuishuang-0.0.1.tar.gz

---
+++ packages/cuishuang/cuishuang-0.0.1.tar.gz/cuishuang-0.0.1/README.md
@@ -0,0 +1,3 @@
+### 爽哥的第一个python包
+
+<font size="3" color="red">输出"爽哥好帅"</font>
---
+++ packages/cuishuang/cuishuang-0.0.1.tar.gz/cuishuang-0.0.1/cuishuang/print_shuangge_handsome.py
@@ -0,0 +1,10 @@
+def shuangge_handsome():
+    print("爽哥好帅!!!")
+
+
+def myBlog():
+    print("www.dashen.tech")
+
+
+def myHometown():
+    print("山东邹平")
