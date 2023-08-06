1b4b5674197082f94c9322d5b9f61137eacc3ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:08 +0000

    Add package tornapi/0.0.1/tornapi-0.0.1-py3-none-any.whl

---
+++ packages/tornapi/tornapi-0.0.1-py3-none-any.whl/tornapi/__init__.py
@@ -0,0 +1,7 @@
+from .app import Client
+
+name = "tornapi"
+
+def create_client():
+    print("Initalised Class")
+    return "Hello World!"
---
+++ packages/tornapi/tornapi-0.0.1-py3-none-any.whl/tornapi/app.py
@@ -0,0 +1,7 @@
+class Client():
+
+    def __init__(self):
+        print('Created API Client')
+
+    def say(self):
+        return 'Hello World!'
