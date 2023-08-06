9e71211f9180d7340e3cacc00f5149eaac7a1042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:31 +0000

    Add package scikit-fem/0.1.21/scikit-fem-0.1.21.tar.gz

---
+++ packages/scikit-fem/scikit-fem-0.1.21.tar.gz/scikit-fem-0.1.21/skfem/assembly/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""This module contains classes and functions related to the
+construction of finite element matrices.
+
+The basic workflow is the following:
+
+1. Create :class:`~skfem.assembly.InteriorBasis` and/or :class:`~skfem.assembly.FacetBasis`
+   objects.
+2. Define the bilinear and linear forms using the decorators :func:`~skfem.assembly.bilinear_form`
+   and :func:`~skfem.assembly.linear_form`.
+3. Assemble finite element matrices using :func:`~skfem.assembly.asm`.
+
+"""
+
+from .global_basis import GlobalBasis, InteriorBasis, FacetBasis, MortarBasis
+from .asm import asm, bilinear_form, linear_form, functional
+from .dofs import Dofs
