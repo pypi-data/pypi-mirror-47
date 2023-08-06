2565d46a5336bfd1d9dca76c1eac413885e2a803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:48 +0000

    Add package ipyfilechooser/0.2.0/ipyfilechooser-0.2.0.tar.gz

---
+++ packages/ipyfilechooser/ipyfilechooser-0.2.0.tar.gz/ipyfilechooser-0.2.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ipyfilechooser",
+    version="0.2.0",
+    author="Thomas Bouve (@crahan)",
+    author_email="crahan@n00.be",
+    description=(
+        "Python file chooser widget for use in "
+        "Jupyter/IPython in conjunction with ipywidgets"
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/crahan/ipyfilechooser",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+    ],
+    install_requires=[
+        'ipywidgets'
+    ]
+)
