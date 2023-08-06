f4a190975c87d2cb223b4b688c73d38f76b2e222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:46 +0000

    Add package ping-cmd/1.2/ping_cmd-1.2.tar.gz

---
+++ packages/ping-cmd/ping_cmd-1.2.tar.gz/ping_cmd-1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='ping_cmd',
+      version='1.2',
+      description='Ping website or server using ping.exe just to check is the host reachable or not.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Dragan Matesic',
+      author_email='dragan.matesic@gmail.com',
+      license='MIT',
+      packages=['ping_cmd'],
+      zip_safe=False
+      )
