bec80606b6d5526dfb3177e1af6c4ea0501278f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:56 +0000

    Add package jatnipy/1.0.2/jatnipy-1.0.2.tar.gz

---
+++ packages/jatnipy/jatnipy-1.0.2.tar.gz/jatnipy-1.0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+long_description = "Justin's, Andrea's, and Torbjörn's network inference package for Python. We re-implemented GeneSPIDER toolbox in Python. Source code and documentation freely available at https://bitbucket.org/temn/JATNIpy/"
+
+DISTNAME = 'jatnipy'
+VERSION = '1.0.2'
+DESCRIPTION = "Justin's, Andrea's, and Torbjörn's network inference package for Python"
+# with open('README.rst') as f:
+#     LONG_DESCRIPTION = f.read()
+MAINTAINER = 'Justin Lin'
+MAINTAINER_EMAIL = 'justin.lin@nordlinglab.org'
+URL = 'https://bitbucket.org/temn/JATNIpy/'
+DOWNLOAD_URL = 'https://pypi.org/project/jatnipy/#files'
+LICENSE = 'LGPL'
+
+
+setup(name=DISTNAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      url=URL,
+      author=MAINTAINER,
+      author_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      long_description = long_description,
+      packages=['jatnipy','jatnipy/analyse','jatnipy/datastruct','jatnipy/Methods'],
+      python_requires='>=3',
+      install_requires=[
+          'numpy',
+          'py-ubjson',
+          'scipy',
+          'pandas',
+          'prettytable',
+          'matplotlib',
+          'networkx',
+          'scikit-learn',
+          'glmnet_py',
+          'requests',
+          'cvxpy',
+      ],
+      zip_safe=False)
