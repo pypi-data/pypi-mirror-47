361c5ce9215cfe9de31dc37eb82dc408c70be516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:43 +0000

    Add package tensorview/0.1.0/tensorview-0.1.0-py2.py3-none-any.whl

---
+++ packages/tensorview/tensorview-0.1.0-py2.py3-none-any.whl/tensorview/__init__.py
@@ -0,0 +1,17 @@
+from tensorview import train
+
+__version__ = '0.1.0'
+__author__ = 'JinQing Lee'
+
+
+def _hello():
+    print("""
+------------------------------------------------------------------------------------
+      Linora
+--------------------
+      Version      : --  {}  --
+      Author       : --  {}  --
+      License      : Apache-2.0
+      Homepage     : https://github.com/Hourout/linora
+      Description  : Simple and efficient tools for data mining and data analysis.
+------------------------------------------------------------------------------------""".format(__version__, __author__))
