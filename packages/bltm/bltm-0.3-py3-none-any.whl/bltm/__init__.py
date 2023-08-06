48097d781713b923921774ac4507b637672b0864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:28 +0000

    Add package bltm/0.3/bltm-0.3-py3-none-any.whl

---
+++ packages/bltm/bltm-0.3-py3-none-any.whl/bltm/__init__.py
@@ -0,0 +1,9 @@
+"""
+    bltm
+    ~~~~~
+    A framework based on python.  It's extensively documented
+    and follows best practice patterns.
+    :copyright: gs.
+    :license: BSD, see LICENSE for more details.
+"""
+from .bltm import *
