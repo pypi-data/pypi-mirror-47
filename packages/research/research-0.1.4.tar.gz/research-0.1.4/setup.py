9d3bf03515730b108e9fde91f331d45c52631b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:38 +0000

    Add package research/0.1.4/research-0.1.4.tar.gz

---
+++ packages/research/research-0.1.4.tar.gz/research-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='research',
+    version='0.1.4',
+    description='Utility modules used for research and/or learning',
+    license='MIT',
+    author='Jack Blandin',
+    author_email='jackblandin@gmail.com',
+    url='https://github.com/jackblandin/research',
+    keywords=['dqn', 'reinforcement-learning', 'machine-learning',
+                'research', 'pomdp', 'python', 'deep-learning'],
+    download_url='https://github.com/jackblandin/research/archive/v_01.tar.gz',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas==0.20.3',
+        'scikit-learn',
+    ],
+)
