63d2a2f75af39428c4a6b119648163abc49f254c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:58 +0000

    Add package dask-mpi/1.0.3/dask_mpi-1.0.3-py3-none-any.whl

---
+++ packages/dask-mpi/dask_mpi-1.0.3-py3-none-any.whl/dask_mpi/cli.py
@@ -0,0 +1,78 @@
+from __future__ import print_function, division, absolute_import
+import click
+
+from mpi4py import MPI
+from tornado.ioloop import IOLoop
+from distributed.cli.utils import check_python_3
+
+from dask_mpi.common import (
+    get_host_from_interface,
+    create_scheduler,
+    run_scheduler,
+    create_and_run_worker
+)
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+
+@click.command()
+@click.option('--scheduler-file', type=str, default='scheduler.json',
+              help='Filename to JSON encoded scheduler information.')
+@click.option('--scheduler-port', default=8786, type=int,
+              help="Specify scheduler port number.  Defaults to port 8786.")
+@click.option('--interface', type=str, default=None,
+              help="Network interface like 'eth0' or 'ib0'")
+@click.option('--nthreads', type=int, default=0,
+              help="Number of threads per worker.")
+@click.option('--memory-limit', default='auto',
+              help="Number of bytes before spilling data to disk. "
+                   "This can be an integer (nbytes) "
+                   "float (fraction of total memory) "
+                   "or 'auto'")
+@click.option('--local-directory', default='', type=str,
+              help="Directory to place worker files")
+@click.option('--scheduler/--no-scheduler', default=True,
+              help=("Whether or not to include a scheduler. "
+                    "Use --no-scheduler to increase an existing dask cluster"))
+@click.option('--worker-port', type=int, default=0,
+              help="Serving computation port, defaults to random")
+@click.option('--nanny/--no-nanny', default=True,
+              help="Start workers in nanny process for management")
+@click.option('--nanny-port', type=int, default=0,
+              help="Serving nanny port, defaults to random")
+@click.option('--bokeh/--no-bokeh', default=True,
+              help="Enable Bokeh visual diagnostics")
+@click.option('--bokeh-port', type=int, default=8787,
+              help="Bokeh port for visual diagnostics")
+@click.option('--bokeh-worker-port', type=int, default=8789,
+              help="Worker's Bokeh port for visual diagnostics")
+@click.option('--bokeh-prefix', type=str, default=None,
+              help="Prefix for the bokeh app")
+def main(scheduler_file, interface, nthreads, local_directory, memory_limit,
+         scheduler, bokeh, bokeh_port, bokeh_prefix, bokeh_worker_port,
+         nanny, scheduler_port, nanny_port, worker_port):
+
+    host = get_host_from_interface(interface)
+
+    loop = IOLoop.current()
+
+    if rank == 0 and scheduler:
+        scheduler_obj = create_scheduler(loop, host=host, scheduler_file=scheduler_file,
+                                         bokeh=bokeh, bokeh_port=bokeh_port, bokeh_prefix=bokeh_prefix,
+                                         scheduler_port=scheduler_port)
+        run_scheduler(scheduler_obj)
+    else:
+        create_and_run_worker(loop, host=host, rank=rank, scheduler_file=scheduler_file, nanny=nanny,
+                              nthreads=nthreads, local_directory=local_directory, memory_limit=memory_limit,
+                              bokeh=bokeh, bokeh_port=bokeh_worker_port,
+                              worker_port=worker_port, nanny_port=nanny_port)
+
+
+def go():
+    check_python_3()
+    main()
+
+
+if __name__ == '__main__':
+    go()
---
+++ packages/dask-mpi/dask_mpi-1.0.3-py3-none-any.whl/dask_mpi/core.py
@@ -0,0 +1,90 @@
+from __future__ import print_function, division, absolute_import
+
+import atexit
+import sys
+
+import dask
+from distributed import Client
+
+from tornado import gen
+from tornado.ioloop import IOLoop
+
+from .common import (
+    get_host_from_interface,
+    create_scheduler,
+    run_scheduler,
+    create_and_run_worker
+)
+
+
+def initialize(interface=None, nthreads=1, local_directory='', memory_limit='auto', nanny=False,
+               bokeh=True, bokeh_port=8787, bokeh_prefix=None, bokeh_worker_port=8789):
+    """
+    Initialize a Dask cluster using mpi4py
+
+    Using mpi4py, MPI rank 0 launches the Scheduler, MPI rank 1 passes through to the
+    client script, and all other MPI ranks launch workers.  All MPI ranks other than
+    MPI rank 1 block while their event loops run and exit once shut down.
+
+    Parameters
+    ----------
+    interface : str
+        Network interface like 'eth0' or 'ib0'
+    nthreads : int
+        Number of threads per worker
+    local_directory : str
+        Directory to place worker files
+    memory_limit : int, float, or 'auto'
+        Number of bytes before spilling data to disk.  This can be an
+        integer (nbytes), float (fraction of total memory), or 'auto'.
+    nanny : bool
+        Start workers in nanny process for management
+    bokeh : bool
+        Enable Bokeh visual diagnostics
+    bokeh_port : int
+        Bokeh port for visual diagnostics
+    bokeh_prefix : str
+        Prefix for the bokeh app
+    bokeh_worker_port : int
+        Worker's Bokeh port for visual diagnostics
+    """
+
+    host = get_host_from_interface(interface)
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    loop = IOLoop.current()
+
+    if rank == 0:
+        scheduler = create_scheduler(loop, host=host, bokeh=bokeh, bokeh_port=bokeh_port, bokeh_prefix=bokeh_prefix)
+        addr = scheduler.address
+    else:
+        addr = None
+
+    scheduler_address = comm.bcast(addr, root=0)
+    dask.config.set(scheduler_address=scheduler_address)
+    comm.Barrier()
+
+    if rank == 0:
+        run_scheduler(scheduler)
+        sys.exit()
+    elif rank == 1:
+        atexit.register(send_close_signal)
+    else:
+        create_and_run_worker(loop, host=host, rank=rank, nanny=nanny, nthreads=nthreads,
+                              local_directory=local_directory, memory_limit=memory_limit,
+                              bokeh=bokeh, bokeh_port=bokeh_worker_port)
+        sys.exit()
+
+
+def send_close_signal():
+    @gen.coroutine
+    def stop(dask_scheduler):
+        yield dask_scheduler.close()
+        yield gen.sleep(0.1)
+        local_loop = dask_scheduler.loop
+        local_loop.add_callback(local_loop.stop)
+
+    with Client() as c:
+        c.run_on_scheduler(stop, wait=False)
