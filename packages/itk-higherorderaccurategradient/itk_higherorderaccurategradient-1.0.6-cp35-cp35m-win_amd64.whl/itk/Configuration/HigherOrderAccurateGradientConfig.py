fbf894e3695e6a09e2d00701ba8caf0c096352ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:13 +0000

    Add package itk-higherorderaccurategradient/1.0.6/itk_higherorderaccurategradient-1.0.6-cp35-cp35m-win_amd64.whl

---
+++ packages/itk-higherorderaccurategradient/itk_higherorderaccurategradient-1.0.6-cp35-cp35m-win_amd64.whl/itk/Configuration/HigherOrderAccurateGradientConfig.py
@@ -0,0 +1,8 @@
+depends = ('ITKPyBase', 'ITKImageIntensity', 'ITKImageGradient', 'ITKImageFeature', 'ITKCommon', )
+templates = (
+  ('HigherOrderAccurateDerivativeImageFilter', 'itk::HigherOrderAccurateDerivativeImageFilter', 'itkHigherOrderAccurateDerivativeImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('HigherOrderAccurateDerivativeImageFilter', 'itk::HigherOrderAccurateDerivativeImageFilter', 'itkHigherOrderAccurateDerivativeImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('HigherOrderAccurateGradientImageFilter', 'itk::HigherOrderAccurateGradientImageFilter', 'itkHigherOrderAccurateGradientImageFilterIF2FF', True, 'itk::Image< float,2 >, float, float'),
+  ('HigherOrderAccurateGradientImageFilter', 'itk::HigherOrderAccurateGradientImageFilter', 'itkHigherOrderAccurateGradientImageFilterIF3FF', True, 'itk::Image< float,3 >, float, float'),
+)
+snake_case_functions = ('higher_order_accurate_gradient_image_filter', 'higher_order_accurate_derivative_image_filter', )
