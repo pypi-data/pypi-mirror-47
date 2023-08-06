d2ce96b8240033137ce6652d0cc54404e2b825eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:14 +0000

    Add package notedata/0.0.3/notedata-0.0.3-py3-none-any.whl

---
+++ packages/notedata/notedata-0.0.3-py3-none-any.whl/notedata/dataset/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/05/23 20:04
+# @Author  : niuliangtao
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm
+
+import os
+
+val = os.system('cd /content/tmp//data/electronics/ && ls -al')
+print(val)
