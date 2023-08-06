93ac3bb8181f8bcd8ae22b114b1f6d1d928dc914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:20 +0000

    Add package pylcs/0.0.2/pylcs-0.0.2.tar.gz

---
+++ packages/pylcs/pylcs-0.0.2.tar.gz/pylcs-0.0.2/src/main.cpp
@@ -0,0 +1,77 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <string.h>
+#include <sstream>
+using namespace std;
+
+
+// 最长公共子序列（不连续）
+int lcs_length_(const string &str1, const string &str2, vector<vector<int> > &dp) {
+    unsigned int i, j;
+    unsigned int m, n;
+    m = str1.length();
+    n = str2.length();
+    if (str1 == "" || str2 == "")
+        return 0;
+
+    for (i = 0; i <= m; i++) {
+        dp[i][0] = 0;
+    }
+    for (j = 0; j <= n; j++) {
+        dp[0][j] = 0;
+    }
+    for (i = 1; i <= m; i++) {
+        for (j = 1; j <= n; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                if (dp[i - 1][j] >= dp[i][j - 1])
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i][j-1];
+            }
+        }
+    }
+    return dp[str1.length()][str2.length()];
+}
+
+
+int lcs(const string &str1, const string &str2){
+    vector<vector<int> > dp(str1.length() + 1, vector<int>(str2.length() + 1));
+    return lcs_length_(str1, str2, dp);
+}
+
+
+vector<int> lcs_of_list(const string &str1, vector<string> &str_list){
+    int size = str_list.size();
+    vector<int> ls(size);
+    for (int i = 0; i < size; i++){
+        unsigned int l = lcs(str1, str_list[i]);
+        ls[i] = l;
+    }
+    return ls;
+}
+
+
+// TODO 最长公共子串（连续）
+// TODO 返回子串
+
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(pylcs, m) {
+    m.def("lcs", &lcs, R"pbdoc(
+        Longest common subsequence
+    )pbdoc");
+
+    m.def("lcs_of_list", &lcs_of_list, R"pbdoc(
+        Longest common subsequence of list
+    )pbdoc");
+
+}
+
