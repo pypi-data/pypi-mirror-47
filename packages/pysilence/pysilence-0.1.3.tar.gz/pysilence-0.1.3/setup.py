39f08e229ee07579c9e8fe6fe8c9e9d1af43307f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:41 +0000

    Add package pysilence/0.1.3/pysilence-0.1.3.tar.gz

---
+++ packages/pysilence/pysilence-0.1.3.tar.gz/pysilence-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='pysilence',
+	version='0.1.3',
+	scripts=[],
+	author="Anton Baumann",
+	author_email="anton@antonbaumann.com",
+	description="A silence detection module",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/antonbaumann/pysilence",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
