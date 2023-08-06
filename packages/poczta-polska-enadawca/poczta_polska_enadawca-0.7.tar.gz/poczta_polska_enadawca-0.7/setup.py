56c5cf7cb6818a303c3b0d4e8d02719c8c0c92fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:28 +0000

    Add package poczta-polska-enadawca/0.7/poczta_polska_enadawca-0.7.tar.gz

---
+++ packages/poczta-polska-enadawca/poczta_polska_enadawca-0.7.tar.gz/poczta_polska_enadawca-0.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(name='poczta_polska_enadawca',
+      version='0.7',
+      description='Client wrapper for Poczta Polska e-nadawca WSDL API.',
+      url='https://github.com/haloween/poczta-polska-e-nadawca-python',
+      keywords = "poczta, polska, e-nadawca, enadawca, wsdl, api",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Framework :: Django :: 1.9',
+          'Framework :: Django :: 2.0',
+          'Framework :: Django :: 2.1',
+          'Framework :: Django :: 2.2',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      author='Tomasz Utracki-Janeta',
+      author_email='halgravity+githubrepo@gmail.com',
+      license='MIT',
+      zip_safe=True,
+      packages=['poczta_polska_enadawca'],
+      include_package_data=True,
+      install_requires=[
+          'zeep',
+          'requests'
+      ]
+      )
