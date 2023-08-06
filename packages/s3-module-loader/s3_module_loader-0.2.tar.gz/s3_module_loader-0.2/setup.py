26e00c712b86a418f678b9813a24594b9e4cf7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:59 +0000

    Add package s3-module-loader/0.2/s3_module_loader-0.2.tar.gz

---
+++ packages/s3-module-loader/s3_module_loader-0.2.tar.gz/s3_module_loader-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='s3_module_loader',
+      version='0.2',
+      description='A module loader to load python packages from s3 buckets',
+      url='https://github.com/ablerman/S3ModuleLoader',
+      author='Sandy Lerman',
+      author_email='alex@ablerman.com',
+      license='MIT',
+      packages=['s3_module_loader'],
+      zip_safe=False)
