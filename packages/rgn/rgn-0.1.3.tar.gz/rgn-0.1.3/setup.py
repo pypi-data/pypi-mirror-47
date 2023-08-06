91c1bba1dde6be2fe768ddc9335bd6780964165c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:26 +0000

    Add package rgn/0.1.3/rgn-0.1.3.tar.gz

---
+++ packages/rgn/rgn-0.1.3.tar.gz/rgn-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rgn",
+    version="0.1.3",
+    #py_modules=['Rank_Gaussian_Normalization'],
+    author="Roger Zhang & Michael Jahrer",
+    author_email="shenchaozhang@gmail.com",
+    description="Rank Gaussian Normalization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RogerZhangsc/Porto-Seguros-Safe-Driver-Prediction",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
