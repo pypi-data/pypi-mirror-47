db9603369364cebba95dd9d5936ecb50bfedeae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:54 +0000

    Add package pydoro/0.1.0/pydoro-0.1.0.tar.gz

---
+++ packages/pydoro/pydoro-0.1.0.tar.gz/pydoro-0.1.0/README.rst
@@ -0,0 +1,51 @@
+pydoro 🍅
+============
+*Terminal Pomodoro Timer*
+
+.. image:: https://github.com/JaDogg/pydoro/raw/master/images/logo.png
+
+Installation 🎉
+-----------------
+Install via pipx:
+
+.. code-block::
+
+    $ pipx install pydoro
+
+Done.
+
+On windows you may try the packaged .exe file. See the releases_ page.
+
+Usage 📖
+---------
+* Run :code:`pydoro` to launch.
+
+.. image:: https://github.com/JaDogg/pydoro/raw/master/images/animation.gif
+
+Credits 🙇‍♂️
+------------------
+* playsound.py - For playing audio file, Copyright (c) 2016 Taylor_ Marks
+* prompt-tookit - For creating TUI
+* b15.wav - Dana_ robinson designs, CC0 from 
+
+.. _releases: https://github.com/JaDogg/pydoro/releases
+.. _Taylor: https://github.com/TaylorSMarks/playsound
+.. _Dana: https://freesound.org/s/377639/
+
+
+Why ? 🤔
+------------
+* I wanted to follow `Pomodoro Technique`_.
+* I don't like to use mobile apps or web apps.
+* No user info is stored, tracked or shared.
+* I spend lot of time on my Terminal.
+* Written in Python 🐍.
+
+.. _Pomodoro Technique: https://en.wikipedia.org/wiki/Pomodoro_Technique
+
+
+Copyright ⚖
+----------------
+This software is Copyright (c) 2019 Bhathiya Perera.
+
+See the LICENSE file for more information.
---
+++ packages/pydoro/pydoro-0.1.0.tar.gz/pydoro-0.1.0/setup.py
@@ -0,0 +1,48 @@
+from codecs import open
+from inspect import getsource
+from os.path import abspath, dirname, join
+from platform import system
+
+from setuptools import setup, find_packages
+
+here = abspath(dirname(getsource(lambda: 0)))
+
+with open(join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+current_system = system()
+
+# WHY: Windows can be handled without extras
+audio_requires = []
+if current_system == "Darwin":
+    audio_requires = ["pyobjc-core>=5.2", "pyobjc-framework-Cocoa>=5.2"]
+elif current_system == "Linux":
+    audio_requires = ["pycairo>=1.18.1", "PyGObject>=3.32.1"]
+
+setup(
+    name="pydoro",
+    version="0.1.0",
+    # Get the description from second line & remove `*` character
+    description=long_description.splitlines()[2][1:-1],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/JaDogg/pydoro",
+    author="Bhathiya Perera",
+    author_email="jadogg.coder@gmail.com",
+    license="MIT",
+    package_data={"": ["*.rst", "*.wav"]},
+    install_requires=["prompt-toolkit>=2.0.9"],
+    extras_require={"audio": audio_requires},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="tomato pomodoro pydoro timer work",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["pydoro = pydoro.pydoro:main"]},
+    setup_requires=["wheel"],
+)
