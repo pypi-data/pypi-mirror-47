63ac3d7cef8fbbe12c8a3bdb7d48e7815503ceb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:15 +0000

    Add package yandexcloud/0.10.0/yandexcloud-0.10.0.tar.gz

---
+++ packages/yandexcloud/yandexcloud-0.10.0.tar.gz/yandexcloud-0.10.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+packages = find_packages('.', include=['yandexcloud*', 'yandex*'])
+
+setup(name='yandexcloud',
+      version='0.10.0',
+      description='The Yandex.Cloud official SDK',
+      url='https://github.com/yandex-cloud/python-sdk',
+      author='Yandex LLC',
+      author_email='cloud@support.yandex.ru',
+      license='MIT',
+      install_requires=[
+          'cryptography',
+          'grpcio>=0.17.0',
+          'googleapis-common-protos',
+          'pyjwt',
+          'six',
+      ],
+      tests_require=['pytest'],
+      packages=packages,
+      zip_safe=False)
