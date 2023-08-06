f6e6102b24cd534172775be4a8e2a36d6df396ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:39 +0000

    Add package dvalidator/0.1.1/dvalidator-0.1.1.tar.gz

---
+++ packages/dvalidator/dvalidator-0.1.1.tar.gz/dvalidator-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+# Package meta-data.
+NAME = 'dvalidator'
+DESCRIPTION = 'dvalidator validation attributes'
+LICENSE = 'MIT'
+AUTHOR = 'Vlasov Sergei'
+EMAIL = 'kakabara@outlook.com'
+URL = 'https://github.com/kakabara/dvalidator'
+VERSION = '0.1.1'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(),
+    install_requires=[
+        'ipaddress==1.0.22',
+        'jsonschema==3.0.1'
+    ]
+)
