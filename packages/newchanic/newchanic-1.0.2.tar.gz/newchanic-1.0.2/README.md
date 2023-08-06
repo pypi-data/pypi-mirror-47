8b02154ed15fbe43be8432b7ded0eb1718e32e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:06 +0000

    Add package newchanic/1.0.2/newchanic-1.0.2.tar.gz

---
+++ packages/newchanic/newchanic-1.0.2.tar.gz/newchanic-1.0.2/README.md
@@ -0,0 +1,5 @@
+# Newchanic
+This package allows you to easily develop simulators based on Newton's mechanic.  
+See [here](http://www.ac-nice.fr/clea/lunap/html/MecaNewton/MecNewEnBref.html) (in french) to better understand it.  
+Define laws, create particles & bind them to the simulator engine : they will evolute in a 2D graphical interface.   
+You can zoom-in using the mouse's scroll and move in the window using arrow keys.
---
+++ packages/newchanic/newchanic-1.0.2.tar.gz/newchanic-1.0.2/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="newchanic",
+    version="1.0.2",
+    description="Simulate a universe powered by Newton's mechanic",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Louis-Saglio/newchanic",
+    author="Louis Saglio",
+    author_email="louis.saglio@ynov.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["newchanic"],
+    include_package_data=True,
+    install_requires=["pygame"],
+)
