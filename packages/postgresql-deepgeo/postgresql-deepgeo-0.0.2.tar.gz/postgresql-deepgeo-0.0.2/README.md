ae116d7a361ecaa50db0e5e512d135018a059d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:35 +0000

    Add package postgresql-deepgeo/0.0.2/postgresql-deepgeo-0.0.2.tar.gz

---
+++ packages/postgresql-deepgeo/postgresql-deepgeo-0.0.2.tar.gz/postgresql-deepgeo-0.0.2/README.md
@@ -0,0 +1,8 @@
+# Postgresql-DeepGeo
+ - Easy Deep Learning
+  - Copyright (c) 2019 [InfoLab](http://infolab.kunsan.ac.kr) ([Donggun LEE](http://duration.digimoon.net))
+
+ - Version
+   - 0.0.1
+   - 0.0.2
+     - Error Fixed.
---
+++ packages/postgresql-deepgeo/postgresql-deepgeo-0.0.2.tar.gz/postgresql-deepgeo-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='postgresql-deepgeo',
+      version='0.0.2',
+      url='https://github.com/Sotaneum/PostgreSQL-DeepGeo',
+      license='MIT',
+      author='Donggun LEE',
+      author_email='gnyotnu39@gmail.com',
+      description='Easy Deep Learning',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      zip_safe=False,
+      setup_requires=['tensorflow-gpu==1.9.0','exifread','piexif','pillow','matplotlib','scikit-image','IPython','keras','cython','deepgeo'],
+      classifiers=[
+          'License :: OSI Approved :: MIT License'
+      ]
+)
