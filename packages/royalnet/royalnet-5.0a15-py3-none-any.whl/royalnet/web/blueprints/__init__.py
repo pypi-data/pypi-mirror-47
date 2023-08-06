4f92dd98529108668cb0c8e48a789e87deddb6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package royalnet/5.0a15/royalnet-5.0a15-py3-none-any.whl

---
+++ packages/royalnet/royalnet-5.0a15-py3-none-any.whl/royalnet/web/blueprints/__init__.py
@@ -0,0 +1,6 @@
+from .helloworld import bp as helloworld
+from .testing import bp as testing
+from .home import bp as home
+from .wikiview import bp as wikiview
+
+__all__ = ["helloworld", "testing", "home", "wikiview"]
