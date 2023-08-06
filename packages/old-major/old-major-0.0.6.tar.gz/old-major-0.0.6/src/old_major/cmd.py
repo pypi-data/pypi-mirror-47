e740e9395d9b46bc8e7583d9e32af63caffff8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:32 +0000

    Add package old-major/0.0.6/old-major-0.0.6.tar.gz

---
+++ packages/old-major/old-major-0.0.6.tar.gz/old-major-0.0.6/src/old_major/cmd.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+
+from getmac import get_mac_address
+
+from .connection import WSConnection, watch_for_collects, get_token, monitor
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('host', type=str, help='a hostname to make http and tcp requests',
+                        nargs='?', default='localhost:8000')
+    
+    parser.add_argument('interface', type=str, help='internet interface used',
+                        nargs='?', default='eth0')
+
+    parser.add_argument('directory', type=str, help='directory where sensor data is saved',
+                        nargs='?', default='~/ricc/data')
+    
+    args = parser.parse_args()
+
+    mac_addr = get_mac_address(args.interface)
+    
+    if mac_addr:
+        if not os.path.exists(args.directory):
+            os.makedirs(args.directory)
+
+        ws_url = 'ws://' + args.host + '/ws/?mac=' + mac_addr
+        http_url = args.host + '/call'
+        print('ws ', ws_url)
+        print('http ', http_url)
+        connection = WSConnection(ws_url, args.interface)
+        connection.start_connection()
+        monitor(args.directory, http_url, mac_addr)
+    else:
+        print("Invalid internet interface, couldn't get device mac address")
