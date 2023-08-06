de8957e95a27c5eb36a4f0085a1520b0b626b675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:09 +0000

    Add package python-weixin/0.5.0/python-weixin-0.5.0.tar.gz

---
+++ packages/python-weixin/python-weixin-0.5.0.tar.gz/python-weixin-0.5.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from io import open
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
+setup(name="python-weixin",
+      version="0.5.0",
+      description="Python Weixin API client support wechat-app",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license="BSD",
+      install_requires=requirements,
+      author="gusibi",
+      author_email="cacique1103@gmail.com",
+      url="https://github.com/gusibi/python-weixin",
+      download_url="https://github.com/gusibi/python-weixin/archive/master.zip",
+      packages=find_packages(),
+      keywords=["python-weixin", "weixin", "wechat", "sdk", "weapp", "wxapp"],
+      zip_safe=True)
