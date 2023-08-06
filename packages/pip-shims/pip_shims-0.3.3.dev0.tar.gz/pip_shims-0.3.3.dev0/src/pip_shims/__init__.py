362f77f9248bda4d2973c5c8686cb42d00c50e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:51 +0000

    Add package pip-shims/0.3.3.dev0/pip_shims-0.3.3.dev0.tar.gz

---
+++ packages/pip-shims/pip_shims-0.3.3.dev0.tar.gz/pip_shims-0.3.3.dev0/src/pip_shims/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import
+
+import sys
+
+__version__ = '0.3.3.dev0'
+
+from . import shims
+
+
+old_module = sys.modules["pip_shims"]
+
+
+module = sys.modules["pip_shims"] = shims._new()
+module.shims = shims
+module.__dict__.update({
+    '__file__': __file__,
+    '__package__': "pip_shims",
+    '__path__': __path__,
+    '__doc__': __doc__,
+    '__all__': module.__all__ + ['shims',],
+    '__version__': __version__,
+    '__name__': __name__
+})
