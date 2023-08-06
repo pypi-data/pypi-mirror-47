2d740a53f74976a2b00bcd4b64b0ad1a610b9877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:41 +0000

    Add package pysuez/0.1.17/pysuez-0.1.17.tar.gz

---
+++ packages/pysuez/pysuez-0.1.17.tar.gz/pysuez-0.1.17/pysuez/__main__.py
@@ -0,0 +1,43 @@
+import argparse
+import sys
+import json
+
+from pysuez import SuezClient
+
+
+def main():
+    """Main function"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username',
+                        required=True, help='Suez username')
+    parser.add_argument('-p', '--password',
+                        required=True, help='Password')
+    parser.add_argument('-c', '--counter_id',
+                        required=True, help='Counter Id')
+
+    args = parser.parse_args()
+
+    client = SuezClient(args.username, args.password, args.counter_id)
+
+    try:
+        if client.check_credentials():
+            print ("Username and password are OK")
+        else:
+            print ("Username and/or password wrong")
+
+    except BaseException as exp:
+        print(exp)
+        return 1
+
+
+    try:
+    	client.update()
+    except BaseException as exp:
+        print(exp)
+        return 1
+    finally:
+        client.close_session()
+    print (client.attributes)
+
+if __name__ == '__main__':
+    sys.exit(main())
