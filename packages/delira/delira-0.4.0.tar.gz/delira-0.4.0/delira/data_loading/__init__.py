ecd353fe5f2475df9baf2709251235e0c4a7b902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:10 +0000

    Add package delira/0.4.0/delira-0.4.0.tar.gz

---
+++ packages/delira/delira-0.4.0.tar.gz/delira-0.4.0/delira/data_loading/__init__.py
@@ -0,0 +1,17 @@
+
+from delira import get_backends
+from .data_loader import BaseDataLoader
+from .data_manager import BaseDataManager
+from .dataset import AbstractDataset, BaseCacheDataset, BaseLazyDataset, \
+    ConcatDataset, BaseExtendCacheDataset
+from .load_utils import default_load_fn_2d, LoadSample, LoadSampleLabel
+from .sampler import LambdaSampler, \
+    WeightedRandomSampler, \
+    PrevalenceRandomSampler, \
+    RandomSampler, \
+    StoppingPrevalenceSequentialSampler, \
+    SequentialSampler
+from .sampler import __all__ as __all_sampling
+
+if "TORCH" in get_backends():
+    from .dataset import TorchvisionClassificationDataset
---
+++ packages/delira/delira-0.4.0.tar.gz/delira-0.4.0/setup.py
@@ -0,0 +1,69 @@
+import os
+import re
+from setuptools import find_packages, setup
+
+
+def resolve_requirements(file):
+    requirements = []
+    with open(file) as f:
+        req = f.read().splitlines()
+        for r in req:
+            if r.startswith("-r"):
+                requirements += resolve_requirements(
+                    os.path.join(os.path.dirname(file), r.split(" ")[1]))
+            else:
+                requirements.append(r)
+    return requirements
+
+
+def read_file(file):
+    with open(file) as f:
+        content = f.read()
+    return content
+
+
+def find_version(file):
+    content = read_file(file)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content,
+                              re.M)
+    if version_match:
+        return version_match.group(1)
+
+
+requirements = resolve_requirements(os.path.join(os.path.dirname(__file__),
+                                                 'requirements.txt'))
+
+requirements_extra_full = []
+
+requirements_extra_torch = resolve_requirements(os.path.join(
+    os.path.dirname(__file__), 'requirements_extra_torch.txt'))
+requirements_extra_full += requirements_extra_torch
+
+requirements_extra_tf = resolve_requirements(os.path.join(
+    os.path.dirname(__file__), 'requirements_extra_tf.txt'))
+requirements_extra_full += requirements_extra_tf
+
+readme = read_file(os.path.join(os.path.dirname(__file__), "README.md"))
+delira_version = find_version(os.path.join(os.path.dirname(__file__), "delira",
+                                           "__init__.py"))
+
+setup(
+    name='delira',
+    version=delira_version,
+    packages=find_packages(),
+    url='https://github.com/justusschock/delira/',
+    test_suite="unittest",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    maintainer="Justus Schock",
+    maintainer_email="justus.schock@rwth-aachen.de",
+    license="BSD-2",
+    install_requires=requirements,
+    tests_require=["coverage"],
+    python_requires=">=3.5",
+    extras_require={
+        "full": requirements_extra_full,
+        "torch": requirements_extra_torch,
+        "tensorflow": requirements_extra_tf
+    }
+)
