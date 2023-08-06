392b5005591ea31d6287c8bcb437385fb99a2369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:45 +0000

    Add package xoinvader/0.1.2/xoinvader-0.1.2-py2.py3-none-any.whl

---
+++ packages/xoinvader/xoinvader-0.1.2-py2.py3-none-any.whl/xoinvader/component.py
@@ -0,0 +1,6 @@
+"""Experimental component library.
+
+Each component should expose some interface.
+TODO: [dynamic-components]:
+      Interweave in bright future components with other ECS objects.
+"""
