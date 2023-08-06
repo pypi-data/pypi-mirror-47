54188d74dc2f412c3df6171120b936f5a04082ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:26 +0000

    Add package Django-Websocket-Server/0.1.1560405323/Django%20Websocket%20Server-0.1.1560405323.tar.gz

---
+++ packages/Django-Websocket-Server/Django%20Websocket%20Server-0.1.1560405323.tar.gz/Django Websocket Server-0.1.1560405323/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setup = dict(
+    name="Django Websocket Server",
+    version="0.1",
+    author="Julian Kimmig",
+    author_email="julian-kimmig@gmx.net",
+    description="Websocket BAckground Server for Django",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    #py_modules=["django_websocket_server"],
+    url="https://github.com/JulianKimmig/django_websocket_server",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=["websockets","FilterDict","PyNaCl"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+if __name__ == "__main__":
+    setuptools.setup(**setup)
