0edc7a3d355e8acd997c8ff7d961449255111e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package pelicun/1.1.6/pelicun-1.1.6.tar.gz

---
+++ packages/pelicun/pelicun-1.1.6.tar.gz/pelicun-1.1.6/setup.py
@@ -0,0 +1,59 @@
+﻿from setuptools import setup, find_packages
+import io
+
+import pelicun
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+setup(
+    name='pelicun',
+    version=pelicun.__version__,
+    url='http://github.com/NHERI-SimCenter/pelicun/',
+    license='BSD License',
+    author='Adam Zsarnóczay',
+    tests_require=['pytest'],
+    author_email='adamzs@stanford.edu',
+    description='Probabilistic Estimation of Losses, Injuries, and Community resilience Under Natural disasters',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    #packages=['pelicun'],
+    packages = find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'numpy>=1.15.1',
+        'scipy>=1.1',
+        'pandas>=0.20',
+    ],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Framework :: Jupyter',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        ],
+    extras_require={
+        'testing': ['pytest'],
+    }
+)
