199db6405524079e2a17b9b8e0575f37b29c785e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:12 +0000

    Add package aws-log-parser/1.3.1/aws-log-parser-1.3.1.tar.gz

---
+++ packages/aws-log-parser/aws-log-parser-1.3.1.tar.gz/aws-log-parser-1.3.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import pathlib
+from setuptools import setup
+
+# The text of the README file
+
+setup(
+    name='aws-log-parser',
+    version='1.3.1',
+    description='Python module for parsing AWS CloudFront and LoadBalancer logs',
+    long_description=(pathlib.Path(__file__).parent / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dpetzold/aws-log-parser',
+    author='Derrick Petzold',
+    author_email='github@petzold.io',
+    license='Apache',
+    packages=[
+        'aws_log_parser',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+    ],
+)
