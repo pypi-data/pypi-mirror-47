4bbfaa3bdfe19a0d8798595640c71adb7ec000a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:50 +0000

    Add package 6du.tv/0.0.22/6du.tv-0.0.22.tar.gz

---
+++ packages/6du.tv/6du.tv-0.0.22.tar.gz/6du.tv-0.0.22/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='6du.tv',
+    version='0.0.22',
+    description='6du.tv',
+    author='6du.tv',
+    author_email='eyun@protonmail.com',
+    url='https://github.com/6du-tv/6du-tv',
+    packages=[],
+    data_files=[
+        "6du.tv.apk"
+    ]
+)
+
