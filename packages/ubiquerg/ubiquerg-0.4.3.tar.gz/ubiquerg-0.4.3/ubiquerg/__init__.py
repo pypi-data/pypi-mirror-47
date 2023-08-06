d2c90a49ae9a86d5a748fc098812888e93d3a29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:31 +0000

    Add package ubiquerg/0.4.3/ubiquerg-0.4.3.tar.gz

---
+++ packages/ubiquerg/ubiquerg-0.4.3.tar.gz/ubiquerg-0.4.3/ubiquerg/__init__.py
@@ -0,0 +1,9 @@
+""" Package exports """
+
+from .cli_tools import *
+from .collection import *
+from .environment import *
+from .paths import *
+from .web import *
+from .files import *
+from ._version import __version__
