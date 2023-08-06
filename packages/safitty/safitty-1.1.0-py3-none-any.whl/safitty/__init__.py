2bd1c8bc140dab7cc63cb29bec05114c825ddbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:51 +0000

    Add package safitty/1.1.0/safitty-1.1.0-py3-none-any.whl

---
+++ packages/safitty/safitty-1.1.0-py3-none-any.whl/safitty/__init__.py
@@ -0,0 +1,24 @@
+from safitty.core import get, set, safe_set, safe_get
+from safitty.types import Storage, Key, Transform
+from safitty.parser import argparser, load, load_config, \
+    update, update_config_from_args, load_from_args, load_config_from_args
+
+
+__all__ = [
+    "get",
+    "safe_get",
+    "set",
+    "safe_set",
+    "Storage",
+    "Key",
+    "Transform",
+    "argparser",
+    "load",
+    "load_config",
+    "update",
+    "update_config_from_args",
+    "load_from_args",
+    "load_config_from_args"
+]
+
+__author__ = "Roman Tezikov"
