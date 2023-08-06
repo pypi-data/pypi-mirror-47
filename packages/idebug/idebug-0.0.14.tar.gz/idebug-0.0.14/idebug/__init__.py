05d36a5604c626ccec939d0bbc52359e7cae537d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:22 +0000

    Add package idebug/0.0.14/idebug-0.0.14.tar.gz

---
+++ packages/idebug/idebug-0.0.14.tar.gz/idebug-0.0.14/idebug/__init__.py
@@ -0,0 +1,10 @@
+
+import os
+print(f"\n os.path.abspath(__file__) : {os.path.abspath(__file__)}")
+
+from .DataStructure import *
+# from .etc import *
+from .html import *
+from .iinspect import *
+from .mongo import *
+from .performance import *
