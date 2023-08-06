0802d399cb8b77dca10e421c433abef9742b1cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:43 +0000

    Add package workstreams/1.0.2/workstreams-1.0.2-py3-none-any.whl

---
+++ packages/workstreams/workstreams-1.0.2-py3-none-any.whl/workstreams/resources/base.py
@@ -0,0 +1,31 @@
+import requests
+
+from workstreams import __version__
+from workstreams.exceptions import WorkstreamsError
+
+_URLS = {
+    'dev': 'https://api-dev.workstreams.ai/api',
+    'prod': 'https://rest.workstreams.ai/api'
+}
+
+
+class BaseResource:
+    def __init__(self, token, production=True):
+        self._token = token
+        self._headers = {
+            'Authorization': f'Bearer {self._token}',
+            'User-Agent': f'Workstreams Python {__version__}'
+        }
+        self._base_url = _URLS['prod'] if production else _URLS['dev']
+        self._base_url_batch = f'{self._base_url}/batch'
+
+    def _request(self, method, url, ws_user_id=None, data=None, params=None):
+        headers = self._headers
+        if ws_user_id:
+            headers['ws-user-id'] = ws_user_id
+        response = requests.request(method=method, url=url, data=data,
+                                    params=params, headers=headers)
+
+        if response.status_code >= 200:
+            return response.json()
+        raise WorkstreamsError(response.content)
