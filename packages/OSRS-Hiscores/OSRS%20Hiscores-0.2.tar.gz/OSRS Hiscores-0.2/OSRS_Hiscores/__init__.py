e6ca9787136e4da5a047d4057b712f34a3c4d46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:49 +0000

    Add package OSRS-Hiscores/0.2/OSRS%20Hiscores-0.2.tar.gz

---
+++ packages/OSRS-Hiscores/OSRS%20Hiscores-0.2.tar.gz/OSRS Hiscores-0.2/OSRS_Hiscores/__init__.py
@@ -0,0 +1,80 @@
+name = "OSRS_Hiscores"
+
+import http.client
+from sys import exit
+
+class Hiscores(object):
+
+	def __init__(self, username):
+		self.username = username
+		self.getHTTPResponse()
+
+	def getHTTPResponse(self):
+		conn = http.client.HTTPSConnection('secure.runescape.com')
+		conn.request("GET", "/m=hiscore_oldschool/index_lite.ws?player={}".format(self.username))
+		self.response = conn.getresponse()
+		self.processResponse()
+
+	def processResponse(self):
+		self.data = self.response.read().decode('ascii')
+		self.parseData()
+
+	def parseData(self):
+		try:
+			self.data = self.data.replace('\n',',')
+			self.data = self.data.split(',')
+			subset = {}
+
+			# Totals
+			info = {}
+			info['rank']             = self.data[0]
+			info['level']      = self.data[1]
+			info['experience'] = self.data[2]
+			subset['total']         = info
+
+			skills = [
+					  'attack',
+			          'defense',
+			          'strength',
+			          'hitpoints',
+			          'ranged',
+			          'prayer',
+			          'magic',
+			          'cooking',
+			          'woodcutting',
+			          'fletching',
+			          'fishing',
+			          'firemaking',
+			          'crafting',
+			          'smithing',
+			          'mining',
+			          'herblore',
+			          'agility',
+			          'thieving',
+			          'slayer',
+			          'farming',
+			          'runecrafting',
+			          'hunter',
+			          'construction'
+			           ]
+			counter = 0
+			for i in range(len(skills)):
+				info = {}
+				info['rank']       = self.data[counter+3]
+				info['level']      = self.data[counter+4]
+				info['experience'] = self.data[counter+5]
+				subset[skills[i]] = info
+				counter += 3
+		except IndexError as IE:
+			print("ERROR: issue with OSRS Hiscores API -- {}".format(IE))
+			print("OSRS Hiscores currently only works with Normal type accounts.")
+
+		# set stats dictionary
+		self.stats = subset
+
+	def skill(self,skill):
+		try:
+			return self.stats[skill.lower()]['level']
+		except KeyError as KE:
+			print("ERROR: skill {} does not exist".format(KE))
+			exit(0)
---
+++ packages/OSRS-Hiscores/OSRS%20Hiscores-0.2.tar.gz/OSRS Hiscores-0.2/README.md
@@ -0,0 +1,35 @@
+# OSRS Hiscores API Library
+
+## Purpose
+  The purpose of this library is to interface with Old School Runescape (OSRS)'s Hiscores page and allow developers to access stat levels, ranks, and experience levels in a more intuitive way (via dictionary).  This library accesses this information via a `http.client` request and parses the information accordingly.
+	
+
+## Installation
+```
+python -m pip install OSRS-Hiscores
+```
+
+## Example Usage
+```Python
+from OSRS_Hiscores import Hiscores
+
+# User to lookup
+username = 'Zezima'
+
+# Initialize user object
+user = Hiscores(username)
+
+# Get the entire stat dictionary
+user.stats
+
+# Get a specific skill's ranking, level, and experience
+user.stats['runecrafting']
+
+# Get skill's level, ranking, and experience separately
+user.stats['runecrafting']['level']
+user.stats['runecrafting']['rank']
+user.stats['runecrafting']['experience']
+
+# A simpler way to just get a skill's level
+user.skill('attack')
+```
