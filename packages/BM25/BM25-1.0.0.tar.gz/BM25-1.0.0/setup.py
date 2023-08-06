945b3bb4f22cdda8fdf3978007538acdd906fea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:20 +0000

    Add package BM25/1.0.0/BM25-1.0.0.tar.gz

---
+++ packages/BM25/BM25-1.0.0.tar.gz/BM25-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'BM25',
+        version     = '1.0.0',
+        py_modules  = ['BM25'],
+        author      = 'jannlp',
+        author_email= '1247621125@qq.com',
+        url         = 'https://github.com/jllan/jannlp/blob/master/similarity/bm25.py',
+        descriptioin= 'none',
+    )
