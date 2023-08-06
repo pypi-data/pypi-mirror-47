a1d2c19420888d8c3385907149f4b7de121567af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package angda/0.1.0rc1/angda-0.1.0rc1-py3-none-any.whl

---
+++ packages/angda/angda-0.1.0rc1-py3-none-any.whl/angda/__init__.py
@@ -0,0 +1,14 @@
+'''
+Package definition for 'angda'.
+'''
+
+VERSION_DATE   = '2019-06-16'
+VERSION_NUMBER = '0.1.0rc1'
+VERSION        = f'Angda version {VERSION_NUMBER} ({VERSION_DATE}).'
+
+from angda import apis
+from angda import items
+from angda import tools
+
+from angda.apis  import roll
+from angda.items import Dice, Roll
