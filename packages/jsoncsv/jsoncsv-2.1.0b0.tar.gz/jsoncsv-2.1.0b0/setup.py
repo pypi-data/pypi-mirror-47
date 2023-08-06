811c2b5b50fc27c13c01e7e06ee7756cb6d7deb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:09 +0000

    Add package jsoncsv/2.1.0b0/jsoncsv-2.1.0b0.tar.gz

---
+++ packages/jsoncsv/jsoncsv-2.1.0b0.tar.gz/jsoncsv-2.1.0b0/setup.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+# author@alingse
+# 2016.10.07
+import io
+
+from setuptools import setup
+
+
+with io.open('README.rst', encoding='utf-8') as f:
+    readme = f.read()
+
+
+setup(
+    name='jsoncsv',
+    version='2.1.0b',
+    url='https://github.com/alingse/jsoncsv',
+    description='A command tool easily convert json file to csv or xlsx.',
+    long_description=readme,
+    author='alingse',
+    author_email='alingse@foxmail.com',
+    license='Apache 2.0',
+    packages=['jsoncsv'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'xlwt',
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'jsoncsv = jsoncsv.main:jsoncsv',
+            'mkexcel = jsoncsv.main:mkexcel',
+        ],
+    },
+    keywords=[
+        'jsontocsv',
+        'jsoncsv',
+        'json',
+        'csv',
+        'xls'
+        'convert',
+        'command',
+    ],
+)
---
+++ packages/jsoncsv/jsoncsv-2.1.0b0.tar.gz/jsoncsv-2.1.0b0/tests/test_jsoncsv.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+# author@alingse
+# 2016.08.09
+
+import unittest
+from click.testing import CliRunner
+
+from jsoncsv.main import jsoncsv
+
+
+class Testjsoncsv(unittest.TestCase):
+
+    def test_jsoncsv_expand(self):
+        runner = CliRunner()
+        args = ['-e', 'fixture/files/raw.0.json', 'fixture/files/tmp.expand.0.json']
+        result = runner.invoke(jsoncsv, args=args)
+        print(result)
+        assert result.exit_code == 0
