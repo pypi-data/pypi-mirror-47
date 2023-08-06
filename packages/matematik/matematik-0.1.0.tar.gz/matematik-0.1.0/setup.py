0744a7a0def625ee21b99a4aa605c0f539fb8d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package matematik/0.1.0/matematik-0.1.0.tar.gz

---
+++ packages/matematik/matematik-0.1.0.tar.gz/matematik-0.1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="matematik",
+    version="0.1.0",
+    author="Demir Antay",
+    author_email="demir99antay@gmail.com",
+    description="An package for solving math equations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/demirantay/matematik",
+    packages=setuptools.find_packages()
+)
