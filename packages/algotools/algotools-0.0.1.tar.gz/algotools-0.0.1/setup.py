a6c4f695533596083f668682feaa18297763a652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:14 +0000

    Add package algotools/0.0.1/algotools-0.0.1.tar.gz

---
+++ packages/algotools/algotools-0.0.1.tar.gz/algotools-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='algotools',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.github.com/jeremyephron/algotools',
+    author='Jeremy Ephron',
+    author_email='jeremyephron@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
