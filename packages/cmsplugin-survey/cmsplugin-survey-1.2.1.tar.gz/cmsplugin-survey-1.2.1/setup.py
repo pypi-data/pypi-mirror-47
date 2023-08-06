185a16d43a9429d0762bd914b9b7938f13f4e4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:48 +0000

    Add package cmsplugin-survey/1.2.1/cmsplugin-survey-1.2.1.tar.gz

---
+++ packages/cmsplugin-survey/cmsplugin-survey-1.2.1.tar.gz/cmsplugin-survey-1.2.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name            = 'cmsplugin-survey',
+    version         = '1.2.1',
+    description     = 'django CMS plugin to show simple surveys',
+    author          = 'Jakub Dorňák',
+    author_email    = 'jakub.dornak@misli.cz',
+    license         = 'BSD',
+    url             = 'https://github.com/misli/cmsplugin-survey',
+    packages        = find_packages(),
+    include_package_data = True,
+    install_requires = [
+        'django-cms',
+    ],
+    classifiers     = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: Czech',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+)
