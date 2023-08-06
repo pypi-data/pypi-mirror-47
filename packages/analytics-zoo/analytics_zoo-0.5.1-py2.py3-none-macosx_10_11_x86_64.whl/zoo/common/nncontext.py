d1eed243eb5b3f24f9845dbe44f5d7717dc4ce68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:34 +0000

    Add package analytics-zoo/0.5.1/analytics_zoo-0.5.1-py2.py3-none-macosx_10_11_x86_64.whl

---
+++ packages/analytics-zoo/analytics_zoo-0.5.1-py2.py3-none-macosx_10_11_x86_64.whl/zoo/common/nncontext.py
@@ -0,0 +1,207 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from bigdl.util.common import *
+import warnings
+import multiprocessing
+import os
+
+
+def init_nncontext(conf=None):
+    """
+    Creates or gets a SparkContext with optimized configuration for BigDL performance.
+    The method will also initialize the BigDL engine.
+
+    Note: if you use spark-shell or Jupyter notebook, as the Spark context is created
+    before your code, you have to set Spark conf values through command line options
+    or properties file, and init BigDL engine manually.
+
+    :param conf: User defined Spark conf
+    """
+    if isinstance(conf, six.string_types):
+        sc = getOrCreateSparkContext(conf=None, appName=conf)
+    else:
+        sc = getOrCreateSparkContext(conf=conf)
+    check_version()
+    redire_spark_logs()
+    show_bigdl_info_logs()
+    init_engine()
+    return sc
+
+
+def getOrCreateSparkContext(conf=None, appName=None):
+    """
+    Get the current active spark context and create one if no active instance
+    :param conf: combining bigdl configs into spark conf
+    :return: SparkContext
+    """
+    with SparkContext._lock:
+        if SparkContext._active_spark_context is None:
+            spark_conf = init_spark_conf() if conf is None else conf
+            if appName:
+                spark_conf.setAppName(appName)
+            return SparkContext.getOrCreate(spark_conf)
+        else:
+            return SparkContext.getOrCreate()
+
+
+def get_analytics_zoo_conf():
+    zoo_conf_file = "spark-analytics-zoo.conf"
+    zoo_python_wrapper = "python-api.zip"
+
+    for p in sys.path:
+        if zoo_conf_file in p and os.path.isfile(p):
+            with open(p) if sys.version_info < (3,) else open(p, encoding='latin-1') as conf_file:
+                return load_conf(conf_file.read())
+        if zoo_python_wrapper in p and os.path.isfile(p):
+            import zipfile
+            with zipfile.ZipFile(p, 'r') as zip_conf:
+                if zoo_conf_file in zip_conf.namelist():
+                    content = zip_conf.read(zoo_conf_file)
+                    if sys.version_info >= (3,):
+                        content = str(content, 'latin-1')
+                    return load_conf(content)
+    return {}
+
+
+def init_env():
+    # Default env
+    kmp_affinity = "granularity=fine,compact,1,0"
+    kmp_settings = "1"
+    omp_num_threads = "1"
+    kmp_blocktime = "0"
+
+    # Check env and override if necessary
+    # Currently, focused on ZOO_NUM_MKLTHREADS,
+    # OMP_NUM_THREADS, KMP_BLOCKTIME, KMP_AFFINITY
+    # and KMP_SETTINGS
+    if "KMP_AFFINITY" in os.environ:
+        kmp_affinity = os.environ["KMP_AFFINITY"]
+    if "KMP_SETTINGS" in os.environ:
+        kmp_settings = os.environ["KMP_SETTINGS"]
+    if "OMP_NUM_THREADS" in os.environ:
+        omp_num_threads = os.environ["OMP_NUM_THREADS"]
+    elif "ZOO_NUM_MKLTHREADS" in os.environ:
+        if os.environ["ZOO_NUM_MKLTHREADS"].lower() == "all":
+            omp_num_threads = multiprocessing.cpu_count()
+        else:
+            omp_num_threads = os.environ["ZOO_NUM_MKLTHREADS"]
+    if "KMP_BLOCKTIME" in os.environ:
+        kmp_blocktime = os.environ["KMP_BLOCKTIME"]
+
+    # Set env
+    os.environ["KMP_AFFINITY"] = kmp_affinity
+    os.environ["KMP_SETTINGS"] = kmp_settings
+    os.environ["OMP_NUM_THREADS"] = omp_num_threads
+    os.environ["KMP_BLOCKTIME"] = kmp_blocktime
+
+
+def init_spark_conf():
+    init_env()
+    zoo_conf = get_analytics_zoo_conf()
+    # Set bigDL and TF conf
+    zoo_conf["spark.executorEnv.KMP_AFFINITY"] = os.environ["KMP_AFFINITY"]
+    zoo_conf["spark.executorEnv.KMP_SETTINGS"] = os.environ["KMP_SETTINGS"]
+    zoo_conf["spark.executorEnv.OMP_NUM_THREADS"] = os.environ["OMP_NUM_THREADS"]
+    zoo_conf["spark.executorEnv.KMP_BLOCKTIME"] = os.environ["KMP_BLOCKTIME"]
+
+    sparkConf = SparkConf()
+    sparkConf.setAll(zoo_conf.items())
+    if os.environ.get("BIGDL_JARS", None) and not is_spark_below_2_2():
+        for jar in os.environ["BIGDL_JARS"].split(":"):
+            extend_spark_driver_cp(sparkConf, jar)
+
+    # add content in PYSPARK_FILES in spark.submit.pyFiles
+    # This is a workaround for current Spark on k8s
+    python_lib = os.environ.get('PYSPARK_FILES', None)
+    if python_lib:
+        existing_py_files = sparkConf.get("spark.submit.pyFiles")
+        if existing_py_files:
+            sparkConf.set(key="spark.submit.pyFiles",
+                          value="%s,%s" % (python_lib, existing_py_files))
+        else:
+            sparkConf.set(key="spark.submit.pyFiles", value=python_lib)
+
+    return sparkConf
+
+
+def check_version():
+    sc = getOrCreateSparkContext()
+    conf = sc._conf
+    if conf.get("spark.analytics.zoo.versionCheck", "False").lower() == "true":
+        report_warn = conf.get(
+            "spark.analytics.zoo.versionCheck.warning", "False").lower() == "true"
+        _check_spark_version(sc, report_warn)
+
+
+def _split_full_version(version):
+    parts = version.split(".")
+    major = parts[0]
+    feature = parts[1]
+    maintenance = parts[2]
+    return (major, feature, maintenance)
+
+
+def _check_spark_version(sc, report_warn):
+    version_info = _get_bigdl_verion_conf()
+    (c_major, c_feature, c_maintenance) = _split_full_version(version_info['spark_version'])
+    (r_major, r_feature, r_maintenance) = _split_full_version(sc.version)
+    error_message = \
+        """
+        The compile time spark version is not compatible with the spark runtime version.
+        Compile time version is %s, runtime version is %s. If you want bypass this check,
+        please set spark.analytics.zoo.versionCheck to false, and if you want to only report
+        an warning message, please set spark.analytics.zoo.versionCheck.warning to true.
+        """ % (version_info['spark_version'], sc.version)
+    if c_major != r_major:
+        if not report_warn:
+            print("***************************Usage Error*****************************")
+            print(error_message)
+            raise RuntimeError(error_message)
+        else:
+            warnings.warn(error_message)
+    elif not (c_maintenance == r_maintenance and c_feature == r_feature):
+        warnings.warn("The compile time spark version may not compatible with " +
+                      "the Spark runtime version. " +
+                      "Compile time version is %s, " % version_info['spark_version'] +
+                      "runtime version is %s" % sc.version)
+
+
+def _get_bigdl_verion_conf():
+    bigdl_build_file = "zoo-version-info.properties"
+    bigdl_python_wrapper = "python-api.zip"
+
+    for p in sys.path:
+        if bigdl_build_file in p and os.path.isfile(p):
+            with open(p) if sys.version_info < (3,) else open(p, encoding='latin-1') as conf_file:
+                return load_conf(conf_file.read(), "=")
+        if bigdl_python_wrapper in p and os.path.isfile(p):
+            import zipfile
+            with zipfile.ZipFile(p, 'r') as zip_conf:
+                if bigdl_build_file in zip_conf.namelist():
+                    content = zip_conf.read(bigdl_build_file)
+                    if sys.version_info >= (3,):
+                        content = str(content, 'latin-1')
+                    return load_conf(content, "=")
+    raise RuntimeError("Error while locating file zoo-version-info.properties, " +
+                       "please make sure the mvn generate-resources phase" +
+                       " is executed and a zoo-version-info.properties file" +
+                       " is located in zoo/target/extra-resources")
+
+
+def load_conf(conf_str, split_char=None):
+    return dict(line.split(split_char) for line in conf_str.split("\n") if
+                "#" not in line and line.strip())
