8869d6c3f81344342f92df831441455c5e3c2e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:43 +0000

    Add package pyrestcli/0.6.10/pyrestcli-0.6.10.tar.gz

---
+++ packages/pyrestcli/pyrestcli-0.6.10.tar.gz/pyrestcli-0.6.10/pyrestcli/exceptions.py
@@ -0,0 +1,66 @@
+import json
+import sys
+
+ERRORS = {
+    400: 'BadRequestException',
+    401: 'UnauthorizedErrorException',
+    403: 'ForbiddenErrorException',
+    404: 'NotFoundException',
+    422: 'UnprocessableEntityError',
+    429: 'RateLimitException',
+    500: 'ServerErrorException'
+}
+
+
+class BaseException(Exception):
+    def __init__(self, message, status_code=None, headers=None, reason=None, url=None):
+        super(Exception, self).__init__(message)
+        self.status_code = status_code
+        self.headers = headers
+        self.reason = reason
+        self.url = url
+
+    @staticmethod
+    def create(response):
+        response_json = {}
+        try:
+            response_json = response.json()
+        except json.decoder.JSONDecodeError:
+            pass
+        message = response_json.get("error", response_json.get("errors", response.text))
+        headers = response.headers
+        status_code = response.status_code
+        reason = response.reason
+        url = response.url
+
+        error = ERRORS.get(status_code, 'BaseException')
+        klass = getattr(sys.modules[__name__], error)
+        raise klass(message, status_code, headers, reason, url)
+
+
+class BadRequestException(BaseException):
+    pass
+
+
+class UnauthorizedErrorException(BaseException):
+    pass
+
+
+class ForbiddenErrorException(BaseException):
+    pass
+
+
+class NotFoundException(BaseException):
+    pass
+
+
+class UnprocessableEntityError(BaseException):
+    pass
+
+
+class RateLimitException(BaseException):
+    pass
+
+
+class ServerErrorException(BaseException):
+    pass
