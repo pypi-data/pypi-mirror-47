bd46ea74503c39746d36f3549e7b02c0ea3cab44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:33 +0000

    Add package fitch/0.4.3/fitch-0.4.3.tar.gz

---
+++ packages/fitch/fitch-0.4.3.tar.gz/fitch-0.4.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fitch',
+    version='0.4.3',
+    description='Android UI automation based on opencv',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/fitch',
+    packages=find_packages(),
+    install_requires=[
+        'pyminitouch',
+        'findit',
+        'findit_client',
+        'fastcap',
+        'pyatool',
+        'loguru',
+        'adbutils'
+    ]
+)
