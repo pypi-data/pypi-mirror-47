89d260d6af6fcadf88cd65693e5547290089f12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:19 +0000

    Add package zlshenpi/1.0.94/zlshenpi-1.0.94.tar.gz

---
+++ packages/zlshenpi/zlshenpi-1.0.94.tar.gz/zlshenpi-1.0.94/zlshenpi/util/data.py
@@ -0,0 +1,11 @@
+zhulong_diqu_dict = {
+        'sheng':['guangdong']
+}
+
+# q, m = 0, 0
+# for i in zhulong_diqu_dict:
+#     m += 1
+#     for k in zhulong_diqu_dict[i]:
+#         print(k)
+#         q += 1
+# print(q, m)
