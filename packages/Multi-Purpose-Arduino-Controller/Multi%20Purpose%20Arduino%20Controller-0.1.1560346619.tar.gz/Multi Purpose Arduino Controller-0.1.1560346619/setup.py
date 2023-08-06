ac81407a4b936580eee0d71877f0af0250e5f48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:47 +0000

    Add package Multi-Purpose-Arduino-Controller/0.1.1560346619/Multi%20Purpose%20Arduino%20Controller-0.1.1560346619.tar.gz

---
+++ packages/Multi-Purpose-Arduino-Controller/Multi%20Purpose%20Arduino%20Controller-0.1.1560346619.tar.gz/Multi Purpose Arduino Controller-0.1.1560346619/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setup = dict(
+    name="Multi Purpose Arduino Controller",
+    version="0.1",
+    author="Julian Kimmig",
+    author_email="julian-kimmig@gmxREADME.md.net",
+    description="Controll Arduino modules",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    #py_modules=["multi_purpose_arduino_controller"],
+    url="https://github.com/JulianKimmig/multi_purpose_arduino_controller",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+if __name__ == "__main__":
+    setuptools.setup(**setup)
