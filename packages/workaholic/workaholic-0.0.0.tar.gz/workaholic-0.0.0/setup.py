0b835c91ed189e870508873c19026adef9856fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:26 +0000

    Add package workaholic/0.0.0/workaholic-0.0.0.tar.gz

---
+++ packages/workaholic/workaholic-0.0.0.tar.gz/workaholic-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='workaholic',
+    version='0.0.0',
+    packages=['workaholic'],
+    url='',
+    license='MIT',
+    author='npgrosser',
+    author_email='nicolai.grosser@googlemail.com',
+    description='',
+    zip_safe=False
+)
