170a6957c2ed7eb2edb0d6e7c5a3e47f2544788a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:03 +0000

    Add package altair/3.1.0/altair-3.1.0.tar.gz

---
+++ packages/altair/altair-3.1.0.tar.gz/altair-3.1.0/altair/utils/__init__.py
@@ -0,0 +1,30 @@
+from .core import (
+    infer_vegalite_type,
+    sanitize_dataframe,
+    parse_shorthand,
+    use_signature,
+    update_subtraits,
+    update_nested,
+    display_traceback,
+    SchemaBase,
+    Undefined
+)
+from .html import spec_to_html
+from .plugin_registry import PluginRegistry
+from .deprecation import AltairDeprecationWarning
+
+
+__all__ = (
+    'infer_vegalite_type',
+    'sanitize_dataframe',
+    'spec_to_html',
+    'parse_shorthand',
+    'use_signature',
+    'update_subtraits',
+    'update_nested',
+    'display_traceback',
+    'AltairDeprecationWarning',
+    'SchemaBase',
+    'Undefined',
+    'PluginRegistry'
+)
