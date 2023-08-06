da664d455e7a92f48fd6c9e0f69322b7caade614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:13 +0000

    Add package pycomment/0.0.6/pycomment-0.0.6-py2.py3-none-any.whl

---
+++ packages/pycomment/pycomment-0.0.6-py2.py3-none-any.whl/pycomment/capture.py
@@ -0,0 +1,41 @@
+import typing as t
+import contextlib
+from collections import namedtuple
+from io import StringIO
+from . import SEP_MARKER
+
+CaptureResult = namedtuple("CaptureResult", "comments, stdout")
+
+
+def capture(code: str, *, g: t.Optional[t.IO] = None):
+    o = StringIO()
+    with contextlib.redirect_stdout(o):
+        g = g or {"__name__": "exec"}
+        exec(code, g)
+
+    result_map = {}
+    rest = []
+
+    reading = False
+    buf = None
+    lineno = None
+
+    lines = o.getvalue().splitlines()
+    for line in lines:
+        if reading:
+            if line.endswith(SEP_MARKER):
+                reading = False
+                buf.append(line.rstrip(SEP_MARKER))
+                result_map[lineno] = buf
+            else:
+                buf.append(line)
+        elif line.startswith(SEP_MARKER):
+            reading = True
+            lineno, line = line.lstrip(SEP_MARKER).split(":", 1)
+            buf = [line.rstrip(SEP_MARKER)]
+            if line.endswith(SEP_MARKER):
+                reading = False
+                result_map[lineno] = buf
+        else:
+            rest.append(line)
+    return CaptureResult(comments=result_map, stdout=rest)
