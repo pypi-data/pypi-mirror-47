f3dcad8c43a13f99be754e327a329a6e5a75ad15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:18 +0000

    Add package pprofile-ext/1.0.5/pprofile_ext-1.0.5-py3-none-any.whl

---
+++ packages/pprofile-ext/pprofile_ext-1.0.5-py3-none-any.whl/pprofile_ext/util.py
@@ -0,0 +1,34 @@
+import collections
+import hashlib
+
+import six
+
+HASH_CACHE = dict()
+
+
+def update(d, u):
+    """
+    Perform a recursive update of dictionary `d` with the dictionary `u`
+
+    :param d: dictionary
+    :param u: dictionary
+
+    :return: updated dictionary
+    """
+    for k, v in six.iteritems(u):
+        if isinstance(v, collections.Mapping):
+            r = update(d.get(k, {}), v)
+            d[k] = r
+        else:
+            d[k] = u[k]
+
+    return d
+
+
+def hashkey(key):
+    """Returns the sha1 hash for key"""
+    # hash keys so we don't pay the sha1 overhead each time we call this one
+    if key not in HASH_CACHE:
+        HASH_CACHE[key] = hashlib.sha1(str.encode(key)).hexdigest()
+
+    return HASH_CACHE[key]
