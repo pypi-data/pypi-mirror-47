915a4c610cce63b84676cb7d24097cacbf22c00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:42 +0000

    Add package medoo/0.0.3/medoo-0.0.3-py2.py3-none-any.whl

---
+++ packages/medoo/medoo-0.0.3-py2.py3-none-any.whl/medoo/__init__.py
@@ -0,0 +1,52 @@
+VERSION = '0.0.3'
+
+class utils(object):
+
+	@staticmethod
+	def alwaysList(x):
+		"""
+		Always return a list
+		"""
+		from six import string_types
+		return [y.strip() for y in x.split(',')] if isinstance(x, string_types) else list(x)
+
+	@staticmethod
+	def reduce_datetimes(row):
+		"""
+		Receives a row, converts datetimes to strings.
+		"""
+		row = list(row)
+
+		for i, r in enumerate(row):
+			if hasattr(r, 'isoformat'):
+				row[i] = r.isoformat()
+		return tuple(row)
+
+import importlib
+from .builder import Raw, Table, Field
+from .dialect import Dialect
+
+DATABASE_TYPES = {
+	'Sqlite': ['sqlite', 'sqlite3'],
+	'Mysql' : 'mysql',
+	'Mssql' : 'mssql',
+	'Pgsql' : ['pgsql', 'postgres', 'postgresql'],
+}
+
+class Medoo(object):
+
+	def __new__(klass, dbtype, *args, **kwargs):
+
+		for key, val in DATABASE_TYPES.items():
+			if not isinstance(val, list):
+				val = [val]
+			if not dbtype.lower() in val:
+				continue
+
+			mod   = importlib.import_module('.database.{}'.format(key.lower()), package = 'medoo')
+			klass = getattr(mod, key)
+			return klass(*args, **kwargs)
+
+		raise ValueError('Database type not supported: {}.'.format(dbtype))
+
+
