a9cd201a7607fa92d4c9d8c80013681e699cfc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:35 +0000

    Add package deepscratch/0.0.1/deepscratch-0.0.1.tar.gz

---
+++ packages/deepscratch/deepscratch-0.0.1.tar.gz/deepscratch-0.0.1/deepscratch/__init__.py
@@ -0,0 +1,6 @@
+from .sequential import Sequential
+# from .layers import Dense, ReLU, Sigmoid
+from .layers import *
+from .losses import mse_loss
+from .optimizer import SGD
+from .activations import *
---
+++ packages/deepscratch/deepscratch-0.0.1.tar.gz/deepscratch-0.0.1/deepscratch/layers.py
@@ -0,0 +1,96 @@
+import numpy as np
+from .activations import *
+
+
+class Layer:
+    def __init__(self):
+        self.trainable = False
+    
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, grad):
+        raise NotImplementedError()
+
+    def __call__(self, x):
+        return self.forward(x)
+
+    def parameters(self):
+        raise NotImplementedError()
+
+    def dparameters(self):
+        raise NotImplementedError()
+
+    def nparameters(self):
+        ''' Total number of trainable parameters in the layer '''
+        return 0
+
+    def repr(self):
+        return ''
+
+    def __repr__(self):
+        name = self.__class__.__name__
+        args = self.repr()
+        params = self.nparameters()
+        return f'\033[01m{name}\033[00m({args}), \033[1;92m{params}\033[00m params'
+
+
+class Linear(Layer):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.W = np.random.random((in_features, out_features))
+        self.b = np.zeros((1, out_features))
+        self.trainable = True
+
+    def forward(self, x):
+        self.layer_input = x
+        return np.dot(x, self.W) + self.b
+
+    def backward(self, grad):
+        # Calculate gradient
+        self.__dW = np.dot(self.layer_input.T, grad)
+        self.__db = np.sum(grad, axis=0, keepdims=True)
+
+        # Calculate gradient for next layer
+        dx = np.dot(grad, self.W.T)
+        return dx
+
+    def parameters(self):
+        return [self.W, self.b]
+
+    def dparameters(self):
+        return [self.__dW, self.__db]
+
+    def nparameters(self):
+        return np.prod(self.W.shape) + np.prod(self.b.shape)
+
+    def repr(self):
+        return f'{self.in_features} → {self.out_features}'
+
+
+class ReLU(Layer):
+    def __init__(self):
+        super().__init__()
+        self.trainable = False
+    
+    def forward(self, x):
+        self.layer_input = x
+        return relu(x)
+
+    def backward(self, grad):
+        return grad * relu(self.layer_input, deriv=True)
+
+
+class Sigmoid(Layer):
+    def __init__(self):
+        super().__init__()
+        self.trainable = False
+
+    def forward(self, x):
+        self.layer_input = x
+        return sigmoid(x)
+
+    def backward(self, grad):
+        return grad * sigmoid(self.layer_input, deriv=True)
