9db3f0db12a11ebeb74740fefa39a7a85c0b00dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package perfy/0.27/perfy-0.27.tar.gz

---
+++ packages/perfy/perfy-0.27.tar.gz/perfy-0.27/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='perfy',
+    packages=['perfy'],
+    version='0.27',
+    license='MIT',
+    description='Perfy - a lightweight performance tracer for python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Gleb Sakhnov',
+    author_email='gleb.sakhnov@gmail.com',
+    url='https://github.com/glebarez/perfy',
+    keywords=['profiling', 'trace', 'runtime', 'analysis'],
+    install_requires=[],
+    python_requires='>=3.2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
