df615fb9235e40ad59bd0f3c6a8515bf0e5c2200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:57 +0000

    Add package bluemax/0.0.43/bluemax-0.0.43-py3-none-any.whl

---
+++ packages/bluemax/bluemax-0.0.43-py3-none-any.whl/bluemax/scripts/bluemax.py
@@ -0,0 +1,14 @@
+"""
+    This is a wrapper from invoke
+"""
+import logging
+from invoke import Collection
+from invoke import Program
+from .bump_version import bump_version
+
+logging.basicConfig(level=logging.INFO)
+
+ns = Collection()
+ns.add_task(bump_version)
+
+program = Program(version="0.0.43", namespace=ns)
