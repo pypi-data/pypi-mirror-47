73a5a373d16f3d8b1587062f3131c780ffcb9651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:59 +0000

    Add package Python-video-annotator-module-path-editor/0.4/Python%20video%20annotator%20-%20module%20-%20path%20editor-0.4.tar.gz

---
+++ packages/Python-video-annotator-module-path-editor/Python%20video%20annotator%20-%20module%20-%20path%20editor-0.4.tar.gz/Python video annotator - module - path editor-0.4/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+setup(
+	name='Python video annotator - module - path editor',
+	version="0.4",
+	description="""""",
+	author=['Ricardo Ribeiro'],
+	author_email='ricardojvr@gmail.com',
+	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-patheditor',
+	long_description = long_description,
+    long_description_content_type = 'text/markdown',
+	packages=find_packages(),	
+)
