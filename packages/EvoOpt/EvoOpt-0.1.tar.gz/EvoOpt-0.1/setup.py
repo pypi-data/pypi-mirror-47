2a0b92c3f9793a91ab2525f218a1e98c323eb509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:56 +0000

    Add package EvoOpt/0.1/EvoOpt-0.1.tar.gz

---
+++ packages/EvoOpt/EvoOpt-0.1.tar.gz/EvoOpt-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='EvoOpt', 
+version='0.1', 
+license='BSD 2-Clause',
+description="EvoOpt: Python Implementation of State-of-Art Evolutionary Algorithms",
+author='Sin Yong Teng',
+author_email='tsyet12@gmail.com',
+keywords = ['Evolutionary', 'Optimization', 'Algorithm'],
+packages=find_packages(),
+setup_requires=['numpy', 'matplotlib'],
+classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: BSD License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  )
