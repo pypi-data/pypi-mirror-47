dd29fd87152dd6d64e377beaa01cb00347717b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:07 +0000

    Add package gauss-binom-distributions/1.1/gauss_binom_distributions-1.1.tar.gz

---
+++ packages/gauss-binom-distributions/gauss_binom_distributions-1.1.tar.gz/gauss_binom_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_binom_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_binom_distributions'],
+      author = "Jan Rasmus Platou",
+      author_email = "jrplatou@gmail.com",
+      zip_safe=False)
