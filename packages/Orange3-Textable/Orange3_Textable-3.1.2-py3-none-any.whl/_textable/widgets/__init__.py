a536100608f880ea68cc23931206c77b1474a873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:01 +0000

    Add package Orange3-Textable/3.1.2/Orange3_Textable-3.1.2-py3-none-any.whl

---
+++ packages/Orange3-Textable/Orange3_Textable-3.1.2-py3-none-any.whl/_textable/widgets/__init__.py
@@ -0,0 +1,40 @@
+"""File __init__.py
+Copyright 2012-2019 LangTech Sarl (info@langtech.ch)
+-----------------------------------------------------------------------------
+This file is part of the Orange3-Textable package.
+
+Orange3-Textable is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Orange3-Textable is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Orange3-Textable. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+Textable
+========
+
+"""
+
+NAME = "Textable"
+
+DESCRIPTION = """Add-on for text analysis"""
+
+LONG_DESCRIPTION = """
+This extension contains widgets for building data tables based on
+heterogeneous text sources, using such operations as segmentation and
+annotation.
+
+"""
+
+ICON = "icons/Category-Textable.png"
+
+BACKGROUND = "#90c0ed"
+
