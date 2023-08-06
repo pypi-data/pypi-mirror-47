5537d70043c6e4d4cb6c20679e060cf2c86ebc6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:43 +0000

    Add package lingtypology/0.8.2/lingtypology-0.8.2.tar.gz

---
+++ packages/lingtypology/lingtypology-0.8.2.tar.gz/lingtypology-0.8.2/lingtypology/__init__.py
@@ -0,0 +1,13 @@
+from lingtypology.maps import *
+import lingtypology.db_apis
+
+__version__ = '0.8.2'
+__citation__ = \
+    '@misc{MichaelVoronov2669068,\n' \
+    '    author = {Michael Voronov},\n' \
+    '    title = {{lingtypology: a Python tool for linguistic interactive mapping}},\n' \
+    '    month = june,\n' \
+    '    year = 2019,\n' \
+    '    doi = {10.5281/zenodo.2669068},\n' \
+    '    url = {https://doi.org/10.5281/zenodo.2669068}\n' \
+    '}'
