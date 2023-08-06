40c3b6bcdb76aa49e43a11be028a7aa9c921de23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:34 +0000

    Add package lspace/0.1.12/lspace-0.1.12.tar.gz

---
+++ packages/lspace/lspace-0.1.12.tar.gz/lspace-0.1.12/lspace/helpers/init_logging.py
@@ -0,0 +1,34 @@
+import logging
+import logging.config
+
+def init_logging(loglevel='info'):
+
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'default': {
+                'format': '%(name)s [%(levelname)s]: %(message)s'
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': loglevel.upper(),
+                'class': 'logging.StreamHandler',
+                'formatter': 'default'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': loglevel.upper(),
+                'propagate': True
+            },
+            'sqlalchemy.engine': {
+                'handlers': ['default'],
+                'level': loglevel.upper(),
+                'propagate': False
+            }
+        }
+    })
+    logging.debug('{loglevel} logging!'.format(loglevel=loglevel))
