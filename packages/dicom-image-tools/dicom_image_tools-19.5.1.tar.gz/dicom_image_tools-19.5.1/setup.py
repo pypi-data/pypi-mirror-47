e60785cbbc4c174f275fc1636fa0a5a9943bb77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package dicom-image-tools/19.5.1/dicom_image_tools-19.5.1.tar.gz

---
+++ packages/dicom-image-tools/dicom_image_tools-19.5.1.tar.gz/dicom_image_tools-19.5.1/setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+README = (Path(__file__).parent / 'README.md').read_text()
+
+setup(
+    name='dicom_image_tools',
+    version='19.5.1',
+    description='Python package for managing DICOM images from different modalities',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://dev.azure.com/bwkodex/DicomImageTools',
+    author='Josef Lundman',
+    author_email="josef@lundman.eu",
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pydicom>=1.2.2',
+        'numpy>=1.16.3',
+        'scikit-image>=0.15.0',
+        'scipy>=1.3.0'
+    ],
+    zip_safe=False
+)
