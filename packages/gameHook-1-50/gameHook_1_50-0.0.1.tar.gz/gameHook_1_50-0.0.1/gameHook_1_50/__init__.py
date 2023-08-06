1ded8b0fb6cd1ebcbe7adcc196fc4c9c47c1db1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:17 +0000

    Add package gameHook-1-50/0.0.1/gameHook_1_50-0.0.1.tar.gz

---
+++ packages/gameHook-1-50/gameHook_1_50-0.0.1.tar.gz/gameHook_1_50-0.0.1/gameHook_1_50/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+#game site:
+#http://wap.jue-huo.com/app/html/game/1to50/1to50.html
+
+import pyautogui as ag
+def autoGame():
+    for i in range(1,51):
+        A=ag.locateCenterOnScreen(str(i)+'.png',confidence=0.92)
+        ag.moveTo(A,duration=0.002)
+        ag.click(A)
+
