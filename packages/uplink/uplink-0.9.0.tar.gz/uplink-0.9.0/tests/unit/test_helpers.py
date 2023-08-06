5997e0e5e257b99d6516927a8b4d29779322ff16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:35 +0000

    Add package uplink/0.9.0/uplink-0.9.0.tar.gz

---
+++ packages/uplink/uplink-0.9.0.tar.gz/uplink-0.9.0/tests/unit/test_helpers.py
@@ -0,0 +1,55 @@
+from uplink import helpers
+
+
+def test_get_api_definitions(request_definition_builder):
+    class Service(object):
+        builder = request_definition_builder
+
+    assert dict(helpers.get_api_definitions(Service)) == {
+        "builder": request_definition_builder
+    }
+
+
+def test_get_api_definitions_from_parent(request_definition_builder):
+    class Parent(object):
+        builder = request_definition_builder
+
+    class Child(Parent):
+        other_builder = request_definition_builder
+
+    assert dict(helpers.get_api_definitions(Child)) == {
+        "builder": request_definition_builder,
+        "other_builder": request_definition_builder,
+    }
+
+
+class TestRequestBuilder(object):
+    def test_return_type(self):
+        # Setup
+        builder = helpers.RequestBuilder(None, {}, "base_url")
+
+        # Run
+        builder.return_type = str
+
+        # Verify
+        assert builder.return_type is str
+
+    def test_add_transaction_hook(self, transaction_hook_mock):
+        # Setup
+        builder = helpers.RequestBuilder(None, {}, "base_url")
+
+        # Run
+        builder.add_transaction_hook(transaction_hook_mock)
+
+        # Verify
+        assert list(builder.transaction_hooks) == [transaction_hook_mock]
+
+    def test_context(self):
+        # Setup
+        builder = helpers.RequestBuilder(None, {}, "base_url")
+
+        # Run
+        builder.context["key"] = "value"
+
+        # Verify
+        assert builder.context["key"] == "value"
