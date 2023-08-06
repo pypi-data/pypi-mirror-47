15c4ebc8e2122d2463f62506be14067af2eb8f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:57 +0000

    Add package aiobitcoin/0.72.2/aiobitcoin-0.72.2.tar.gz

---
+++ packages/aiobitcoin/aiobitcoin-0.72.2.tar.gz/aiobitcoin-0.72.2/aiobitcoin/bitcoinerrors.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+class BitcoinErrors(Exception):
+    def __init__(self, error_msg=''):
+        Exception.__init__(self, f'{error_msg}')
+
+
+class InvalidPrivateKeyEncoding(BitcoinErrors):
+    pass
+
+
+class PrivateKeyForThisAddressAlreadyInWallet(BitcoinErrors):
+    pass
+
+
+class InvalidAddress(BitcoinErrors):
+    pass
+
+
+class InvalidIpOrSubnet(BitcoinErrors):
+    pass
+
+
+class NoConnectionToTheDaemon(BitcoinErrors):
+    def __init__(self, error_msg='No connection to the daemon.'):
+        super().__init__(error_msg=error_msg)
+
+
+class IncorrectCreds(BitcoinErrors):
+    def __init__(self, uri):
+        super().__init__(error_msg=f'RPC on {uri} is alive, but RPC credentials are incorrect.')
+
