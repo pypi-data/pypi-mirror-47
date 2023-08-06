7a3d49901af32adac871982f5cf0e552a226be2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:27 +0000

    Add package tangledweb/1.0.0/tangledweb-1.0.0.tar.gz

---
+++ packages/tangledweb/tangledweb-1.0.0.tar.gz/tangledweb-1.0.0/README.md
@@ -0,0 +1,3 @@
+# tangledweb
+
+**place holder**
---
+++ packages/tangledweb/tangledweb-1.0.0.tar.gz/tangledweb-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from os import path
+
+__version__ = '1.0.0'
+__author__ = 'Hapsida @securisec'
+
+def read_requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name="tangledweb",
+    version=__version__,
+    author=__author__,
+    packages=find_packages(),
+    install_requires = [
+        'requests',
+        'bs4'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7"
+    ],
+    # entry_points={
+    #     'console_scripts': [
+    #         'saramPy = saramPy.__main__:main'
+    #     ]
+    # }
+)
