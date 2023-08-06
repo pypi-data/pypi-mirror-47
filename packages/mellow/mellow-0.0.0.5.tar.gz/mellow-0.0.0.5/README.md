fc4b001c20895460bdd136543bc27473c1049674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:22 +0000

    Add package mellow/0.0.0.5/mellow-0.0.0.5.tar.gz

---
+++ packages/mellow/mellow-0.0.0.5.tar.gz/mellow-0.0.0.5/README.md
@@ -0,0 +1,11 @@
+### Machine Learning - just do it ! mellow got your back. 
+###### mellow is the python package contains well known machine learning algorithms.
+It provides:
+- most popular machine learning algorithms
+- sophisticated utility functions useful in these algorithms
+- and much more
+
+Often we Developers/Students feel hesitation to implement the machine learning algorithms because</br>
+either they find it hefty or time consuming, what we believe is understanding the machine learning</br>
+is much more important than converting it into code. So we come up with this beautiful library mellow</br>
+to help you out.
---
+++ packages/mellow/mellow-0.0.0.5.tar.gz/mellow-0.0.0.5/mellow/ml.py
@@ -0,0 +1,36 @@
+import numpy as np
+import utility as util
+
+
+# knn function to find the k nearest points to the input point first we will calc
+# distance from input to each training data point then sort(obv in increasing order)
+# them and take first k and make predictions based upon them
+
+def knn(X, Y, test, k = 30):
+    """predicts the result based on nearest neighbour
+    :param X: training data
+    :param Y: label data
+    :param test: test data
+    :param k: neighbour points
+    :return:
+    """
+    d = []
+    size = X.shape[0]
+    for i in range(size):
+        d.append((util.distance(test, X[i]), Y[i]))
+
+    # l is the list of sorted distance label
+    l = np.array(sorted(d))[:, 1]
+    l = l[:k]
+
+    u = np.unique(l, return_counts=True)
+
+    # convert the unique labels with their frequency into key value pair
+    freq_dict = dict()
+    for i in range(len(u[0])):
+        freq_dict[u[0][i]] = u[1][i]
+
+    # get the key whose value is maxmimum in the dictionary
+    pred = int(max(freq_dict, key=freq_dict.get))
+    return pred
+
