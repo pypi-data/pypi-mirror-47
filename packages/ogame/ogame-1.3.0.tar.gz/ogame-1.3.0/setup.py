a1d55407619fba8b2e75509925ed69e76a0eab72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:32 +0000

    Add package ogame/1.3.0/ogame-1.3.0.tar.gz

---
+++ packages/ogame/ogame-1.3.0.tar.gz/ogame-1.3.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ogame',
+      version='1.3.0',
+      description='OGame wrapper.',
+      author='Alain Gilbert',
+      author_email='alain.gilbert.15@gmail.com',
+      packages=['ogame'],
+      url='https://github.com/alaingilbert/pyogame',
+      install_requires=['requests',
+                        'arrow',
+                        'beautifulsoup4'],
+      )
