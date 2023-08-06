e8ddcfcac15b4c4a6c279683ade0db439c9b59f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:32 +0000

    Add package binhoshell/0.2.1/binhoshell-0.2.1.tar.gz

---
+++ packages/binhoshell/binhoshell-0.2.1.tar.gz/binhoshell-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='binhoshell',
+    version='0.2.1',
+    description='A shell in python (???)',
+    url='https://bitbucket.org/paikka/shellpy/src',
+    author='Fábio',
+    author_email='fabio@paikka.me',
+    license='MIT',
+    packages=['shellpy'],
+    zip_safe=False
+)
---
+++ packages/binhoshell/binhoshell-0.2.1.tar.gz/binhoshell-0.2.1/shellpy/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+from .shell import *
+
+__all__ = [ 'cd', 'cmd', 'User', 'sudo' ]
