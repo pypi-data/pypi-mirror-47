7519873d004d0046de7ea4c5ab975493cf647d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:00 +0000

    Add package ppts/1.0.16/ppts-1.0.16.tar.gz

---
+++ packages/ppts/ppts-1.0.16.tar.gz/ppts-1.0.16/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-slide></v-slide>
+</template>
+
+<script>
+import Vue from "vue";
+import Slide from "./slide.vue";
+
+export default {
+  name: "app",
+
+  components: {
+    "v-slide": Slide
+  }
+};
+</script>
+
+<style lang="less"></style>
