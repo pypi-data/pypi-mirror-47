03858929e020f742b3bcd812b3ae3adacd3eee4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:53 +0000

    Add package rhea/0.4.9/rhea-0.4.9.tar.gz

---
+++ packages/rhea/rhea-0.4.9.tar.gz/rhea-0.4.9/rhea/specs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from collections import namedtuple
+
+
+class UriSpec(namedtuple("UriSpec", "user password host")):
+    """
+    A specification for uris configuration.
+    """
+    pass
+
+
+class AuthSpec(namedtuple("UriSpec", "user password")):
+    """
+    A specification for auth configuration.
+    """
+    pass
