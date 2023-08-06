ece1a823e53c8a3172c4bda60edda5ec34ae8524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:16 +0000

    Add package backutil/0.1.4/backutil-0.1.4.tar.gz

---
+++ packages/backutil/backutil-0.1.4.tar.gz/backutil-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='backutil',
+      version='0.1.4',
+      description='Python backup utility',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      url='https://github.com/heywoodlh/backutil',
+      author='Spencer Heywood',
+      author_email='l.spencer.heywood@gmail.com',
+      license='APACHE-2.0',
+      packages=['backutil'],
+      scripts=['bin/backutil'],
+      install_requires=[
+          'requests',
+          'python-gnupg',
+      ],
+      zip_safe=False)
