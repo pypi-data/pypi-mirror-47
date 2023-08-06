05d98cb88b636fcaeaae96e5690137dcb3fbe736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:46 +0000

    Add package waddle/0.5/waddle-0.5-py3-none-any.whl

---
+++ packages/waddle/waddle-0.5-py3-none-any.whl/waddle/aws/session.py
@@ -0,0 +1,27 @@
+from boto3.session import Session
+
+
+__all__ = [
+    'create_session',
+    'ssm_client',
+]
+
+
+def create_session():
+    """
+    A handy helper function that will create the
+    boto session using our waddle-level settings
+    """
+    from waddle import settings
+    session = Session(
+        aws_access_key_id=settings.aws_access_key_id,
+        aws_secret_access_key=settings.aws_secret_access_key,
+        region_name=settings.aws_region,
+        profile_name=settings.aws_profile,
+    )
+    return session
+
+
+def ssm_client():
+    session = create_session()
+    return session.client('ssm')
