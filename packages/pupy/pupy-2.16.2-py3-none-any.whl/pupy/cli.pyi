082d687eb89aa96ae4be5737c5ffd70318abeef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:35 +0000

    Add package pupy/2.16.2/pupy-2.16.2-py3-none-any.whl

---
+++ packages/pupy/pupy-2.16.2-py3-none-any.whl/pupy/cli.pyi
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+
+from typing import Any
+from typing import Optional
+
+def unescaped_str(arg_str: Any): ...
+
+PARSER: Any
+
+def main(ARGS: Optional[Any] = ...) -> None: ...
