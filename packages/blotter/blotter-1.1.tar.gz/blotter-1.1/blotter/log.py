470f8cfc5abe7b33685c438556a35378bab6f6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:43 +0000

    Add package blotter/1.1/blotter-1.1.tar.gz

---
+++ packages/blotter/blotter-1.1.tar.gz/blotter-1.1/blotter/log.py
@@ -0,0 +1,34 @@
+import os
+import logging
+
+FILENAME = os.getenv('LOG_LOCATION') or 'blot.log'
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename=FILENAME,
+                    filemode='w')
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warn(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
