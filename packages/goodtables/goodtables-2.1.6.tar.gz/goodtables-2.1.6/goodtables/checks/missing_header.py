710d35e2694c43cd7ae4ca08b7dd1b241972e139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:41 +0000

    Add package goodtables/2.1.6/goodtables-2.1.6.tar.gz

---
+++ packages/goodtables/goodtables-2.1.6.tar.gz/goodtables-2.1.6/goodtables/checks/missing_header.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from copy import copy
+from ..registry import check
+from ..error import Error
+
+
+# Module API
+
+@check('missing-header')
+def missing_header(cells, sample):
+    errors = []
+
+    for cell in copy(cells):
+
+        # Skip if header in cell
+        if cell.get('header') is not None:
+            continue
+
+        # Add error
+        error = Error('missing-header', cell)
+        errors.append(error)
+
+        # Remove cell
+        cells.remove(cell)
+
+    return errors
