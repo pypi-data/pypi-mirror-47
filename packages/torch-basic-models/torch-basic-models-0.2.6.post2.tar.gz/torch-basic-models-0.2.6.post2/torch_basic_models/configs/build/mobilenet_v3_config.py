7099661bed96bfe8e4eb82742214f7c587a15a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:45 +0000

    Add package torch-basic-models/0.2.6.post2/torch-basic-models-0.2.6.post2.tar.gz

---
+++ packages/torch-basic-models/torch-basic-models-0.2.6.post2.tar.gz/torch-basic-models-0.2.6.post2/torch_basic_models/configs/build/mobilenet_v3_config.py
@@ -0,0 +1,7 @@
+class MobileNetV3Config:
+    def __init__(self, values: dict = None):
+        values = values if values is not None else {}
+        self.type: str = values.get("type", None)
+        self.feature_dim: int = values.get("feature_dim", 1000)
+        self.width_multiple: float = values.get("width_multiple", 1.0)
+        self.dropout_ratio: float = values.get("dropout_ratio", 0.0)
