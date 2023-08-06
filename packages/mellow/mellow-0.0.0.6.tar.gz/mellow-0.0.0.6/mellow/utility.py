92fa48a19eaf6862e0f734b42ececad6970e1959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:25 +0000

    Add package mellow/0.0.0.6/mellow-0.0.0.6.tar.gz

---
+++ packages/mellow/mellow-0.0.0.6.tar.gz/mellow-0.0.0.6/mellow/utility.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def distance(p1, p2):
+    """ calculates the distance between two points in euclidean space
+    :param p1: numpy.array - euclidean coordinates
+    :param p2: numpy.array - euclidean coordinates
+    :return: numpy.float64
+    """
+    return np.sum((p2 - p1) ** 2) ** .5
