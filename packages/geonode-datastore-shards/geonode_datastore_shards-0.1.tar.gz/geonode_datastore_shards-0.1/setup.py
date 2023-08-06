7e2db307653e0f79ce1fce288b1db6acd4c706d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:35 +0000

    Add package geonode-datastore-shards/0.1/geonode_datastore_shards-0.1.tar.gz

---
+++ packages/geonode-datastore-shards/geonode_datastore_shards-0.1.tar.gz/geonode_datastore_shards-0.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='geonode_datastore_shards',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPL',
+    description='GeoNode Datastore Shard Application',
+    #long_description=README,
+    #long_description_content_type='text/markdown',
+    url='http://geonode.org',
+    author='GeoNode Developers',
+    author_email='dev@geonode.org',
+    install_requires=[
+    ],
+)
