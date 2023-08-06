95e92277dd71ab0769696c36ab585c365f98265d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:41 +0000

    Add package system-query/0.2.5/system-query-0.2.5.tar.gz

---
+++ packages/system-query/system-query-0.2.5.tar.gz/system-query-0.2.5/setup.py
@@ -0,0 +1,44 @@
+"""This is setup.py file for system_query package."""
+
+import setup_boilerplate
+
+
+class Package(setup_boilerplate.Package):
+
+    """Package metadata."""
+
+    name = 'system-query'
+    description = 'comprehensive and concise system information querying tool'
+    download_url = 'https://github.com/mbdevpl/system-query'
+    author = 'Aleksandr Drozd, Mateusz Bysiek'
+    maintainer = 'Mateusz Bysiek'
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities'
+        ]
+    keywords = ['system', 'software', 'hardware']
+    extras_require = {
+        'all': ['psutil', 'py-cpuinfo', 'pycuda', 'pyudev'],
+        'cpu': ['psutil', 'py-cpuinfo'],
+        'gpu': ['pycuda'],
+        'hdd': ['pyudev'],
+        'ram': ['psutil'],
+        'swap': ['psutil']}
+
+
+if __name__ == '__main__':
+    Package.setup()
