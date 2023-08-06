70d4a201746de8ffcabbffeeaba1f4a084bb930e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:32 +0000

    Add package Menu/3.2.2/Menu-3.2.2.tar.gz

---
+++ packages/Menu/Menu-3.2.2.tar.gz/Menu-3.2.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='Menu',
+    version='3.2.2',
+    author='Dylan Richardson',
+    author_email='dylanrichardson1996@gmail.com',
+    packages=setuptools.find_packages(),
+    scripts=[],
+    url='https://github.com/drich14/Menu/',
+    license='LICENSE.txt',
+    description='Easily create command-line menus',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown"
+)
---
+++ packages/Menu/Menu-3.2.2.tar.gz/Menu-3.2.2/test/tests.py
@@ -0,0 +1,35 @@
+import unittest
+from menu import Menu
+
+
+class TestMenu(unittest.TestCase):
+    def setUp(self):
+        self.menu = Menu([(None, lambda: None)])
+
+    @unittest.expectedFailure
+    def test_set_options_not_iterable(self):
+        self.menu.set_options(None)
+
+    @unittest.expectedFailure
+    def test_add_option_not_tuple(self):
+        self.menu.set_options([None])
+
+    @unittest.expectedFailure
+    def test_add_option_not_length_2(self):
+        self.menu.set_options([(None,)])
+
+    @unittest.expectedFailure
+    def test_add_option_handler_not_callable(self):
+        self.menu.set_options([(None, None)])
+
+    def test_message_enabled(self):
+        self.assertTrue(Menu([(None, lambda: None)], message="").is_message_enabled)
+        self.assertFalse(Menu([(None, lambda: None)]).is_message_enabled)
+
+    def test_title_enabled(self):
+        self.assertTrue(Menu([(None, lambda: None)], title="").is_title_enabled)
+        self.assertFalse(Menu([(None, lambda: None)]).is_title_enabled)
+
+
+if __name__ == '__main__':
+    unittest.main()
