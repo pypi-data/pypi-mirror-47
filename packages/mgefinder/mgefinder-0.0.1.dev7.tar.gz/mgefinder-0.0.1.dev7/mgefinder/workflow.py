f926c8f87a26be32ea6cc85e15fe4974306ad33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:13 +0000

    Add package mgefinder/0.0.1.dev7/mgefinder-0.0.1.dev7.tar.gz

---
+++ packages/mgefinder/mgefinder-0.0.1.dev7.tar.gz/mgefinder-0.0.1.dev7/mgefinder/workflow.py
@@ -0,0 +1,7 @@
+from snakemake import snakemake
+
+
+def _workflow(workdir, snakefile, configfile, cores, memory, unlock, rerun_incomplete, keep_going):
+
+    snakemake(snakefile, configfile=configfile, config={'wd': workdir, 'memory': memory}, cores=cores, unlock=unlock,
+              force_incomplete=rerun_incomplete, keepgoing=keep_going)
