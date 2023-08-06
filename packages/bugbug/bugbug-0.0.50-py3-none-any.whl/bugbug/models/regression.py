14ecc45f4113b0217d0be0047da03822f6153598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:13 +0000

    Add package bugbug/0.0.50/bugbug-0.0.50-py3-none-any.whl

---
+++ packages/bugbug/bugbug-0.0.50-py3-none-any.whl/bugbug/models/regression.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from bugbug.models.defect import DefectModel
+
+
+class RegressionModel(DefectModel):
+    def __init__(self, lemmatization=False, historical=False):
+        DefectModel.__init__(self, lemmatization, historical)
+
+    def get_labels(self):
+        return self.get_bugbug_labels("regression"), [0, 1]
