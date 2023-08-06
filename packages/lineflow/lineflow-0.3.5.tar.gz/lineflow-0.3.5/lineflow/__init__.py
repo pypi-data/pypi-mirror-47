0fe0a038e2febe155c98a9a0c929f09c4e74267b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:08 +0000

    Add package lineflow/0.3.5/lineflow-0.3.5.tar.gz

---
+++ packages/lineflow/lineflow-0.3.5.tar.gz/lineflow-0.3.5/lineflow/__init__.py
@@ -0,0 +1,8 @@
+from lineflow.core import Dataset  # NOQA
+from lineflow.text import TextDataset  # NOQA
+from lineflow.text import CsvDataset  # NOQA
+from lineflow.core import lineflow_concat as concat  # NOQA
+from lineflow.core import lineflow_zip as zip  # NOQA
+from lineflow.core import lineflow_filter as filter  # NOQA
+from lineflow.core import lineflow_flat_map as flat_map  # NOQA
+from lineflow.core import lineflow_load as load  # NOQA
