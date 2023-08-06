2795507ec991e889525748b57dec460355ea9e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:36 +0000

    Add package DroneSim/0.2/DroneSim-0.2.tar.gz

---
+++ packages/DroneSim/DroneSim-0.2.tar.gz/DroneSim-0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DroneSim',
+    version=0.2,
+    description=(
+        'Yet Another Python API for SJTU Drone Contest.'
+    ),
+    author='DUT Drone Lab',
+    maintainer='Yuan Yichen',
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'msgpack-rpc-python',
+        'numpy',
+    ],
+)
