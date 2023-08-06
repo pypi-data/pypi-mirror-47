f18f332849c6c2c26a48a72ceec5cbddcf5c661b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:20 +0000

    Add package cimcb/1.0.22/cimcb-1.0.22.tar.gz

---
+++ packages/cimcb/cimcb-1.0.22.tar.gz/cimcb-1.0.22/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="cimcb",
+    version="1.0.22",
+    description="This is a pre-release.",
+    long_description=long_description,
+    license="http://www.apache.org/licenses/LICENSE-2.0.html",
+    url="https://github.com/KevinMMendez/cimcb",
+    packages=["cimcb", "cimcb.bootstrap", "cimcb.cross_val", "cimcb.model", "cimcb.plot", "cimcb.utils"],
+    python_requires=">=3.5",
+    install_requires=["bokeh>=1.0.0",
+                      "keras",
+                      "numpy>=1.12",
+                      "pandas",
+                      "scipy",
+                      "scikit-learn",
+                      "statsmodels",
+                      "tensorflow",
+                      "tqdm",
+                      "xlrd",
+                      "joblib"],
+    author="Kevin Mendez, David Broadhurst",
+    author_email="k.mendez@ecu.edu.au, d.broadhurst@ecu.edu.au",
+)
