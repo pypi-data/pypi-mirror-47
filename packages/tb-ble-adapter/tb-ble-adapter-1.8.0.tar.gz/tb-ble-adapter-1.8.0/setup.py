e8b701db3aee062e61cb9b7e0c9419534d16b1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:56 +0000

    Add package tb-ble-adapter/1.8.0/tb-ble-adapter-1.8.0.tar.gz

---
+++ packages/tb-ble-adapter/tb-ble-adapter-1.8.0.tar.gz/tb-ble-adapter-1.8.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'tb-ble-adapter',
+  packages = [ 'tb-ble-adapter/extensions' ],
+  scripts = [ 'scripts/tb-ble-adapter.py' ],
+  version = '1.8.0',
+  license='Apache',
+  description = 'BLE adapter demo, that connects to available devices and sends data from them to ThingsBoard server',
+  author = 'ThingsBoard',
+  author_email = 'support@thingsboard.io',
+  url = 'https://github.com/thingsboard/tb-ble-adapter',
+  download_url = 'https://github.com/thingsboard/tb-ble-adapter',
+  keywords = ['tb-ble-adapter', 'demo', 'bluetooth low energy', 'thingsboard'],
+  install_requires=[
+          'btlewrap',
+          'bluepy',
+          'tb-mqtt-client',
+      ],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.4',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+  ],
+)
