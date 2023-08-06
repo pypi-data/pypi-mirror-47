d52fdda02ae9e20258fea5acb1ce6c7f14a078d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:17 +0000

    Add package tartiflette-starlette/0.2.0/tartiflette-starlette-0.2.0.tar.gz

---
+++ packages/tartiflette-starlette/tartiflette-starlette-0.2.0.tar.gz/tartiflette-starlette-0.2.0/tartiflette_starlette/app.py
@@ -0,0 +1,33 @@
+import typing
+
+from tartiflette import Engine
+
+from .graphql import GraphQLHandler
+
+
+class TartifletteApp:
+
+    handler_class = GraphQLHandler
+
+    def __init__(
+        self,
+        *,
+        engine: Engine = None,
+        sdl: str = None,
+        graphiql: bool = True,
+        path: str = "",
+        schema_name: str = "default",
+    ):
+        if engine is None:
+            assert sdl, "`sdl` expected if `engine` not given"
+            engine = Engine(sdl, schema_name)
+
+        assert engine, "`engine` expected if `sdl` not given"
+
+        self.handler = self.handler_class(engine, graphiql=graphiql, path=path)
+
+    async def __call__(
+        self, scope: dict, receive: typing.Callable, send: typing.Callable
+    ):
+        response = await self.handler(scope, receive)
+        await response(scope, receive, send)
