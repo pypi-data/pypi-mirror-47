8b41f096b6ea57338c90e68a3d97aa5b7d02169b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:30 +0000

    Add package squidmonitor/0.2/squidmonitor-0.2.tar.gz

---
+++ packages/squidmonitor/squidmonitor-0.2.tar.gz/squidmonitor-0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+     name='squidmonitor',  
+     version='0.2',
+     scripts=['squidmonitor'] ,
+     author="Sai Allu",
+     author_email="allu@usc.edu",
+     description="A tool to monitor Squid server logs"
+ )
