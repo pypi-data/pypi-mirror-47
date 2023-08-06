a721133f04a7ae9e94edcb4cebb7a6a888c12046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:41 +0000

    Add package ping-cmd/1.1/ping_cmd-1.1.tar.gz

---
+++ packages/ping-cmd/ping_cmd-1.1.tar.gz/ping_cmd-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ping_cmd',
+      version='1.1',
+      description='Ping website or server using ping.exe just to check is the host reachable or not.',
+      author='Dragan Matesic',
+      author_email='dragan.matesic@gmail.com',
+      license='MIT',
+      packages=['ping_cmd'],
+      zip_safe=False
+      )
