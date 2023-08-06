ee4808fea15b74af7278eb9bbda16037a01227ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:32 +0000

    Add package klein/19.6.0/klein-19.6.0-py2.py3-none-any.whl

---
+++ packages/klein/klein-19.6.0-py2.py3-none-any.whl/klein/app.py
@@ -0,0 +1,22 @@
+from ._app import (
+    Klein,
+    KleinRequest,
+    handle_errors,
+    resource,
+    route,
+    run,
+    urlFor,
+    url_for,
+)
+
+
+__all__ = (
+    "Klein",
+    "KleinRequest",
+    "handle_errors",
+    "resource",
+    "route",
+    "run",
+    "urlFor",
+    "url_for",
+)
