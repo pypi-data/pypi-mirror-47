a1767f391d2941af69efceb83d1b7e5023de3376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:36 +0000

    Add package jatin-module/0.1/jatin_module-0.1.tar.gz

---
+++ packages/jatin-module/jatin_module-0.1.tar.gz/jatin_module-0.1/jatin_module/pckg.py
@@ -0,0 +1,13 @@
+def ascii(value):
+	output = ord(str(value))
+	return output
+
+# class Person:
+# 	def __init__(mysillyobject, name):
+# 		mysillyobject.name = name
+
+# 	def myfunc(abc):
+# 		return ord(str(value))
+
+# p1 = Person("John", 36)
+# p1.myfunc()	
