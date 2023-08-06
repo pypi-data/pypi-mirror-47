5059f2c419ab6177b705ff34dc682e8b408bae3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:16 +0000

    Add package plasmith/0.1.0/plasmith-0.1.0-py3-none-any.whl

---
+++ packages/plasmith/plasmith-0.1.0-py3-none-any.whl/plasmith/__init__.py
@@ -0,0 +1,6 @@
+# This source code is part of the Plasmith package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+__version__ = "0.1.0"
+__author__ = "Patrick Kunzmann"
