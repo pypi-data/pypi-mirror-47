80be70fee713734c57d576dcc808bc832dfa7f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:08 +0000

    Add package epsagon/1.12.3/epsagon-1.12.3.tar.gz

---
+++ packages/epsagon/epsagon-1.12.3.tar.gz/epsagon-1.12.3/epsagon/constants.py
@@ -0,0 +1,20 @@
+"""General constants"""
+
+import os
+
+__version__ = '1.12.3'
+
+DEFAULT_REGION = 'us-east-1'
+REGION = os.getenv('AWS_REGION', DEFAULT_REGION)
+
+TRACE_COLLECTOR_URL = '{protocol}{region}.tc.epsagon.com'
+COLD_START = True
+
+# Customer original handler.
+EPSAGON_HANDLER = 'EPSAGON_HANDLER'
+
+TIMEOUT_GRACE_TIME_MS = 200
+# How long we try to send traces in seconds.
+SEND_TIMEOUT = TIMEOUT_GRACE_TIME_MS / 1000.0
+
+MAX_LABEL_SIZE = 100 * 1024
