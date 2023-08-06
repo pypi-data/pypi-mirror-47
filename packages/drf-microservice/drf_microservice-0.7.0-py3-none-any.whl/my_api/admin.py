4aa7f9c0468b22182ad0d5a04437bf891f9b15cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:27 +0000

    Add package drf-microservice/0.7.0/drf_microservice-0.7.0-py3-none-any.whl

---
+++ packages/drf-microservice/drf_microservice-0.7.0-py3-none-any.whl/my_api/admin.py
@@ -0,0 +1,8 @@
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from my_api.rest.models import User
+#
+#
+# @admin.register(User)
+# class UserAdmin(UserAdmin):
+#     pass
