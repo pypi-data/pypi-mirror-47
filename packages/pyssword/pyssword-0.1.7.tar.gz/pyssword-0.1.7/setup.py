099cf59f4e2fa0c177dff675457980134f8b1203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:20 +0000

    Add package pyssword/0.1.7/pyssword-0.1.7.tar.gz

---
+++ packages/pyssword/pyssword-0.1.7.tar.gz/pyssword-0.1.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="pyssword",
+    version="0.1.7",
+    author="Corleo",
+    author_email="corleo.git@gmail.com",
+    description="A Python password generator.",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'colorama',
+        'click',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    tests_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
