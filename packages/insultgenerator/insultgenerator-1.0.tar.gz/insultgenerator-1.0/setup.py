feaf78ef6245b9224ab6d3477db5f8c7ed93a22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:24 +0000

    Add package insultgenerator/1.0/insultgenerator-1.0.tar.gz

---
+++ packages/insultgenerator/insultgenerator-1.0.tar.gz/insultgenerator-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from glob import glob
+setup(name='insultgenerator',
+	version='1.0',
+	packages=['insultgenerator'],
+	license='MIT',
+	author='James Cheese',
+	author_email='trust@tr00st.co.uk',
+	install_requires=['six'],
+	test_suite='insultgenerator.tests',
+	description='Random insult generator',
+	url="https://github.com/tr00st/insult_generator",
+	package_data = {
+		'insultgenerator': ['wordlists/*.txt'],
+	},
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+	    'Programming Language :: Python :: 2.7',
+	    'Programming Language :: Python :: 3',
+	    'Programming Language :: Python :: 3.4',
+	    'Programming Language :: Python :: 3.5',
+	    'Programming Language :: Python :: 3.6',
+	    'Programming Language :: Python :: 3.7',
+	    'License :: OSI Approved :: MIT License',
+	]
+)
