83b9cd842ffedf674340d0e657aaaa3eff0699ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:21 +0000

    Add package MrSnippets/1.1.1/MrSnippets-1.1.1-py3-none-any.whl

---
+++ packages/MrSnippets/MrSnippets-1.1.1-py3-none-any.whl/MrSnippets/__version__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "MrSnippets"
+__description__ = "A complete collection of common code snippets"
+__author__ = "Dhamodharan Karuppuswamy"
+__version__ = "1.1.1"
