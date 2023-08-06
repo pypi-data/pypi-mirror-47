b0702b603cc0d819f0145a544b65a9dafeb3de6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:01 +0000

    Add package ppts/1.0.1/ppts-1.0.1.tar.gz

---
+++ packages/ppts/ppts-1.0.1.tar.gz/ppts-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ppts',
+    version='1.0.1',
+    description='ppt with web',
+    author='simdd',
+    author_email='dev.simdd@gmail.com',
+    packages=['cli'],
+    scripts=['cli/bin.py']
+)
