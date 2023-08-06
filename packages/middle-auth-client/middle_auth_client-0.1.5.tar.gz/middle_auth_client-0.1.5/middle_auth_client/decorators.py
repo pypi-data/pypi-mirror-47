59339c68af1522c0774f3d5dcc9214bdd46ec22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:11 +0000

    Add package middle-auth-client/0.1.5/middle_auth_client-0.1.5.tar.gz

---
+++ packages/middle-auth-client/middle_auth_client-0.1.5.tar.gz/middle_auth_client-0.1.5/middle_auth_client/decorators.py
@@ -0,0 +1,81 @@
+from functools import wraps
+import flask
+import json
+import os
+import redis
+
+r = redis.Redis(
+        host=os.environ.get('REDISHOST', 'localhost'),
+        port=int(os.environ.get('REDISPORT', 6379)))
+
+AUTH_URI = os.environ.get('AUTH_URI', 'localhost:5000/auth')
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if hasattr(flask.g, 'auth_token'):
+            # if authorization header has already been parsed, don't need to re-parse
+            # this allows auth_required to be an optional decorator if auth_requires_role is also used
+            return f(*args, **kwargs)
+
+        token = flask.request.headers.get('authorization')
+        if not token:
+            resp = flask.Response("Unauthorized", 401)
+            resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '"'
+            return resp
+        elif not token.startswith('Bearer '):
+            resp = flask.Response("Invalid Request", 400)
+            resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_request", error_description="Header must begin with \'Bearer\'"'
+            return resp
+        else:
+            token = token.split(' ')[1] # remove schema
+            cached_user_data = r.get("token_" + token)
+
+            if cached_user_data:
+                flask.g.auth_user = json.loads(cached_user_data.decode('utf-8'))
+                flask.g.auth_token = token
+                return f(*args, **kwargs)
+            else:
+                resp = flask.Response("Invalid/Expired Token", 401)
+                resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_token", error_description="Invalid/Expired Token"'
+                return resp
+    return decorated_function
+
+def auth_requires_roles(*required_roles):
+    def decorator(f):
+        @wraps(f)
+        @auth_required
+        def decorated_function(*args, **kwargs):
+            users_roles = flask.g.auth_user['roles']
+            missing_roles = []
+
+            for role in required_roles:
+                if not role in users_roles:
+                    missing_roles += [role]
+
+            if missing_roles:
+                resp = flask.Response("Missing role(s): {0}".format(missing_roles), 403)
+                return resp
+            else:
+                return f(*args, **kwargs)
+
+        return decorated_function
+    return decorator
+
+def auth_requires_roles_any(*required_roles):
+    def decorator(f):
+        @wraps(f)
+        @auth_required
+        def decorated_function(*args, **kwargs):
+            users_roles = flask.g.auth_user['roles']
+
+            for role in required_roles:
+                if role in users_roles:
+                    return f(*args, **kwargs)
+
+            resp = flask.Response("Requires one of the following roles: {0}".format(list(required_roles)), 403)
+            return resp
+           
+        return decorated_function
+    return decorator
