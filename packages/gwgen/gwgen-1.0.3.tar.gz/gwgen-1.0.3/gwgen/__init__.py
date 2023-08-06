545f6e641dbc8f0de717fa178952f79e555993e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:32 +0000

    Add package gwgen/1.0.3/gwgen-1.0.3.tar.gz

---
+++ packages/gwgen/gwgen-1.0.3.tar.gz/gwgen-1.0.3/gwgen/__init__.py
@@ -0,0 +1,9 @@
+"""GWGEN: A WGEN-like weather generator for global daily weather
+"""
+from gwgen.version import __version__
+
+__author__ = "Philipp Sommer and Jed Kaplan"
+__maintainer__ = "Philipp Sommer"
+__licence__ = "GPLv2"
+__email__ = "philipp.sommer@unil.ch"
+__copyright__ = "Copyright 2016, ARVE-Research"
