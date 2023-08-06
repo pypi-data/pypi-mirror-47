5e052504360960d6249e3c2ef0053cb110c6f3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:30 +0000

    Add package unv.utils/0.3/unv.utils-0.3.tar.gz

---
+++ packages/unv.utils/unv.utils-0.3.tar.gz/unv.utils-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unv.utils',
+    version='0.3',
+    description="""Common helpers for unv framework""",
+    url='http://github.com/c137digital/unv_utils',
+    author='Morty Space',
+    author_email='morty.space@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    zip_safe=True
+)
