50c2aff8127b98f1f91265c14a0f71b1be866de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:54 +0000

    Add package simplepy/1.1.0/simplepy-1.1.0.tar.gz

---
+++ packages/simplepy/simplepy-1.1.0.tar.gz/simplepy-1.1.0/simplepy/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Python-common-utils Library
+~~~~~~~~~~~~~~~~~~~~~
+
+
+:copyright: (c) 2019 by fovegage.
+:license: Apache 2.0, see LICENSE for more details.
+"""
+
+from .String import String
+
+
+
+
+
+
