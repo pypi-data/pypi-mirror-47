490d5dd859db318cf254a616a566827a6279bbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:21 +0000

    Add package diagnosticism/0.1.2/diagnosticism-0.1.2.tar.gz

---
+++ packages/diagnosticism/diagnosticism-0.1.2.tar.gz/diagnosticism-0.1.2/diagnosticism/__init__.py
@@ -0,0 +1,16 @@
+
+__author__      =   'Matt Wilson'
+__copyright__   =   'Copyright 2019, Synesis Software'
+__credits__     =   [
+        'Garth Lancaster',
+        'Matt Wilson',
+ ]
+__email__       =   'matthew@synesis.com.au'
+__license__     =   'BSD-3-Clause'
+__maintainer__  =   'Matt Wilson'
+__status__      =   'Beta'
+__version__     =   '0.1.2'
+
+from .trace import enable_tracing, is_tracing_enabled, trace
+
+
