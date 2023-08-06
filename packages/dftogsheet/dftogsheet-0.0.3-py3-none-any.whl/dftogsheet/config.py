7ac6ec2ae899f956911c5d43602bc9f81533da85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:37 +0000

    Add package dftogsheet/0.0.3/dftogsheet-0.0.3-py3-none-any.whl

---
+++ packages/dftogsheet/dftogsheet-0.0.3-py3-none-any.whl/dftogsheet/config.py
@@ -0,0 +1,9 @@
+# If modifying these scopes, delete the file token.pickle.
+scopes = ['https://www.googleapis.com/auth/spreadsheets']
+
+# Type of input
+value_input_option = 'RAW'
+
+# Paths to files
+token = 'dfgsheet/secret/token.pickle'
+credentials = 'dfgsheet/secret/credentials.json'
