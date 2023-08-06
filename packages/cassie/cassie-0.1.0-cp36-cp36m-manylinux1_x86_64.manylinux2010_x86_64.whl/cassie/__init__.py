68293af85702ca71503354e504608b6bab8cd492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:29 +0000

    Add package cassie/0.1.0/cassie-0.1.0-cp36-cp36m-manylinux1_x86_64.manylinux2010_x86_64.whl

---
+++ packages/cassie/cassie-0.1.0-cp36-cp36m-manylinux1_x86_64.manylinux2010_x86_64.whl/cassie/__init__.py
@@ -0,0 +1,6 @@
+"""cassie is a library for concurrent programming in python.
+
+It currently includes:
+    - a Treiber Stack implementation (TreiberStack).
+"""
+from .treiber_stack import TreiberStack
