887252fc1ffb819bcb9809f18bb424796a719a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:16 +0000

    Add package trytond-stock/4.6.7/trytond_stock-4.6.7-py3-none-any.whl

---
+++ packages/trytond-stock/trytond_stock-4.6.7-py3-none-any.whl/trytond/modules/stock/party.py
@@ -0,0 +1,18 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.pool import PoolMeta
+
+__all__ = ['PartyReplace']
+
+
+class PartyReplace(metaclass=PoolMeta):
+    __name__ = 'party.replace'
+
+    @classmethod
+    def fields_to_replace(cls):
+        return super(PartyReplace, cls).fields_to_replace() + [
+            ('stock.shipment.in', 'supplier'),
+            ('stock.shipment.in.return', 'supplier'),
+            ('stock.shipment.out', 'customer'),
+            ('stock.shipment.out.return', 'customer'),
+            ]
