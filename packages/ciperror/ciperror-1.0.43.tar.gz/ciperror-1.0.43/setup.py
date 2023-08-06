84228207baf9f8f481cc73ba2b651d6e4a640e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:31 +0000

    Add package ciperror/1.0.43/ciperror-1.0.43.tar.gz

---
+++ packages/ciperror/ciperror-1.0.43.tar.gz/ciperror-1.0.43/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+VERSION='1.0.43'
+PACKAGES=['ciperror']
+
+setup(name='ciperror',
+      version=VERSION,
+      description='TCIP error definitions.',
+      author='Gustavo Alves',
+      author_email='gustavo.oliveira@tvlgobo.com.br',
+      license='MIT',
+      packages=PACKAGES,
+      install_requires=[],
+      zip_safe=False)
