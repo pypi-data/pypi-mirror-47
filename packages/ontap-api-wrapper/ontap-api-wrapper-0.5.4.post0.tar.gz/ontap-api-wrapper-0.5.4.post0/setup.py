1704ba9a414a738bf30cee22646405edfa4a0877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:59 +0000

    Add package ontap-api-wrapper/0.5.4.post0/ontap-api-wrapper-0.5.4.post0.tar.gz

---
+++ packages/ontap-api-wrapper/ontap-api-wrapper-0.5.4.post0.tar.gz/ontap-api-wrapper-0.5.4.post0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ontap-api-wrapper',
+      version='0.5.4.post0',
+      description='Python wrapper for NetApp Manageability SDK',
+      author='Andrew Leonard',
+      author_email='andy.leonard@sbri.org',
+      maintainer='Jiri Machalek',
+      maintainer_email='machalekj@gmail.com',
+      license='Apache License',
+      py_modules=['Ontap'],
+      packages=['netapp'],
+      install_requires=['six'],
+      url='https://github.com/machalekj/ontap-api-wrapper',
+      download_url='https://github.com/machalekj/ontap-api-wrapper/archive/v0.5.4.tar.gz',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ]
+     )
+
