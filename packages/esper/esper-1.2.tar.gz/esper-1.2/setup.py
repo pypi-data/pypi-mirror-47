5e86eda76d8511304907f2d24aea9cf407cf83cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:18 +0000

    Add package esper/1.2/esper-1.2.tar.gz

---
+++ packages/esper/esper-1.2.tar.gz/esper-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+readme = open('README.rst').read()
+
+setup(name='esper',
+      version='1.2',
+      author='Benjamin Moran',
+      author_email='benmoran@protonmail.com',
+      description="Esper is a lightweight Entity System for Python, with a focus on performance.",
+      long_description=readme,
+      license='MIT',
+      keywords='ecs,entity component system,game',
+      url='https://github.com/benmoran56/esper',
+      download_url='https://github.com/benmoran56/esper/releases',
+      platforms='POSIX, Windows, MacOS X',
+      py_modules=['esper'],
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Topic :: Games/Entertainment",
+                   "Topic :: Software Development :: Libraries"])
