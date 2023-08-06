7f3747b3a7444955539591ae1fd5bd5be5eee9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package OmicsTools/0.2.1/OmicsTools-0.2.1.tar.gz

---
+++ packages/OmicsTools/OmicsTools-0.2.1.tar.gz/OmicsTools-0.2.1/README.md
@@ -0,0 +1,13 @@
+# OmicsTools
+
+Suite of tools to enable processing and analysis of -omics data. 
+
+## Setup 
+
+Download ontology files: 
+
+```
+cd OmicsTools/enrich
+
+bash ./download_db.sh
+```
---
+++ packages/OmicsTools/OmicsTools-0.2.1.tar.gz/OmicsTools-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OmicsTools',
+    packages=find_packages(),
+    version='0.2.1',
+    url='https://github.com/fraenkel-lab/OmicsTools',
+    python_requires='>=3.5',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'],
+    license='MIT',
+    author='iamjli',
+    author_email='iamjli@mit.edu',
+    description='',
+    install_requires=[
+    ]
+)
