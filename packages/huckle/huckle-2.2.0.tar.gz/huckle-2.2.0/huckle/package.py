86aef34171351a6bee9942dbbf32cff827b8febb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:32 +0000

    Add package huckle/2.2.0/huckle-2.2.0.tar.gz

---
+++ packages/huckle/huckle-2.2.0.tar.gz/huckle-2.2.0/huckle/package.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = "2.2.0"
+dependencies = ["restnavigator==1.0.1",
+                "requests[socks]==2.18.4",
+                "configparser==3.5.0",
+                "requests_toolbelt==0.8.0"]
