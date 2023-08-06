c61023bddedf8c65369c90a179607c729e074af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:13 +0000

    Add package ssxtd/1.0.9/ssxtd-1.0.9.tar.gz

---
+++ packages/ssxtd/ssxtd-1.0.9.tar.gz/ssxtd-1.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ssxtd',
+    version='1.0.9',
+    packages=['ssxtd',],
+    install_requires=['bs4','tqdm'],
+    license='MIT',
+    author='Xavier Godon',
+    author_email='xavier.godon@protonmail.com',
+    url='https://github.com/xgodon/ssxtd',
+    description='semi structured xml to dict',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
