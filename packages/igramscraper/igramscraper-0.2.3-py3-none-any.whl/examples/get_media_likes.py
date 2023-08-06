92a23743b2f0bbbc004fd4e929cf202468e92f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:17 +0000

    Add package igramscraper/0.2.3/igramscraper-0.2.3-py3-none-any.whl

---
+++ packages/igramscraper/igramscraper-0.2.3-py3-none-any.whl/examples/get_media_likes.py
@@ -0,0 +1,21 @@
+#TODO does not work currently instagram changed api
+from context import Instagram # pylint: disable=no-name-in-module
+
+instagram = Instagram.with_credentials('username', 'password', 'path/to/cache/folder')
+instagram.login()
+
+# Get media comments by shortcode
+likes = instagram.get_media_likes_by_code('BG3Iz-No1IZ', 8000)
+
+print("Result count: " + len(likes))
+
+print(vars(likes))
+
+# foreach ($likes as $like) {
+# 	echo "username: " . $like->getUserName() . "\n";
+# }
+
+# ...
+
+
+
