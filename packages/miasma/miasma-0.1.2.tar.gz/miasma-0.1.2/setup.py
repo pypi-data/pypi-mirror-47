720950ee7b1a27c740890e10b24128bea841f8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:31 +0000

    Add package miasma/0.1.2/miasma-0.1.2.tar.gz

---
+++ packages/miasma/miasma-0.1.2.tar.gz/miasma-0.1.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name = 'miasma',
+    version = '0.1.2',
+
+    url = 'https://github.com/bhuztez/miasma',
+    description = '',
+    license = 'BSD',
+
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+
+    author = 'bhuztez',
+    author_email = 'bhuztez@gmail.com',
+
+    packages = ['miasma'],
+)
