d1baed970bb3a0a83bd3988a7a03b8515aebf9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:38 +0000

    Add package pretf/0.1.1/pretf-0.1.1.tar.gz

---
+++ packages/pretf/pretf-0.1.1.tar.gz/pretf-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import os
+import re
+
+from setuptools import find_namespace_packages, setup
+
+
+def get_version():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, "pretf", "version.py")) as open_file:
+        contents = open_file.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", contents, re.MULTILINE)
+    return match.group(1)
+
+
+setup(
+    name="pretf",
+    version=get_version(),
+    author="Raymond Butcher",
+    author_email="ray.butcher@claranet.uk",
+    license="MIT License",
+    packages=find_namespace_packages("pretf.*"),
+    entry_points={"console_scripts": ("pretf=pretf.cli:main",)},
+    install_requires=["colorama", "pyhcl"],
+    zip_safe=False,
+)
