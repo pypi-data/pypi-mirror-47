2fb47fe06ad4b7f3adbc88a886135eb5a9082f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:07 +0000

    Add package sc_tim/0.1.0/sc_tim-0.1.0.tar.gz

---
+++ packages/sc_tim/sc_tim-0.1.0.tar.gz/sc_tim-0.1.0/sc_tim/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 22 19:23:29 2018
+
+@author: Zhanying Feng
+"""
+from sc_tim.PreProcess import PreProcess
+from sc_tim.CellRedMatrix import CellRedMatrix
+from sc_tim.GeneSpecificity import GeneSpecificity
+from sc_tim.GeneRedMatrix import GeneRedMatrix
+from sc_tim.ExtractGene import ExtractGene
