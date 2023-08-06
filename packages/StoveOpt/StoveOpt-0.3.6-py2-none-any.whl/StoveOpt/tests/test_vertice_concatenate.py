a973c89667eff308827a2eac45729108227fe42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:09 +0000

    Add package StoveOpt/0.3.6/StoveOpt-0.3.6-py2-none-any.whl

---
+++ packages/StoveOpt/StoveOpt-0.3.6-py2-none-any.whl/StoveOpt/tests/test_vertice_concatenate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 10:12:40 2019
+
+@author: Lee
+"""
+
+# Goal is to make sure that the fuel vertices are the following:
+# type = str
+# max length = 3x4 (3 coords), + 2(spaces) + 2 parens = 16
+
+
+
+
+
+
+
