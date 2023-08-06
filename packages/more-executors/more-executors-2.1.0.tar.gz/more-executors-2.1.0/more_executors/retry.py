b0ae616930d7d7510b9eb37dd5c7e6761ba4a012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:43 +0000

    Add package more-executors/2.1.0/more-executors-2.1.0.tar.gz

---
+++ packages/more-executors/more-executors-2.1.0.tar.gz/more-executors-2.1.0/more_executors/retry.py
@@ -0,0 +1,10 @@
+from ._impl.retry import \
+    RetryExecutor, \
+    RetryPolicy, \
+    ExceptionRetryPolicy
+
+__all__ = [
+    'RetryExecutor',
+    'RetryPolicy',
+    'ExceptionRetryPolicy',
+]
