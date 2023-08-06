37795b6d2a89a157e9ac1f069edb830a657f1a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:24 +0000

    Add package kitten/0.5.1/kitten-0.5.1.tar.gz

---
+++ packages/kitten/kitten-0.5.1.tar.gz/kitten-0.5.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="kitten",
+    version="0.5.1",
+    author="Chris Rehn",
+    author_email="chris@rehn.me",
+    description="Tiny multi-server automation tool.",
+    long_description=long_description,
+    url="https://github.com/hoffa/kitten",
+    packages=setuptools.find_packages(),
+    entry_points={"console_scripts": {"kitten=kitten:main"}},
+    install_requires=["boto3>=1.9", "fabric>=2.4"],
+    license="MIT",
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ),
+)
