8f74a2208878978c42ab0cb437141ce15967b502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:00 +0000

    Add package tbt/1.0.2/tbt-1.0.2.tar.gz

---
+++ packages/tbt/tbt-1.0.2.tar.gz/tbt-1.0.2/tbt/__init__.py
@@ -0,0 +1,18 @@
+def load_cookies_file(filename):
+	from http.cookiejar import Cookie, MozillaCookieJar
+	ns_cookiejar = MozillaCookieJar()
+	ns_cookiejar.load(filename, ignore_discard=True, ignore_expires=True)
+	return ns_cookiejar
+def load_cookies_string(string):
+	from http.cookiejar import Cookie, MozillaCookieJar
+	import os, random
+	cookieFile = "_cookie_"+str(random.randint(1000000,9999999))
+	
+	writeCookie = open(cookieFile,"w",encoding="utf8")
+	writeCookie.write(string)
+	writeCookie.close()
+	
+	ns_cookiejar = MozillaCookieJar()
+	ns_cookiejar.load(cookieFile, ignore_discard=True, ignore_expires=True)
+	os.remove(cookieFile)
+	return ns_cookiejar
