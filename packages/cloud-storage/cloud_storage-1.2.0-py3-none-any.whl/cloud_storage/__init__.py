567da3be689baa7002a30331e5d62532d9e17a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:20 +0000

    Add package cloud-storage/1.2.0/cloud_storage-1.2.0-py3-none-any.whl

---
+++ packages/cloud-storage/cloud_storage-1.2.0-py3-none-any.whl/cloud_storage/__init__.py
@@ -0,0 +1,8 @@
+def int_or_str(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+__version__ = '1.2.0'
+VERSION = tuple(map(int_or_str, __version__.split('.')))
