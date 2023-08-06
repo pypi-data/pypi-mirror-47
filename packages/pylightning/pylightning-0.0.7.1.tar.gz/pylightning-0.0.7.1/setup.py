139891e3585f19fe74e7620d43b772a3693ba016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:22 +0000

    Add package pylightning/0.0.7.1/pylightning-0.0.7.1.tar.gz

---
+++ packages/pylightning/pylightning-0.0.7.1.tar.gz/pylightning-0.0.7.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import lightning
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pylightning',
+      version=lightning.__version__,
+      description='Client library for lightningd',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/ElementsProject/lightning',
+      author='Christian Decker',
+      author_email='decker.christian@gmail.com',
+      license='MIT',
+      packages=['lightning'],
+      scripts=['lightning-pay'],
+      zip_safe=True)
