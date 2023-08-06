8268065e8d4aeff2263e204e7eb4b4a8b2e9cc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:19 +0000

    Add package CBP-SMF/0.0.7/CBP_SMF-0.0.7.tar.gz

---
+++ packages/CBP-SMF/CBP_SMF-0.0.7.tar.gz/CBP_SMF-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'CBP_SMF',
+    version = '0.0.7',
+    keywords = ('Data integration', 'multi-view NMF'),
+    description = 'A NMF-based method to discover biological pattern in multi-view data',
+    license = 'MIT License',
+
+    author = 'wuyue',
+    author_email = 'wuyue@stu.xidian.edu.cn',
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+	python_requires='>=2.7',
+
+    install_requires = [],
+)
