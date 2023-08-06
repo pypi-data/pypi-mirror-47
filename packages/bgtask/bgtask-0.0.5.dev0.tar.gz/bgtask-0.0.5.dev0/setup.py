58b9d8a084282c831f3beceda456df9df93d2206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:10 +0000

    Add package bgtask/0.0.5.dev0/bgtask-0.0.5.dev0.tar.gz

---
+++ packages/bgtask/bgtask-0.0.5.dev0.tar.gz/bgtask-0.0.5.dev0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.5'
+
+setup(name='bgtask',
+      version=version,
+      description="Simple, Fast, Redis based background task manager",
+      long_description="""\
+Simple, Fast, Redis based background task manager""",
+      classifiers=[],
+      keywords='task, redis, background worker muilti process',
+      author='Alexander.Li',
+      author_email='superpowerlee@gmail.com',
+      url='https://github.com/ipconfiger/bgtask',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'redis',
+          'errorbuster'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
