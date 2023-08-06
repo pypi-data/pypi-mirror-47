0bc7959f02c8d5a74b2902fad60f13cb4d0cca14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:44 +0000

    Add package jrfork-kegberry/0.0.5/jrfork-kegberry-0.0.5.tar.gz

---
+++ packages/jrfork-kegberry/jrfork-kegberry-0.0.5.tar.gz/jrfork-kegberry-0.0.5/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""Johnnyruzdev fork of the Kegberry Package
+
+Kegberry turns a Raspberry Pi into a keg monitor and digital taplist.
+
+Kegberry is based on Kegbot, and makes it easy to install and run a
+full keg monitoring solution on Raspberry Pi.  For more information, see the
+project home page at kegberry.com.
+
+Originally forked from version 2.1.1
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DOCLINES = __doc__.split('\n')
+
+SHORT_DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
+DEPENDENCIES = [
+  'python-gflags',
+]
+
+def setup_package():
+  setup(
+      name = 'jrfork-kegberry',
+      version = VERSION,
+      description = SHORT_DESCRIPTION,
+      long_description = LONG_DESCRIPTION,
+      author = 'johnnyruzdev',
+      author_email = 'johnnyruzdev01@outlook.com',
+      url = 'http://kegberry.kegbot.org/',
+      packages = find_packages(),
+      scripts = [
+        'bin/kegberry',
+      ],
+      install_requires = DEPENDENCIES,
+      include_package_data = True,
+  )
+
+if __name__ == '__main__':
+  setup_package()
