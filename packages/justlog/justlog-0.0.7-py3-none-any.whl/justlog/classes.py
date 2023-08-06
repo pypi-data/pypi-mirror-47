e1cfd4a3024d8e4391f58a769b4565fb7bbf18a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:53 +0000

    Add package justlog/0.0.7/justlog-0.0.7-py3-none-any.whl

---
+++ packages/justlog/justlog-0.0.7-py3-none-any.whl/justlog/classes.py
@@ -0,0 +1,19 @@
+from enum import IntEnum, Enum
+
+class Severity(IntEnum):
+    DBG = 1
+    INF = 2
+    WRN = 3
+    ERR = 4
+
+class Format(Enum):
+    JSON = 0
+    TEXT = 1
+
+class Output(Enum):
+    STDOUT = 0
+    STDERR = 1
+    FILE = 2
+    SYSLOG = 3
+    HTTP = 4
+    TCP = 5
