36bc88d4dd69ed71fb0182194776b780b8b8dc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:53 +0000

    Add package nbpages/0.0.1.dev3/nbpages-0.0.1.dev3.tar.gz

---
+++ packages/nbpages/nbpages-0.0.1.dev3.tar.gz/nbpages-0.0.1.dev3/nbpages/__main__.py
@@ -0,0 +1,20 @@
+
+from nbsetup import nbsetup
+from nbcollection import NbCollection
+
+def main():
+    nbsetup()
+    notebooks = NbCollection()
+    #notebooks.lint()
+    notebooks.write_headers()
+    notebooks.write_navbars()
+    notebooks.write_toc()
+    notebooks.write_keyword_index()
+    notebooks.write_readme()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
