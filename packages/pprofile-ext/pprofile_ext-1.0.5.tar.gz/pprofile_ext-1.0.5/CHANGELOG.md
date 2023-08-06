846763c28a03531b9c3448cf625982ae555c4116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:18 +0000

    Add package pprofile-ext/1.0.5/pprofile_ext-1.0.5.tar.gz

---
+++ packages/pprofile-ext/pprofile_ext-1.0.5.tar.gz/pprofile_ext-1.0.5/CHANGELOG.md
@@ -0,0 +1,24 @@
+# `pprofile_ext` changelog
+
+### version 1.0.5
+* parser fix to deal with extracting line-number from decorator reference
+
+### version 1.0.4
+* add python3 compatibility
+
+### version 1.0.2
+* fixing typo in setup.py
+
+### version 1.0.1
+* updates to setup.py to include .md and .txt files in distribution
+* adding MANIFEST.in file
+
+### version 1.0.0
+* releasing first pip
+
+### version 0.0.1
+* adding reverse call information and ability to drill down into call-graph from HTML
+
+### version 0.0.0
+* initial implementation and rudimentary test of an ipython extension that runs, processes, and displays
+  in HTML the output of the pprofile python performance profiler.
---
+++ packages/pprofile-ext/pprofile_ext-1.0.5.tar.gz/pprofile_ext-1.0.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+setup(
+    name='pprofile_ext',
+    version='1.0.5',
+    license='MIT',
+
+    author='Ronnie Hoogerwerf',
+    author_email='rohooger@gmail.com',
+    url='http://github.com/rohooger/pprofile_ext',
+
+    description='',
+    long_description='',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development',
+    ],
+
+    package_data={
+        '': ['*.md', '*.txt']
+    },
+
+    packages=[
+        'pprofile_ext',
+        'pprofile_ext.html',
+    ],
+
+    # dependencies
+    install_requires=[
+        'six',
+        'pprofile>=1.7.3',
+        'pygments>=2.0.0',
+    ]
+)
