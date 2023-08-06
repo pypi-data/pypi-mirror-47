2da775176c97ceff938b8c4f032df6aebc4786fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:03 +0000

    Add package pupy/2.15.2/pupy-2.15.2-py3-none-any.whl

---
+++ packages/pupy/pupy-2.15.2-py3-none-any.whl/pupy/ui.pyi
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+
+from typing import Any, Iterator, List
+
+def yesno(question: Any, default: bool = ..., tries: int = ...): ...
+def term_table(
+    strings: List[str], row_wise: bool = ..., filler: str = ...
+) -> Iterator[Any]: ...
