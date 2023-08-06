4bf64b6bf8e1695ce079c5d75830955173ae3318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:45 +0000

    Add package flashtorch/0.0.5/flashtorch-0.0.5.tar.gz

---
+++ packages/flashtorch/flashtorch-0.0.5.tar.gz/flashtorch-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+version = '0.0.5'
+
+setup(
+    name='flashtorch',
+    version=version,
+    author='Misa Ogura',
+    author_email='misa.ogura01@gmail.com',
+    description='Visualisation toolkit for neural networks in PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/MisaOgura/flashtorch',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'Pillow',
+        'torch',
+        'torchvision',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
