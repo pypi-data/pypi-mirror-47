2d7e546c040efa4b867336a415ce53df4752591f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:15 +0000

    Add package xyscript/0.2.4.9/xyscript-0.2.4.9.tar.gz

---
+++ packages/xyscript/xyscript-0.2.4.9.tar.gz/xyscript-0.2.4.9/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+from xyscript.config import Config
+def main():
+    print("进入入口主程序")
+    pass
+
+def test():
+    pass
+
+if __name__ == '__main__':
+    main()
+    # print(Config().get_version())
