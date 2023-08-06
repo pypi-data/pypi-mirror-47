b69ae529d87e1e6ec6a71874a10114075073c8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:34 +0000

    Add package pyderman/1.2.0/pyderman-1.2.0.tar.gz

---
+++ packages/pyderman/pyderman-1.2.0.tar.gz/pyderman-1.2.0/pyderman/test.py
@@ -0,0 +1,33 @@
+import os
+from pyderman import install, all_drivers
+import subprocess
+import unittest
+
+
+class TestDriverInstalls(unittest.TestCase):
+
+	def test_all_installs(self):
+		for driver in all_drivers:
+			print("Testing %s..." % driver.__name__)
+			data = install(browser=driver, verbose=True, chmod=True, overwrite=True, return_info=True)
+			path = data['path']
+			if not os.path.exists(path):
+				raise FileNotFoundError('The %s executable was not properly downloaded.' % driver.__name__)
+			output = subprocess.check_output([path, '--version']).decode('utf-8')
+			print('Version:', output)
+			self.assertIn(data['version'], output.lower(), "Driver %s did not output proper version! ('%s')" % (driver.__name__, data['version']))
+			print('%s is installed at: "%s"' % (data['driver'], path))
+			print('\n\n\n')
+
+
+if __name__ == "__main__":
+	unittest.main()
+
+
+"""
+TODO:
+	https://github.com/operasoftware/operachromiumdriver/releases
+	http://phantomjs.org/download.html
+		https://bitbucket.org/ariya/phantomjs/downloads/?tab=downloads
+		https://api.bitbucket.org/2.0/repositories/ariya/phantomjs/downloads/
+"""
