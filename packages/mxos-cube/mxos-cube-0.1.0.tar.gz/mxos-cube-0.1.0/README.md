a8c9ce5d751b7e2a02c2c7ef089e97e2c3b2080a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:45 +0000

    Add package mxos-cube/0.1.0/mxos-cube-0.1.0.tar.gz

---
+++ packages/mxos-cube/mxos-cube-0.1.0.tar.gz/mxos-cube-0.1.0/README.md
@@ -0,0 +1,3 @@
+## Introduction
+
+MXOS cube is the name of the MXCHIP MXOS command-line tool, packaged as `mxos-cube`. MXOS Cube enables Git-based version control, dependencies management, code publishing, support for remotely hosted repositories, use of the MXCHIP MXOS build system and export functions and other operations.
---
+++ packages/mxos-cube/mxos-cube-0.1.0.tar.gz/mxos-cube-0.1.0/setup.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2016 MXCHIP Limited, All Rights Reserved
+# SPDX-License-Identifier: Apache-2.0
+
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License.
+
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+# either express or implied.
+
+import os
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mxos-cube",
+    version="0.1.0",
+    description="MXCHIP MXOS command line tool for repositories version control, publishing and updating code from remotely hosted repositories, and invoking MXOS own build system and export functions, among other operations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/MXCHIP/mxos-cube',
+    author='MXCHIP',
+    author_email='yangsw@mxchip.com',
+    packages=["mxos"],
+    entry_points={
+        'console_scripts': [
+            'mxos=mxos.mxos:main',
+            'mxos-cube=mxos.mxos:main',
+        ]
+    },
+    python_requires='>=2.7.10,!=3.0.*,!=3.1.*,<4',
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
