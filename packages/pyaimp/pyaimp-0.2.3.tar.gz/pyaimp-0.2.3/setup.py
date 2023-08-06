57b8cbef4655f9a783afe61b66cb65a98ba35056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:40 +0000

    Add package pyaimp/0.2.3/pyaimp-0.2.3.tar.gz

---
+++ packages/pyaimp/pyaimp-0.2.3.tar.gz/pyaimp-0.2.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import pyaimp
+
+setup(
+    name='pyaimp',
+    version=pyaimp.__version__,
+    description='Python AIMP remote API wrapper with some extras',
+    long_description='Everything you need to know is located `here <https://epocdotfr.github.io/pyaimp/>`_.',
+    url='https://github.com/EpocDotFr/pyaimp',
+    author='Maxime "Epoc" G.',
+    author_email='contact.nospam@epoc.nospam.fr',
+    license='DBAD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: Microsoft :: Windows'
+    ],
+    keywords='aimp remote api wrapper client',
+    py_modules=['pyaimp'],
+    install_requires=[
+        'pypiwin32'
+    ],
+    download_url='https://github.com/EpocDotFr/pyaimp/archive/pyaimp-{version}.tar.gz'.format(version=pyaimp.__version__)
+)
