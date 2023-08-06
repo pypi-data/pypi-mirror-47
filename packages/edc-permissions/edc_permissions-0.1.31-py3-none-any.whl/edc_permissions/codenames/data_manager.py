570d87cbd910c92842c3ac47715ba500cc0232cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:55 +0000

    Add package edc-permissions/0.1.31/edc_permissions-0.1.31-py3-none-any.whl

---
+++ packages/edc-permissions/edc_permissions-0.1.31-py3-none-any.whl/edc_permissions/codenames/data_manager.py
@@ -0,0 +1,27 @@
+data_manager = [
+    "edc_metadata.add_crfmetadata",
+    "edc_metadata.add_requisitionmetadata",
+    "edc_metadata.change_crfmetadata",
+    "edc_metadata.change_requisitionmetadata",
+    "edc_metadata.delete_crfmetadata",
+    "edc_metadata.delete_requisitionmetadata",
+    "edc_metadata.view_crfmetadata",
+    "edc_metadata.view_requisitionmetadata",
+
+    "edc_navbar.nav_data_manager_section",
+
+    "edc_data_manager.add_dataquery",
+    "edc_data_manager.change_dataquery",
+    "edc_data_manager.delete_dataquery",
+    "edc_data_manager.view_historicaldataquery",
+
+    "edc_data_manager.view_datadictionary",
+    "edc_data_manager.view_historicaldatadictionary",
+
+    "edc_data_manager.view_queryvisitschedule",
+    "edc_data_manager.view_historicalqueryvisitschedule",
+
+    "edc_data_manager.view_queryuser",
+    "edc_data_manager.view_datamanageruser",
+
+]
---
+++ packages/edc-permissions/edc_permissions-0.1.31-py3-none-any.whl/edc_permissions/codenames/data_query.py
@@ -0,0 +1,19 @@
+data_query = [
+    "edc_metadata.view_crfmetadata",
+    "edc_metadata.view_requisitionmetadata",
+
+    "edc_navbar.nav_data_manager_section",
+
+    "edc_data_manager.change_dataquery",
+    "edc_data_manager.view_historicaldataquery",
+
+    "edc_data_manager.view_datadictionary",
+    "edc_data_manager.view_historicaldatadictionary",
+
+    "edc_data_manager.view_queryvisitschedule",
+    "edc_data_manager.view_historicalqueryvisitschedule",
+
+    "edc_data_manager.view_queryuser",
+    "edc_data_manager.view_datamanageruser",
+
+]
