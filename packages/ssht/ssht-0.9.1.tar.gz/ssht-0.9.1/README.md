a1a2975c9cec51e3c407b06ea22a51cdf93fec0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:55 +0000

    Add package ssht/0.9.1/ssht-0.9.1.tar.gz

---
+++ packages/ssht/ssht-0.9.1.tar.gz/ssht-0.9.1/README.md
@@ -0,0 +1,94 @@
+# ssht
+SSH client wrapper for easily connecting to hosts
+
+[![Build Status](https://travis-ci.org/hkraal/ssht.svg?branch=master)](https://travis-ci.org/hkraal/ssht)
+[![Coverage Status](https://coveralls.io/repos/github/hkraal/ssht/badge.svg?branch=master)](https://coveralls.io/github/hkraal/ssht?branch=master)
+[![Requirements Status](https://requires.io/github/hkraal/ssht/requirements.svg?branch=master)](https://requires.io/github/hkraal/ssht/requirements/?branch=master)
+![](https://img.shields.io/pypi/pyversions/ssht.svg?style=flat)
+
+This wrapper for the well known `ssh` client makes it possible to connect to the right server with just typing a part of the host name. The external sources are queried using the search term and those matching the string will be presented as an option.
+
+Current supported sources:
+
+* JSON file
+* MySQL database
+* JSON API endpoint
+
+# Installation
+
+Install ssht using pip:
+
+    pip install ssht
+
+or if you want the latest version:
+
+    pip install https://github.com/hkraal/ssht/archive/master.zip
+
+Create ssht folder in home directory
+
+    mkdir ~/.ssht
+
+Configure sources in ~/.ssht:
+
+**Define servers in JSON format**:
+
+    {
+    	"hosts": [
+    	  {
+    		"port": "2222",
+    		"hostname": "host01.example.com",
+    		"ipv4": "192.168.0.2",
+    		"user": "root"
+    	  },
+    	  {
+    		"port": "2222",
+    		"hostname": "host01.example.com",
+    		"ipv4": "192.168.0.2",
+    	  }
+    	]
+    }
+
+**Define servers in MySQL**:
+
+    {
+      "config": {
+        "host": "localhost",
+        "user": "username",
+        "password": "passeword",
+        "database": "infra"
+      },
+      "query": "SELECT hostname, port, ipv4, ipv6, user FROM servers"
+    }
+
+**Define JSON API endpoint**:
+
+	{
+		"config": {
+			"url": "http://ssht-api.dev",
+			"headers": {
+				"Authentication": "bla:huk"
+			}
+		}
+	}
+
+# Usage
+
+    ssht [-h] name [-4] [-6] 
+    
+    positional arguments:
+      name        name of the host to connect to
+    
+    optional arguments:
+      -h, --help  show this help message and exit
+      -4          connect using ipv4 (skip dns if ipv4 address is defined)
+      -6          connect using ipv6 (skip dns of ipv6 address is defined)
+
+Example of a connection
+
+    $ ssht host01
+    1) root@host01.exmaple.com
+    2) host01.exmaple.com
+    Connect to: 1
+    Connecting to "host01.example.com"
+    root@host01:~$
+
---
+++ packages/ssht/ssht-0.9.1.tar.gz/ssht-0.9.1/ssht/ssht.py
@@ -0,0 +1,102 @@
+'''
+Created on 17 Dec 2016
+
+@author: Henk Kraal
+'''
+import argparse
+import logging
+import os
+import shlex
+import subprocess
+import sys
+
+from .plugins import JsonParser, MySQLParser, APIParser
+
+
+def ssh_connect(host, args):
+    logging.debug('args = {0}'.format(args))
+
+    # Connect to IPv6 if forced
+    if hasattr(host, 'ipv6') and host.ipv6 and '-6' in args:
+        ssh_cmds = shlex.split('ssh {0}'.format(host.ipv6))
+        print('Connecting to "{0}" ({1})'.format(host.hostname, host.ipv6))
+    # Connect to IPv4 if specified
+    elif hasattr(host, 'ipv4') and host.ipv4:
+        ssh_cmds = shlex.split('ssh {0}'.format(host.ipv4))
+        print('Connecting to "{0}" ({1})'.format(host.hostname, host.ipv4))
+    # Connect on host name
+    else:
+        ssh_cmds = shlex.split('ssh {0}'.format(host.hostname))
+        print('Connecting to "{0}"'.format(host.hostname))
+    logging.debug('ssh_cmds = {0}'.format(ssh_cmds))
+
+    # Connect to host port
+    if hasattr(host, 'port') and host.port is not None and '-p' not in args:
+        ssh_cmds += shlex.split('-p {0}'.format(host.port))
+
+    # Connect as user
+    if hasattr(host, 'user') and host.user is not None and '-l' not in args:
+        ssh_cmds += shlex.split('-l {0}'.format(host.user))
+
+    logging.debug('ssh_cmds = {0}'.format(ssh_cmds + args))
+    subprocess.call(ssh_cmds + args)
+
+
+def select_host(hosts):
+    for idx, host in enumerate(hosts):
+        if hasattr(host, 'user') and host.user is not None:
+            print('{0}) {1}@{2}'.format(idx + 1, host.user, host.hostname))
+        else:
+            print('{0}) {1}'.format(idx + 1, host.hostname))
+    option = get_answer('Connect to: ')
+    try:
+        return hosts[int(option) - 1]
+    except ValueError as ex:
+        return
+
+
+def get_answer(text):   # pragma: nocover
+    try:
+        return input(text)
+    except SyntaxError:
+        return ''
+
+
+def get_log_level():
+    if os.getenv('SSHT_DEBUG', None):
+        return logging.DEBUG
+    return logging.WARNING
+
+
+def main():     # pragma: nocover
+    try:
+        logging.basicConfig(level=get_log_level())
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("name", help="name of the host to connect to")
+        args, unknown = parser.parse_known_args()
+
+        home_dir = os.path.expanduser('~')
+        jsonparser = JsonParser(os.path.join(home_dir, '.ssht'))
+        mysqlparser = MySQLParser(os.path.join(home_dir, '.ssht'))
+        apiparser = APIParser(os.path.join(home_dir, '.ssht'))
+
+        hosts = jsonparser.search(args.name)
+        hosts.extend(mysqlparser.search(args.name))
+        hosts.extend(apiparser.search(args.name))
+        logging.info(hosts)
+
+        host = None
+        if len(hosts) == 1:
+            host = hosts[0]
+        elif len(hosts) > 1:
+            host = select_host(hosts)
+
+        if host is None:
+            print('No host, exiting.')
+            sys.exit(0)
+
+        ssh_connect(host, unknown)
+    except KeyboardInterrupt as ex:
+        print()
+        sys.exit(0)
