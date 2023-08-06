005e957a328010a32bfdc5c3f47c3ba68d4c6b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:41 +0000

    Add package disjoint-set/0.6.1/disjoint_set-0.6.1.tar.gz

---
+++ packages/disjoint-set/disjoint_set-0.6.1.tar.gz/disjoint_set-0.6.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='disjoint_set',
+    version='0.6.1',
+    packages=setuptools.find_packages(),
+    description='Disjoint set data structure implementation for Python',
+    url='https://github.com/mrapacz/disjoint_set',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    maintainer="Maciej Rapacz",
+    maintainer_email="python.disjointset@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
