7a15176b153e221e3030837851347cff5a5d888b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:51 +0000

    Add package lowsigma/0.0.5/lowsigma-0.0.5.tar.gz

---
+++ packages/lowsigma/lowsigma-0.0.5.tar.gz/lowsigma-0.0.5/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="lowsigma",
+    version="0.0.5",
+    description="Option Pricing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
