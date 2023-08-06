dd97367ab19bcd8f8d11999576a81137003d295a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:34 +0000

    Add package cutter-ng/2.5/cutter_ng-2.5-py3-none-any.whl

---
+++ packages/cutter-ng/cutter_ng-2.5-py3-none-any.whl/Cutter/__init__.py
@@ -0,0 +1,9 @@
+from .Cutter import Cutter
+from .Cutter import profiles
+from .Cutter import WHITESPACE_TOKENS
+from .Cutter import EMPTY_TOKENS 
+from .Cutter import TOKENIZATION_TAGS
+from .Cutter import WHITESPACE_TAG
+
+name = 'Cutter'
+version = '2.5'
