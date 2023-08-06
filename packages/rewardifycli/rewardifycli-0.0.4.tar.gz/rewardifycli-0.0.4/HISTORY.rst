5537541b5a4673aaaca4ae9795faa7ad0d57ff91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:38 +0000

    Add package rewardifycli/0.0.4/rewardifycli-0.0.4.tar.gz

---
+++ packages/rewardifycli/rewardifycli-0.0.4.tar.gz/rewardifycli-0.0.4/HISTORY.rst
@@ -0,0 +1,24 @@
+=======
+History
+=======
+
+0.0.0.0 (2019-06-14)
+--------------------
+
+* First release on PyPI.
+
+0.0.1 (2019-06-16)
+------------------
+
+* Initial test version
+
+0.0.2 (2019-06-16)
+------------------
+
+* Fixed the bug of the "__internal" package not being included in the pypi release
+
+0.0.3 (2019-06-16)
+------------------
+
+* Fixed the "run" command not being associated with the "install" command group
+
---
+++ packages/rewardifycli/rewardifycli-0.0.4.tar.gz/rewardifycli-0.0.4/rewardifycli/main.py
@@ -0,0 +1,28 @@
+# third party
+import click
+
+from rewardify.env import EnvironmentConfig
+
+# local
+from rewardifycli.install import install
+from rewardifycli.packs import packs
+from rewardifycli.rewards import rewards
+from rewardifycli.inventory import inventory
+from rewardifycli.users import users
+
+
+@click.group(name='rewardify')
+def cli():
+    environment_config: EnvironmentConfig = EnvironmentConfig.instance()
+    environment_config.load()
+
+
+cli.add_command(install)
+cli.add_command(packs)
+cli.add_command(rewards)
+cli.add_command(inventory)
+cli.add_command(users)
+
+
+if __name__ == '__main__':
+    cli()
