afe9106627eca2c420d9f1899a0142183d131dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:20 +0000

    Add package odds-client/0.16/odds_client-0.16-py3-none-any.whl

---
+++ packages/odds-client/odds_client-0.16-py3-none-any.whl/odds_client/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from sports_client_factory import SportsClientFactory
+from sports_client_nba import SportsClientNBA
+
+name = 'odds_client'
