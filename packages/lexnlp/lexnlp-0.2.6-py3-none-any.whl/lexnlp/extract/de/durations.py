45599a37a47b55a657c3581c0be2d34d2ee614a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package lexnlp/0.2.6/lexnlp-0.2.6-py3-none-any.whl

---
+++ packages/lexnlp/lexnlp-0.2.6-py3-none-any.whl/lexnlp/extract/de/durations.py
@@ -0,0 +1,102 @@
+import regex as re
+from typing import Generator
+
+from lexnlp.extract.de.amounts import AmountParserDE
+
+
+__author__ = "ContraxSuite, LLC; LexPredict, LLC"
+__copyright__ = "Copyright 2015-2019, ContraxSuite, LLC"
+__license__ = "https://github.com/LexPredict/lexpredict-lexnlp/blob/master/LICENSE"
+__version__ = "0.2.6"
+__maintainer__ = "LexPredict, LLC"
+__email__ = "support@contraxsuite.com"
+
+
+amounts_parser = AmountParserDE()
+get_amounts = amounts_parser.parse
+
+
+DURATION_MAP = {
+    "second": 1 / (60 * 60 * 24),
+    "minute": 1 / (60 * 24),
+    "hour": 1 / 24,
+    "day": 1,
+    "week": 7,
+    "month": 30,  # 365.25/12.,
+    "quarter": 365 / 4,
+    "year": 365,  # 365.25,
+}
+
+DURATION_TRANSLATION_MAP = {
+    "secunde": 'second',
+    "secunden": 'second',
+    "minute": 'minute',
+    "minuten": 'minute',
+    "stunde": 'hour',
+    "stunden": 'hour',
+    "tag": 'day',
+    "tage": 'day',
+    "woche": 'week',
+    "wochen": 'week',
+    "monat": 'month',
+    "monate": 'month',
+    "vierteljahr": 'quarter',
+    "vierteljahre": 'quarter',
+    "jahr": 'year',
+    "jahre": 'year',
+    "jahres": 'year',
+    "jahren": 'year',
+}
+duration_items = sorted(DURATION_TRANSLATION_MAP.keys(), key=len, reverse=True)
+duration_items_joined = '|'.join(duration_items)
+DURATION_MAP_RE = re.compile(duration_items_joined)
+
+DURATION_PTN = r"""
+(?P<text>(?P<num_text>{num_ptn})?
+(?P<unit_prefix>(?:kalend[ae]r|lebens|actual))?
+(?P<unit_name>{unit_names}))
+(?:\W|$)
+""".format(num_ptn=amounts_parser.NUM_PTN, unit_names=duration_items_joined)
+
+DURATION_PTN_RE = re.compile(DURATION_PTN, re.IGNORECASE | re.MULTILINE | re.DOTALL | re.VERBOSE)
+
+
+def get_durations(text, float_digits=4) -> Generator:
+    """
+    Get duration usages within text.
+    :param text:
+    :param float_digits:
+    :return:
+    """
+    for match in DURATION_PTN_RE.finditer(text):
+        capture = match.capturesdict()
+        amount_text = ''.join(capture.get('num_text', ''))
+        amount = list(get_amounts(amount_text, float_digits=float_digits))
+        if len(amount) != 1:
+            amount = 1
+        else:
+            amount = amount[0]
+        unit_name_local = ''.join(capture.get('unit_name', '')).lower()
+        unit_prefix = ''.join(capture.get('unit_prefix', '')).lower()
+        unit_name_local = DURATION_MAP_RE.findall(unit_name_local)
+        if not unit_name_local:
+            continue
+        unit_name_local = unit_name_local[0]
+        unit_name_en = DURATION_TRANSLATION_MAP.get(unit_name_local)
+
+        amount_days = DURATION_MAP[unit_name_en] * amount
+        if float_digits:
+            amount_days = round(amount_days, float_digits)
+        yield dict(
+                location_start=match.start(),
+                location_end=match.end(),
+                source_text=''.join(capture.get('text', '')),
+                unit_name_local=unit_name_local,
+                unit_name=unit_name_en,
+                unit_prefix=unit_prefix,
+                amount=amount,
+                amount_days=amount_days)
+
+
+def get_duration_list(*args, **kwargs):
+    return list(get_durations(*args, **kwargs))
---
+++ packages/lexnlp/lexnlp-0.2.6-py3-none-any.whl/lexnlp/extract/en/courts.py
@@ -0,0 +1,82 @@
+"""Court extraction for English.
+
+This module implements extraction functionality for courts in English, including formal names, abbreviations,
+and aliases.
+
+Todo:
+  * Add utilities for loading court data
+"""
+# pylint: disable=unused-argument
+
+from typing import List, Tuple, Generator, Any
+
+from lexnlp.extract.common.annotations.court_annotation import CourtAnnotation
+from lexnlp.extract.en.dict_entities import find_dict_entities, conflicts_take_first_by_id
+
+import os
+import re
+
+from lexnlp.extract.common.universal_court_parser import UniversalCourtsParser, ParserInitParams
+from lexnlp.extract.en.en_language_tokens import EnLanguageTokens
+from lexnlp.utils.lines_processing.line_processor import LineSplitParams
+
+__author__ = "ContraxSuite, LLC; LexPredict, LLC"
+__copyright__ = "Copyright 2015-2019, ContraxSuite, LLC"
+__license__ = "https://github.com/LexPredict/lexpredict-lexnlp/blob/master/LICENSE"
+__version__ = "0.2.6"
+__maintainer__ = "LexPredict, LLC"
+__email__ = "support@contraxsuite.com"
+
+
+def get_courts(text: str,
+               court_config_list: List[Tuple[int, str, int, List[Tuple[str, str, bool, int]]]],
+               priority: bool = False,
+               text_languages: List[str] = None) -> Generator[Tuple[Tuple, Tuple], Any, Any]:
+    """
+    Searches for courts from the provided config list and yields tuples of (court_config, court_alias).
+    Court config is: (court_id, court_name, [list of aliases])
+    Alias is: (alias_text, language, is_abbrev, alias_id)
+
+    This method uses general searching routines for dictionary entities from dict_entities.py module.
+    Methods of dict_entities module can be used for comfortable creating the config: entity_config(),
+    entity_alias(), add_aliases_to_entity().
+    :param text:
+    :param court_config_list: List list of all possible known courts in the form of tuples:
+     (id, name, [(alias, lang, is_abbrev], ...).
+    :param return_source:
+    :param priority: If two courts found with the totally equal matching aliases - then use the one with the lowest id.
+    :param text_languages: Language(s) of the source text. If a language is specified then only aliases of this
+    language will be searched for. For example: this allows ignoring "Island" - a German language
+     alias of Iceland for English texts.
+    :return: Generates tuples: (court entity, court alias)
+    """
+    yield from find_dict_entities(text, court_config_list,
+                                  conflict_resolving_func=conflicts_take_first_by_id if priority else None,
+                                  text_languages=text_languages)
+
+
+def setup_en_parser():
+    ptrs = ParserInitParams()
+    ptrs.dataframe_paths = [os.path.join(os.path.dirname(__file__), "../../config/en/us_state_courts.csv"),
+             os.path.join(os.path.dirname(__file__), "../../config/en/us_courts.csv"),
+             os.path.join(os.path.dirname(__file__), "../../config/en/ca_courts.csv"),
+             os.path.join(os.path.dirname(__file__), "../../config/en/au_courts.csv")]
+    ptrs.split_ptrs = LineSplitParams()
+    ptrs.split_ptrs.line_breaks = {'\n', '.', ';', ','}.union(set(EnLanguageTokens.conjunctions))
+    ptrs.split_ptrs.abbreviations = EnLanguageTokens.abbreviations
+    ptrs.split_ptrs.abbr_ignore_case = True
+    ptrs.court_pattern_checker = re.compile('court', re.IGNORECASE)
+    return UniversalCourtsParser(ptrs)
+
+
+parser = setup_en_parser()
+
+
+def _get_court_list(text: str, language: str = None) -> List[CourtAnnotation]:
+    return parser.parse(text, language if language else 'en')
+
+
+def _get_courts(text: str, language: str = None) -> Generator[dict, None, None]:
+    courts = parser.parse(text, language if language else 'en')
+    for c in courts:
+        yield c.to_dictionary()
