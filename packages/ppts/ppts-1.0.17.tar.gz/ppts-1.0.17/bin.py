498576a14ab03ad3a470c305c32844d83cf5ac75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:21 +0000

    Add package ppts/1.0.17/ppts-1.0.17.tar.gz

---
+++ packages/ppts/ppts-1.0.17.tar.gz/ppts-1.0.17/bin.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import subprocess
+from flask import Flask
+from flask import jsonify
+from .parse import parseUser, parsePages
+
+app = Flask(__name__)
+filename = os.path.abspath(sys.argv[1])
+
+with open(filename, 'r') as file:
+    text = file.read()
+
+userinfo = parseUser(text)
+pages = parsePages(text)
+
+
+@app.route("/")
+def user():
+    return str(pages)
+
+
+def startWeb():
+    subprocess.call(["yarn", "start"])
+
+
+def main():
+    app.run()
+    startWeb()
+
+
+if __name__ == "__main__":
+    main()
+    startWeb()
