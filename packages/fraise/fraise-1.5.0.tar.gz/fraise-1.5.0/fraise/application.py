50da2ab43bf257ba4d3db5ab318a6488b12d47f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:46 +0000

    Add package fraise/1.5.0/fraise-1.5.0.tar.gz

---
+++ packages/fraise/fraise-1.5.0.tar.gz/fraise-1.5.0/fraise/application.py
@@ -0,0 +1,38 @@
+import argparse
+
+from . import phrases
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Generate memorable passphrases")
+    parser.add_argument('--max-word-length',
+                        default=8,
+                        dest='max_word_length',
+                        help="The maximum length of each word (default 8)",
+                        type=int)
+    parser.add_argument('--minimum-length',
+                        default=16,
+                        dest='minimum_length',
+                        help="Minimum length of the phrase (default 16)",
+                        type=int)
+    parser.add_argument('--separator',
+                        default=' ',
+                        dest='separator',
+                        help="What to put inbetween the words (default space)"
+                        )
+    parser.add_argument('--word-count',
+                        default=4,
+                        dest='word_count',
+                        help="How many words to include in the phrase (default 4)",
+                        type=int)
+    return parser.parse_args()
+
+
+def run():
+    args = get_args()
+    print(phrases.generate(
+        max_word_length=args.max_word_length,
+        minimum_length=args.minimum_length,
+        separator=args.separator,
+        word_count=args.word_count
+    ))
---
+++ packages/fraise/fraise-1.5.0.tar.gz/fraise-1.5.0/setup.py
@@ -0,0 +1,16 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(name='fraise',
+      version='1.5.0',
+      description='Generate memorable pass phrases',
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+      long_description_content_type="text/markdown",
+      url='http://github.com/daveygit2050/fraise',
+      author='Dave Randall',
+      author_email='dave@goldsquare.co.uk',
+      license='Apache v2',
+      packages=find_packages(exclude=('tests', 'docs', 'target')),
+      zip_safe=False,
+      entry_points={'console_scripts': ['fraise=fraise.application:run']})
