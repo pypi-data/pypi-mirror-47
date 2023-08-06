dc044644025e37c15c67774c00e306a7814129d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:56 +0000

    Add package Python-video-annotator-module-path-map/0.5/Python%20video%20annotator%20-%20module%20-%20path%20map-0.5.tar.gz

---
+++ packages/Python-video-annotator-module-path-map/Python%20video%20annotator%20-%20module%20-%20path%20map-0.5.tar.gz/Python video annotator - module - path map-0.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+setup(
+	name='Python video annotator - module - path map',
+	version="0.5",
+	description="""""",
+	author=['Ricardo Ribeiro'],
+	author_email='ricardojvr@gmail.com',
+	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-pathmap',
+	long_description = long_description,
+    long_description_content_type = 'text/markdown',
+	packages=find_packages(),	
+)
