0e05605d323fbc79f8fa29be48af130d8964a840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:27 +0000

    Add package file-tools/0.0.5/file_tools-0.0.5-py3-none-any.whl

---
+++ packages/file-tools/file_tools-0.0.5-py3-none-any.whl/file_tools/utils/functions.py
@@ -0,0 +1,20 @@
+import os
+import inspect
+
+#: Get Filename with Context
+
+def get_file(file, relative=False):
+	if not relative:
+		return f'{os.getcwd()}/{file}'
+	else:
+		return f'{get_caller_path()}/{file}'
+
+#: Get Path of Caller
+
+def get_caller_path():
+	for item in inspect.stack():
+		if item and __file__ not in item and item.function='<module>':
+			if item.index == 0:
+				return os.path.dirname(os.path.abspath(item.filename))
+
+#::: END PROGRAM :::
