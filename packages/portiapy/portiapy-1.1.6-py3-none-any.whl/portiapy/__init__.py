e82f30dd8db63de3b0b6bb819af9bc21f71c46a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:39 +0000

    Add package portiapy/1.1.6/portiapy-1.1.6-py3-none-any.whl

---
+++ packages/portiapy/portiapy-1.1.6-py3-none-any.whl/portiapy/__init__.py
@@ -0,0 +1,20 @@
+#####################################    
+#             PortiaPy              #
+#####################################
+
+# Variables
+name = "portiapy"
+
+portiaConfigExample = {
+	'baseurl': 'http://localhost',
+	'authorization': '<your API key>',
+	'debug': False,
+	'Accept': 'text/csv'
+}
+
+# Functions
+def about():
+    print("portiapy - an Agriness Edge project")
+
+def version():
+	print("portiapy - v1.1.6")
