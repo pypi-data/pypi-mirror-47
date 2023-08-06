d19d3dfad51c755cf5b96111c4c9228a5e80c847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:37 +0000

    Add package arcana/0.4.9/arcana-0.4.9.tar.gz

---
+++ packages/arcana/arcana-0.4.9.tar.gz/arcana-0.4.9/arcana/__about__.py
@@ -0,0 +1,18 @@
+
+__version__ = '0.4.9'
+
+__authors__ = [
+    ("Thomas G. Close", "tom.g.close@gmail.com"),
+    ("Francesco Sforazzini", "francesco.sforazzini@gmail.com"),
+    ("Phillip G. D. Ward", "phillipgdward@gmail.com")]
+
+install_requires = [
+    'xnat>=0.3.17',
+    'nipype>=1.1.7',
+    'pydicom>=1.0.2',
+    'networkx>=2.2',
+    'fasteners>=0.7.0',
+    'future>=0.16.0',
+    'pybids>=0.5.1',
+    'contextlib2>=0.5.5',
+    'deepdiff>=3.3']
