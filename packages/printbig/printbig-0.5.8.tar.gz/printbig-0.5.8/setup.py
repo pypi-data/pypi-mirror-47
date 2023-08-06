b7ddbc333114747c96e7083cc99caf6951c35697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:24 +0000

    Add package printbig/0.5.8/printbig-0.5.8.tar.gz

---
+++ packages/printbig/printbig-0.5.8.tar.gz/printbig-0.5.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings
+    name='printbig',
+    url='https://github.com/AlexAndrei98/testpackage',
+    author='Alex Andrei',
+    author_email='alexandrei1998@hotmail.it',
+    # Needed to actually package something
+    packages=['printbig'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.5.8',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.txt').read(),
+    # if there are any scripts
+)
