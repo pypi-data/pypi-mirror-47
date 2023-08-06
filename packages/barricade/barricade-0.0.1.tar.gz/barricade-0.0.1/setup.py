b6a0d6374914295441e40eeefb0114005497c950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:34 +0000

    Add package barricade/0.0.1/barricade-0.0.1.tar.gz

---
+++ packages/barricade/barricade-0.0.1.tar.gz/barricade-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='barricade',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='nanubau',
+    author_email='purav237@gmail.com',
+    description='Serializer and Validator '
+)
