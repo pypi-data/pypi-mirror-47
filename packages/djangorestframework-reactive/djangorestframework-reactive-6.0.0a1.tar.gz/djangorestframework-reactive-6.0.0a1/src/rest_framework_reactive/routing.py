e0dc43da38f308cda54384877e338837787a0bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:24 +0000

    Add package djangorestframework-reactive/6.0.0a1/djangorestframework-reactive-6.0.0a1.tar.gz

---
+++ packages/djangorestframework-reactive/djangorestframework-reactive-6.0.0a1.tar.gz/djangorestframework-reactive-6.0.0a1/src/rest_framework_reactive/routing.py
@@ -0,0 +1,22 @@
+from channels.routing import ChannelNameRouter, ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+
+from .consumers import ClientConsumer, MainConsumer, WorkerConsumer
+from .protocol import CHANNEL_MAIN, CHANNEL_WORKER
+
+application = ProtocolTypeRouter(
+    {
+        # Client-facing consumers.
+        'websocket': URLRouter(
+            [
+                # To change the prefix, you can import ClientConsumer in your custom
+                # Channels routing definitions instead of using these defaults.
+                url(r'^ws/(?P<subscriber_id>.+)$', ClientConsumer)
+            ]
+        ),
+        # Background worker consumers.
+        'channel': ChannelNameRouter(
+            {CHANNEL_MAIN: MainConsumer, CHANNEL_WORKER: WorkerConsumer}
+        ),
+    }
+)
