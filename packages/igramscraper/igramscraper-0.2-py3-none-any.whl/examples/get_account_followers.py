c9195f7e020f64d5d6d6310301dd2e0f98c951d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:10 +0000

    Add package igramscraper/0.2/igramscraper-0.2-py3-none-any.whl

---
+++ packages/igramscraper/igramscraper-0.2-py3-none-any.whl/examples/get_account_followers.py
@@ -0,0 +1,15 @@
+#TODO does not work currently instagram changed api
+from time import sleep
+from context import Instagram # pylint: disable=no-name-in-module
+
+instagram = Instagram.with_credentials('username', 'password', 'path/to/cache/folder')
+instagram.login()
+
+sleep(2) # Delay to mimic user
+
+username = 'kevin'
+followers = []
+account = instagram.get_account(username)
+sleep(1)
+followers = instagram.get_followers(account.identifier, 1000, 100, True) # Get 1000 followers of 'kevin', 100 a time with random delay between requests
+print(followers)
