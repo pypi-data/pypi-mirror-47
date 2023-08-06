d3beef7301262a9689c9d10372766720625e60f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:18 +0000

    Add package isharp/0.105/isharp-0.105.tar.gz

---
+++ packages/isharp/isharp-0.105.tar.gz/isharp-0.105/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+  name = 'isharp',
+  packages = find_packages(),
+  version = '0.105',
+  description = 'Algorithmic workbench',
+  author = 'jeremycward',
+  author_email = 'jeremycward@yahoo.co.uk',
+  url = 'https://github.com/jeremycward/isharp-core',
+  download_url = 'https://github.com/jeremycward/isharp-core/tarball/0.1',
+  keywords = ['string', 'reverse'],
+  classifiers = [],
+)
