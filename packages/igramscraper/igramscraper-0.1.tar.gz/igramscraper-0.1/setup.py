7aaa4fc1facbc1e6d73317fa70e194e42e3987a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:08 +0000

    Add package igramscraper/0.1/igramscraper-0.1.tar.gz

---
+++ packages/igramscraper/igramscraper-0.1.tar.gz/igramscraper-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="igramscraper",
+    version=0.1,
+    description=('scrapes medias, likes, followers, tags and all metadata'),
+    long_description=('Interacts with Instagrams private API through http requests only, perfect for multithreading, scrapes medias, likes, followers and all metadata. Also can be used to like, unlike, follow, unfollow, comment, uncomment.'),
+    packages=setuptools.find_packages(),
+    license="MIT",
+    maintainer="realsirjoe"
+)
