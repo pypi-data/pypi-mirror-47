90ac336b34f678f0c1e504cfdf9576fc3d53e8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:23 +0000

    Add package jasdistributions/0.1/jasdistributions-0.1.tar.gz

---
+++ packages/jasdistributions/jasdistributions-0.1.tar.gz/jasdistributions-0.1/README.md
@@ -0,0 +1,5 @@
+# Something something 
+Summary 
+
+# Something 
+explanation of file 
---
+++ packages/jasdistributions/jasdistributions-0.1.tar.gz/jasdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jasdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jasdistributions'],
+      zip_safe=False)
