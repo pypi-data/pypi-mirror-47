41d60bacc3d50d603adeb903de9022ea117201e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:27 +0000

    Add package django-mapstore-adapter/1.0.1/django_mapstore_adapter-1.0.1-py2.py3-none-any.whl

---
+++ packages/django-mapstore-adapter/django_mapstore_adapter-1.0.1-py2.py3-none-any.whl/mapstore2_adapter/apps.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright 2018, GeoSolutions Sas.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#########################################################################
+
+from django.apps import AppConfig as BaseAppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class AppConfig(BaseAppConfig):
+
+    name = "mapstore2_adapter"
+    label = "mapstore2_adapter"
+    verbose_name = _("Django MapStore2 Adapter")
+
+    def ready(self):
+        """Connect relevant signals to their corresponding handlers"""
+        """
+            NO SIGNALS DEFINED YET
+        """
+        super(AppConfig, self).ready()
