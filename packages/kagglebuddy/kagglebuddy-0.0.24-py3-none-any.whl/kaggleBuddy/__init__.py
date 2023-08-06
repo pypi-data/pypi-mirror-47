40d4d233d9c1d9fa3f37b178a1da73c15f9ecbb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:10 +0000

    Add package kagglebuddy/0.0.24/kagglebuddy-0.0.24-py3-none-any.whl

---
+++ packages/kagglebuddy/kagglebuddy-0.0.24-py3-none-any.whl/kaggleBuddy/__init__.py
@@ -0,0 +1,9 @@
+from . import utils
+from . import data_exploration
+from . import ensemble
+from . import preprocessing
+from . import nn
+
+name = "kaggleBuddy"
+__author__ = "kun"
+__version__ = "0.0.24"
