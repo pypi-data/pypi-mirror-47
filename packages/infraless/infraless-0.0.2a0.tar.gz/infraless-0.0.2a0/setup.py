38c4a7ebb9493816410f8be977fe486137551417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:29 +0000

    Add package infraless/0.0.2a0/infraless-0.0.2a0.tar.gz

---
+++ packages/infraless/infraless-0.0.2a0.tar.gz/infraless-0.0.2a0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    LONG_DESCRIPTION = readme_file.read()
+
+with open('requirements.txt', 'r') as reqs:
+    requirements = reqs.read()
+
+setup(
+    name='infraless',
+    version='0.0.2-alpha',
+    description='Run your code at scale without worrying about the cloud',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Pulkit Kumar',
+    author_email='ispulkitkr@gmail.com',
+    url='https://github.com/ispulkit',
+    license='unlicensed',
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_data={'infraless': ['templates/*']},
+    include_package_data=True,
+    install_requires=[
+        requirements,
+    ],
+    entry_points="""
+        [console_scripts]
+        infraless = infraless.cli_router:main
+    """,
+)
