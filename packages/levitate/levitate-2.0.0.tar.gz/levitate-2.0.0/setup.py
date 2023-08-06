94cc294671c9397489925cba3215d2917d38c078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:57 +0000

    Add package levitate/2.0.0/levitate-2.0.0.tar.gz

---
+++ packages/levitate/levitate-2.0.0.tar.gz/levitate-2.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+__version__ = 'unknown'
+for line in open('levitate/__init__.py'):
+    if line.startswith('__version__'):
+        exec(line)
+        break
+
+
+setup(
+    name='levitate',
+    version=__version__,
+    description='Python implementations from the Levitate research project',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/AppliedAcousticsChalmers/levitate',
+    author='Carl Andersson',
+    author_email='carl.andersson@chalmers.se',
+    license='MIT',
+    packages=find_packages('.'),
+    python_requires='>=3.5',
+    install_requires=[
+        'numpy',
+        'scipy'],
+    tests_require=['pytest', 'pytest-cov'],
+    setup_requires=['pytest-runner'],
+    include_package_data=True,
+)
