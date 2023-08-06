e49e94589034120bd123585de53537412f6c7336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:28 +0000

    Add package import-nbmodule/0.0.1/import_nbmodule-0.0.1.tar.gz

---
+++ packages/import-nbmodule/import_nbmodule-0.0.1.tar.gz/import_nbmodule-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(name                  		=   'import_nbmodule',
+      version               		=   '0.0.1',
+      description           		=   'import jupyter notebook .ipynb module',
+      long_description      		=   long_description,
+      long_description_content_type	=	"text/markdown",
+      author 						= 	"Lingling Yang",
+      author_email 					= 	"yll0620@gmail.com",
+      py_modules            		=   ['import_nbmodule'],
+      packages 						= 	find_packages(),
+      # the project's homepage
+      url                   		=   'https://github.com/yangll0620/import_nbmodule',
+      classifiers					= [
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      ],
+)
