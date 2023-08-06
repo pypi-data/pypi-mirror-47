0c75e105d1d163babd1a561055e78cb2f0943e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:39 +0000

    Add package hcorex/0.0.0.5/hcorex-0.0.0.5-py3-none-any.whl

---
+++ packages/hcorex/hcorex-0.0.0.5-py3-none-any.whl/hcorex/__init__.py
@@ -0,0 +1,9 @@
+"""
+    hcorex
+    ~~~~~
+    A framework based on python.  It's extensively documented
+    and follows best practice patterns.
+    :copyright: gs.
+    :license: BSD, see LICENSE for more details.
+"""
+from .hcorex import *
