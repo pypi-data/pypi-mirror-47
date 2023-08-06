7638dd3f10fa181cedb95c4f9baa3de348ff473d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:44 +0000

    Add package peek-admin/1.3.2/peek-admin-1.3.2.tar.gz

---
+++ packages/peek-admin/peek-admin-1.3.2.tar.gz/peek-admin-1.3.2/peek_admin/build-web/src/plugin-root-modules.ts
@@ -0,0 +1,6 @@
+// This file is auto generated, the git version is blank and .gitignored
+import {PluginRootServicePersistentLoadModule} 
+                        from "./plugin-root-services";
+export const pluginRootModules = [
+	PluginRootServicePersistentLoadModule
+];
