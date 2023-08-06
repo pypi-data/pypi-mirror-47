fa703a7cd59f8628328079b1a8688c5db4158ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:51 +0000

    Add package moderngl/5.5.2/moderngl-5.5.2.tar.gz

---
+++ packages/moderngl/moderngl-5.5.2.tar.gz/moderngl-5.5.2/moderngl/mgl/extensions.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "mgl.hpp"
+
+struct MGLContext;
+
+PyObject * meth_extensions(PyObject * self, PyObject * context);
+PyObject * meth_hwinfo(PyObject * self, PyObject * context);
