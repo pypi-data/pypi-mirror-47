7c6156e2709e2b6035297fb3f30aa2a217903b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:35 +0000

    Add package imgextract/0.0.1/imgextract-0.0.1.tar.gz

---
+++ packages/imgextract/imgextract-0.0.1.tar.gz/imgextract-0.0.1/README.md
@@ -0,0 +1,71 @@
+# imgextract 
+
+[imgextract v0.0.1](https://pypi.org/project/imgextract/)
+
+This python package can be used for extracting pages from PDF and TIF/TIFF files.
+
+
+This packages uses poppler for reading pdf files, for windows platform it has been included in package but for linux we have to install it manually.
+
+## How to get poppler
+
+Download poppler from [poppler](https://poppler.freedesktop.org/)
+
+Install poppler in linux [stackoverflow](https://stackoverflow.com/questions/32156047/how-to-install-poppler-in-ubuntu-15-04)
+
+## How to use
+
+### pdf page extraction
+
+```python
+
+from imgextract.imgext import ImgExtractor
+
+if __name__ == "__main__":
+    ss = ImgExtractor()
+    # input files path, out put files path and file type
+    ss.extract("/home/user/pdf_files", "/home/user/image_files", "pdf")
+
+
+```
+
+### tif/tiff image extraction
+
+```python
+
+from imgextract.imgext import ImgExtractor
+
+if __name__ == "__main__":
+    ss = ImgExtractor()
+    # input files path, out put files path and file type
+    ss.extract("/home/user/tif_files", "/home/user/image_files", "tif")
+
+
+```
+
+### GUI for Image extraction
+
+
+[![GUI for Image extraction](screenshot.png)](https://pypi.org/project/imgextract/)
+
+
+```python
+
+from imgextract.imggui import GuiExtractor
+
+if __name__ == "__main__":
+    ss = GuiExtractor()
+    ss.openwindow()
+
+
+```
+
+### Installation
+
+```shell
+$ pip install imgextract
+```
+
+### License
+
+  [MIT](LICENSE)
---
+++ packages/imgextract/imgextract-0.0.1.tar.gz/imgextract-0.0.1/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="imgextract",
+    version="0.0.1",
+    scripts=['imgextract/imgext.py', 'imgextract/imggui.py'],
+    author="Sai Kumar Yava",
+    author_email="saikumar.geek@gmail.com",
+    description="A python package for extracting images from PDF/TIF files",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/scionoftech/imgextract",
+    download_url="https://github.com/scionoftech/imgextract/releases/tag/imgextract_stable_v0.0.1",
+    install_requires=['pdf2image', 'scikit-image', 'Pillow'],
+    packages=['imgextract'],
+    package_data={'': ['proppler/*']},
+    include_package_data=True,
+    keywords=['pdf', 'tif', 'tiff', 'image'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: MIT License',
+
+    ],
+
+)
+# packages=setuptools.find_packages(include=['lib', 'lib.*', 'frames']),
+# package_data = {'': ['*.py', 'lib/*']},
+# include_package_data = True,
+# data_files = [('lib/*')],
