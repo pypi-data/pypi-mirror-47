59b7395fdafd8ff5edc1a5445f005215dded6e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:48 +0000

    Add package graphene-jwt-auth-registration/0.3.0/graphene-jwt-auth-registration-0.3.0.tar.gz

---
+++ packages/graphene-jwt-auth-registration/graphene-jwt-auth-registration-0.3.0.tar.gz/graphene-jwt-auth-registration-0.3.0/gjwt_auth/schema.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from graphene import relay, ObjectType, Field
+from graphene_django.types import DjangoObjectType
+from graphene_django.filter.fields import DjangoFilterConnectionField
+
+UserModel = get_user_model()
+
+class User(DjangoObjectType):
+    """
+    User Node
+    """
+    class Meta:
+        model = UserModel
+        filter_fields = {
+            'email': ['exact', ]
+            }
+        exclude_fields = ('password', 'is_superuser', )
+        interfaces = (relay.Node, )
+
+
+class UserQuery(object):
+    """
+    what is an abstract type?
+    http://docs.graphene-python.org/en/latest/types/abstracttypes/
+    """
+    user = relay.Node.Field(User)
+    users = DjangoFilterConnectionField(User)
+
+
+class Viewer(ObjectType):
+    user = Field(User)
+
+    def resolve_user(self, info, **kwargs):
+        if info.context.user.is_authenticated:
+            return info.context.user
+        return None
