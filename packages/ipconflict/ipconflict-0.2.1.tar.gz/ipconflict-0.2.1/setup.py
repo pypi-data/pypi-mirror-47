412d39e1e88de593e9c256a51557a1af7011081e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:35 +0000

    Add package ipconflict/0.2.1/ipconflict-0.2.1.tar.gz

---
+++ packages/ipconflict/ipconflict-0.2.1.tar.gz/ipconflict-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ipconflict',
+    version='0.2.1',
+    description='Check for conflicts between network subnets',
+    author='Federico Giuba',
+    author_email='federico.giuba@gmail.com',
+    url='https://github.com/fgiuba/ipconflict',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'netaddr==0.7.19',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ipconflict=ipconflict:main',
+        ],
+    },
+)
