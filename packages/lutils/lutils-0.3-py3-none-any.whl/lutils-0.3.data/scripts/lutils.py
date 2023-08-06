37bb41b4293be724448ea94faa704b124b278e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:45 +0000

    Add package lutils/0.3/lutils-0.3-py3-none-any.whl

---
+++ packages/lutils/lutils-0.3-py3-none-any.whl/lutils-0.3.data/scripts/lutils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from datetime import date, timedelta
+
+#yesterday
+def yesterday():
+    return (date.today() - timedelta(days=1))
+
+def yesterdaystr(format="%Y/%m/%d"):
+    # print yesterday datetime
+    return(yesterday().strftime(format))
+
