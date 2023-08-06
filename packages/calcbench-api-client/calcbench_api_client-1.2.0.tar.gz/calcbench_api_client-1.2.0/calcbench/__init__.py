b7bf55211de7b617a9d0b1cb8d7f9098a6f48cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:43 +0000

    Add package calcbench-api-client/1.2.0/calcbench_api_client-1.2.0.tar.gz

---
+++ packages/calcbench-api-client/calcbench_api_client-1.2.0.tar.gz/calcbench_api_client-1.2.0/calcbench/__init__.py
@@ -0,0 +1,28 @@
+__version__ = '1.0.0'
+from .api_client import (normalized_data, 
+                         normalized_dataframe, 
+                         standardized_data,
+                         tickers, 
+                         set_credentials, 
+                         set_proxies,
+                         companies, 
+                         normalized_raw, 
+                         as_reported_raw, 
+                         dimensional_raw, 
+                         companies_raw,
+                         company_disclosures, 
+                         disclosure_text, 
+                         available_metrics, 
+                         document_search,
+                         filings,
+                         mapped_raw,
+                         point_in_time,
+                         document_contents,
+                         tag_contents,
+                         business_combinations,
+                         document_types,
+                         html_diff,
+                         press_release_raw,
+                         )
+
+from .listener import (handle_filings)
