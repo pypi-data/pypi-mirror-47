7cbc6b10bc3991b81f5a37aecb581ae42115c887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:25 +0000

    Add package felix-scholz-website-python-client/0.0.2/felix_scholz_website_python_client-0.0.2-py3-none-any.whl

---
+++ packages/felix-scholz-website-python-client/felix_scholz_website_python_client-0.0.2-py3-none-any.whl/website_python_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from website_python_client.api.category_api import CategoryApi
+from website_python_client.api.file_api import FileApi
+from website_python_client.api.post_api import PostApi
+from website_python_client.api.tag_api import TagApi
