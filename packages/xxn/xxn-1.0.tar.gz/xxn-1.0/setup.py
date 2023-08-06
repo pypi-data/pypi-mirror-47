4e76303cdc247ee72960afdbd19c1bba0f0702be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:56 +0000

    Add package xxn/1.0/xxn-1.0.tar.gz

---
+++ packages/xxn/xxn-1.0.tar.gz/xxn-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='xxn',
+    version='1.0',
+    description='xxn python test',
+    author='xxn',
+    author_email='18908142690@163.com',
+    py_modules=['xxn.module1', 'xxn.module2']
+)
+
+
+
+
