4c92f195374d63e3923866a86be21129e6a34de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:08 +0000

    Add package unv.deploy/0.3.6/unv.deploy-0.3.6.tar.gz

---
+++ packages/unv.deploy/unv.deploy-0.3.6.tar.gz/unv.deploy-0.3.6/src/unv/deploy/components/systemd.py
@@ -0,0 +1,96 @@
+from unv.utils.tasks import register
+
+
+class SystemdTasksMixin:
+    SCHEMA = {
+        'type': 'dict',
+        'schema': {
+            'template': {'type': 'string'},
+            'name': {'type': 'string'},
+            'boot': {'type': 'boolean'},
+            'instances': {
+                'type': 'dict',
+                'schema': {
+                    'count': {'type': 'integer'},
+                    'percent': {'type': 'integer'}
+                }
+            },
+            'context': {
+                'type': 'dict',
+                'schema': {
+                    'limit_nofile': {'type': 'integer'},
+                    'description': {'type': 'string'}
+                }
+            }
+        }
+    }
+
+    async def _get_systemd_services(self):
+        systemd = self.settings.systemd
+        name = systemd['name']
+        count = await self._calc_instances_count(**systemd['instances'])
+        for instance in range(1, count + 1):
+            service = systemd.copy()
+            service['name'] = name.format(instance=instance)
+            service['instance'] = instance
+            yield service
+
+    async def _sync_systemd_units(self):
+        services = [service async for service in self._get_systemd_services()]
+        user = self.user if self.settings.systemd_local else 'root'
+
+        for service in services:
+            service_path = self.settings.systemd_dir / service['name']
+            if self.settings.systemd_local:
+                await self._mkdir(self.settings.systemd_dir)
+
+            context = {'instance': service['instance']}.copy()
+            context.update(service.get('context', {}))
+            path = service['template']
+            if not str(path).startswith('/'):
+                path = (self.settings.local_root / service['template'])
+                path = path.resolve()
+
+            with self._set_user(user):
+                await self._upload_template(path, service_path, context)
+
+        with self._set_user(user):
+            user_flag = '--user ' if self.settings.systemd_local else ''
+            await self._run(f'systemctl {user_flag}daemon-reload')
+            await self._systemctl('enable', boot_only=True)
+
+    async def _systemctl(
+            self, command: str, display=False, boot_only=False):
+        results = []
+        async for service in self._get_systemd_services():
+            if 'manage' in service and not service['manage']:
+                continue
+            if boot_only and not service.get('boot', False):
+                continue
+
+            user_flag = '--user ' if self.settings.systemd_local else ''
+            user = self.user if user_flag else 'root'
+
+            with self._set_user(user):
+                result = await self._run(
+                    f'systemctl {user_flag}{command} {service["name"]}')
+            results.append(result)
+        return results
+
+    @register
+    async def start(self):
+        await self._systemctl('start')
+
+    @register
+    async def stop(self):
+        await self._systemctl('stop')
+
+    @register
+    async def restart(self):
+        await self._systemctl('restart')
+
+    @register
+    async def status(self):
+        results = await self._systemctl('status')
+        for result in results:
+            print(result)
