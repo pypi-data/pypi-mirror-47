8492b40f63b1be068843e278ee9db04f1b1e5f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:12 +0000

    Add package StoveOpt/0.3.7/StoveOpt-0.3.7.tar.gz

---
+++ packages/StoveOpt/StoveOpt-0.3.7.tar.gz/StoveOpt-0.3.7/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 15:34:23 2019
+
+@author: Lee
+"""
+import setuptools
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+setup(
+      name='StoveOpt',
+      version='0.3.7',
+      description='Biomass cookstove optimization package',
+      author='Liam Cassidy',
+	  url='https://github.com/Liam-Cassidy/StoveOpt',
+      author_email='cassidyl@oregonstate.edu',
+      classifiers=[
+              'License :: OSI Approved :: BSD License',
+              'Natural Language :: English',
+              'Programming Language :: Python',
+      ],
+      
+      license='MIT',
+      python_requires='>=2.7',
+      zip_safe=False,
+      packages=setuptools.find_packages(),
+      
+	  package_dir={'StoveOpt': 'StoveOpt',
+	  },
+	  include_package_data=True,		
+      )
+      
