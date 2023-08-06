74359fffd4fb80eba29869ec85939f6c4f9dbd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:39 +0000

    Add package mpwt/0.4.2.4/mpwt-0.4.2.4.tar.gz

---
+++ packages/mpwt/mpwt-0.4.2.4.tar.gz/mpwt-0.4.2.4/mpwt/__init__.py
@@ -0,0 +1,2 @@
+from mpwt.multipwt import multiprocess_pwt
+from mpwt.utils import cleaning, cleaning_input, find_ptools_path, list_pgdb, remove_pgbds
---
+++ packages/mpwt/mpwt-0.4.2.4.tar.gz/mpwt-0.4.2.4/mpwt/__main__.py
@@ -0,0 +1,7 @@
+from mpwt.multipwt import run_mpwt
+
+def main(args=None):
+    run_mpwt()
+
+if __name__ == "__main__":
+    main()
