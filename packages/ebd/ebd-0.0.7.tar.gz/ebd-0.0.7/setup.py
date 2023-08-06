3d8bece7095d39a4c6c2461b2d8f72ddf125957b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:21 +0000

    Add package ebd/0.0.7/ebd-0.0.7.tar.gz

---
+++ packages/ebd/ebd-0.0.7.tar.gz/ebd-0.0.7/setup.py
@@ -0,0 +1,64 @@
+import setuptools
+import setuptools.command
+import setuptools.command.install
+import setuptools.command.develop
+import os
+import pathlib
+
+
+def get_rc_candidate():
+    home = os.path.expanduser("~")
+    bashrc = os.path.join(home, ".bashrc")
+    if os.path.exists(bashrc):
+        return bashrc
+    profile = os.path.join(home, ".profile")
+    if os.path.exists(profile):
+        return profile
+
+
+def setup_odus():
+    rc = get_rc_candidate()
+    if rc is not None:
+        with open(rc) as file:
+            print("rc file content:")
+            print(file.read())
+    else:
+        print("no suitable rc found")
+
+
+class PostInstallCommand(setuptools.command.install.install):
+    def run(self):
+        print("install command")
+        setup_odus()
+        setuptools.command.install.install.run(self)
+
+
+class PostDevelopCommand(setuptools.command.develop.develop):
+    def run(self):
+        print("develop command")
+        setuptools.command.develop.develop.run(self)
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ebd",
+    version="0.0.7",
+    author="Fiely Teach",
+    author_email="admin@admin.ru",
+    description="Wrapper for LFSM-enabled systems.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    cmdclass={
+        "install": PostInstallCommand,
+        "develop": PostDevelopCommand
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
