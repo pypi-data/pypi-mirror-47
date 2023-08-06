73fe4195cdca29223696e301044814d3f96278e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:16 +0000

    Add package pycotools3/2.1.10/pycotools3-2.1.10.tar.gz

---
+++ packages/pycotools3/pycotools3-2.1.10.tar.gz/pycotools3-2.1.10/pycotools3/__init__.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+from sys import version_info, platform
+
+from . import viz
+from . import tasks
+from . import errors
+from . import misc
+from . import model
+from . import munch
+
+if version_info[0] < 3:
+    raise RuntimeError('On python version >3 you must use "import pycotools3" rather '
+                       'than "import pycotools". To install pycotools3 '
+                       ' use "pip install pycotools3". Please note'
+                       ' that improvements will only be made to pycotools3 in '
+                       'future. ')
+
+import logging
+import logging.config
+import warnings
+
+warnings.filterwarnings("ignore", message=".*numpy.dtype.*")
+warnings.filterwarnings("ignore", message=".*numpy.ufunc.*")
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.WARNING)
+
+## define the list of modules imported with the "from pycotools3 import *" statement
+__all__ = ['tasks', 'model', 'viz', 'utils']
+
+
+import matplotlib
+try:
+    matplotlib.use('Qt5Agg')
+except Exception:
+    try:
+        matplotlib.use('Qt4Agg')
+    except Exception:
+        try:
+            matplotlib.use('TkAgg')
+        except Exception:
+            pass
