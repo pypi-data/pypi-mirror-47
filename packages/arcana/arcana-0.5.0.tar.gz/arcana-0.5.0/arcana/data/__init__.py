918dce311e3f851c6a14d9610f4084fbcf54f2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:23 +0000

    Add package arcana/0.5.0/arcana-0.5.0.tar.gz

---
+++ packages/arcana/arcana-0.5.0.tar.gz/arcana-0.5.0/arcana/data/__init__.py
@@ -0,0 +1,8 @@
+from .item import Fileset, Field
+from .collection import FilesetCollection, FieldCollection
+from .spec import (
+    FilesetSpec, FieldSpec, InputFilesetSpec, InputFieldSpec, BaseSpec,
+    BaseInputSpec)
+from .base import BaseField, BaseFileset, BaseData
+from .input import InputFilesets, InputFields, BaseInput
+from .file_format import FileFormat, Converter, IdentityConverter
