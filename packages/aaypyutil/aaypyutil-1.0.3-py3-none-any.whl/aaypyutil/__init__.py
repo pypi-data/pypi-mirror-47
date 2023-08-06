dd492671f52d33bf1cd17057b78e6b278dddc6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:51 +0000

    Add package aaypyutil/1.0.3/aaypyutil-1.0.3-py3-none-any.whl

---
+++ packages/aaypyutil/aaypyutil-1.0.3-py3-none-any.whl/aaypyutil/__init__.py
@@ -0,0 +1,9 @@
+"""aaypyutil - Common python util functions
+
+    >>> import aaypyutil
+
+See https://github.com/aayushuppal/aaypyutil for more information
+"""
+
+# Version of aaypyutil package
+__version__ = "1.0.3"
