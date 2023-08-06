5cfad260490e09c974c37eb55c8644b5ace4856a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:23 +0000

    Add package pyrez/1.0.6.4/pyrez-1.0.6.4-py2.py3-none-any.whl

---
+++ packages/pyrez/pyrez-1.0.6.4-py2.py3-none-any.whl/pyrez/models/APIResponseBase.py
@@ -0,0 +1,12 @@
+from pyrez.models.Mixin import Dict
+class APIResponseBase(Dict):
+	"""Represents a generic Pyrez object.
+
+	Keyword Arguments
+	-----------------
+	json : |DICT| or |LIST|
+		The request as JSON, if you prefer.
+	"""
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.json = kwargs or []
