2c634e3c8d828cd045a8758a30241edcde9f0bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:19 +0000

    Add package sbmlutils/0.3.6/sbmlutils-0.3.6.tar.gz

---
+++ packages/sbmlutils/sbmlutils-0.3.6.tar.gz/sbmlutils-0.3.6/sbmlutils/examples/models/yeast_glycolysis/yeast_glycolysis.py
@@ -0,0 +1,10 @@
+import os.path
+
+from sbmlutils.report import sbmlreport
+
+if __name__ == "__main__":
+
+    # create the sbml report
+    f_path = os.path.join(os.path.curdir, 'yeast_glycolysis.xml')
+    print(f_path)
+    sbmlreport.create_sbml_report(sbml_path=f_path, out_dir=os.path.curdir)
