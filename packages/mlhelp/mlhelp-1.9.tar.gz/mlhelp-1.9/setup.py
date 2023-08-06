b40ab4ca3577c553a7af3b6f5ffe710422378ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:36 +0000

    Add package mlhelp/1.9/mlhelp-1.9.tar.gz

---
+++ packages/mlhelp/mlhelp-1.9.tar.gz/mlhelp-1.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mlhelp',
+      version='1.9',
+      description='Exists cause of Package problems',
+      packages=['mlhelp'],
+      zip_safe=False,
+      install_requires=['pandas','numpy','sklearn' ,'scipy' , 'matplotlib'],
+      author='Sai Krishna',
+      author_email='saimanthena13579@gmail.com')
