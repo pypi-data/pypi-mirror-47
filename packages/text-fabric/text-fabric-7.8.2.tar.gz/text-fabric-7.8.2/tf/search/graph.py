cfec6673250963d719f63725a5c48c2b436ba9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:31 +0000

    Add package text-fabric/7.8.2/text-fabric-7.8.2.tar.gz

---
+++ packages/text-fabric/text-fabric-7.8.2.tar.gz/text-fabric-7.8.2/tf/search/graph.py
@@ -0,0 +1,39 @@
+# INSPECTING WITH THE SEARCH GRAPH ###
+
+
+def connectedness(searchExe):
+  error = searchExe.api.error
+  qnodes = searchExe.qnodes
+  qedges = searchExe.qedges
+  msgCache = searchExe.msgCache
+
+  componentIndex = dict(((q, {q}) for q in range(len(qnodes))))
+  for (f, rela, t) in qedges:
+    if f != t:
+      componentIndex[f] |= componentIndex[t]
+      componentIndex[t] = componentIndex[f]
+      for u in componentIndex[f] - {f}:
+        componentIndex[u] = componentIndex[f]
+  components = sorted(set(frozenset(c) for c in componentIndex.values()))
+  componentIndex = {}
+  for c in components:
+    for q in c:
+      componentIndex[q] = c
+  componentEdges = {}
+  for (e, (f, rela, t)) in enumerate(qedges):
+    c = componentIndex[f]
+    componentEdges.setdefault(c, []).append(e)
+  searchExe.components = []
+  for c in components:
+    searchExe.components.append([sorted(c), componentEdges.get(c, [])])
+  lComps = len(searchExe.components)
+  if lComps == 0:
+    error('Search without instructions. Tell me what to look for.', cache=msgCache)
+    searchExe.good = False
+  elif lComps > 1:
+    error(f'More than one connected components ({len(searchExe.components)}):', cache=msgCache)
+    error(
+        'Either run the subqueries one by one, or connect the components by a relation', tm=False,
+        cache=msgCache
+    )
+    searchExe.good = False
