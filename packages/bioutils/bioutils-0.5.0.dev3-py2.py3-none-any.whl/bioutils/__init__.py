898c9e6ff6324bfbdc1d4de55ecec9c99c816941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:42 +0000

    Add package bioutils/0.5.0.dev3/bioutils-0.5.0.dev3-py2.py3-none-any.whl

---
+++ packages/bioutils/bioutils-0.5.0.dev3-py2.py3-none-any.whl/bioutils/__init__.py
@@ -0,0 +1,16 @@
+import pkg_resources
+import re
+import warnings
+
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+    if re.match(r"^\d+\.\d+\.\d+$", __version__) is not None:
+        _is_released_version = True  # pragma: no cover
+except pkg_resources.DistributionNotFound as e:  # pragma: no cover
+    warnings.warning("can't get __version__ because %s package isn't installed" %
+                  __package__, Warning)
+    __version__ = None
+
+
+from ._versionwarning import *
