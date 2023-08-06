c2d7458ac03da7c861b3ea79d75404e0099042f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:36 +0000

    Add package LiDARtoolkit/0.4/LiDARtoolkit-0.4.tar.gz

---
+++ packages/LiDARtoolkit/LiDARtoolkit-0.4.tar.gz/LiDARtoolkit-0.4/LiDARtoolkit/__init__.py
@@ -0,0 +1,1 @@
+from LiDARtoolkit import *
---
+++ packages/LiDARtoolkit/LiDARtoolkit-0.4.tar.gz/LiDARtoolkit-0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md",'r') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = "LiDARtoolkit",
+	version = "0.4",
+	author = "Ekan5h",
+	author_email = "ekanshmahendru@gmail.com",
+	description = "A toolkit to handle LIDAR point cloud data.",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/Ekan5h/LIDARtoolkit",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent"
+	]
+)
+
