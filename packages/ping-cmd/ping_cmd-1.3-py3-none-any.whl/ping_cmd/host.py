8054485359d7d6746af33c9555c71e6e7b0c7b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:49 +0000

    Add package ping-cmd/1.3/ping_cmd-1.3-py3-none-any.whl

---
+++ packages/ping-cmd/ping_cmd-1.3-py3-none-any.whl/ping_cmd/host.py
@@ -0,0 +1,14 @@
+from subprocess import PIPE, Popen
+
+
+def ping(host, packages=1, wait=2):
+    """
+    :param str host:
+    :param int packages:
+    :param int wait:
+    :return int:
+    """
+    command = ['ping', '-n', packages, '-w', wait, host]
+    p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"input data")
+    return p.returncode
