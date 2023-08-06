4914336c4af75cb32745b92e2fb1827736e2e377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:07 +0000

    Add package diguidaying/1.0.0/diguidaying-1.0.0.tar.gz

---
+++ packages/diguidaying/diguidaying-1.0.0.tar.gz/diguidaying-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        ='diguidaying',
+        version     ='1.0.0',
+        py_modules  =['diguidaying'],
+        author      ='ZF_IOOI',
+        author_email='zhoufengahjd@163.com',
+        url         ='http://www.baidu.com',
+        description ='A simple printer of nested lists',
+    )
