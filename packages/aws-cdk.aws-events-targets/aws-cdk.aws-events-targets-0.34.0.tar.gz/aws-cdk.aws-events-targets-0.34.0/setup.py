4f1d0f1e97a61fb96ab2ac296b577feeae140d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:34 +0000

    Add package aws-cdk.aws-events-targets/0.34.0/aws-cdk.aws-events-targets-0.34.0.tar.gz

---
+++ packages/aws-cdk.aws-events-targets/aws-cdk.aws-events-targets-0.34.0.tar.gz/aws-cdk.aws-events-targets-0.34.0/setup.py
@@ -0,0 +1,53 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-events-targets",
+    "version": "0.34.0",
+    "description": "Event targets for AWS CloudWatch Events",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_events_targets",
+        "aws_cdk.aws_events_targets._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_events_targets._jsii": [
+            "aws-events-targets@0.34.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_events_targets": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.11.2",
+        "publication>=0.0.3",
+        "aws-cdk.aws-cloudformation~=0.34.0",
+        "aws-cdk.aws-codebuild~=0.34.0",
+        "aws-cdk.aws-codepipeline~=0.34.0",
+        "aws-cdk.aws-ec2~=0.34.0",
+        "aws-cdk.aws-ecs~=0.34.0",
+        "aws-cdk.aws-events~=0.34.0",
+        "aws-cdk.aws-iam~=0.34.0",
+        "aws-cdk.aws-lambda~=0.34.0",
+        "aws-cdk.aws-sns~=0.34.0",
+        "aws-cdk.aws-stepfunctions~=0.34.0",
+        "aws-cdk.cdk~=0.34.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
