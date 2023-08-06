6200dee83c080304ff9380619fd4cde9f81b049f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:11 +0000

    Add package PyMakerEngine/0.0.1/PyMakerEngine-0.0.1.tar.gz

---
+++ packages/PyMakerEngine/PyMakerEngine-0.0.1.tar.gz/PyMakerEngine-0.0.1/README.md
@@ -0,0 +1,4 @@
+Welcome to PyMaker, the tool to make games in python!
+
+Documentation: Soon!
+Check our discord: https://discord.gg/9nTu3kW
---
+++ packages/PyMakerEngine/PyMakerEngine-0.0.1.tar.gz/PyMakerEngine-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyMakerEngine",
+    version="0.0.1",
+    author="iByNiki_",
+    author_email="therealpingmc@gmail.com",
+    description="Make games with python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://discord.gg/9nTu3kW",
+    packages=setuptools.find_packages(),
+    keywords=["games", "python games", "game", "pygame"],
+    install_requires=[
+        "keyboard==0.13.3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
