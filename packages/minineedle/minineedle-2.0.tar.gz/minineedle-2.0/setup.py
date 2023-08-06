997c2a7c50124ef768798a0c75568eec07488b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:07 +0000

    Add package minineedle/2.0/minineedle-2.0.tar.gz

---
+++ packages/minineedle/minineedle-2.0.tar.gz/minineedle-2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='minineedle',
+      version='2.0',
+      description='Minimal needleman-wunsch',
+      url='http://github.com/scastlara/minineedle',
+      author='S. Castillo-Lara',
+      author_email='sergiocastillo@ub.edu',
+      install_requires='miniseq',
+      license='GPL-3.0',
+      packages=['minineedle'],
+      zip_safe=False)
