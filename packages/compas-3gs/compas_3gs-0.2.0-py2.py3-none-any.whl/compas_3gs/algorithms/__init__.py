5003d9aa049741fca0b0f7697eea02f76aafdc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:36 +0000

    Add package compas-3gs/0.2.0/compas_3gs-0.2.0-py2.py3-none-any.whl

---
+++ packages/compas-3gs/compas_3gs-0.2.0-py2.py3-none-any.whl/compas_3gs/algorithms/__init__.py
@@ -0,0 +1,85 @@
+"""
+********************************************************************************
+compas_3gs.algorithms
+********************************************************************************
+
+.. currentmodule:: compas_3gs.algorithms
+
+
+EGI
+===
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    egi_from_vectors
+
+
+____
+
+
+Duality
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    volmesh_dual_volmesh
+    volmesh_dual_network
+
+
+____
+
+
+Reciprocation
+=============
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    volmesh_reciprocate
+
+
+____
+
+
+Planarisation
+=============
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    volmesh_planarise
+
+
+____
+
+
+Unified Diagram
+===============
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    volmesh_ud
+    cellnetwork_ud
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .duality import *
+from .egi import *
+from .planarisation import *
+from .reciprocation import *
+from .unifieddiagram import *
+
+
+__all__ = [name for name in dir() if not name.startswith('_')]
