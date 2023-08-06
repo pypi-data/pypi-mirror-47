40c4794c1bc52232059df643c5719abebceb1fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:05 +0000

    Add package s3-dog-food/0.0.4/s3_dog_food-0.0.4.tar.gz

---
+++ packages/s3-dog-food/s3_dog_food-0.0.4.tar.gz/s3_dog_food-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="s3_dog_food",
+    version="0.0.4",
+    author="rnachiappan, sbagavathy",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "boto3", "botocore"
+    ],
+    include_package_data=True
+)
