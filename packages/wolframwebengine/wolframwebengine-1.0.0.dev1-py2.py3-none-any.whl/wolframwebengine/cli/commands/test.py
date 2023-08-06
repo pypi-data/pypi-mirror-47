2866627f207b38345444143238f913334a39b6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:35 +0000

    Add package wolframwebengine/1.0.0.dev1/wolframwebengine-1.0.0.dev1-py2.py3-none-any.whl

---
+++ packages/wolframwebengine/wolframwebengine-1.0.0.dev1-py2.py3-none-any.whl/wolframwebengine/cli/commands/test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from wolframclient.cli.commands.test import Command as TestCommand
+from wolframclient.utils.functional import iterate
+
+
+class Command(TestCommand):
+
+    modules = ["wolframwebengine.tests"]
+
+    dependencies = tuple(iterate((("django", "2.2.1"),), TestCommand.dependencies))
---
+++ packages/wolframwebengine/wolframwebengine-1.0.0.dev1-py2.py3-none-any.whl/wolframwebengine/tests/folder_explorer.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import os
+
+from wolframclient.utils.importutils import module_path
+from wolframclient.utils.tests import TestCase as BaseTestCase
+from wolframwebengine.server.explorer import get_wl_handler_path_from_folder
+
+
+class TestCase(BaseTestCase):
+    def test_sample_explorer(self):
+
+        folder = module_path("wolframwebengine", "examples", "sampleapp")
+
+        for path, resolved in (
+            ("/", "index.m"),
+            ("/random.m", "random.m"),
+            ("/foo/bar/", "foo/bar/index.m"),
+            ("/foo/", "foo/index.m"),
+            ("/foo/bar/index.m", "foo/bar/index.m"),
+            ("/foo/bar/something.m", "foo/bar/something.m"),
+        ):
+
+            self.assertEqual(
+                get_wl_handler_path_from_folder(folder, path), os.path.join(folder, resolved)
+            )
